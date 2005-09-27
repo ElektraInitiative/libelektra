5c71569b96424fe8c8ace34c1f425921827f0f2a
ylecaillez <ylecaillez@3c96aef2-7adb-0310-a8e7-cbd0541efc33>
2005-09-27 15:11:35 +0000

    git-svn-id: https://svn.libelektra.org/svn/elektra/trunk@262 3c96aef2-7adb-0310-a8e7-cbd0541efc33

--- Homepage/index.php
+++ Homepage/index.php
@@ -5,7 +5,7 @@
 @Licence: GPL
 */
 ?>
-<!-- Generated with homephp 1.1 -->
+<!-- test date Generated with homephp 1.1 -->
 <? echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
