94437da6f9f2b60b7dedb6dcdd923f87f751c50a
mraab <mraab@3c96aef2-7adb-0310-a8e7-cbd0541efc33>
2005-10-11 13:18:27 +0000

    added .c files
    git-svn-id: https://svn.libelektra.org/svn/elektra/trunk@332 3c96aef2-7adb-0310-a8e7-cbd0541efc33

---
+++ example/backend.c
@@ -0,0 +1,183 @@
+#include <kdb.h>
+
+/**backend.c
+ * This is comprehensive Test for the
+ * whole Elektra Api and the Backends
+ * in special.
+ * main() can be used to start the
+ * wished test-functions.
+ */
+
+void printKey (Key * k);
+void printKeySet (KeySet * set);
+void useKeySet();
+void getChildKeys ();
+void getKey();
+void setNullKey();
+void setAnotherKey();
+
+#define CHILD_KEY_ROOT "user/test/another"
+#define KEY_ROOT "user/test/another/key"
+
+void printKey (Key * k)
+{
+	size_t s;
+	char * str;
+
+	size_t c;
+	char * com;
+
+	size_t n;
+	char * nam;
+	
+	n = keyGetNameSize (k);
+	nam = (char*) malloc (n);
+	if (nam == NULL) {fprintf (stderr, "malloc error\n");}
+	keyGetName (k, nam, n);
+	
+	s = keyGetDataSize (k);
+	str = (char*) malloc (s+100);
+	if (str == NULL) {fprintf (stderr, "malloc error\n");}
+	keyGetString (k, str, s);
+
+	c = keyGetCommentSize (k);
+	com = (char*) malloc (c+100);
+	if (com == NULL) {fprintf (stderr, "malloc error\n");}
+	keyGetComment (k, com, c);
+	
+	printf ("Name[%d,%d]: %s\t", n, strlen(nam)+1, nam);
+	printf ("String[%d,%d]: %s\t", s, strlen(str)+1, str);
+	printf ("Kommentar[%d,%d]: %s\n", c, strlen(com)+1, com);
+
+	free (nam);
+	free (str);
+	free (com);
+}
+
+void printKeySet (KeySet * set)
+{
+	Key * k;
+	fprintf (stderr, "Will print keys\n");
+
+	ksRewind (set);
+
+	fprintf (stderr, "Get next key\n");
+	k = ksNext (set);
+	while (k) {
+		printKey (k);
+		k = ksNext (set);
+	}
+}
+
+void getChildKeys ()
+{
+	int ret;
+	Key * root;
+	KeySet * set;
+
+/**BEGIN - should be same as kdbGetChildKeys*/
+/*	Key *parentKey;
+        ssize_t rc;
+	set = ksNew();
+
+        parentKey=keyNew("user/sw/MyApp",KEY_SWITCH_END);
+        rc=kdbGetKeyChildKeys(parentKey,set,0);
+
+        keyDel(parentKey);*/
+/**END*/
+	
+	/* Get all value keys for this application */
+	set = ksNew();
+	root = keyNew (CHILD_KEY_ROOT, KEY_SWITCH_END);
+	ret = kdbGetKeyChildKeys (root, set, 0); // should be renamed to kdbGetKeys
+
+	if (ret >= 0)
+	{
+		fprintf (stderr, "Got all keys\n");
+	} else {
+		fprintf (stderr, "Error in kdbGetChildKeys\n");
+//		exit (0);
+	}
+		
+	printKeySet(set);
+
+	ksDel (set);
+}
+
+void getKey ()
+{
+	Key * k;
+	k = keyNew(KEY_ROOT, KEY_SWITCH_END);
+
+	if (kdbGetKey (k) >= 0) {
+		fprintf (stderr, "got key\n");
+	} else {
+		fprintf (stderr, "Error in kdbGetKey, errno: %d\n", errno);
+		exit (0);
+	}
+	
+	printKey (k);
+
+	keyDel (k);
+}
+
+/**This function sets a new made Key with keyNew.
+ * This must not sigfault*/
+void setNullKey ()
+{
+	Key * k;
+	k = keyNew(KEY_ROOT, KEY_SWITCH_END);
+	
+	fprintf (stderr, "Write same key back\n");
+	if (kdbSetKey (k) >= 0) {
+		fprintf (stderr, "key set\n");
+	} else {
+		fprintf (stderr, "Error in kdbSetKey, errno: %d\n", errno);
+		exit (0);
+	}
+	
+	printKey (k);
+
+	keyDel (k);
+}
+
+/**This sets a valid key with a string and comment*/
+void setAnotherKey ()
+{
+	Key * k;
+	k = keyNew(KEY_ROOT, KEY_SWITCH_END);
+	
+	keySetString (k, "whatta");
+	keySetComment(k, "commant");
+	
+	if (kdbSetKey (k) >= 0) {
+		fprintf (stderr, "key set\n");
+	} else {
+		fprintf (stderr, "Error in kdbSetKey, errno: %d\n", errno);
+		exit (0);
+	}
+	
+	printKey (k);
+
+	keyDel (k);
+}
+
+
+int main(int argc, char **argv) {
+	fprintf(stderr, "start app\n");
+	/* Open the kdb */
+	kdbOpen();
+	fprintf(stderr, "after kdbOpen\n");
+
+	// getChildKeys();
+	setNullKey();
+	setAnotherKey();
+	getKey();
+	//TODO: split backend testing and api testing!
+	// useKeySet();
+	
+	/* Close the Key database */
+	kdbClose();
+	return 0;
+}
+
