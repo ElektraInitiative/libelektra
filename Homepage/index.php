6555dada95a07e63960d276765ece81f5ef29024
mraab <mraab@3c96aef2-7adb-0310-a8e7-cbd0541efc33>
2005-10-11 13:32:17 +0000

    updated style.css and index.php for new design by Denis Huentz from http://www.studio-hb.com
    git-svn-id: https://svn.libelektra.org/svn/elektra/trunk@333 3c96aef2-7adb-0310-a8e7-cbd0541efc33

--- Homepage/index.php
+++ Homepage/index.php
@@ -1,14 +1,15 @@
 <?php
 /**
 @Author: Markus Raab
-@Version: homephp 1.1-custom (utf+xhtml version)
+@Version: homephp 1.1-custom (utf+xhtml+div version)
+@Design: Denis Huentz from http://www.studio-hb.com
 @Licence: GPL
 */
 ?>
 <!-- test date Generated with homephp 1.1 -->
 <? echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title>
@@ -21,49 +22,40 @@
 </head>
 
 <body>
-	<table class="mytable" cellspacing="0" cellpadding="0" border="0">
-	<colgroup>
-		<col width="20" />
-		<col />
-	</colgroup>
-	
-<tbody>
-<tr>
-	<td class="mylogo">
-		<a href="/"><img class="pic_logo" src="/logo.png" alt="Home" title="Home" /></a>
-	</td>
-	<td class="myheader">
-<? show_position()?>
-	</td>
-</tr>
-<tr>
-	<td class="mysidebar" valign="top">
-	<div class="ro"><div class="lo"><div class="ru"><div class="lu">
-	<div class="mynavigator">
+<div id="conteneur">
+	<div id="header">
+		<a href="http://www.libelektra.org">
+		<img src="/logo.jpg" border="0"/></a>
+	</div>
+	<div id="bloc_liens_conteneur">
+	<div id="bloc_liens_header">HOME</div>
+	<div id="navcontainer">
 	<ul>
 <? generate_menu()?>
 	</ul>
 	</div>
-	</div></div></div></div>
-	</td>
-	<td valign="top">
-	<a name="top"></a>
-	<div class="mybody">
-<? show_file()?><br/>
-	</div>
-	</td>
-</tr>
-<tr>
-	<td class="mydate">
-<?readfile ("date.txt")?>
-	</td>
-	<td class="mybottom">
-<? generate_banner()?>
-	</td>
-	</tr>
-</tbody>
-</table>
+</div>
 
+<div id="centre">
+	<a name="top">
+<? show_file()?>
+	</div>
+	
+
+	<div id="mytop">
+		<a  href="#top" title="Go to top">TOP</a>
+	</div>
+</div>
+
+<div id="pied">
+	<div id="mydate">
+<?readfile ("date.txt")?>
+	</div>
+	<div id="pic_banner">
+<? generate_banner()?>
+	</div>
+</div>
+</div>
 </body>
 </html>
 
@@ -343,6 +335,7 @@ function generate_menu ()
 			$entry == ".." || 
 			$entry == "banner" ||
 			$entry == "pic" ||
+			$entry == "images" ||
 			$entry == ".svn") 
 			continue;
 		if ($pathname != ".")
@@ -375,6 +368,7 @@ function generate_menu ()
 				$entry == ".." || 
 				$entry == "banner" ||
 				$entry == "pic" ||
+				$entry == "images" ||
 				$entry == ".svn") 
 				continue;
 			if (is_dir (dirname($pathname) . "/" . $entry))
