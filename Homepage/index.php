944762b453fad5b82805cb846663020c4e74b0b8
mraab <mraab@3c96aef2-7adb-0310-a8e7-cbd0541efc33>
2005-10-14 07:40:35 +0000

    xml proplem to be allowed to validate css
    git-svn-id: https://svn.libelektra.org/svn/elektra/trunk@361 3c96aef2-7adb-0310-a8e7-cbd0541efc33

--- Homepage/index.php
+++ Homepage/index.php
@@ -6,9 +6,9 @@
 @Licence: GPL
 */
 ?>
-<!-- test date Generated with homephp 1.1 -->
 <? echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!-- test date Generated with homephp 1.1 -->
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
