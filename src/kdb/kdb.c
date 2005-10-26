e11aee64e4dff7cbad84d41bb09665fc1cfc88b1
mraab <mraab@3c96aef2-7adb-0310-a8e7-cbd0541efc33>
2005-10-26 12:21:15 +0000

    command help per function works now documentation itself needs really lot of work!
    git-svn-id: https://svn.libelektra.org/svn/elektra/trunk@448 3c96aef2-7adb-0310-a8e7-cbd0541efc33

--- src/kdb/kdb.c
+++ src/kdb/kdb.c
@@ -92,6 +92,7 @@ int argFullName=0;
 int argShow=1;
 int argShell=0;
 int argXML=0;
+int argHelp=0;
 mode_t argMode=0;
 int argType=KEY_TYPE_UNDEFINED;
 
@@ -136,7 +137,7 @@ int parseCommandLine(int argc, char *argv[]) {
 			strncpy(argGroup,optarg,ARGSIZE);
 			break;
 		case 'h':
-			commandHelp();
+			argHelp=1;
 			break;
 		case 'i':
 			argShow=0;
@@ -181,8 +182,14 @@ int parseCommandLine(int argc, char *argv[]) {
 		strncpy(sargCommand,argv[optind],ARGSIZE);
 		optind ++;
 	} else {
-		fprintf (stderr, "No command given\n");
-		exit (1);
+		if (argHelp)
+		{
+			commandHelp();
+			exit(0);
+		} else {
+			fprintf (stderr, "No command given\n");
+			exit (1);
+		}
 	}
 		
 	if (optind < argc) /*parse key name*/
@@ -926,14 +933,16 @@ int commandHelp() {
 	printf("\n");
 
 	printf("ARGUMENTS\n");
-	printf("Commands are explained with kdb -h command (Implemented soon)\n");
+	printf("Commands are explained with kdb -h command\n");
 	printf("<key> is the name of the key. It can be prefixed\n");
 	printf(" with environment KDB_ROOT. The slash between will\n");
 	printf("be inserted.\n");
+	printf(" export KDB_ROOT=\"user/test/dir\"\n");
+	printf(" kdb get file/key ... will expand to user/test/dir/file/key\n");
 	printf("[value ...] hold the value which should be set\n");
 	printf("\n");
 	
-	printf("EXAMPLES:\n");
+	printf("SUBCOMMANDS\n");
 	printf(" kdb get [-dlr] key/name\n");
 	printf(" kdb set [-t type] [-c \"A comment about this key\"] [-m mode] [-u uid]\n");
 	printf("         [-g gid] key/name \"the value\"\n");
@@ -949,91 +958,192 @@ int commandHelp() {
 	printf(" kdb import < file.xml\n");
 	printf(" kdb import file.xml\n");
 	printf(" kdb monitor some/key/name\n");
-	printf(" export KDB_ROOT=\"user/test/dir\"\n");
-	printf(" kdb get file/key ... will expand to user/test/dir/file/key\n");
 	printf("\n");
-	
-	printf("SUBCOMMANDS\n");
-	printf("get\n");
-	printf(" Get the value from the specified key. Accepts options: -d, -l, -f, -s \n");
-	printf("\n");
-	printf("set\n");
-	printf(" Set the value to the specified key. Accepts options: -c, -t, -m, -b \n");
-	printf("\n");
-	printf("ls\n");
-	printf(" As the ls(1) command, list key names for the specified key, or children keys, if specified a folder key. The -v argument will make it show also the values of each key. The -d (descriptive) will make it show the comment, key name and its value, as you are watching a plain text file. Accepts options: -x, -d, -l, -f, -v, -R \n");
-	printf("\n");
-	printf("ln\n");
-	printf(" Creates a key that is a symbolic links to another key. \n");
-	printf("\n");
-	printf("mv\n");
-	printf(" Move, or renames a key. Currently it can't move keys across different filesystems. \n");
-	printf("\n");
-	printf("rm\n");
-	printf(" As the rm(1) command, removes the key specified. \n");
-	printf("\n");
-	printf("edit\n");
-	printf(" A very powerfull subcommand that lets you edit an XML representation of the keys. The parameters it accepts is usually a parent key, so its child keys will be gathered. Can be used with the -R flag to work recursively. The editor used is the one set in the $EDITOR environment variable, or vi. After editing the keys, kdb edit will analyze them and commit only the changed keys, remove the keys removed, and add the keys added. \n");
-	printf("\n");
-	printf("export, save, \n");
-	printf("Export a subtree of keys to XML. If no subtree is defined right after the export command, system and current user trees will be exported. Output is written to standard output. The output encoding will allways be UTF-8, regardeless of your system encoding. UTF-8 is the most universal charset you can get when exchanging data between multiple systems. Accepts -f. \n");
-	printf("\n");
-	printf("import, load, \n");
-	printf("Import an XML representation of keys and save it to the keys database. If no filename is passed right after the import command, standard input is used. \n");
-	printf("\n");
-	printf("monitor, mon, \n");
-	printf("Monitor a key for some value change. It will block your command line until a change in the key value is detected, then return its new value. Options \n");
-	printf("-R\n");
+}
+
+void optionr()
+{
+	printf("-R -r\n");
 	printf(" Causes to work recursively. In ls, will list recursively. \n");
 	printf("\n");
+}
+
+void optionx()
+{
 	printf("-x\n");
 	printf(" Makes ls output an XML representation of the keys, instead of an ls-compatible output. \n");
 	printf("\n");
+}
+
+void optionl()
+{
 	printf("-l\n");
 	printf(" Causes to display long results. With ls, will generate lists similar to ls -l. With get, will show also the key name. \n");
 	printf("\n");
+}
+
+void optiona()
+{
 	printf("-a\n");
 	printf(" Causes ls to display also inactive keys. Generate lists similar to ls -a. Inactive keys are keys which basename begins with a '.' (dot). An example of inactive key: system/sw/XFree/current/Monitor/.Monitor1 \n");
 	printf("\n");
+}
+
+void optionf()
+{
 	printf("-f\n");
 	printf(" Causes to work with full key names. A full key name makes sense only on user/* keys, and differentiate from the regular key names in specifying the owner user. If the current user is someuser, the user/some/key full name is user:someuser/some/key. Makes effect in ls, export and get subcommands. \n");
 	printf("\n");
+}
+
+void optiond()
+{
 	printf("-d\n");
 	printf(" Causes get to work descriptivelly. When requesting a key it will show the comment, key name and its value in a fancy format \n");
 	printf("\n");
+}
+
+void options()
+{
 	printf("-s\n");
 	printf(" Causes get to be more friendly to Shell scripts. For example, when requesting user/env/env2/PATH, the output will be PATH=\"the value\", that is, only the basename of the key will be showed and the value will be surrounded by  \".\n");
 	printf("\n");
+}
+
+void optiont()
+{
 	printf("-t type\n");
 	printf(" When setting a key's value, you can specify the type with this switch. Currently accepted types are string for plain text, bin for binary as-is values, dir to create folder keys and link to create symbolic links between keys. Plain text are always stored as UTF-8(7) in Elektra, regardeless of your current encoding ($LANG). If you want to force a value to be stored without the UTF-8(7) encoding (a bad idea), you can set it as binary. Binary values should be avoided, because they are black boxes for system administrators. \n");
 	printf("\n");
+}
+
+void optionb()
+{
 	printf("-b filename\n");
 	printf(" Set the key value as the content of file filename. This option is more usefull when setting binary keys. \n");
 	printf("\n");
+}
+
+void optionm()
+{
 	printf("-m mode\n");
 	printf(" For the set command. Will set the key access permission to mode, which must be an octal number as for chmod(1). \n");
 	printf("\n");
+}
+
+void optionu()
+{
 	printf("-u uid\n");
 	printf(" Create the key with uid user ID. It can be a user name or a uid number. \n");
 	printf("\n");
+}
+
+void optiong()
+{
 	printf("-g gid\n");
 	printf(" Create the key with gid group ID. It can be a group name or a gid number \n");
 	printf("\n");
+}
+
+void optionc()
+{
 	printf("-c comment\n");
 	printf(" When setting keys, you can use this argument to set a descriptive comment for it. This comment is exactly as a comment in a plain text configuration file. The comment is stored as UTF-8(7) regardeless of your current encoding ($LANG). \n");
 	printf("\n");
-	printf("-v\n");
-	printf(" With the ls subcommand, will make it show also the value stored in the key. \n");
-	printf("\n");
-	printf("--\n");
-	printf(" With the set subcommand, everything after it will be considered the value, even text with dashes (-). Best Practices When Creating Keys\n");
-	printf("\n");
-	exit (0);
 }
 
 
+void optionv()
+{
+	printf("-v\n");
+	printf(" With the ls subcommand, will make it show also the value stored in the key. \n");
+	printf("\n");
+}
 
+void commandGetHelp ()
+{	
+	printf("get\n");
+	printf(" Get the value from the specified key. Accepts options: -d, -l, -f, -s \n");
+	printf("\n");
+	optiond();
+	optionl();	
+	optionf();	
+	options();	
+}
 
+void commandSetHelp ()
+{
+	printf("set\n");
+	printf(" Set the value to the specified key. Accepts options: -c, -t, -m, -b \n");
+	printf("\n");
+	optionc();	
+	optiont();	
+	optionm();	
+	optionb();	
+}
+
+void commandListHelp ()
+{
+	printf("ls\n");
+	printf(" As the ls(1) command, list key names for the specified key, or children keys, if specified a folder key. The -v argument will make it show also the values of each key. The -d (descriptive) will make it show the comment, key name and its value, as you are watching a plain text file. Accepts options: -x, -d, -l, -f, -v, -R \n");
+	printf("\n");
+	optionx();	
+	optiond();	
+	optionl();	
+	optionf();	
+	optionv();	
+	optionr();	
+}
+
+void commandLinkHelp ()
+{
+	printf("ln\n");
+	printf(" Creates a key that is a symbolic links to another key. \n");
+	printf("\n");
+}
+
+void commandMoveHelp()
+{
+	printf("mv\n");
+	printf(" Move, or renames a key. Currently it can't move keys across different filesystems. \n");
+	printf("\n");
+}
+
+void commandRemoveHelp()
+{
+	printf("rm\n");
+	printf(" As the rm(1) command, removes the key specified. \n");
+	printf("\n");
+}
+
+void commandEditHelp()
+{
+	printf("edit\n");
+	printf(" A very powerfull subcommand that lets you edit an XML representation of the keys. The parameters it accepts is usually a parent key, so its child keys will be gathered. Can be used with the -R flag to work recursively. The editor used is the one set in the $EDITOR environment variable, or vi. After editing the keys, kdb edit will analyze them and commit only the changed keys, remove the keys removed, and add the keys added. \n");
+	printf("\n");
+}
+
+void commandExportHelp()
+{
+	printf("export, save, \n");
+	printf("Export a subtree of keys to XML. If no subtree is defined right after the export command, system and current user trees will be exported. Output is written to standard output. The output encoding will allways be UTF-8, regardeless of your system encoding. UTF-8 is the most universal charset you can get when exchanging data between multiple systems. Accepts -f. \n");
+	printf("\n");
+	optionf();	
+}
+
+void commandImportHelp()
+{
+	printf("import, load, \n");
+	printf("Import an XML representation of keys and save it to the keys database. If no filename is passed right after the import command, standard input is used. \n");
+	printf("\n");
+}
+
+void commandMonitorHelp()
+{
+	printf("monitor, mon, \n");
+	printf("Monitor a key for some value change. It will block your command line until a change in the key value is detected, then return its new value.\n");
+	printf("\n");
+}
 
 
 /**
@@ -1353,6 +1463,22 @@ int doCommand(int command) {
 	return 0;
 }
 
+int helpCommand(int command) {
+	switch (command) {
+		case CMD_SET:             commandSetHelp(); break;
+		case CMD_LIST:            commandListHelp(); break;
+		case CMD_LINK:            commandLinkHelp(); break;
+		case CMD_GET:             commandGetHelp(); break;
+		case CMD_REMOVE:          commandRemoveHelp(); break;
+		case CMD_EDIT:            commandEditHelp(); break;
+		case CMD_LOAD:            commandImportHelp(); break;
+		case CMD_SAVE:            commandExportHelp(); break;
+		case CMD_MONITOR:         commandMonitorHelp(); break;
+		case CMD_MOVE:            commandMoveHelp(); break;
+	}
+	exit (0);
+}
+
 void cleanup()
 {
 	kdbClose();
@@ -1367,6 +1493,8 @@ int main(int argc, char **argv) {
 		fprintf(stderr,"kdb: XML importing and editing disabled\n");
 	
 	command=parseCommandLine(argc,argv);
+
+	if (argHelp) helpCommand(command);
 
 	kdbOpen();
 	atexit(cleanup); /**Make sure kdbClose will be used*/
