1b908643ce80be85ca453ef9bbaf4e3321fd84f9
ylecaillez <ylecaillez@3c96aef2-7adb-0310-a8e7-cbd0541efc33>
2006-02-05 15:48:42 +0000

    * Add backends symbols
    git-svn-id: https://svn.libelektra.org/svn/elektra/trunk@546 3c96aef2-7adb-0310-a8e7-cbd0541efc33

---
+++ src/libelektra/backend_symbols.c
@@ -0,0 +1,23 @@
+#include "kdbbackend.h"
+#include "kdbLibLoader.h"
+
+#ifdef __STATIC
+
+/* Make the compiler happy */
+extern int libelektra_filesys_LTX_kdbBackendFactory;
+extern int libelektra_ini_LTX_kdbBackendFactory;
+
+kdblib_symbol kdb_exported_syms[] =
+{
+	{"libelektra-filesys", NULL },
+	{"kdbBackendFactory", &libelektra_filesys_LTX_kdbBackendFactory },
+
+	{"libelektra-ini", NULL },
+        {"kdbBackendFactory", &libelektra_ini_LTX_kdbBackendFactory },
+	
+	{ NULL, NULL }
+};
+
+
+#endif
+
