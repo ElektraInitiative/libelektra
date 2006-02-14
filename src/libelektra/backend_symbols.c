de0f4322caacf127b52bb61a5d019f48951a6d2f
aviram <aviram@3c96aef2-7adb-0310-a8e7-cbd0541efc33>
2006-02-14 03:16:51 +0000

    Disabled the ini backend due to compilation errors.
    git-svn-id: https://svn.libelektra.org/svn/elektra/trunk@582 3c96aef2-7adb-0310-a8e7-cbd0541efc33

--- src/libelektra/backend_symbols.c
+++ src/libelektra/backend_symbols.c
@@ -8,16 +8,16 @@
 
 /* Make the compiler happy */
 extern int libelektra_filesys_LTX_kdbBackendFactory;
-extern int libelektra_ini_LTX_kdbBackendFactory;
+/* extern int libelektra_ini_LTX_kdbBackendFactory; */
 
 kdblib_symbol kdb_exported_syms[] =
 {
 	{"libelektra-filesys", NULL },
 	{"kdbBackendFactory", &libelektra_filesys_LTX_kdbBackendFactory },
-
+/*
 	{"libelektra-ini", NULL },
         {"kdbBackendFactory", &libelektra_ini_LTX_kdbBackendFactory },
-	
+*/	
 	{ NULL, NULL }
 };
 
