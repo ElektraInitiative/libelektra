f1dd9ea018878edb4086e37bcefe2fe73f9c1a62
rayman <rayman@3c96aef2-7adb-0310-a8e7-cbd0541efc33>
2006-02-15 11:22:37 +0000

    Changed static kdbLibInit call for windows to return "no error" correctly. (instead of always error)
    git-svn-id: https://svn.libelektra.org/svn/elektra/trunk@606 3c96aef2-7adb-0310-a8e7-cbd0541efc33

--- src/include/kdbLibLoader.h
+++ src/include/kdbLibLoader.h
@@ -37,7 +37,7 @@ typedef kdblib_symbol* kdbLibHandle;
 #ifdef WIN32
 /* Windows case, non static */
 #include <windows.h>
-#define kdbLibInit() 1
+#define kdbLibInit() 0
 typedef HMODULE kdbLibHandle;
 #else
 /* Default case */
