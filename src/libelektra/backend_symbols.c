190cdc4a05ac70ba60a2817258954bcba88605d8
ylecaillez <ylecaillez@3c96aef2-7adb-0310-a8e7-cbd0541efc33>
2006-02-15 07:02:43 +0000

    * Add -Wall flags * Include INI backends in static libelektra when compiled with --enable-experimental
    git-svn-id: https://svn.libelektra.org/svn/elektra/trunk@604 3c96aef2-7adb-0310-a8e7-cbd0541efc33

--- src/libelektra/backend_symbols.c
+++ src/libelektra/backend_symbols.c
@@ -8,16 +8,18 @@
 
 /* Make the compiler happy */
 extern int libelektra_filesys_LTX_kdbBackendFactory;
-/* extern int libelektra_ini_LTX_kdbBackendFactory; */
+#ifdef EXPERIMENTAL
+extern int libelektra_ini_LTX_kdbBackendFactory;
+#endif
 
 kdblib_symbol kdb_exported_syms[] =
 {
 	{"libelektra-filesys", NULL },
 	{"kdbBackendFactory", &libelektra_filesys_LTX_kdbBackendFactory },
-/*
+#ifdef EXPERIMENTAL
 	{"libelektra-ini", NULL },
         {"kdbBackendFactory", &libelektra_ini_LTX_kdbBackendFactory },
-*/	
+#endif
 	{ NULL, NULL }
 };
 
