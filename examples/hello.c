c3669a09f42872327e5b8b5a2c037c1ccf3cbbf2
mraab <mraab@3c96aef2-7adb-0310-a8e7-cbd0541efc33>
2006-04-10 13:28:45 +0000

    added hello world program
    git-svn-id: https://svn.libelektra.org/svn/elektra/trunk@731 3c96aef2-7adb-0310-a8e7-cbd0541efc33

---
+++ examples/hello.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <kdb.h>
+
+void BailOut (char * msg) {
+        kdbPrintError (msg);
+        exit (1);
+}
+
+int main() {
+	KDBHandle h;
+        KeySet * myConfig = ksNew();
+        Key    * myKey;
+        char   * myValue;
+
+        /* Open the Key Database */
+        if (kdbOpen(&h) == -1)
+                BailOut ("Could not open Key Database");
+
+        /* Get the hello world keyset */
+        if (kdbGetChildKeys(h,"user", myConfig, KDB_O_RECURSIVE ) == -1)
+                BailOut ("Could not get Keys");
+
+        /* Find the key in the keyset */
+        if ((myKey = ksLookupByName (myConfig, "/hello", 0)) == NULL)
+                BailOut ("Could not Lookup Key");
+
+        /* Get the value of the key */
+        if ((myValue = (char*) keyStealValue (myKey)) == NULL)
+                BailOut ("Could not get Keyvalue");
+
+        /* Actually print the key */
+        printf ("%s\n", myValue);
+        /* Close and free KeySet */
+        ksDel(myConfig);
+        /* Close the Key database */
+        kdbClose(&h);
+}
+
