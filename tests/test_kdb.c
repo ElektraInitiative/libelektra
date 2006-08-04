7425a46ff6220bfd9361cd553f7efdadf6beb453
ylecaillez <ylecaillez@3c96aef2-7adb-0310-a8e7-cbd0541efc33>
2006-08-04 14:40:23 +0000

    Added some more check ...
    git-svn-id: https://svn.libelektra.org/svn/elektra/trunk@878 3c96aef2-7adb-0310-a8e7-cbd0541efc33

--- tests/test_kdb.c
+++ tests/test_kdb.c
@@ -92,15 +92,58 @@ int loadToolsLib(void) {
 	return 0;
 }
 
+int delete_keysRecurse(KDBHandle handle, const char *root)
+{
+	Key	*key, *cur;
+	KeySet	*ks;
+	int	ret = 0;
+
+	/* Fetch all _not_ directory keys */
+	key = keyNew(root, KEY_SWITCH_END);
+	ks = ksNew();
+	if ( kdbGetKeyChildKeys(handle, key, ks, KDB_O_RECURSIVE | KDB_O_STATONLY | KDB_O_INACTIVE) == -1 ) {
+		ksDel(ks);
+		keyDel(key);
+		return 1;
+	}
+	
+	/* Delete these ... */
+	ksRewind(ks);
+	while ( (cur = ksPop(ks)) != NULL ) {
+		if ( kdbRemoveKey(handle, cur) )
+			ret = 1;
+	}
+	ksClose(ks);
+
+	/* Fetch all directory keys */
+	ksInit(ks);
+	if ( kdbGetKeyChildKeys(handle, key, ks, KDB_O_DIRONLY | KDB_O_SORT | KDB_O_INACTIVE) == -1 ) {
+		ksDel(ks);
+		keyDel(key);
+		return 1;
+	}
+
+	/* Delete these ... */
+	ksRewind(ks);
+	while ( (cur = ksPopLast(ks)) != NULL ) {
+		if ( kdbRemoveKey(handle, cur) )
+			ret = 1;
+	}
+	ksDel(ks);
+	keyDel(key);
+	
+	return ret;
+}
 
 void test_backend(char *backendName)
 {
 	KDBHandle	handle;
-	KeySet		*ks;
+	KeySet		*ks, *ks2, *removed;
 	Key     	*cur, *key, *key2;
 	int		ret, exist;
 	char		buf[1024], root[1024];
 	char		tmpname[L_tmpnam];
+	int		counter;
 	
 	printf("Testing elektra-%s backend.\n", backendName);
 
@@ -131,21 +174,26 @@ void test_backend(char *backendName)
 	printf("\tkey root = %s\n", root);
 	
 	
-	/* Testing key import 
-	 *   - Import key one by one to 
-	 *   - Fetch the same key from backend
-	 *   - Compare these two keys using keyCompare()
+	/* 
+	 * Here we'll test functions which act on Key 
 	 */
+	counter = 0;
 	ksRewind(ks);
 	while ( (cur = ksNext(ks)) ) {
 		key = keyNew(KEY_SWITCH_END);
 		key2 = keyNew(KEY_SWITCH_END);
 		
+		/* Prepend key root */	
 		snprintf(buf, sizeof(buf), "%s/%s", root, keyStealName(cur));
 		keySetName(cur, buf);
 		
-		/* Import a key to the backend. Then fetch the same 
-		 * key from the backend. Compare these two */
+		/* Test kdbGetKey()/kdbSetKey() 
+		 * ----------------------------
+		 *  
+		 * Import a key to the backend. Fetch the same key from the backend.
+		 * Compare key stored/key fetched.
+		 * The fetched key will be used as basis for other tests.
+		 */
 		keyInit(key);
 		keySetName(key, keyStealName(cur));
 		succeed_if( kdbSetKey(handle, cur) == 0, "kdbSetKey failed.");
@@ -153,8 +201,10 @@ void test_backend(char *backendName)
 		succeed_if( keyCompare( cur, key) == 0, "keyCompare failed : Differences between key stored/key readed");
 
 		/* Test kdbStatKey() 
-		 * Compare stat info from the previously loaded key
-		 * against a new one loaded by kdbStatKey()
+		 * -----------------
+		 *  
+		 * Stat the key previously stored. Compare stat infotmation
+		 * from the "stated" key against the previously fetched one.
 		 * */
 		keyInit(key2);
 		keySetName(key2, keyStealName(cur));
@@ -169,8 +219,10 @@ void test_backend(char *backendName)
 		keyClose(key2);
 
 		/* Test kdbRename()
-		 * Rename the current key to <keyname>-renamed
-		 * stat the renamed key
+		 * ----------------
+		 *  
+		 * Rename the prefiously stored key ro <keyname>-renamed.
+		 * Stat the renamed key for check if existing
 		 */
 		snprintf(buf, sizeof(buf), "%s-renamed", keyStealName(key));
 		keyInit(key2);
@@ -178,18 +230,61 @@ void test_backend(char *backendName)
 		
 		succeed_if( kdbRename(handle, key, buf) == 0, "kdbRename failed.");
 		succeed_if( kdbStatKey(handle, key2) == 0, "kdbRename failed : kdbStatKey on the renamed key failed.");
+		keySetName(cur, buf);
+		keySetName(key, buf);
 		keyClose(key2);
 
-		/* Test kdbRemoveKey()
-		 * Try to remove DIR key. Key type DIR shouldn't be removed
-		 * since kdbRemoveKey() isn't recursive */
-		if ( keyIsDir(key) ) {
-			succeed_if( kdbRemoveKey(handle, key), "kdbRemoveKey failed : Allowed deletion of a non-empty directory.");
-		}
+		/* Test kdbLink()
+		 * --------------
+		 *  
+		 * Link the previously stored key to root/link-<counter>
+		 */
+		snprintf(buf, sizeof(buf), "%s/link-%d", root, counter++);
+		keyInit(key2);
+		keySetName(key2, buf);
+	
+		succeed_if( kdbLink(handle, keyStealName(key), buf) == 0, "kdbLink failed.");
+		succeed_if( kdbStatKey(handle, key2) == 0, "kdbStatKey failed on link key");
+		succeed_if( strcmp(keyStealValue(key2), keyStealName(key)) == 0, "kdbLink link target isn't set correctly.");
+		succeed_if( kdbGetKey(handle, key2) == 0, "kdbGetKey failed on link key");
+		succeed_if( keyCompare(key, key2) == 0, "kdbLink kdbGetKey(link)/kdbGetKey(link-target) not equal.");
+		succeed_if( kdbRemoveKey(handle, key2) == 0, "kdbRemoveKey failed on link key");
+		keyClose(key2);
 				
 		keyDel(key);
 		keyDel(key2);
 	}
+	
+	succeed_if( delete_keysRecurse(handle, root) == 0, "delete_keysRecurse failed.");
+	
+	/*
+	 * Here we'll test functions which act on KeySet
+	 */
+	ks2 = ksNew();
+	ksRewind(ks);
+	key = keyNew(root, KEY_SWITCH_END);
+	
+	succeed_if( kdbSetKeys(handle, ks) == 0, "kdbSetKeys failed.");
+	succeed_if( kdbGetKeyChildKeys(handle, key, ks2, KDB_O_RECURSIVE | KDB_O_DIR | KDB_O_INACTIVE ) == 0, "kdbGetKeyChildKeys failed.");
+
+	/* I would have a _true_ ksCompare() ... */
+	keyDel(key);
+	ksSort(ks); ksRewind(ks);
+	ksSort(ks2); ksRewind(ks2);
+	if ( ksGetSize(ks) == ksGetSize(ks2) ) {
+		while ( ((key = ksNext(ks)) != NULL) && ((key2 = ksNext(ks2)) != NULL) ) {
+			ret = keyCompare(key, key2);
+			if ( ret )
+				break;
+		}
+		succeed_if( ret == 0, "kdbGetKeyChildKeys: Keys fetched aren't equat to the submited ones.");
+	} else {
+		succeed_if( 0, "kdbGetKeyChildKeys: There are less keys fetched than keys which have been submited.");
+	}
+	ksDel(ks);
+	ksDel(ks2);
+	
+	succeed_if( delete_keysRecurse(handle, root) == 0, "delete_keysRecurse failed.");
 
 	kdbClose(&handle);
 	
