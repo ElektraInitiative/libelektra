e2a86fa56be302c1553328f228e57560ceb2cc8c
mraab <mraab@3c96aef2-7adb-0310-a8e7-cbd0541efc33>
2006-08-17 06:25:01 +0000

    delete temporary files before boostrapping
    git-svn-id: https://svn.libelektra.org/svn/elektra/trunk@905 3c96aef2-7adb-0310-a8e7-cbd0541efc33

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,37 @@
 #!/bin/sh
+#this script bootstraps elektra
+echo "BOOTSTRAPING ELEKTRA"
+
+#Testing if in correct directory
+test -f elektra.xml || exit 1
+
+# Removing temporary directories
+echo "CLEANING UP DIRECTORIES"
+rm -rf autom4te.cache
+rm -rf libltdl
+
+# Removing temporary files
+echo "CLEANING UP FILES"
+rm -f aclocal.m4
+rm -f compile
+rm -f config.guess
+rm -f config.log
+rm -f config.status
+rm -f config.sub
+rm -f configure
+rm -f depcomp
+rm -f elektra.pc
+rm -f elektra.spec
+rm -f elektratools.pc
+rm -f install-sh
+rm -f libtool
+rm -f ltmain.sh
+rm -f Makefile
+rm -f Makefile.in
+rm -f missing
+rm -f mkinstalldirs
 
 # Bootstraping Elektra
-echo "BOOTSTRAPING ELEKTRA"
 echo "LIBTOOLIZE"
 libtoolize --ltdl --copy
 
