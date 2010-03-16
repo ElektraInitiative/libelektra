4eb2a476f151a562c42fd7f46afa4e3fac173cb9
Markus Raab <uni@markus-raab.org>
2010-03-16 10:19:59 +0100

    give away warning flags

--- cmake/ElektraCompiling.cmake
+++ cmake/ElektraCompiling.cmake
@@ -1,6 +1,16 @@
+##
+# This file sets compiler flags and things related
+# to OS detection
+##
+
 # Set default compile flags for GCC
 if (CMAKE_COMPILER_IS_GNUCXX)
 	set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -pedantic -std=c++98 -Wall -Wextra -Wno-missing-field-initializers -O2")
-	set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -pedantic -std=c99 -Wall -O2")
+	#set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -pedantic -std=c99 -Wall -Wsign-compare -Wfloat-equal -Wformat-security -O2")
+	set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -O2")
 	message (STATUS "GCC detected, adding compile flags: ${CMAKE_C_FLAGS}")
 endif (CMAKE_COMPILER_IS_GNUCXX)
+
+if (WIN32)
+	set (HAVE_WIN32 "1")
+endif (WIN32)
