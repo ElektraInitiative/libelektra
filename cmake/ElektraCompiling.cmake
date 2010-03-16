5e2ec4731add6f34ef866260bce07da3c8a8961b
Markus Raab <uni@markus-raab.org>
2010-03-16 09:04:24 +0100

    elektra compiling flags

---
+++ cmake/ElektraCompiling.cmake
@@ -0,0 +1,6 @@
+# Set default compile flags for GCC
+if(CMAKE_COMPILER_IS_GNUCXX)
+	set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -pedantic -std=c++98 -Wall -Wextra -Wno-missing-field-initializers -O2")
+	set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -pedantic -std=c99 -Wall -O2")
+	message (STATUS "GCC detected, adding compile flags: ${CMAKE_C_FLAGS}")
+endif(CMAKE_COMPILER_IS_GNUCXX)
