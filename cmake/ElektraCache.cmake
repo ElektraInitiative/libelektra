77deb6594b50a16f54018aea45f7d385943181c6
Markus Raab <uni@markus-raab.org>
2010-03-22 22:40:52 +0100

    conditional BUILD_TOOLS

--- cmake/ElektraCache.cmake
+++ cmake/ElektraCache.cmake
@@ -25,6 +25,8 @@ option (BUILD_SHARED "Build the shared version of elektra." ON)
 option (BUILD_FULL "Build the full version of elektra (shared with all selected backends included)." ON)
 option (BUILD_STATIC "Build the static version of elektra (all selected backends included)." ON)
 
+option (BUILD_TOOLS "Build the library libelektratools (shared, full and static options take affect here too)" ON)
+
 set (PROJECT_CMAKE_DIR
 		"${CMAKE_INSTALL_PREFIX}/share/cmake-2.6/Modules"
 		CACHE PATH
