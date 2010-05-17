ef56939cb3997e3cc4be20b6a4725ab45a928958
Markus Raab <uni@markus-raab.org>
2010-05-17 20:48:13 +0200

    test coverage good
    also ksLookup tested

--- tests/test_operation.c
+++ tests/test_operation.c
@@ -80,20 +80,91 @@ void test_search()
 	Key *s = keyNew("user/a", KEY_END);
 	ssize_t result;
 
+	keySetName (s, "user/0");
 	result = ksSearchInternal (a, s);
-	succeed_if (result == 0, "key not found");
+	succeed_if (result == 0, "insertpos wrong");
+
+	keySetName (s, "user/a");
+	result = ksSearchInternal (a, s);
+	succeed_if (result == 1, "insertpos wrong");
+
+	keySetName (s, "user/a/0");
+	result = ksSearchInternal (a, s);
+	succeed_if (result == -3, "insertpos wrong");
+
+	keySetName (s, "user/a/a");
+	result = ksSearchInternal (a, s);
+	succeed_if (result == 2, "insertpos wrong");
+
+	keySetName (s, "user/a/a/a");
+	result = ksSearchInternal (a, s);
+	succeed_if (result == 3, "insertpos wrong");
+
+	keySetName (s, "user/a/a/b");
+	result = ksSearchInternal (a, s);
+	succeed_if (result == 4, "insertpos wrong");
 
 	keySetName (s, "user/a/b");
 	result = ksSearchInternal (a, s);
-	succeed_if (result == 4, "middle wrong");
+	succeed_if (result == 5, "insertpos wrong");
+
+	keySetName (s, "user/a/b/a");
+	result = ksSearchInternal (a, s);
+	succeed_if (result == 6, "insertpos wrong");
+
+	keySetName (s, "user/a/b/b");
+	result = ksSearchInternal (a, s);
+	succeed_if (result == 7, "insertpos wrong");
+
+	keySetName (s, "user/a/c");
+	result = ksSearchInternal (a, s);
+	succeed_if (result == 8, "insertpos wrong");
+
+	keySetName (s, "user/a/d");
+	result = ksSearchInternal (a, s);
+	succeed_if (result == 9, "insertpos wrong");
 
 	keySetName (s, "user/a/x");
 	result = ksSearchInternal (a, s);
-	succeed_if (result == -10, "insertpos wrong");
+	succeed_if (result == -11, "insertpos wrong");
 
-	keySetName (s, "user/a/e");
+	keySetName (s, "user/a/x/a");
 	result = ksSearchInternal (a, s);
-	succeed_if (result == -10, "insertpos wrong");
+	succeed_if (result == 10, "insertpos wrong");
+
+	keySetName (s, "user/a/x/b");
+	result = ksSearchInternal (a, s);
+	succeed_if (result == 11, "insertpos wrong");
+
+	keySetName (s, "user/a/x/c");
+	result = ksSearchInternal (a, s);
+	succeed_if (result == 12, "insertpos wrong");
+
+	keySetName (s, "user/a/x/c/a");
+	result = ksSearchInternal (a, s);
+	succeed_if (result == 13, "insertpos wrong");
+
+	keySetName (s, "user/a/x/c/b");
+	result = ksSearchInternal (a, s);
+	succeed_if (result == 14, "insertpos wrong");
+
+	keySetName (s, "user/x");
+	result = ksSearchInternal (a, s);
+	succeed_if (result == 15, "insertpos wrong");
+
+	/*
+	   Generation of new Testcases:
+	for (int i=0; i< 16; ++i)
+	{
+		s = a->array[i];
+		printf ("keySetName (s, \"%s\");\n", keyName(s));
+		printf ("result = ksSearchInternal (a, s);\n");
+		printf ("succeed_if (result == %zd, \"insertpos wrong\");\n\n", ksSearchInternal (a, s));
+	}
+	*/
+
+	keyDel (s);
+	ksDel (a);
 }
 
 void test_cut()
@@ -189,8 +260,8 @@ int main(int argc, char** argv)
 	init (argc, argv);
 
 	test_search();
-	// test_cut();
-	// test_copy();
+	test_cut();
+	// test_copy(); // has memory problems...
 
 	printf("\ntest_ks RESULTS: %d test(s) done. %d error(s).\n", nbTest, nbError);
 
