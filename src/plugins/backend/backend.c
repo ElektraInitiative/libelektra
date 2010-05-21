0474360e74f7cbbc2063401bf6b41f428ea73658
Markus Raab <uni@markus-raab.org>
2010-05-21 12:32:33 +0200

    backend plugin added
    doc can now be build too

---
+++ src/plugins/backend/backend.c
@@ -0,0 +1,75 @@
+/***************************************************************************
+            backend.c  -  Plugin which composes other plugins together
+                             -------------------
+    begin                : Fri May 21 2010
+    copyright            : (C) 2010 by Markus Raab
+    email                : elektra@markus-raab.org
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the BSD License (revised).                      *
+ *                                                                         *
+ ***************************************************************************/
+
+
+
+/***************************************************************************
+ *                                                                         *
+ *   This is the skeleton of the methods you'll have to implement in order *
+ *   to provide libelektra.so a valid plugin.                             *
+ *   Simple fill the empty _backend functions with your code and you are   *
+ *   ready to go.                                                          *
+ *                                                                         *
+ ***************************************************************************/
+
+
+#include "backend.h"
+
+int kdbOpen_backend(Plugin *handle)
+{
+	/* plugin initialization logic */
+
+	return 0; /* success */
+}
+
+int kdbClose_backend(Plugin *handle)
+{
+	/* free all plugin resources and shut it down */
+
+	return 0; /* success */
+}
+
+ssize_t kdbGet_backend(Plugin *handle, KeySet *returned, const Key *parentKey)
+{
+	ssize_t nr_keys = 0;
+	/* get all keys below parentKey and count them with nr_keys */
+
+	return nr_keys; /* success */
+}
+
+ssize_t kdbSet_backend(Plugin *handle, KeySet *returned, const Key *parentKey)
+{
+	ssize_t nr_keys = 0;
+	/* set all keys below parentKey and count them with nr_keys */
+
+	return nr_keys;
+}
+
+Plugin *KDBEXPORT(backend)
+{
+	return pluginExport(BACKENDNAME,
+		KDB_PLUGIN_OPEN,	&kdbOpen_backend,
+		KDB_PLUGIN_CLOSE,	&kdbClose_backend,
+		KDB_PLUGIN_GET,		&kdbGet_backend,
+		KDB_PLUGIN_SET,		&kdbSet_backend,
+		KDB_PLUGIN_VERSION,	BACKENDVERSION,
+		KDB_PLUGIN_AUTHOR,	"Markus Raab <elektra@markus-raab.org>",
+		KDB_PLUGIN_LICENCE,	"BSD",
+		KDB_PLUGIN_DESCRIPTION,	"This plugin composes other plugins together to a backend",
+		KDB_PLUGIN_NEEDS,	"tracer dump logger",
+		KDB_PLUGIN_PROVIDES,	"",
+		KDB_PLUGIN_END);
+}
+
---
+++ src/plugins/backend/backend.h
@@ -0,0 +1,39 @@
+/***************************************************************************
+            backend.h  -  Skeleton of backends to access the Key Database
+                             -------------------
+    begin                : Fri May 21 2010
+    copyright            : (C) 2010 by Markus Raab
+    email                : elektra@markus-raab.org
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the BSD License (revised).                      *
+ *                                                                         *
+ ***************************************************************************/
+
+
+
+/***************************************************************************
+ *                                                                         *
+ *   This is the skeleton of the methods you'll have to implement in order *
+ *   to provide libelektra.so a valid backend.                             *
+ *   Simple fill the empty _backend functions with your code and you are   *
+ *   ready to go.                                                          *
+ *                                                                         *
+ ***************************************************************************/
+
+
+
+#include <kdbplugin.h>
+
+
+#define BACKENDNAME "backend"
+#define BACKENDVERSION "0.0.1"
+
+int kdbOpen_backend(Plugin *handle);
+int kdbClose_backend(Plugin *handle);
+ssize_t kdbGet_backend(Plugin *handle, KeySet *ks, const Key *parentKey);
+ssize_t kdbSet_backend(Plugin *handle, KeySet *ks, const Key *parentKey);
+Plugin *KDBEXPORT(backend);
