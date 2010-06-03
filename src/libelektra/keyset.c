f536d6991d7f01afeec9493b3ac9739116cfb98a
Markus Raab <uni@markus-raab.org>
2010-06-03 11:39:42 +0200

    max size check wrong

--- src/libelektra/keyset.c
+++ src/libelektra/keyset.c
@@ -971,7 +971,7 @@ Key *ksPop(KeySet *ks)
 Key * ksIndex (KeySet *ks, size_t index)
 {
 	if (!ks) return 0;
-	if (index < SSIZE_MAX) return 0;
+	if (index > SSIZE_MAX) return 0;
 
 	return ks->array[index];
 }
