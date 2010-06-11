511b604b43db437b52a1156442d438e62d819e6b
Markus Raab <uni@markus-raab.org>
2010-06-11 12:02:04 +0200

    support warnings

--- src/error/exporterrors.cpp
+++ src/error/exporterrors.cpp
@@ -13,6 +13,7 @@ ostream& operator << (ostream& os, parse_t& p)
 	   << "#define KDBERRORS_H" << endl
 	   << endl
 	   << "#include <kdb.h>" << endl
+	   << "#include <string.h>" << endl
 	   << endl
 	   << "#define ELEKTRA_SET_ERROR(number, key, text) ELEKTRA_SET_ERROR_HELPER\\" << endl
 	   << "	(number, key, text, __FILE__, __LINE__)" << endl
@@ -22,24 +23,77 @@ ostream& operator << (ostream& os, parse_t& p)
 	   << endl
 	   << "#define ELEKTRA_SET_ERROR_HELPER_HELPER(number, key, text, file, line) elektraSetError ## number\\" << endl
 	   << "	(key, text, file, #line)" << endl
+	   << endl
+	   << endl
+	   << "#define ELEKTRA_ADD_WARNING(number, key, text) ELEKTRA_ADD_WARNING_HELPER\\" << endl
+	   << "	(number, key, text, __FILE__, __LINE__)" << endl
+	   << "" << endl
+	   << "#define ELEKTRA_ADD_WARNING_HELPER(number, key, text, file, line) ELEKTRA_ADD_WARNING_HELPER_HELPER\\" << endl
+	   << "	(number, key, text, file, line)" << endl
+	   << "" << endl
+	   << "#define ELEKTRA_ADD_WARNING_HELPER_HELPER(number, key, text, file, line) elektraAddWarning ## number\\" << endl
+	   << "	(key, text, file, #line)" << endl
+	   << endl
 	   << endl;
 
 	for (size_t i = 1; i<p.size(); ++i)
 	{
+		if (p[i]["severity"] == "warning")
+		{
+		os << "static inline void elektraAddWarning" << i << "(Key *warningKey, const char *reason," << endl
+		   << "	const char *file, const char *line)" << endl
+		   << "{" << endl
+		   << "	if (!warningKey) return;" << endl
+		   << "" << endl
+		   << "	char buffer[] = \"warnings/#00\\0description\";" << endl
+		   << "	const Key *meta = keyGetMeta(warningKey, \"warnings\");" << endl
+		   << "	if (meta)" << endl
+		   << "	{" << endl
+		   << "		buffer[10] = keyString(meta)[0];" << endl
+		   << "		buffer[11] = keyString(meta)[1];" << endl
+		   << "		buffer[11]++;" << endl
+		   << "		if (buffer[11] > '9')" << endl
+		   << "		{" << endl
+		   << "			buffer[11] = '0';" << endl
+		   << "			buffer[10]++;" << endl
+		   << "			if (buffer[10] > '9') buffer[10] = '0';" << endl
+		   << "		}" << endl
+		   << "		keySetMeta(warningKey, \"warnings\", &buffer[10]);" << endl
+		   << "	} else  keySetMeta(warningKey, \"warnings\", \"00\");" << endl
+		   << "" << endl
+		   << "	keySetMeta(warningKey, buffer, \"number description ingroup module file line function reason\");" << endl
+		   << "	strncat(buffer, \"/number\" , sizeof(buffer));" << endl
+		   << "	keySetMeta(warningKey, buffer, \"7\");" << endl
+		   << "	buffer[12] = '\\0'; strncat(buffer, \"/description\" , sizeof(buffer));" << endl
+		   << "	keySetMeta(warningKey, buffer, \"" << p[i]["description"] << "\");" << endl
+		   << "	buffer[12] = '\\0'; strncat(buffer, \"/ingroup\" , sizeof(buffer));" << endl
+		   << "	keySetMeta(warningKey, buffer, \"" << p[i]["ingroup"] << "\");" << endl
+		   << "	buffer[12] = '\\0'; strncat(buffer, \"/module\" , sizeof(buffer));" << endl
+		   << "	keySetMeta(warningKey, buffer, \"" << p[i]["module"] << "\");" << endl
+		   << "	buffer[12] = '\\0'; strncat(buffer, \"/file\" , sizeof(buffer));" << endl
+		   << "	keySetMeta(warningKey, buffer, file);" << endl
+		   << "	buffer[12] = '\\0'; strncat(buffer, \"/line\" , sizeof(buffer));" << endl
+		   << "	keySetMeta(warningKey, buffer, line);" << endl
+		   << "	buffer[12] = '\\0'; strncat(buffer, \"/reason\" , sizeof(buffer));" << endl
+		   << "	keySetMeta(warningKey, buffer, reason);" << endl
+		   << "}" << endl
+		   << endl;
+		} else {
 		os << "static inline void elektraSetError" << i << "(Key *errorKey, const char *reason," << endl
 		   << "	const char *file, const char *line)" << endl
 		   << "{" << endl
 		   << "	if (!errorKey) return;" << endl
-		   << "	 keySetMeta(errorKey, \"error\", \"" << "number description ingroup module file line function reason" << "\");" << endl
-		   << "	 keySetMeta(errorKey, \"error/number\", \"" << i << "\");" << endl
-		   << "	 keySetMeta(errorKey, \"error/description\", \"" << p[i]["description"] << "\");" << endl
-		   << "	 keySetMeta(errorKey, \"error/ingroup\", \"" << p[i]["ingroup"] << "\");" << endl
-		   << "	 keySetMeta(errorKey, \"error/module\", \"" << p[i]["module"] << "\");" << endl
-		   << "	 keySetMeta(errorKey, \"error/file\", " << "file" << ");" << endl
-		   << "	 keySetMeta(errorKey, \"error/line\", " << "line" << ");" << endl
-		   << "	 keySetMeta(errorKey, \"error/reason\", " << "reason" << ");" << endl
+		   << "	keySetMeta(errorKey, \"error\", \"" << "number description ingroup module file line function reason" << "\");" << endl
+		   << "	keySetMeta(errorKey, \"error/number\", \"" << i << "\");" << endl
+		   << "	keySetMeta(errorKey, \"error/description\", \"" << p[i]["description"] << "\");" << endl
+		   << "	keySetMeta(errorKey, \"error/ingroup\", \"" << p[i]["ingroup"] << "\");" << endl
+		   << "	keySetMeta(errorKey, \"error/module\", \"" << p[i]["module"] << "\");" << endl
+		   << "	keySetMeta(errorKey, \"error/file\", " << "file" << ");" << endl
+		   << "	keySetMeta(errorKey, \"error/line\", " << "line" << ");" << endl
+		   << "	keySetMeta(errorKey, \"error/reason\", " << "reason" << ");" << endl
 		   << "}" << endl
 		   << endl;
+		}
 	}
 
 	os << "#endif" << endl;
--- src/kdb/ls.cpp
+++ src/kdb/ls.cpp
@@ -1,6 +1,9 @@
 #include <ls.hpp>
 
 #include <iostream>
+#include <sstream>
+#include <iomanip>
+
 #include <kdb>
 
 using namespace kdb;
@@ -31,6 +34,29 @@ int LsCommand::execute(int argc, char** argv)
 	while (k=ks.next())
 	{
 		cout << "key: " << k.getName() << " " << k.getString() << endl;;
+	}
+
+	try{
+		int nr = root.getMeta<int>("warnings");
+		cerr << nr+1 << " Warnings were issued" << endl;
+
+		for (int i=0; i<=nr; i++)
+		{
+			ostringstream name;
+			name << "warnings/#" << setfill('0') << setw(2) << i;
+			std::cerr << name.str() << ": " << root.getMeta<std::string>(name.str()) << std::endl;
+			std::cerr << "number: " << root.getMeta<std::string>(name.str() + "/number") << std::endl;
+			std::cerr << "description: " << root.getMeta<std::string>(name.str() + "/description") << std::endl;
+			std::cerr << "ingroup: " << root.getMeta<std::string>(name.str() + "/ingroup") << std::endl;
+			std::cerr << "module: " << root.getMeta<std::string>(name.str() + "/module") << std::endl;
+			std::cerr << "at: " << root.getMeta<std::string>(name.str() + "/file") << ":"
+				<< root.getMeta<std::string>("error/line") << std::endl;
+			std::cerr << "reason: " << root.getMeta<std::string>(name.str() + "/reason") << std::endl;
+		}
+
+	} catch (KeyMetaException const& e)
+	{
+		// no warnings were issued
 	}
 
 	return 0;
