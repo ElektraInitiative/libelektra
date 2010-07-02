e41f6358a2e9c890b86995e7145bdaee3f2121e5
Markus Raab <uni@markus-raab.org>
2010-07-02 14:23:29 +0200

    error plugin
    in separate functions so that they can be generated
    in header file

--- src/plugins/error/error.c
+++ src/plugins/error/error.c
@@ -29,6 +29,23 @@
 
 #include <stdlib.h>
 
+static inline KeySet *elektraErrorSpecification (void)
+{
+	return ksNew (30,
+		keyNew ("system/elektra/modules/error/specification",
+			KEY_VALUE, "TODO: add the specification of all error codes below", KEY_END),
+		keyNew ("system/elektra/modules/error/specification/1",
+			KEY_VALUE, "", KEY_END),
+		keyNew ("system/elektra/modules/error/specification/1/module",
+			KEY_VALUE, "dl", KEY_END),
+		keyNew ("system/elektra/modules/error/specification/1/ingroup",
+			KEY_VALUE, "modules", KEY_END),
+		keyNew ("system/elektra/modules/error/specification/1/description",
+			KEY_VALUE, "could not load module, dlopen failed", KEY_END),
+		KS_END);
+
+}
+
 int elektraErrorGet(Plugin *handle, KeySet *returned, Key *parentKey)
 {
 	ksAppend (returned, ksNew (30,
@@ -59,18 +76,31 @@ int elektraErrorGet(Plugin *handle, KeySet *returned, Key *parentKey)
 			KEY_VALUE, "", KEY_END),
 		keyNew ("system/elektra/modules/error/infos/version",
 			KEY_VALUE, "1.0", KEY_END),
-		keyNew ("system/elektra/modules/error/specification",
-			KEY_VALUE, "TODO: add the specification of all error codes below", KEY_END),
-		keyNew ("system/elektra/modules/error/specification/1",
-			KEY_VALUE, "", KEY_END),
-		keyNew ("system/elektra/modules/error/specification/1/module",
-			KEY_VALUE, "dl", KEY_END),
-		keyNew ("system/elektra/modules/error/specification/1/ingroup",
-			KEY_VALUE, "modules", KEY_END),
-		keyNew ("system/elektra/modules/error/specification/1/description",
-			KEY_VALUE, "could not load module, dlopen failed", KEY_END),
 		KS_END));
+	ksAppend (returned, elektraErrorSpecification());
 	return 1;
+}
+
+static inline void elektraTriggerWarnings (int nr, Key *parentKey, const char *message)
+{
+	switch (nr)
+	{
+		case 7: ELEKTRA_ADD_WARNING (7, parentKey, message);
+			break;
+		default: ELEKTRA_ADD_WARNING (45, parentKey, "in default branch");
+			 break;
+	}
+}
+
+static inline void elektraTriggerError (int nr, Key *parentKey, const char *message)
+{
+	switch (nr)
+	{
+		case 1: ELEKTRA_SET_ERROR (1, parentKey, "from error plugin");
+			break;
+		default: ELEKTRA_SET_ERROR (44, parentKey, "in default branch");
+			 break;
+	}
 }
 
 int elektraErrorSet(Plugin *handle, KeySet *returned, Key *parentKey)
@@ -83,25 +113,13 @@ int elektraErrorSet(Plugin *handle, KeySet *returned, Key *parentKey)
 		meta = keyGetMeta (cur, "trigger/warnings");
 		if (meta)
 		{
-			switch (atoi(keyString(meta)))
-			{
-				case 7: ELEKTRA_ADD_WARNING (7, parentKey, "from error plugin");
-					break;
-				default: ELEKTRA_ADD_WARNING (45, parentKey, "in default branch");
-					 break;
-			}
+			elektraTriggerWarnings(atoi(keyString(meta)), parentKey, "from error plugin");
 		}
 
 		meta = keyGetMeta (cur, "trigger/error");
 		if (meta)
 		{
-			switch (atoi(keyString(meta)))
-			{
-				case 1: ELEKTRA_SET_ERROR (1, parentKey, "from error plugin");
-					break;
-				default: ELEKTRA_SET_ERROR (44, parentKey, "in default branch");
-					 break;
-			}
+			elektraTriggerError (atoi(keyString(meta)), parentKey, "from error plugin");
 			return -1; /* error */
 		}
 	}
