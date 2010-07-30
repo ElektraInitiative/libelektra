51b76a3894910ee189b4f861ac80d8838bf68c95
Markus Raab <uni@markus-raab.org>
2010-07-30 12:44:09 +0200

    network checker
    checks ip addresses

---
+++ src/plugins/network/network.c
@@ -0,0 +1,124 @@
+/***************************************************************************
+                     network.c  -  Skeleton of a plugin
+                             -------------------
+    begin                : Fri May 21 2010
+    copyright            : (C) 2010 by Markus Raab
+    email                : elektra@markus-raab.org
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the BSD License (revised).                      *
+ *                                                                         *
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This is the skeleton of the methods you'll have to implement in order *
+ *   to provide a valid plugin.                                            *
+ *   Simple fill the empty functions with your code and you are            *
+ *   ready to go.                                                          *
+ *                                                                         *
+ ***************************************************************************/
+
+
+#include "network.h"
+
+/* Obtain address(es) matching host/port */
+int elektraNetworkAddrInfo(Key *toCheck)
+{
+	struct addrinfo hints;
+	struct addrinfo *result;
+	int s;
+
+
+	memset(&hints, 0, sizeof(struct addrinfo));
+
+	const Key *meta = keyGetMeta (toCheck, "check/ipaddr");
+
+	if (!meta) return 0; /* No check to do */
+
+	hints.ai_family = AF_UNSPEC;     /* Allow IPv4 or IPv6 */
+	hints.ai_socktype = SOCK_DGRAM;  /* Datagram socket */
+	hints.ai_flags = AI_NUMERICHOST; /* Only accept numeric hosts */
+	hints.ai_protocol = 0;           /* Any protocol */
+
+	s = getaddrinfo(keyString(toCheck), keyString(meta), &hints, &result);
+
+	if (s != 0) {
+		return s;
+	}
+
+	freeaddrinfo(result);
+
+	return 0;
+}
+
+int elektraNetworkGet(Plugin *handle, KeySet *returned, Key *parentKey)
+{
+	/* configuration only */
+	KeySet *n;
+	ksAppend (returned, n=ksNew (30,
+		keyNew ("system/elektra/modules/network",
+			KEY_VALUE, "network plugin waits for your orders", KEY_END),
+		keyNew ("system/elektra/modules/network/exports", KEY_END),
+		keyNew ("system/elektra/modules/network/exports/get",
+			KEY_FUNC, elektraNetworkGet,
+			KEY_END),
+		keyNew ("system/elektra/modules/network/exports/set",
+			KEY_FUNC, elektraNetworkSet,
+			KEY_END),
+		keyNew ("system/elektra/modules/network/exports/elektraNetworkAddrInfo",
+			KEY_FUNC, elektraNetworkAddrInfo,
+			KEY_END),
+		keyNew ("system/elektra/modules/network/infos",
+			KEY_VALUE, "All information you want to know", KEY_END),
+		keyNew ("system/elektra/modules/network/infos/author",
+			KEY_VALUE, "Markus Raab <elektra@markus-raab.org>", KEY_END),
+		keyNew ("system/elektra/modules/network/infos/licence",
+			KEY_VALUE, "BSD", KEY_END),
+		keyNew ("system/elektra/modules/network/infos/description",
+			KEY_VALUE, "Checks keys if they contain a valid ip address", KEY_END),
+		keyNew ("system/elektra/modules/network/infos/provides",
+			KEY_VALUE, "check", KEY_END),
+		keyNew ("system/elektra/modules/network/infos/placements",
+			KEY_VALUE, "presetstorage", KEY_END),
+		keyNew ("system/elektra/modules/network/infos/needs",
+			KEY_VALUE, "", KEY_END),
+		keyNew ("system/elektra/modules/network/infos/version",
+			KEY_VALUE, PLUGINVERSION, KEY_END),
+		KS_END));
+	ksDel (n);
+
+	return 1; /* success */
+}
+
+int elektraNetworkSet(Plugin *handle, KeySet *returned, Key *parentKey)
+{
+	/* set all keys */
+	Key *cur;
+	int s;
+
+	ksRewind (returned);
+	while ((cur = ksNext(returned)) != 0)
+	{
+		s = elektraNetworkAddrInfo(cur);
+		if (s != 0)
+		{
+			ELEKTRA_SET_ERROR (51, parentKey, gai_strerror(s));
+			return -1;
+		}
+	}
+
+	return 1; /* success */
+}
+
+Plugin *ELEKTRA_PLUGIN_EXPORT(network)
+{
+	return elektraPluginExport("network",
+		ELEKTRA_PLUGIN_GET,	&elektraNetworkGet,
+		ELEKTRA_PLUGIN_SET,	&elektraNetworkSet,
+		ELEKTRA_PLUGIN_END);
+}
+
---
+++ src/plugins/network/network.h
@@ -0,0 +1,47 @@
+/***************************************************************************
+                     network.c  -  Skeleton of a plugin
+                             -------------------
+    begin                : Fri May 21 2010
+    copyright            : (C) 2010 by Markus Raab
+    email                : elektra@markus-raab.org
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the BSD License (revised).                      *
+ *                                                                         *
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This is the skeleton of the methods you'll have to implement in order *
+ *   to provide a valid plugin.                                            *
+ *   Simple fill the empty functions with your code and you are            *
+ *   ready to go.                                                          *
+ *                                                                         *
+ ***************************************************************************/
+
+
+#ifndef ELEKTRA_PLUGIN_NETWORK_H
+#define ELEKTRA_PLUGIN_NETWORK_H
+
+#include <kdbplugin.h>
+#include <kdberrors.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+int elektraNetworkAddrInfo(Key *toCheck);
+
+int elektraNetworkGet(Plugin *handle, KeySet *ks, Key *parentKey);
+int elektraNetworkSet(Plugin *handle, KeySet *ks, Key *parentKey);
+
+Plugin *ELEKTRA_PLUGIN_EXPORT(network);
+
+#endif
