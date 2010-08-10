c2d88c6ddaf9f9fdc4bf257311090eccdf78f4bd
Markus Raab <uni@markus-raab.org>
2010-08-10 17:00:42 +0200

    hexcode initial commit

---
+++ src/plugins/hexcode/hexcode.c
@@ -0,0 +1,255 @@
+/***************************************************************************
+                     hexcode.c  -  Skeleton of a plugin
+                             -------------------
+    begin                : Fri May 21 2010
+    copyright            : (C) 2010 by Markus Raab
+    email                : elektra@markus-raab.org
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the BSD License (revised).                      *
+ *                                                                         *
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This is the skeleton of the methods you'll have to implement in order *
+ *   to provide a valid plugin.                                            *
+ *   Simple fill the empty functions with your code and you are            *
+ *   ready to go.                                                          *
+ *                                                                         *
+ ***************************************************************************/
+
+
+#include "hexcode.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+/**
+  * Gives the integer number 0-15 to a corresponding
+  * hex character '0'-'9', 'a'-'f' or 'A'-'F'.
+  */
+static inline int elektraHexcodeConvFromHex(char c)
+{
+	if    (c == '0') return 0;
+	else if (c=='1') return 1;
+	else if (c=='2') return 2;
+	else if (c=='3') return 3;
+	else if (c=='4') return 4;
+	else if (c=='5') return 5;
+	else if (c=='6') return 6;
+	else if (c=='7') return 7;
+	else if (c=='8') return 8;
+	else if (c=='9') return 9;
+	else if (c=='a' || c=='A') return 10;
+	else if (c=='b' || c=='B') return 11;
+	else if (c=='c' || c=='C') return 12;
+	else if (c=='d' || c=='D') return 13;
+	else if (c=='e' || c=='E') return 14;
+	else if (c=='f' || c=='F') return 15;
+	else return 0; /* Unknown escape char */
+}
+
+/** Reads the value of the key and decodes all escaping
+  * codes into the buffer.
+  * @pre the buffer needs to be as large as value's size.
+  * @param cur the key holding the value to decode
+  * @param buf the buffer to write to
+  */
+void elektraHexcodeDecode (Key *cur, char* buf)
+{
+	size_t valsize = keyGetValueSize(cur);
+	const char *val = keyValue(cur);
+
+	size_t out=0;
+	for (size_t in=0; in<valsize-1; ++in)
+	{
+		char c = val[in];
+		char *n = buf+out;
+
+		if (c == '\\')
+		{
+			in+=2; /* Advance twice (2 hex numbers) */
+			char first = val[in-1];
+			char second = val[in];
+			int res;
+
+			res = elektraHexcodeConvFromHex(second);
+			res += elektraHexcodeConvFromHex(first)*16;
+			*n = res & 255;
+		} else {
+			*n = c;
+		}
+		++out; /* Only one char is written */
+	}
+
+	buf[out] = 0; // null termination for keyString()
+
+	keySetRaw(cur, buf, out+1);
+}
+
+
+int elektraHexcodeGet(Plugin *handle, KeySet *returned, Key *parentKey)
+{
+	/* get all keys */
+
+	if (!strcmp (keyName(parentKey), "system/elektra/modules/hexcode"))
+	{
+		KeySet *pluginConfig = ksNew (30,
+			keyNew ("system/elektra/modules/hexcode",
+				KEY_VALUE, "hexcode plugin waits for your orders", KEY_END),
+			keyNew ("system/elektra/modules/hexcode/exports", KEY_END),
+			keyNew ("system/elektra/modules/hexcode/exports/get",
+				KEY_FUNC, elektraHexcodeGet, KEY_END),
+			keyNew ("system/elektra/modules/hexcode/exports/set",
+				KEY_FUNC, elektraHexcodeSet, KEY_END),
+			keyNew ("system/elektra/modules/hexcode/infos",
+				KEY_VALUE, "All information you want to know", KEY_END),
+			keyNew ("system/elektra/modules/hexcode/infos/author",
+				KEY_VALUE, "Markus Raab <elektra@markus-raab.org>", KEY_END),
+			keyNew ("system/elektra/modules/hexcode/infos/licence",
+				KEY_VALUE, "BSD", KEY_END),
+			keyNew ("system/elektra/modules/hexcode/infos/description",
+				KEY_VALUE, "Decoding/Encoding engine which escapes unwanted characters.", KEY_END),
+			keyNew ("system/elektra/modules/hexcode/infos/provides",
+				KEY_VALUE, "code", KEY_END),
+			keyNew ("system/elektra/modules/hexcode/infos/placements",
+				KEY_VALUE, "postgetstorage presetstorage", KEY_END),
+			keyNew ("system/elektra/modules/hexcode/infos/needs",
+				KEY_VALUE, "", KEY_END),
+			keyNew ("system/elektra/modules/hexcode/infos/version",
+				KEY_VALUE, PLUGINVERSION, KEY_END),
+			KS_END);
+		ksAppend (returned, pluginConfig);
+		ksDel (pluginConfig);
+		return 1;
+	}
+
+	Key *cur;
+	char *buf = malloc (1000);
+	size_t bufalloc = 1000;
+
+	ksRewind(returned);
+	while ((cur = ksNext(returned)) != 0)
+	{
+		size_t valsize = keyGetValueSize(cur);
+		if (valsize > bufalloc)
+		{
+			bufalloc = valsize;
+			buf = realloc (buf, bufalloc);
+		}
+
+		elektraHexcodeDecode (cur, buf);
+	}
+
+	free (buf);
+
+	return 1; /* success */
+}
+
+
+/**
+  * Gives the integer number 0-15 to a corresponding
+  * hex character '0'-'9', 'a'-'f' or 'A'-'F'.
+  */
+static inline char elektraHexcodeConvToHex(int c)
+{
+	switch (c)
+	{
+		case 0: return '0';
+		case 1: return '1';
+		case 2: return '2';
+		case 3: return '3';
+		case 4: return '4';
+		case 5: return '5';
+		case 6: return '6';
+		case 7: return '7';
+		case 8: return '8';
+		case 9: return '9';
+		case 10: return 'A';
+		case 11: return 'B';
+		case 12: return 'C';
+		case 13: return 'D';
+		case 14: return 'E';
+		case 15: return 'F';
+		default: return '0';
+	}
+}
+
+
+/** Reads the value of the key and encodes it in
+  * c-style in the buffer.
+  *
+  * @param cur the key which value is to encode
+  * @param buf the buffer
+  * @pre the buffer needs to have thrice as much space as the value's size
+  */
+void elektraHexcodeEncode (Key *cur, char* buf)
+{
+	size_t valsize = keyGetValueSize(cur);
+	const char *val = keyValue(cur);
+
+	size_t out=0;
+	for (size_t in=0; in<valsize-1; ++in)
+	{
+		char c = val[in];
+
+		if    (c == '\0' || c=='\n' || c=='\\' || c==' '
+				 || c=='='  || c==';'  || c=='#')
+		{
+			buf[out] = '\\'; out ++;
+			buf[out] = elektraHexcodeConvToHex(c/16); out ++;
+			buf[out] = elektraHexcodeConvToHex(c%16); out ++;
+		}
+		else
+		{
+			// just copy one character
+			buf[out] = val[in];
+			// advance out cursor
+			out ++;
+		}
+	}
+
+	buf[out] = 0; // null termination for keyString()
+
+	keySetRaw(cur, buf, out+1);
+}
+
+
+int elektraHexcodeSet(Plugin *handle, KeySet *returned, Key *parentKey)
+{
+	/* set all keys */
+
+	Key *cur;
+	char *buf = malloc (1000);
+	size_t bufalloc = 1000;
+
+	ksRewind(returned);
+	while ((cur = ksNext(returned)) != 0)
+	{
+		size_t valsize = keyGetValueSize(cur);
+		if (valsize*3 > bufalloc)
+		{
+			bufalloc = valsize*3;
+			buf = realloc (buf, bufalloc);
+		}
+
+		elektraHexcodeEncode (cur, buf);
+	}
+
+	free (buf);
+
+	return 1; /* success */
+}
+
+Plugin *ELEKTRA_PLUGIN_EXPORT(hexcode)
+{
+	return elektraPluginExport("hexcode",
+		ELEKTRA_PLUGIN_GET,	&elektraHexcodeGet,
+		ELEKTRA_PLUGIN_SET,	&elektraHexcodeSet,
+		ELEKTRA_PLUGIN_END);
+}
+
---
+++ src/plugins/hexcode/testmod_hexcode.c
@@ -0,0 +1,101 @@
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <stdio.h>
+#ifdef HAVE_STDLIB_H
+#include <stdlib.h>
+#endif
+#ifdef HAVE_STRING_H
+#include <string.h>
+#endif
+
+#include "hexcode.h"
+
+#include <tests.h>
+
+const char *encoded_string = "a\\20value\\20with\\3D\\3B\\23\\20and\\20\\5C\\20itself\\0";
+const char *decoded_string = "a value with=;# and \\ itself";
+
+void test_encode()
+{
+	printf ("test encode\n");
+
+	char buf[1000];
+	Key *test = keyNew ("user/test",
+			KEY_VALUE, decoded_string,
+			KEY_END);
+	elektraHexcodeEncode (test, buf);
+	succeed_if (!memcmp(keyValue(test), encoded_string, sizeof(encoded_string)-1), "string not correctly encoded");
+
+	keyDel (test);
+}
+
+void test_decode()
+{
+	printf ("test decode\n");
+
+	char buf[1000];
+	Key *test = keyNew ("user/test",
+			KEY_SIZE, sizeof(encoded_string)-1,
+			KEY_VALUE, encoded_string,
+			KEY_END);
+	elektraHexcodeDecode (test, buf);
+	succeed_if (!strcmp(keyString(test), decoded_string), "string not correctly encoded");
+
+	printf ("%s\n", keyString(test));
+
+	keyDel (test);
+}
+
+void check_reversibility(const char* msg)
+{
+	char buf[1000];
+	Key *decode = keyNew ("user/test",
+			KEY_VALUE, msg,
+			KEY_END);
+
+	Key *encode = keyDup (decode);
+	elektraHexcodeEncode (encode, buf);
+
+	elektraHexcodeDecode (encode, buf);
+	succeed_if (compare_key(encode, decode) == 0, "was not reversible");
+
+	keyDel (decode);
+	keyDel (encode);
+}
+
+void test_reversibility()
+{
+	printf ("test reversibility\n");
+
+	check_reversibility("hello world");
+	check_reversibility("hello world!\nnew line");
+	check_reversibility("\0");
+	check_reversibility("\n");
+	check_reversibility("\\");
+	check_reversibility(" ");
+	check_reversibility("=");
+	check_reversibility(";");
+	check_reversibility("#");
+	check_reversibility(" =;#");
+	check_reversibility("\n\\");
+}
+
+
+int main(int argc, char** argv)
+{
+	printf("   ICONV   TESTS\n");
+	printf("====================\n\n");
+
+	init (argc, argv);
+
+	test_encode();
+	test_decode();
+	test_reversibility();
+
+	printf("\ntest_backendhelpers RESULTS: %d test(s) done. %d error(s).\n", nbTest, nbError);
+
+	return nbError;
+}
+
