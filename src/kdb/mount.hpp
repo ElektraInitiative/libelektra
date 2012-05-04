752b767c61c166482c7c77dc75d4fa512c91a760
Markus Raab <elektra@markus-raab.org>
2012-05-04 14:31:37 +0200

    0.8.0
    commit changelog
    more in help in mount

--- src/kdb/mount.hpp
+++ src/kdb/mount.hpp
@@ -64,6 +64,9 @@ public:
 			"[path mountpoint] [plugin [..]]\n"
 			"Allows you to mount\n"
 			"a new backend.\n";
+			"path .. a filename (absolute for system, relative for cascading or user)\n"
+			"mountpoint .. where to mount the backend, start with / for cascading mp\n"
+			"plugin .. a list of plugins to mount at that place\n"
 	}
 
 	virtual int execute (Cmdline const& cmdline);
