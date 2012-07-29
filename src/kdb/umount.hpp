61b857f78a0fa094a1d311b200f279906ae59c68
Markus Raab <elektra@markus-raab.org>
2012-07-29 21:23:42 +0200

    Implemented umount command

---
+++ src/kdb/umount.hpp
@@ -0,0 +1,39 @@
+#ifndef UMOUNT_HPP
+#define UMOUNT_HPP
+
+#include <command.hpp>
+
+#include <kdb.hpp>
+
+class UmountCommand : public Command
+{
+	kdb::KDB kdb;
+
+public:
+	UmountCommand();
+	~UmountCommand();
+
+	virtual std::string getShortOptions()
+	{
+		return "";
+	}
+
+	virtual std::string getSynopsis()
+	{
+		return "<name>";
+	}
+
+	virtual std::string getShortHelpText()
+	{
+		return "Unmount backend from key database.";
+	}
+
+	virtual std::string getLongHelpText()
+	{
+		return "";
+	}
+
+	virtual int execute (Cmdline const& cmdline);
+};
+
+#endif
