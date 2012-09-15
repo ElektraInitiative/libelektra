1db1d9784d2250d1b02f2b45edab3bf5a51f25af
Markus Raab <elektra@markus-raab.org>
2012-09-15 14:34:56 +0200

    fix clang, gcc compiler flags
    remove extra COMPILERS document, but describe it directly in COMPILE

--- cmake/ElektraCompiling.cmake
+++ cmake/ElektraCompiling.cmake
@@ -3,26 +3,34 @@
 # to OS detection
 ##
 
+set (C_MODE "-std=gnu99 -pedantic")
+
 if (COMPILE_CXX11_MODE)
-	set (CXX_MODE "-std=c++0x")
+	set (CXX_MODE "-std=c++0x -pedantic")
 else()
-	set (CXX_MODE "-std=c++98")
+	set (CXX_MODE "-std=c++98 -pedantic")
 endif()
 
+set (EXTRA_FLAGS "${EXTRA_FLAGS} -Wall -Wextra")
 
 if (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
-	message (STATUS "Detected Clang compiler")
-	set (CXX_EXTRA "${CXX_EXTRA} -Wno-error=non-pod-varargs")
+	set (EXTRA_FLAGS "${EXTRA_FLAGS} -Wno-error=non-pod-varargs")
+	message (STATUS "Clang detected")
 endif()
 
-
-# Set default compile flags for GCC
 if (CMAKE_COMPILER_IS_GNUCXX)
-	set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${CXX_MODE} ${CXX_EXTRA} -Wshadow -Wall -Wextra -Wno-missing-field-initializers")
-	set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${CXX_EXTRA} -pedantic -std=gnu99 -Wall -Wsign-compare -Wfloat-equal -Wformat-security")
-	message (STATUS "GCC detected, compile flags: ${CMAKE_C_FLAGS}")
+	message (STATUS "GCC detected")
 endif (CMAKE_COMPILER_IS_GNUCXX)
+
+if (CMAKE_CXX_COMPILER_ID MATCHES "Clang" OR CMAKE_COMPILER_IS_GNUCXX)
+	set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${C_MODE} ${EXTRA_FLAGS} -Wsign-compare -Wfloat-equal -Wformat-security")
+	set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${CXX_MODE} ${EXTRA_FLAGS} -Wshadow -Wno-missing-field-initializers")
+
+	message (STATUS "C flags are ${CMAKE_C_FLAGS})
+	message (STATUS "CXX flags are ${CMAKE_CXX_FLAGS})
+endif (CMAKE_CXX_COMPILER_ID MATCHES "Clang" OR CMAKE_COMPILER_IS_GNUCXX)
 
 if (WIN32)
 	set (HAVE_WIN32 "1")
+	message (STATUS "Win32 detected")
 endif (WIN32)
