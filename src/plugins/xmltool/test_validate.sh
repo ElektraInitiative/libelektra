e9ed86c3da4b7919ac091efe7c09f51547e91ca0
Markus Raab <elektra@markus-raab.org>
2013-12-08 18:58:40 +0100

    fix validation with new path

--- src/plugins/xmltool/test_validate.sh
+++ src/plugins/xmltool/test_validate.sh
@@ -2,23 +2,34 @@
 
 VALIDATOR=/usr/bin/xmllint
 
-[ "z$srcdir" = 'z' ] && srcdir=.
+[ "z$srcdir" = 'z' ] && srcdir=$(dirname $0)
 
-SCHEMA=$srcdir/../xmlschema/elektra.xsd
+SCHEMA=$srcdir/xmlschema/elektra.xsd
 
-[ -x "$VALIDATOR" ] || exit 0
+if [ ! -f $SCHEMA ]
+then
+	echo "Schema $SCHEMA not found, will do nothing"
+	exit 0
+fi
+
+
+if [ ! -x "$VALIDATOR" ]
+then
+	echo "Tool $VALIDATOR not found, will do nothing"
+	exit 0
+fi
 
 echo
 echo ELEKTRA VALIDATE TESTS
 echo ========================================
 echo
 
-$VALIDATOR --schema "$SCHEMA" $srcdir/key.xml > /dev/null || exit 5
-$VALIDATOR --schema "$SCHEMA" $srcdir/keyset.xml > /dev/null || exit 5
+$VALIDATOR --schema "$SCHEMA" $srcdir/tests/key.xml > /dev/null || exit 5
+$VALIDATOR --schema "$SCHEMA" $srcdir/tests/keyset.xml > /dev/null || exit 5
 
-$VALIDATOR --schema "$SCHEMA" $srcdir/filesys.xml > /dev/null || exit 5
-$VALIDATOR --schema "$SCHEMA" $srcdir/fstab.xml > /dev/null || exit 5
-$VALIDATOR --schema "$SCHEMA" $srcdir/passwd.xml > /dev/null || exit 5
+$VALIDATOR --schema "$SCHEMA" $srcdir/tests/filesys.xml > /dev/null || exit 5
+$VALIDATOR --schema "$SCHEMA" $srcdir/tests/fstab.xml > /dev/null || exit 5
+$VALIDATOR --schema "$SCHEMA" $srcdir/tests/passwd.xml > /dev/null || exit 5
 
 exit 0
 
