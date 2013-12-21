6e706d0799bb44553bd2001aba8b4f169ff7fd54
Markus Raab <elektra@markus-raab.org>
2013-12-21 13:23:51 +0100

    do not hide errors in shell test suite

--- tests/shell/check_get_set.sh
+++ tests/shell/check_get_set.sh
@@ -67,7 +67,7 @@ do
 	for ROOT in $USER_ROOT $SYSTEM_ROOT
 	do
 		echo "do preparation for $PLUGIN in $ROOT"
-		$KDB set $ROOT "root" 1>/dev/null 2>/dev/null
+		$KDB set $ROOT "root" 1>/dev/null
 		succeed_if "could not set root"
 
 		[ "x`$KDB sget $ROOT/value defvalue 2> /dev/null`" = "xdefvalue" ]
@@ -83,7 +83,7 @@ do
 			succeed_if "could not shell get root"
 		fi
 
-		$KDB set "$ROOT/value" "$VALUE" 1>/dev/null 2>/dev/null
+		$KDB set "$ROOT/value" "$VALUE" 1>/dev/null
 		succeed_if "could not set value"
 
 		[ "x`$KDB get $ROOT/value 2> /dev/null`" = "x$VALUE" ]
@@ -99,20 +99,20 @@ do
 
 		echo "testing rm command"
 
-		$KDB rm $ROOT/value 1> /dev/null 2> /dev/null
+		$KDB rm $ROOT/value 1> /dev/null
 		succeed_if "could not remove user/test/value"
 
-		$KDB get $ROOT/value 1> /dev/null  2>/dev/null
+		$KDB get $ROOT/value 1> /dev/null
 		[ $? != "0" ]
 		succeed_if "got removed key $ROOT/value"
 
-		$KDB rm $ROOT 1>/dev/null 2>/dev/null
+		$KDB rm $ROOT 1>/dev/null
 		succeed_if "could not remove user/test/value"
 
 		[ "x`$KDB sget $ROOT/value value 2> /dev/null`" = "xvalue" ]
 		succeed_if "Did not get default value after remove"
 
-		$KDB get $ROOT/value 1>/dev/null  2> /dev/null
+		$KDB get $ROOT/value 1>/dev/null
 		[ $? != "0" ]
 		succeed_if "got removed key $ROOT"
 
