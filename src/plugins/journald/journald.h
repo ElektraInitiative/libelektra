489941d191b8219c4395e4b4985f1da13b7c528b
Felix Berlakovich <elektra@berlakovich.net>
2014-03-19 19:32:54 +0100

    added a plugin which uses the systemd-journal native interface instead of syslog

---
+++ src/plugins/journald/journald.h
@@ -0,0 +1,37 @@
+/***************************************************************************
+                     journald.c  -  Skeleton of a plugin
+                             -------------------
+    begin                : Fri May 21 2010
+    copyright            : (C) 2010 by Markus Raab
+    email                : elektra@markus-raab.org
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the BSD License (revised).                      *
+ *                                                                         *
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This is the skeleton of the methods you'll have to implement in order *
+ *   to provide a valid plugin.                                            *
+ *   Simple fill the empty functions with your code and you are            *
+ *   ready to go.                                                          *
+ *                                                                         *
+ ***************************************************************************/
+
+
+#ifndef ELEKTRA_PLUGIN_JOURNALD_H
+#define ELEKTRA_PLUGIN_JOURNALD_H
+
+#include <kdbplugin.h>
+
+int elektraJournaldGet(Plugin *handle, KeySet *ks, Key *parentKey);
+int elektraJournaldSet(Plugin *handle, KeySet *ks, Key *parentKey);
+int elektraJournaldError(Plugin *handle, KeySet *ks, Key *parentKey);
+
+Plugin *ELEKTRA_PLUGIN_EXPORT(journald);
+
+#endif
