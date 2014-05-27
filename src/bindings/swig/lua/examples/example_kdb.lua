c22daebafb1f19403569b1bb8a213420ea0e9b3e
manuel <manuel@mausz.at>
2014-05-27 17:11:04 +0200

    swig: example applications for python+lua bindings

---
+++ src/bindings/swig/lua/examples/example_kdb.lua
@@ -0,0 +1,24 @@
+require("kdb")
+
+do
+	-- open database
+	local db = kdb.KDB()
+	ks = kdb.KeySet(100)
+	-- get keys
+	db:get(ks, "user/MyApp")
+
+	-- check if key exists
+	key = ks["user/MyApp/mykey"]
+	if not key then
+		-- create a new key + append to keyset
+		key = kdb.Key("user/MyApp/mykey")
+		ks:append(key)
+	end
+	-- change keys value
+	key.string = "new_value"
+
+	-- store new keyset
+	db:set(ks, "user/MyApp")
+
+	-- database closes at end of context is implicit
+end
