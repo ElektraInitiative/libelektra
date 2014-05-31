9a7d7570fd67c9ace977287766384e725ac5107a
Felix Berlakovich <elektra@berlakovich.net>
2014-06-01 01:04:09 +0200

    Added some tests for functions autogenerated by swig

--- src/bindings/swig/python/tests/test_keyset.py
+++ src/bindings/swig/python/tests/test_keyset.py
@@ -55,6 +55,47 @@ class KeySet(unittest.TestCase):
 		ks.append(kdb.Key("user/bar"))
 		self.assertEqual(len(ks), 2)
 
+		' test clear '
+		ks = kdb.KeySet(0)
+		ks.append(kdb.Key("user/test1"))
+		ks.append(kdb.Key("user/test2"))
+		ks.append(kdb.Key("user/test3"))
+		ks.clear()
+		self.assertEqual(len(ks), 0);
+
+		' test pop '
+		ks = kdb.KeySet(0)
+		ks.append(kdb.Key("user/test1"))
+		ks.append(kdb.Key("user/test2"))
+		ks.append(kdb.Key("user/test3"))
+		self.assertEqual(ks.pop(), kdb.Key("user/test3"))
+		self.assertEqual(len(ks), 2)
+		
+		' test cut '
+		ks = kdb.KeySet(0)
+		ks.append(kdb.Key("user/level11"))
+		ks.append(kdb.Key("user/level13"))
+		ks.append(kdb.Key("user/level13/level21"))
+		ks.append(kdb.Key("user/level13/level22/level31"))
+		ks.append(kdb.Key("user/level13/level23"))
+		ks.append(kdb.Key("user/level15"))
+		
+		cutresult = ks.cut(kdb.Key("user/level13"))
+		
+		self.assertEqual(len(cutresult), 4)
+		self.assertTrue(kdb.Key("user/level13") in cutresult)
+		self.assertTrue(kdb.Key("user/level13/level21") in cutresult)
+		self.assertTrue(kdb.Key("user/level13/level22/level31") in cutresult)
+		self.assertTrue(kdb.Key("user/level13/level23") in cutresult)
+	
+		self.assertEqual(len(ks), 2)
+		self.assertTrue(kdb.Key("user/level11") in ks)
+		self.assertTrue(kdb.Key("user/level15") in ks)
+			
+		cutresult = ks.cut(kdb.Key("user/does/not/exist"))
+		self.assertEqual(len(cutresult), 0)
+		self.assertEqual(len(ks), 2)
+
 	def test_iterator(self):
 		self.assertEqual(sum(1 for _ in self.ks),           4)
 		self.assertEqual(sum(1 for _ in reversed(self.ks)), 4)
