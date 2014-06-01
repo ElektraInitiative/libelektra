19381617555b06fea31250e9b221b9befb2fc1db
Markus Raab <elektra@markus-raab.org>
2014-06-01 09:24:47 +0200

    add example for backend

---
+++ src/libtools/examples/backend.cpp
@@ -0,0 +1,24 @@
+#include <backend.hpp>
+#include <backends.hpp>
+
+#include <iostream>
+
+int main()
+{
+	using namespace kdb;
+	Backend b("my_backend", "/");
+	b.addPlugin("resolver");
+	b.addPlugin("dump");
+
+	Key rootKey(Backends::mountpointsPath, KEY_END);
+	KeySet mountConfig;
+	b.serialize(rootKey, mountConfig);
+
+	mountConfig.rewind();
+	while(mountConfig.next())
+	{
+		std::cout << mountConfig.current().getName()
+			  << " = " << mountConfig.current().getString()
+			  << std::endl;
+	}
+}
