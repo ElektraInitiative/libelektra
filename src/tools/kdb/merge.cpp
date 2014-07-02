cff167e224782af9016b477c9267d6dceae764d8
Felix Berlakovich <elektra@berlakovich.net>
2014-07-02 22:56:28 +0200

    Registered the merge command, conflicts are now reported.

--- src/tools/kdb/merge.cpp
+++ src/tools/kdb/merge.cpp
@@ -8,48 +8,45 @@
 #include <iostream>
 #include <string>
 
-
 using namespace kdb;
 using namespace kdb::tools::merging;
 using namespace std;
 
-
 MergeCommand::MergeCommand()
-{}
+{
+}
 
-int MergeCommand::execute (Cmdline const& cl)
+int MergeCommand::execute(Cmdline const& cl)
 {
 	int ret = 0;
 
-	if (cl.arguments.size() != 4)
+	if (cl.arguments.size () != 4)
 	{
-		throw invalid_argument("wrong number of arguments, 4 needed");
+		throw invalid_argument ("wrong number of arguments, 4 needed");
 	}
 
-	Key root1 (cl.arguments[0], KEY_END);
-	if (!root1.isValid())
+	Key mergeRoot (cl.arguments[0], KEY_END);
+	if (!mergeRoot.isValid ())
 	{
-		throw invalid_argument(cl.arguments[0] +
-				" is not a valid keyname");
+		throw invalid_argument (cl.arguments[0] + " is not a valid keyname");
 	}
 
-	Key root2 (cl.arguments[1], KEY_END);
-	if (!root2.isValid())
+	Key ourRoot (cl.arguments[1], KEY_END);
+	if (!ourRoot.isValid ())
 	{
-		throw invalid_argument(cl.arguments[1] +
-				" is not a valid keyname");
+		throw invalid_argument (cl.arguments[1] + " is not a valid keyname");
 	}
-	Key root3 (cl.arguments[2], KEY_END);
-	if (!root3.isValid())
+
+	Key theirRoot (cl.arguments[2], KEY_END);
+	if (!theirRoot.isValid ())
 	{
-		throw invalid_argument(cl.arguments[2] +
-				" is not a valid keyname");
+		throw invalid_argument (cl.arguments[2] + " is not a valid keyname");
 	}
-	Key root4 (cl.arguments[3], KEY_END);
-	if (!root4.isValid())
+
+	Key baseRoot (cl.arguments[3], KEY_END);
+	if (!baseRoot.isValid ())
 	{
-		throw invalid_argument(cl.arguments[3] +
-				" is not a valid keyname");
+		throw invalid_argument (cl.arguments[3] + " is not a valid keyname");
 	}
 
 	KeySet original;
@@ -57,27 +54,47 @@ int MergeCommand::execute (Cmdline const& cl)
 	KeySet ours;
 	KeySet theirs;
 
-	kdb.get(original, root4);
-	base = original.cut(root1);
-	ours = original.cut(root2);
-	theirs = original.cut(root3);
-	original.append(base);
-	original.append(ours);
-	original.append(theirs);
-	MergeResult result = ThreeWayMerge::mergeKeySet(MergeTask(BaseMergeKeys(base, root1), OurMergeKeys(ours, root2), TheirMergeKeys(theirs, root3), root4));
-	
+	kdb.get (original, baseRoot);
+	base = original.cut (mergeRoot);
+	ours = original.cut (ourRoot);
+	theirs = original.cut (theirRoot);
+	original.append (base);
+	original.append (ours);
+	original.append (theirs);
+	MergeResult result = ThreeWayMerge::mergeKeySet (
+			MergeTask (
+					BaseMergeKeys (base, mergeRoot),
+					OurMergeKeys (ours, ourRoot),
+					TheirMergeKeys (theirs, theirRoot), baseRoot));
+
 	KeySet empty;
-	if( result.hasConflicts()){
+	if (result.hasConflicts ())
+	{
+		cerr << "Conflicts where detected that could not be resolved automatically:" << endl;
+
+		KeySet conflicts = result.getConflictSet();
+		conflicts.rewind();
+		Key current;
+		while ((current = conflicts.next()))
+		{
+			string ourConflict = current.getMeta<string> ("conflict/operation/our");
+			string theirConflict = current.getMeta<string> ("conflict/operation/their");
+
+			cerr << current << endl;
+			cerr << "ours: " << ourConflict << ", theirs: " << theirConflict << endl;
+			cerr << endl;
+		}
+
 		cerr << "Merge unsuccessful." << endl;
 		ret = -1;
 	}
 
-	original.append(result.getMergedKeys());
-	kdb.set(original, root4);
+	original.append (result.getMergedKeys ());
+	kdb.set (original, baseRoot);
 
 	return ret;
 }
 
-
 MergeCommand::~MergeCommand()
-{}
+{
+}
---
+++ src/tools/kdb/merge.hpp
@@ -0,0 +1,40 @@
+#ifndef MERGE_HPP
+#define MERGE_HPP
+
+#include <command.hpp>
+#include <kdb.hpp>
+#include <merging/threewaymerge.hpp>
+
+class MergeCommand : public Command
+{
+	kdb::KDB kdb;
+
+public:
+	MergeCommand();
+	~MergeCommand();
+
+	virtual int execute (Cmdline const& cmdline);
+
+	virtual std::string getShortOptions()
+	{
+		return "Htsv";
+	}
+
+	virtual std::string getSynopsis()
+	{
+		return "[options] mergepath ourpath theirpath basepath";
+	}
+
+	virtual std::string getShortHelpText()
+	{
+		return "Three-way merge of KeySets.";
+	}
+
+	virtual std::string getLongHelpText()
+	{
+		return
+			"Completes a three-way merge between keysets and saves the resulting keyset to mergepath\n\nmergepath .. path where the merged keyset should be saved\nourpath .. path to the keyset to serve as ours\ntheirpath .. path to the keyset to serve as theirs\nbasepath .. path to the base keyset\n\n-H --help                print help text\n-I --interactive         interactive mode to manually merge\n-V --version             print version info\n";
+	}
+};
+
+#endif
