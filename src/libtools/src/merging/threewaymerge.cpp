4cfbae6448c1cff1add8c985cc46db65e4f75214
Felix Berlakovich <elektra@berlakovich.net>
2014-08-14 20:07:53 +0200

    Changed the threewaymerge to also merge the parentkey

--- src/libtools/src/merging/threewaymerge.cpp
+++ src/libtools/src/merging/threewaymerge.cpp
@@ -43,9 +43,6 @@ void ThreeWayMerge::detectConflicts(const MergeTask& task, MergeResult& mergeRes
 
 	while ((our = task.ours.next ()))
 	{
-		if (our.getName() == task.ourParent.getName())
-			continue;
-
 		string theirLookup = rebasePath (our, task.ourParent, task.theirParent);
 		Key theirLookupResult = task.theirs.lookup (theirLookup);
 
