e3833d2454d2da939220abc2b6d2b1dfe0ffc5f2
Markus Raab <elektra@markus-raab.org>
2014-08-21 14:35:04 +0200

    add removed parts of abi tests

---
+++ tests/ctest/test_meta.c
@@ -0,0 +1,37 @@
+#include <tests_internal.h>
+
+void test_ro()
+{
+	Key *key;
+
+	key = keyNew(KEY_END);
+	key->flags |= KEY_FLAG_RO;
+
+	succeed_if (keySetString(key, "a") == -1, "read only string, not allowed to set");
+	succeed_if (keySetBinary(key, "a", 2) == -1, "read only string, not allowed to set");
+	succeed_if (keySetName(key, "user") == -1, "read only string, not allowed to set");
+	succeed_if (keySetMeta(key, "meta", "value") == -1, "read only string, not allowed to set");
+
+	keyDel (key);
+
+	key = keyNew(KEY_END);
+	succeed_if (keySetMeta(key, "meta", "value") == sizeof("value"), "could not set meta");
+
+	// TODO check if RO
+	keyDel (key);
+}
+
+int main(int argc, char** argv)
+{
+	printf("KEY META     TESTS\n");
+	printf("==================\n\n");
+
+	init (argc, argv);
+	test_ro();
+
+
+	printf("\ntest_meta RESULTS: %d test(s) done. %d error(s).\n", nbTest, nbError);
+
+	return nbError;
+}
+
