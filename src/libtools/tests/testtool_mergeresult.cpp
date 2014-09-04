ab583c7852f81e311845a7e75c69e80bad6b450d
Felix Berlakovich <elektra@berlakovich.net>
2014-09-04 14:18:01 +0200

    Small cleanups and interface improvements

--- src/libtools/tests/testtool_mergeresult.cpp
+++ src/libtools/tests/testtool_mergeresult.cpp
@@ -57,3 +57,63 @@ TEST(MergeResult, ResolveConflictRemovesKeyFromConflicts)
 
 	EXPECT_EQ(0, result.getConflictSet().size ());
 }
+
+TEST(MergeResult, HasConflictsWorks)
+{
+	Key conflictKey = Key ("user/test/config/key1", KEY_END);
+	KeySet conflicts;
+	conflicts.append (conflictKey);
+	KeySet merged;
+	MergeResult result (conflicts, merged);
+	EXPECT_TRUE (result.hasConflicts());
+	conflicts = KeySet ();
+	result = MergeResult (merged, conflicts);
+	EXPECT_FALSE (result.hasConflicts());
+}
+
+TEST(MergeResult, IsConflictWorks)
+{
+	Key conflictKey = Key ("user/test/config/key1", KEY_END);
+	KeySet conflicts;
+	conflicts.append (conflictKey);
+	KeySet merged;
+	MergeResult result (conflicts, merged);
+	EXPECT_TRUE (result.isConflict(conflictKey));
+	EXPECT_FALSE (result.isConflict(Key ("user/test/config/key2", KEY_END)));
+}
+
+TEST(MergeResult, CountsResolvedKeysCorrectly)
+{
+	Key conflictKey1 = Key ("user/test/config/key1", KEY_END);
+	Key conflictKey2 = Key ("user/test/config/key2", KEY_END);
+	KeySet conflicts;
+	conflicts.append (conflictKey1);
+	conflicts.append (conflictKey2);
+	KeySet merged;
+	MergeResult result (conflicts, merged);
+	EXPECT_EQ (0, result.getNumberOfResolvedKeys());
+	result.resolveConflict (conflictKey1);
+	result.resolveConflict (conflictKey2);
+	EXPECT_EQ (2, result.getNumberOfResolvedKeys());
+}
+
+TEST(MergeResult, CountsEqualKeysCorrectly)
+{
+	Key mergedKey1 = Key ("user/test/config/key1", KEY_END);
+	Key mergedKey2 = Key ("user/test/config/key2", KEY_END);
+	Key mergedKey3 = Key ("user/test/config/key3", KEY_END);
+	Key conflictKey1 = Key ("user/test/config/key4", KEY_END);
+	KeySet conflicts;
+	conflicts.append(conflictKey1);
+	KeySet merged;
+	merged.append(mergedKey1);
+	merged.append(mergedKey2);
+	MergeResult result (conflicts, merged);
+	EXPECT_EQ (2, result.getNumberOfEqualKeys()) << "Initially merged keys not counted";
+	result.resolveConflict (conflictKey1);
+	result.addMergeKey(conflictKey1);
+	EXPECT_EQ (2, result.getNumberOfEqualKeys()) << "Resolved key is counted as equal key";
+	result.addMergeKey(mergedKey3);
+	EXPECT_EQ (3, result.getNumberOfEqualKeys()) << "Merged key is not counted as equal key";
+}
+
