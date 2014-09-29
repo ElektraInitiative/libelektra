c93cf48546c0954ecadda2e67cabc4dd3973f3a2
Markus Raab <elektra@markus-raab.org>
2014-09-29 21:28:58 +0200

    add some more tests

--- src/bindings/cpp/tests/test_contextual.cpp
+++ src/bindings/cpp/tests/test_contextual.cpp
@@ -32,6 +32,9 @@ TEST(test_contextual, staticGetPolicy)
 	cv = 40;
 	EXPECT_EQ(cv, 40);
 
+	ContextualValue<int, GetPolicyIs<MyStaticGetPolicy>> cv2(cv);
+	EXPECT_EQ(cv, cv2);
+
 }
 
 class MyDynamicGetPolicy
@@ -63,6 +66,9 @@ TEST(test_contextual, dynamicGetPolicy)
 	EXPECT_EQ(cv, cv);
 	cv = 40;
 	EXPECT_EQ(cv, 40);
+
+	ContextualValue<int, GetPolicyIs<MyDynamicGetPolicy>> cv2(cv);
+	EXPECT_EQ(cv, cv2);
 
 }
 
@@ -99,5 +105,74 @@ TEST(test_contextual, root)
 	EXPECT_EQ(cv, 40);
 	c.activate<RootLayer>();
 	EXPECT_EQ(cv, 40);
+
+	ContextualValue<int, GetPolicyIs<MyDynamicGetPolicy>> cv2(cv);
+	EXPECT_EQ(cv, cv2);
 }
 
+class MyCV : public kdb::ContextualValue<int, kdb::GetPolicyIs<MyStaticGetPolicy>>
+{
+public:
+	MyCV(kdb::KeySet & ks_, kdb::Context & context_)
+		: ContextualValue<int, kdb::GetPolicyIs<MyStaticGetPolicy>>(ks_,
+			context_,
+			kdb::Key(
+				"/",
+				ckdb::KDB_O_CASCADING_NAME,
+				KEY_END))
+	{}
+};
+
+TEST(test_contextual, myCVRoot)
+{
+	using namespace kdb;
+	KeySet ks;
+	Context c;
+
+	MyCV m(ks, c);
+
+	EXPECT_EQ(m, 23);
+}
+
+class none_t
+{};
+
+namespace kdb {
+
+template <>
+inline void Key::set(none_t)
+{}
+
+template <>
+inline none_t Key::get() const
+{
+	none_t ret;
+	return ret;
+}
+
+}
+
+class MyCV2 : public kdb::ContextualValue<none_t, kdb::GetPolicyIs<MyStaticGetPolicy>>
+{
+public:
+	MyCV2(kdb::KeySet & ks_, kdb::Context & context_)
+		: ContextualValue<none_t, kdb::GetPolicyIs<MyStaticGetPolicy>>(ks_,
+			context_,
+			kdb::Key(
+				"/",
+				ckdb::KDB_O_CASCADING_NAME,
+				KEY_END)),
+		m_m(ks_, context_)
+	{}
+
+	MyCV m_m;
+};
+
+TEST(test_contextual, myCV2Root)
+{
+	using namespace kdb;
+	KeySet ks;
+	Context c;
+
+	MyCV2 m(ks, c);
+}
