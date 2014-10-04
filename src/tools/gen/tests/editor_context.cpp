a50f9ff55b436c6e947a2de0d1900dd719c68cd7
Markus Raab <elektra@markus-raab.org>
2014-10-04 08:50:59 +0200

    minimal editor example

---
+++ src/tools/gen/tests/editor_context.cpp
@@ -0,0 +1,29 @@
+#include "editor_context.hpp"
+#include <kdb.hpp>
+
+#include <iostream>
+
+
+int main()
+{
+	using namespace kdb;
+
+	KDB kdb;
+	KeySet ks;
+	Context c;
+	// some predefined values (for convenience):
+	ks.append(Key("user/kde/quit",
+				   KEY_VALUE,
+				   "Ctrl+k",
+				   KEY_END));
+	KeySet ks2;
+	kdb.get(ks2, "/myapp");
+	// overwrite them if something is available in config files:
+	ks.append(ks2);
+
+	Parameters par(ks,c);
+
+	std::cout << par.myapp.shortcut.quitMyapp << std::endl;
+
+	return 0;
+}
