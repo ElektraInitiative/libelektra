b8c0bbf59e0ebe55911a74404a95c6754f62b55b
Markus Raab <elektra@markus-raab.org>
2014-10-08 15:07:21 +0200

    avoid code duplication for dynamic context

--- src/tools/gen/template/context_static.hpp
+++ src/tools/gen/template/context_static.hpp
@@ -41,12 +41,9 @@ inline kdb::none_t Key::get() const
 
 
 
-
-
-@def outputClasses(support, hierarchy)
-
+@def outputPolicies(support, hierarchy)
 @for $child in hierarchy.children
-$outputClasses(support, child)
+$outputPolicies(support, child)
 @end for
 
 
@@ -71,90 +68,10 @@ static type get(kdb::KeySet &ks, kdb::Key const&)
 }
 };
 
-/** \brief class of $hierarchy.name
- *
- * Full Name (with contextual placeholders):
- * $hierarchy.info.get('name')
- * Dirname: $hierarchy.dirname
- * Basename: $hierarchy.basename
- * */
-class $hierarchy.prettyclassname(support) : public ContextualValue
-	<$support.typeof($hierarchy.info),
-	GetPolicyIs<${hierarchy.prettyclassname(support)}GetPolicy>>
-{
-public:
-
-
-	/** \brief Constructor for $hierarchy.prettyclassname(support)
-	 * \param ks keyset to work with
-	 */
-	${hierarchy.prettyclassname(support)}(kdb::KeySet & ks, kdb::Context & context)
-		: ContextualValue<$support.typeof($hierarchy.info),
-		  GetPolicyIs<${hierarchy.prettyclassname(support)}GetPolicy>>(ks,
-			context,
-			kdb::Key(
-@if $hierarchy.info.get('name'):
-				"$hierarchy.info.get('name')",
-@else
-				"/",
-@end if
-				ckdb::KDB_O_CASCADING_NAME,
-@if $hierarchy.info.get('default'):
-				KEY_META, "default", $support.quote($hierarchy.info.get('default')),
-@end if
-@if $hierarchy.info.get('unit'):
-				KEY_META, "unit", $support.quote($hierarchy.info.get('unit')),
-@end if
-@if $hierarchy.info.get('explanation'):
-				KEY_META, "explanation", $support.quote($hierarchy.info.get('explanation')),
-@end if
-@if $hierarchy.info.get('rationale'):
-				KEY_META, "rationale", $support.quote($hierarchy.info.get('rationale')),
-@end if
-@set $fa = $support.fallback(hierarchy.info)
-@for $f in $fa
-				KEY_META, "fallback/#$fa.index($f)", "$f",
-@end for
-@set $ov = $support.override(hierarchy.info)
-@for $o in $ov
-				KEY_META, "override/#$ov.index($o)", "$o",
-@end for
-				KEY_END))
-
-
-@for k in hierarchy.children
-@set nestedname = $support.nestedpretty(k.basename)
-		, ${nestedname}(ks, context)
-@end for
-	{}
-
-	using ContextualValue<$support.typeof($hierarchy.info), GetPolicyIs<${hierarchy.prettyclassname(support)}GetPolicy>>::operator =;
-
-@for k in hierarchy.children
-@set nsname = $support.nspretty(k.dirname)
-@set nestedname = $support.nestedpretty(k.basename)
-@set nestedclassname = $support.classpretty(k.basename)
-	/** \return nested subclass */
-	$nsname$nestedclassname ${nestedname};
-@end for
-};
-
-@if $support.typeof($hierarchy.info)=='std::string':
-inline std::ostream & operator<<(std::ostream & os,
-		$hierarchy.prettyclassname(support) const & c)
-{
-	os << static_cast<std::string>(c);
-	return os;
-}
-@end if
-
 @for n in hierarchy.name.split('/')[1:-1]
 }
 @end for
-
 @end def
-
-
 
 
 
@@ -174,7 +91,8 @@ $hierarchy
 
 
 $cpp_util.generateForwardDecl($support, $hierarchy)
-$outputClasses(support, hierarchy)
+$outputPolicies(support, hierarchy)
+$cpp_util.outputClasses($support, $hierarchy)
 
 } // namespace kdb
 $util.footer($args.output)
