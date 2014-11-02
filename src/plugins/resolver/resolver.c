7922559fd9d1e1a63cb122a1512cc2f703fd0305
Markus Raab <elektra@markus-raab.org>
2014-11-02 08:27:06 +0100

    make file locking configureable
    remove lock helper library

--- src/plugins/resolver/resolver.c
+++ src/plugins/resolver/resolver.c
@@ -85,6 +85,106 @@ static void resolverClose (resolverHandle *p)
 }
 
 /**
+ * Locks file for exclusive read/write mode.
+ *
+ * This function will not block until all reader
+ * and writer have left the file.
+ * -> conflict with other cooperative process detected,
+ *    but we were later (and lost)
+ *
+ * @exception 27 set if locking failed, most likely a conflict
+ *
+ * @param fd is a valid filedescriptor
+ * @return 0 on success
+ * @return -1 on failure
+ * @ingroup backendhelper
+ */
+static int elektraLockFile (int fd ELEKTRA_UNUSED,
+		Key *parentKey ELEKTRA_UNUSED)
+{
+#ifdef ELEKTRA_LOCK_FILE
+	struct flock l;
+	l.l_type = F_WRLCK; /*Do exclusive Lock*/
+	l.l_start= 0;	/*Start at begin*/
+	l.l_whence = SEEK_SET;
+	l.l_len = 0;	/*Do it with whole file*/
+	int ret = fcntl (fd, F_SETLK, &l);
+
+	if (ret == -1)
+	{
+		if (errno == EAGAIN || errno == EACCES)
+		{
+			ELEKTRA_SET_ERROR (30, parentKey, "conflict because other process writes to configuration indicated by lock");
+		}
+		else
+		{
+			char buffer[ERROR_SIZE];
+			strerror_r(errno, buffer, ERROR_SIZE);
+			ELEKTRA_ADD_WARNING(27, parentKey, buffer);
+
+			ELEKTRA_SET_ERROR (30, parentKey, "assuming conflict because of failed lock (warning 27 for strerror)");
+		}
+		return -1;
+	}
+
+	return ret;
+#else
+	return 0;
+#endif
+}
+
+
+/**
+ * Unlocks file.
+ *
+ * @param fd is a valid filedescriptor
+ * @return 0 on success
+ * @return -1 on failure
+ * @ingroup backendhelper
+ */
+static int elektraUnlockFile (int fd ELEKTRA_UNUSED,
+		Key *parentKey ELEKTRA_UNUSED)
+{
+#ifdef ELEKTRA_LOCK_FILE
+	struct flock l;
+	l.l_type = F_UNLCK; /*Give Lock away*/
+	l.l_start= 0;	/*Start at begin*/
+	l.l_whence = SEEK_SET;
+	l.l_len = 0;	/*Do it with whole file*/
+	int ret = fcntl (fd, F_SETLK, &l);
+
+	if (ret == -1)
+	{
+		char buffer[ERROR_SIZE];
+		strerror_r(errno, buffer, ERROR_SIZE);
+		ELEKTRA_ADD_WARNING(32, parentKey, buffer);
+	}
+
+	return ret;
+#else
+	return 0;
+#endif
+}
+
+
+
+/**
+ * @brief Close a file
+ *
+ * @param fd the filedescriptor to close
+ * @param parentKey the key to write warnings to
+ */
+static void elektraCloseFile(int fd, Key *parentKey)
+{
+	if (close (fd) == -1)
+	{
+		char buffer[ERROR_SIZE];
+		strerror_r(errno, buffer, ERROR_SIZE);
+		ELEKTRA_ADD_WARNING (33, parentKey, buffer);
+	}
+}
+
+/**
  * @brief Add error text received from strerror_r
  *
  * @param errorText should have at least ERROR_SIZE bytes in reserve
