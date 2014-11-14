8242f860445fa4d06a2350db4600265c76d8e756
Markus Raab <elektra@markus-raab.org>
2014-11-14 22:45:33 +0100

    JNA now allows to read from KDB

--- src/bindings/jna/HelloElektra.java
+++ src/bindings/jna/HelloElektra.java
@@ -1,19 +1,15 @@
-import com.sun.jna.Library;
-import com.sun.jna.Native;
-import com.sun.jna.Platform;
-import com.sun.jna.Pointer;
- 
 /** Simple hello world example how Elektra could be wrapped to java. */
 public class HelloElektra {
-	public interface Elektra extends Library {
-		Elektra INSTANCE = (Elektra)
-			Native.loadLibrary(("elektra"), Elektra.class);
-		Pointer keyNew(String name, Object... args);
-		String keyName(Pointer k);
-	}
 
 	public static void main(String[] args) {
-		Pointer k = Elektra.INSTANCE.keyNew("user/sw//./key");
-		System.out.println(Elektra.INSTANCE.keyName(k));
+		Key key = Key.create("user/hello_world");
+		System.out.println(key.name());
+		System.out.println(key.string());
+
+		KeySet ks = KeySet.create(0);
+		KDB kdb = KDB.open(key);
+		kdb.get(ks, key);
+		Key k = ks.lookup(key);
+		System.out.println(k.string());
 	}
 }
