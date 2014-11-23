23ebd4c9ace016050e55949c70d5ca6506a95710
Markus Raab <elektra@markus-raab.org>
2014-11-23 11:55:47 +0100

    add two more example plugins

---
+++ src/bindings/jna/Elektra/PluginException.java
@@ -0,0 +1,27 @@
+package Elektra;
+
+public class PluginException implements Plugin {
+	public PluginException() {
+		throw new RuntimeException("constr");
+	}
+
+	public int open(Key errorKey) {
+		throw new RuntimeException("open");
+	}
+
+	public int get(KeySet ks, Key parentKey) {
+		throw new RuntimeException("get");
+	}
+
+	public int set(KeySet ks, Key parentKey) {
+		throw new RuntimeException("set");
+	}
+
+	public int error(KeySet ks, Key parentKey) {
+		throw new RuntimeException("error");
+	}
+
+	public int close(Key parentKey) {
+		throw new RuntimeException("close");
+	}
+}
