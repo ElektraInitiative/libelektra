c7718f89010d29f9a7db452a2e024296ffa01bdc
Markus Raab <elektra@markus-raab.org>
2014-12-01 19:09:17 +0100

    add cascading name to cascading example

--- examples/cascading.c
+++ examples/cascading.c
@@ -14,7 +14,7 @@ void printError(char *what, Key const* parentKey)
 
 int main()
 {
-	Key *parentKey = keyNew("", KEY_END);
+	Key *parentKey = keyNew("", KEY_CASCADING_NAME, KEY_END);
 	KDB *kdb = kdbOpen(parentKey);
 	KeySet *ks = ksNew(0, KS_END);
 	if (kdbGet(kdb, ks, parentKey) == -1)
@@ -42,11 +42,13 @@ int main()
 		printf ("%s = %s\n", keyName(k), keyString(k));
 	}
 
+	/*
 	k = keyNew("user/test/shell/somewhere/key", KEY_VALUE, "value", KEY_END);
 	ksAppendKey(ks, k);
 	keyDel(k);
+	*/
 
-	parentKey = keyNew("", KEY_END);
+	parentKey = keyNew("/", KEY_CASCADING_NAME, KEY_END);
 	if (kdbSet(kdb, ks, parentKey) == -1)
 	{
 		printError("kdbSet", parentKey);
