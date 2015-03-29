acaea370057c89a9d4383ab3f9a7860edd87bf4b
Markus Raab <elektra@markus-raab.org>
2015-03-29 12:56:53 +0200

    add yajl in real world tests

--- tests/shell/check_real_world.sh
+++ tests/shell/check_real_world.sh
@@ -69,18 +69,16 @@ then
 	succeed_if "could not mount simpleini: $APPS_FILE at $APPS_MOUNTPOINT"
 fi
 
+DESKTOP_FILE=${FILE_SUFFIX}_desktop.yajl
+DESKTOP_MOUNTPOINT=/test/script/apps/desktop
+if is_plugin_available yajl
+then
+	$KDB mount $DESKTOP_FILE $DESKTOP_MOUNTPOINT yajl
+	succeed_if "could not mount DESKTOP: $DESKTOP_FILE at $DESKTOP_MOUNTPOINT"
 
-#DESKTOP_FILE=${FILE_SUFFIX}_desktop.ini
-#DESKTOP_MOUNTPOINT=/test/script/apps/desktop
-#if is_plugin_available yajl
-#then
-#	$KDB mount $DESKTOP_FILE $DESKTOP_MOUNTPOINT yajl
-#	succeed_if "could not mount DESKTOP: $DESKTOP_FILE at $DESKTOP_MOUNTPOINT"
-#
-#	check_set_rm system/test/script/apps/desktop/x y
-#	check_set_rm user/test/script/apps/desktop/x y
-#fi
-
+	check_set_rm system/test/script/apps/desktop/x y
+	check_set_rm user/test/script/apps/desktop/x y
+fi
 
 
 
@@ -148,11 +146,11 @@ fi
 
 
 
-#if is_plugin_available yajl
-#then
-#	$KDB umount $DESKTOP_MOUNTPOINT >/dev/null
-#	succeed_if "could not umount $DESKTOP_MOUNTPOINT"
-#fi
+if is_plugin_available yajl
+then
+	$KDB umount $DESKTOP_MOUNTPOINT >/dev/null
+	succeed_if "could not umount $DESKTOP_MOUNTPOINT"
+fi
 
 if is_plugin_available simpleini
 then
