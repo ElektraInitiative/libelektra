3768a2520d18c855c30881f27769ca00165e8959
Markus Raab <elektra@markus-raab.org>
2015-05-24 21:08:16 +0200

    template: fix testmod name

--- src/plugins/template/testmod_template.c
+++ src/plugins/template/testmod_template.c
@@ -54,7 +54,7 @@ int main(int argc, char** argv)
 
 	test_basics();
 
-	printf ("\ntest_template RESULTS: %d test(s) done. %d error(s).\n",
+	printf ("\ntestmod_template RESULTS: %d test(s) done. %d error(s).\n",
 			nbTest, nbError);
 
 	return nbError;
