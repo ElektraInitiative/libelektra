3d355ea8aeafbb459f8f48608b495e58be01cdde
Peter Nirschl <peter.nirschl@gmail.com>
2015-09-09 17:32:05 +0200

    crypto-plugin: add plugin config

--- src/plugins/crypto/crypto.c
+++ src/plugins/crypto/crypto.c
@@ -7,31 +7,56 @@
 *
 */
 
+#ifndef HAVE_KDBCONFIG
+# include "kdbconfig.h"
+#endif
 #include "crypto.h"
 #include "gcrypt_operations.h"
 
 
-int elektraCryptoOpen(Plugin *handle, Key *errorKey)
+int elektraCryptoOpen(Plugin *handle ELEKTRA_UNUSED, Key *errorKey ELEKTRA_UNUSED)
 {
 	return 1;
 }
 
-int elektraCryptoClose(Plugin *handle, Key *errorKey)
+int elektraCryptoClose(Plugin *handle ELEKTRA_UNUSED, Key *errorKey ELEKTRA_UNUSED)
 {
 	return 1;
 }
 
-int elektraCryptoGet(Plugin *handle, KeySet *ks, Key *parentKey)
+int elektraCryptoGet(Plugin *handle ELEKTRA_UNUSED, KeySet *ks, Key *parentKey)
+{
+	// Publish module configuration to Elektra
+	if (!strcmp (keyName(parentKey), "system/elektra/modules/crypto"))
+	{
+		KeySet *moduleConfig = ksNew (30,
+			keyNew ("system/elektra/modules/crypto", KEY_VALUE, "crypto plugin waits for your orders", KEY_END),
+			keyNew ("system/elektra/modules/crypto/exports", KEY_END),
+			keyNew ("system/elektra/modules/crypto/exports/get", KEY_FUNC, elektraCryptoGet, KEY_END),
+			keyNew ("system/elektra/modules/crypto/exports/set", KEY_FUNC, elektraCryptoSet, KEY_END),
+			keyNew ("system/elektra/modules/crypto/infos/version", KEY_VALUE, PLUGINVERSION, KEY_END),
+			keyNew ("system/elektra/modules/crypto/infos", KEY_VALUE, "Information about crypto plugin is paced in the keys below", KEY_END),
+			keyNew ("system/elektra/modules/crypto/infos/author", KEY_VALUE, "Peter Nirschl <peter.nirschl@gmail.com>", KEY_END),
+			keyNew ("system/elektra/modules/crypto/infos/licence", KEY_VALUE, "BSD", KEY_END),
+			keyNew ("system/elektra/modules/crypto/infos/needs", KEY_VALUE, "", KEY_END),
+			keyNew ("system/elektra/modules/crypto/infos/provides", KEY_VALUE, "", KEY_END),
+			keyNew ("system/elektra/modules/crypto/infos/placements", KEY_VALUE, "postgetstorage presetstorage", KEY_END),
+			keyNew ("system/elektra/modules/crypto/infos/description", KEY_VALUE, "filter plugin for cryptographic operations", KEY_END),
+			KS_END);
+		ksAppend (ks, moduleConfig);
+		ksDel (moduleConfig);
+		return 1;
+	}
+
+	return 1;
+}
+
+int elektraCryptoSet(Plugin *handle ELEKTRA_UNUSED, KeySet *ks ELEKTRA_UNUSED, Key *parentKey ELEKTRA_UNUSED)
 {
 	return 1;
 }
 
-int elektraCryptoSet(Plugin *handle, KeySet *ks, Key *parentKey)
-{
-	return 1;
-}
-
-int elektraCryptoError(Plugin *handle, KeySet *ks, Key *parentKey)
+int elektraCryptoError(Plugin *handle ELEKTRA_UNUSED, KeySet *ks ELEKTRA_UNUSED, Key *parentKey ELEKTRA_UNUSED)
 {
 	return 1;
 }
