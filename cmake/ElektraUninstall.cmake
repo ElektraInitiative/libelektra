cb4ca2e0e51c10ad3097647b23771ed43e7a22f8
Markus Raab <elektra@markus-raab.org>
2015-09-15 13:56:24 +0200

    search manifest correctly
    (top level CMAKE_BINARY_DIR  cannot be found because of
     invocation of new cmake)

--- cmake/ElektraUninstall.cmake
+++ cmake/ElektraUninstall.cmake
@@ -3,11 +3,13 @@
 # xargs rm < install_manifest.txt
 #
 
-if (NOT EXISTS "@CMAKE_CURRENT_BINARY_DIR@/install_manifest.txt")
-	message (FATAL_ERROR "Cannot find install manifest: @CMAKE_CURRENT_BINARY_DIR@/install_manifest.txt")
-endif (NOT EXISTS "@CMAKE_CURRENT_BINARY_DIR@/install_manifest.txt")
+set(MANIFEST "@CMAKE_BINARY_DIR@/../install_manifest.txt")
 
-file (READ "@CMAKE_CURRENT_BINARY_DIR@/install_manifest.txt" files)
+if (NOT EXISTS "@MANIFEST@")
+	message (FATAL_ERROR "Cannot find install manifest: @MANIFEST@")
+endif (NOT EXISTS "@MANIFEST@")
+
+file (READ "@MANIFEST@" files)
 string (REGEX REPLACE "\n" ";" files "${files}")
 foreach (file ${files})
 	message(STATUS "Uninstalling $ENV{DESTDIR}${file}")
