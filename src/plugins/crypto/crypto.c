95d8da960bf1a97c32c7def98fe7a9394ca49fef
Peter Nirschl <peter.nirschl@gmail.com>
2015-09-16 16:39:31 +0200

    crypto-plugin: considered feedback from pull request #275 - adapted error description to be more generic (and independet from libgcrypt - made test cases static - improved README - moved plugin information (contract) to contract.h

--- src/plugins/crypto/crypto.c
+++ src/plugins/crypto/crypto.c
@@ -8,7 +8,7 @@
 */
 
 #ifndef HAVE_KDBCONFIG
-# include "kdbconfig.h"
+#include "kdbconfig.h"
 #endif
 #include "crypto.h"
 #include "gcrypt_operations.h"
@@ -30,18 +30,7 @@ int elektraCryptoGet(Plugin *handle ELEKTRA_UNUSED, KeySet *ks, Key *parentKey)
 	if (!strcmp (keyName(parentKey), "system/elektra/modules/crypto"))
 	{
 		KeySet *moduleConfig = ksNew (30,
-			keyNew ("system/elektra/modules/crypto", KEY_VALUE, "crypto plugin waits for your orders", KEY_END),
-			keyNew ("system/elektra/modules/crypto/exports", KEY_END),
-			keyNew ("system/elektra/modules/crypto/exports/get", KEY_FUNC, elektraCryptoGet, KEY_END),
-			keyNew ("system/elektra/modules/crypto/exports/set", KEY_FUNC, elektraCryptoSet, KEY_END),
-			keyNew ("system/elektra/modules/crypto/infos/version", KEY_VALUE, PLUGINVERSION, KEY_END),
-			keyNew ("system/elektra/modules/crypto/infos", KEY_VALUE, "Information about crypto plugin is paced in the keys below", KEY_END),
-			keyNew ("system/elektra/modules/crypto/infos/author", KEY_VALUE, "Peter Nirschl <peter.nirschl@gmail.com>", KEY_END),
-			keyNew ("system/elektra/modules/crypto/infos/licence", KEY_VALUE, "BSD", KEY_END),
-			keyNew ("system/elektra/modules/crypto/infos/needs", KEY_VALUE, "", KEY_END),
-			keyNew ("system/elektra/modules/crypto/infos/provides", KEY_VALUE, "", KEY_END),
-			keyNew ("system/elektra/modules/crypto/infos/placements", KEY_VALUE, "postgetstorage presetstorage", KEY_END),
-			keyNew ("system/elektra/modules/crypto/infos/description", KEY_VALUE, "filter plugin for cryptographic operations", KEY_END),
+#include "contract.h"
 			KS_END);
 		ksAppend (ks, moduleConfig);
 		ksDel (moduleConfig);
