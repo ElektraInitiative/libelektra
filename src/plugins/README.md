01ceda7ee8ae0d8b6e32fbba86f12dc3ed0e7751
thomas <thomas.waser@kabsi.at>
2015-09-18 16:14:19 +0200

    Added calculate Plugin

--- src/plugins/README.md
+++ src/plugins/README.md
@@ -56,7 +56,7 @@ productive use:
 - [fstab](fstab) reads fstab files.
 - [regexstore](regexstore)
 - [simpleini](simpleini) is ini without sections
-- [csvstorage](csvstorage) uses csv files
+- [csvstorage](csvstorage) for csv files
 
 ## System Information ##
 
@@ -127,7 +127,7 @@ copied by another plugin just before):
 - [path](path) by checking files on filesystem
 - [type](type) using runtime type checking (CORBA types)
 - [enum](enum) compares the keyvalue against a list of valid values
-
+- [calculate](calculate) compares the keyvalue with a mathematical expression
 ## Interpreter ##
 
 These plugins start an interpreter and allow you to use a bindings.
---
+++ src/plugins/calculate/testmod_calculate.c
@@ -0,0 +1,90 @@
+/**
+* \file
+*
+* \brief Tests for calculate plugin
+*
+* \copyright BSD License (see doc/COPYING or http://www.libelektra.org)
+*
+*/
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <kdbconfig.h>
+
+#include <tests_plugin.h>
+
+static void test_sum()
+{
+	Key *parentKey = keyNew("user/tests/calculate", KEY_VALUE, "", KEY_END);
+	KeySet *ks = ksNew(5,
+	keyNew("user/tests/enum/sum", KEY_VALUE, "153", KEY_META, "check/calc", "== + bla/val1 + bla/val2 bla/val3", KEY_END),
+	keyNew("user/tests/calculate/bla/val1", KEY_VALUE, "100", KEY_END),
+	keyNew("user/tests/calculate/bla/val2", KEY_VALUE, "50", KEY_END),
+	keyNew("user/tests/calculate/bla/val3", KEY_VALUE, "3", KEY_END),
+		KS_END);
+	KeySet *conf = ksNew(0, KS_END);
+	PLUGIN_OPEN("calculate");
+	ksRewind(ks);
+	succeed_if(plugin->kdbSet(plugin, ks, parentKey) == 1, "error");
+	ksDel(ks);
+	keyDel(parentKey);
+	PLUGIN_CLOSE();
+
+}
+
+static void test_inv_lt()
+{
+	Key *parentKey = keyNew("user/tests/calculate", KEY_VALUE, "", KEY_END);
+	KeySet *ks = ksNew(5,
+	keyNew("user/tests/enum/sum", KEY_VALUE, "250", KEY_META, "check/calc", "< + bla/val1 + bla/val2 bla/val3", KEY_END),
+	keyNew("user/tests/calculate/bla/val1", KEY_VALUE, "100", KEY_END),
+	keyNew("user/tests/calculate/bla/val2", KEY_VALUE, "50", KEY_END),
+	keyNew("user/tests/calculate/bla/val3", KEY_VALUE, "3", KEY_END),
+		KS_END);
+	KeySet *conf = ksNew(0, KS_END);
+	PLUGIN_OPEN("calculate");
+	ksRewind(ks);
+	succeed_if(plugin->kdbSet(plugin, ks, parentKey) == (-1), "error");
+	ksDel(ks);
+	keyDel(parentKey);
+	PLUGIN_CLOSE();
+
+}
+
+static void test_val_ge()
+{
+	Key *parentKey = keyNew("user/tests/calculate", KEY_VALUE, "", KEY_END);
+	KeySet *ks = ksNew(5,
+	keyNew("user/tests/enum/sum", KEY_VALUE, "250", KEY_META, "check/calc", ">= + bla/val1 + bla/val2 bla/val3", KEY_END),
+	keyNew("user/tests/calculate/bla/val1", KEY_VALUE, "100", KEY_END),
+	keyNew("user/tests/calculate/bla/val2", KEY_VALUE, "50", KEY_END),
+	keyNew("user/tests/calculate/bla/val3", KEY_VALUE, "3", KEY_END),
+		KS_END);
+	KeySet *conf = ksNew(0, KS_END);
+	PLUGIN_OPEN("calculate");
+	ksRewind(ks);
+	succeed_if(plugin->kdbSet(plugin, ks, parentKey) == (1), "error");
+	ksDel(ks);
+	keyDel(parentKey);
+	PLUGIN_CLOSE();
+
+}
+
+
+int main(int argc, char** argv)
+{
+	printf ("CALCULATE     TESTS\n");
+	printf ("==================\n\n");
+
+	init (argc, argv);
+	test_sum();
+	test_inv_lt();
+	test_val_ge();
+
+	printf ("\ntestmod_calculate RESULTS: %d test(s) done. %d error(s).\n",
+			nbTest, nbError);
+
+	return nbError;
+}
+
