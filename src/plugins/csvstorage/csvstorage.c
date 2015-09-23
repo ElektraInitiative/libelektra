585d0dc66361c39da32554c269d4e8890c29de6e
thomas <thomas.waser@kabsi.at>
2015-09-23 06:14:18 +0200

    fixed bug in csvstorage

--- src/plugins/csvstorage/csvstorage.c
+++ src/plugins/csvstorage/csvstorage.c
@@ -315,11 +315,13 @@ static int csvWrite(KeySet *returned, Key *parentKey, char delim, short printHea
 		if(keyRel(parentKey, cur) != 1)
 			continue;
 		toWriteKS = ksCut(returned, cur);
-		ksRewind(toWriteKS);
 		colCounter = 0;
 		if(printHeader)
 		{
-			while(1)
+		
+			ksAppend(returned, toWriteKS);
+			ksRewind(returned);
+	    		while(1)
 			{
 				if(colCounter == ULONG_MAX)
 				{
@@ -343,7 +345,6 @@ static int csvWrite(KeySet *returned, Key *parentKey, char delim, short printHea
 				return -1;
 			}
 			columns = colCounter;
-			colCounter = 0;
 			printHeader = 0;
 		}
 		else
