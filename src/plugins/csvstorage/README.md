406b4ed406d30fb4781860cc3dd73f929bdd73e3
thomas <thomas.waser@kabsi.at>
2015-10-29 20:05:05 +0100

    updated README, and added testcase

--- src/plugins/csvstorage/README.md
+++ src/plugins/csvstorage/README.md
@@ -17,12 +17,21 @@ Tells the plugin what delimiter is used in the file.
 The default delimiter is `;` and will be used if `delimiter` is not set.
 
 `useheader`
-Tells the plugin to use the first line as a header if it's set to anything but "0". The columns will get the coresponding names.
-If useheader is not set, the columns get namend #0,#1,...
+Tells the plugin to use the first line as a header if it's set to "1". The columns will get the coresponding names.
+Ignore the first line if it's set to "-1" or treat the first line as a record if it's set to "0". 
+If useheader is not set, or set to "0", the columns get namend #0,#1,...
 
 `columns`
 If this key is set the plugin will yield an error for every file that doesn't have exactly the amount of columns ans specified in `columns`.
 
+`colNames`
+Sets the column names. Only useable in combination with the `columns` key. The number of subkeys must match the number of columns.
+```
+columns = 2
+colNames
+colNames/#0 = col0Name
+colNames/#1 = col1Name
+```
 
 ## Example ##
 
--- src/plugins/csvstorage/testmod_csvstorage.c
+++ src/plugins/csvstorage/testmod_csvstorage.c
@@ -104,6 +104,35 @@ static void testwritevalidemptycol(const char *file)
 	keyDel(parentKey);
 	PLUGIN_CLOSE();
 }
+static void testSetColnames(const char *file)
+{
+	printf("testing on %s:%s\n", srcdir_file(file), file);
+	Key * parentKey = keyNew ("user/tests/csvstorage", KEY_VALUE, srcdir_file(file), KEY_END);
+	KeySet *conf = ksNew (20,
+			keyNew ("system/useheader", KEY_VALUE, "1", KEY_END), 
+			keyNew ("system/columns", KEY_VALUE, "2", KEY_END),
+			keyNew ("system/colNames", KEY_VALUE, "", KEY_END),
+			keyNew ("system/colNames/#0", KEY_VALUE, "col0Name", KEY_END),
+			keyNew ("system/colNames/#1", KEY_VALUE, "col1Name", KEY_END),
+			KS_END);
+	PLUGIN_OPEN("csvstorage");
+	KeySet *ks = ksNew(0, KS_END);
+	succeed_if (plugin->kdbGet(plugin, ks, parentKey) >= 1, "call to kdbGet was not successful");
+	output_keyset(ks);
+	Key *key;
+	key = ksLookupByName(ks, "user/tests/csvstorage/#1/col0Name", 0);
+	exit_if_fail(key, "key not found");
+	succeed_if(strcmp(keyString(key), "l1c1") == 0, "wrong key");
+	key = ksLookupByName(ks, "user/tests/csvstorage/#2/col1Name", 0);
+	exit_if_fail(key, "key not found");
+	succeed_if(strcmp(keyString(key), "l2c2") == 0, "wrong key");
+
+	ksDel(ks);
+	keyDel(parentKey);
+
+	PLUGIN_CLOSE();
+
+}
 int main(int argc, char** argv)
 {
 	printf ("CSVSTORAGE     TESTS\n");
@@ -116,7 +145,7 @@ int main(int argc, char** argv)
 	testreadwriteinvalid("csvstorage/invalid_columns.csv");
 	testwriteinvalidheader("csvstorage/invalid_columns_header2.csv");
 	testwritevalidemptycol("csvstorage/valid_empty_col.csv");
-
+	testSetColnames("csvstorage/valid.csv");
 
 	printf ("\ntestmod_csvstorage RESULTS: %d test(s) done. %d error(s).\n",
 			nbTest, nbError);
