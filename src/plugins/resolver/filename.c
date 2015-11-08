980c37717095ea2b4dd0f11d76e7615f0b86d7e1
Markus Raab <elektra@markus-raab.org>
2015-11-08 11:46:20 +0100

    resolver: avoid crash if getpwd fails

--- src/plugins/resolver/filename.c
+++ src/plugins/resolver/filename.c
@@ -457,7 +457,7 @@ static char *elektraGetCwd(Key *warningsKey)
 static int elektraResolveDir(resolverHandle *p, Key *warningsKey)
 {
 	char * cwd = elektraGetCwd(warningsKey);
-	if (!cwd) cwd = "/";
+	if (!cwd) cwd = elektraStrDup("/");
 
 	char *dn = elektraStrDup(cwd);
 	char *dnOrig = dn;
