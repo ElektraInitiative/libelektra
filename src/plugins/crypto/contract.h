e1f2c68b588ee9c7e06f649d82e16c51f494515b
Peter Nirschl <peter.nirschl@me.com>
2015-12-10 15:49:52 +0100

    crypto-plugin: fix contract

--- src/plugins/crypto/contract.h
+++ src/plugins/crypto/contract.h
@@ -8,8 +8,11 @@
 
 keyNew ("system/elektra/modules/crypto", KEY_VALUE, "crypto plugin waits for your orders", KEY_END),
 keyNew ("system/elektra/modules/crypto/exports", KEY_END),
+keyNew ("system/elektra/modules/crypto/exports/open", KEY_FUNC, elektraCryptoOpen, KEY_END),
+keyNew ("system/elektra/modules/crypto/exports/close", KEY_FUNC, elektraCryptoClose, KEY_END),
 keyNew ("system/elektra/modules/crypto/exports/get", KEY_FUNC, elektraCryptoGet, KEY_END),
 keyNew ("system/elektra/modules/crypto/exports/set", KEY_FUNC, elektraCryptoSet, KEY_END),
+keyNew ("system/elektra/modules/crypto/exports/error", KEY_FUNC, elektraCryptoError, KEY_END),
 keyNew ("system/elektra/modules/crypto/infos/version", KEY_VALUE, PLUGINVERSION, KEY_END),
 keyNew ("system/elektra/modules/crypto/infos", KEY_VALUE, "Information about crypto plugin is paced in the keys below", KEY_END),
 keyNew ("system/elektra/modules/crypto/infos/author", KEY_VALUE, "Peter Nirschl <peter.nirschl@gmail.com>", KEY_END),
