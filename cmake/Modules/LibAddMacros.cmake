ElektraInitiative/libelektra

commit 89e6678b34ae6f6b436b0d1dd8cad8c39345a4e9

make use of cmake_parse_arguments
and remove our now obsolete parse_arguments macro

cmake/Modules/LibAddMacros.cmake
@@ -1,5 +1,3 @@
-include(LibParseArguments)
-
 # Copy a file from source dir to binary dir
 #
 # copy_file or directory
@@ -107,11 +105,12 @@ endmacro()
 # and adds a test
 macro (add_plugintest testname)
 	if (BUILD_TESTING AND (BUILD_STATIC OR BUILD_FULL))
-		parse_arguments(ARG
-			"" # no arguments
-			"MEMLEAK" #options
+		cmake_parse_arguments (ARG
+			"MEMLEAK" # optional keywords
+			""        # one value keywords
+			""        # multi value keywords
 			${ARGN}
-			)
+		)
 		set (TEST_SOURCES
 				$<TARGET_OBJECTS:cframework>
 				${ARG_UNPARSED_ARGUMENTS}
@@ -141,11 +140,12 @@ endmacro (add_plugintest)
 # Add a test for cpp plugins
 macro (add_cpp_plugintest testname)
 	if (BUILD_TESTING)
-		parse_arguments(ARG
-			"" # no arguments
-			"MEMLEAK" #options
+		cmake_parse_arguments (ARG
+			"MEMLEAK" # optional keywords
+			""        # one value keywords
+			""        # multi value keywords
 			${ARGN}
-			)
+		)
 		set (source "testmod_${testname}")
 		include_directories ("${CMAKE_CURRENT_SOURCE_DIR}")
 		include_directories ("${CMAKE_SOURCE_DIR}/src/bindings/cpp/tests")
@@ -301,7 +301,7 @@ endmacro (remove_tool)
 #
 # copied from http://www.cmake.org/Wiki/CMakeMacroListOperations
 MACRO(list_filter)
-  parse_arguments(LIST_FILTER "OUTPUT_VARIABLE" "" ${ARGV})
+  cmake_parse_arguments(LIST_FILTER "" "OUTPUT_VARIABLE" "" ${ARGV})
   # Check arguments.
   LIST(LENGTH LIST_FILTER_DEFAULT_ARGS LIST_FILTER_default_length)
   IF(${LIST_FILTER_default_length} EQUAL 0)

cmake/Modules/LibAddPlugin.cmake
@@ -1,4 +1,3 @@
-include(LibParseArguments)
 include(LibAddMacros)
 
 # add_plugin: add a plugin to Elektra
@@ -20,12 +19,12 @@ include(LibAddMacros)
 # INCLUDE_DIRECTORIES:
 #  Append to include path (globally+plugin specific).
 function(add_plugin PLUGIN_SHORT_NAME)
-	parse_arguments(ARG
-		"SOURCES;SHARED_SOURCES;LINK_LIBRARIES;COMPILE_DEFINITIONS;INCLUDE_DIRECTORIES"
-		"CPP"
+	cmake_parse_arguments (ARG
+		"CPP" # optional keywords
+		"" # one value keywords
+		"SOURCES;SHARED_SOURCES;LINK_LIBRARIES;COMPILE_DEFINITIONS;INCLUDE_DIRECTORIES" # multi value keywords
 		${ARGN}
-		)
-
+	)
 
 	set (PLUGIN_NAME elektra-${PLUGIN_SHORT_NAME})
 	set (PLUGIN_OBJS ${PLUGIN_NAME}-objects)

cmake/Modules/LibAddTest.cmake
@@ -9,11 +9,12 @@
 
 
 macro (add_gtest source)
-	parse_arguments(ARG
-		""
-		"NO_MAIN;NO_TOOLS"
+	cmake_parse_arguments (ARG
+		"NO_MAIN;NO_TOOLS" # optional keywords
+		"" # one value keywords
+		"" # multi value keywords
 		${ARGN}
-		)
+	)
 
 	if (BUILD_TESTING)
 	set (SOURCES ${HDR_FILES} ${source}.cpp)

cmake/Modules/LibParseArguments.cmake
@@ -1,36 +1,0 @@
-#
-# copied from http://www.cmake.org/Wiki/CMakeMacroParseArguments
-#
-MACRO(PARSE_ARGUMENTS prefix arg_names option_names)
-	SET(DEFAULT_ARGS)
-	FOREACH(arg_name ${arg_names})    
-		SET(${prefix}_${arg_name})
-	ENDFOREACH(arg_name)
-	FOREACH(option ${option_names})
-		SET(${prefix}_${option} FALSE)
-	ENDFOREACH(option)
-
-	SET(current_arg_name DEFAULT_ARGS)
-	SET(current_arg_list)
-	FOREACH(arg ${ARGN})
-		SET(larg_names ${arg_names})
-		LIST(FIND larg_names "${arg}" is_arg_name)
-		IF (is_arg_name GREATER -1)
-			SET(${prefix}_${current_arg_name}
-				${current_arg_list})
-			SET(current_arg_name ${arg})
-			SET(current_arg_list)
-		ELSE (is_arg_name GREATER -1)
-			SET(loption_names ${option_names})
-			LIST(FIND loption_names "${arg}" is_option)
-			IF (is_option GREATER -1)
-				SET(${prefix}_${arg} TRUE)
-			ELSE (is_option GREATER -1)
-				SET(current_arg_list
-					${current_arg_list} ${arg})
-			ENDIF (is_option GREATER -1)
-		ENDIF (is_arg_name GREATER -1)
-	ENDFOREACH(arg)
-	SET(${prefix}_${current_arg_name} ${current_arg_list})
-ENDMACRO(PARSE_ARGUMENTS)
-

commit 4bd6544ce028f6b05d202deef77ecd8179ee5c5a

fix #425
parse_arguments -> cmake_parse_arguments
    regression introduced in 89e6678b34ae6f6b436b0d1dd8cad8c39345a4e9
*_DEFAULT_ARGS is now called *_UNPARSED_ARGUMENTS

cmake/Modules/LibAddMacros.cmake
@@ -301,6 +301,7 @@ endmacro (remove_tool)
 # copied from http://www.cmake.org/Wiki/CMakeMacroListOperations
 MACRO(list_filter)
   cmake_parse_arguments(LIST_FILTER "" "OUTPUT_VARIABLE" "" ${ARGV})
+  set(LIST_FILTER_DEFAULT_ARGS ${LIST_FILTER_UNPARSED_ARGUMENTS})
   # Check arguments.
   LIST(LENGTH LIST_FILTER_DEFAULT_ARGS LIST_FILTER_default_length)
   IF(${LIST_FILTER_default_length} EQUAL 0)