8a1b3f80247a0acfc5eed9711b63f7fa3c23479d
Markus Raab <elektra@markus-raab.org>
2015-12-28 17:18:28 +0100

    add testcase for all permutations
    comparision is non-transitive, sorting won't work

--- src/libs/tools/tests/testtool_backendbuilder.cpp
+++ src/libs/tools/tests/testtool_backendbuilder.cpp
@@ -13,6 +13,7 @@
 #include <backendbuilder.hpp>
 
 #include <iostream>
+#include <algorithm>
 #include <string>
 #include <kdb.hpp>
 #include <gtest/gtest.h>
@@ -37,7 +38,6 @@ TEST(BackendBuilder, basicAddRem)
 	EXPECT_TRUE(bb.validated());
 }
 
-
 TEST(BackendBuilder, basicSort)
 {
 	using namespace kdb;
@@ -54,5 +54,25 @@ TEST(BackendBuilder, basicSort)
 
 	bb.addPlugin(BackendBuilder::PluginSpec("augeas"));
 	EXPECT_TRUE(bb.validated()) << "Reordering not successful?";
+}
+
+
+TEST(BackendBuilder, allSort)
+{
+	using namespace kdb;
+	using namespace kdb::tools;
+
+	std::vector <std::string> permutation = {"augeas", "glob", "keytometa", "resolver"};
+
+	do {
+		// for (auto const & p : permutation) std::cout << p << " ";
+		// std::cout << std::endl;
+		BackendBuilder bb;
+		bb.addPlugin(BackendBuilder::PluginSpec(permutation[0]));
+		bb.addPlugin(BackendBuilder::PluginSpec(permutation[1]));
+		bb.addPlugin(BackendBuilder::PluginSpec(permutation[2]));
+		bb.addPlugin(BackendBuilder::PluginSpec(permutation[3]));
+		EXPECT_TRUE(bb.validated()) << "Reordering not successful?";
+	} while (std::next_permutation(permutation.begin(), permutation.end()));
 }
 
