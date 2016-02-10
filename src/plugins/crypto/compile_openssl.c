614772dc4c9872ca327f67b218f4c10b4bcea9dd
Peter Nirschl <peter.nirschl@gmail.com>
2016-02-10 21:46:56 +0100

    improve build file gen. for crypto-plugin on OS X
    fixes #487

---
+++ src/plugins/crypto/compile_openssl.c
@@ -0,0 +1,17 @@
+/**
+ * @file
+ *
+ * @brief tests if compilation works (include and build paths set correct, etc...)
+ *
+ * @copyright BSD License (see doc/COPYING or http://www.libelektra.org)
+ *
+ */
+ 
+#include <openssl/evp.h>
+
+int main()
+{
+	EVP_CIPHER_CTX opensslSpecificType;
+
+	return 0;
+}
