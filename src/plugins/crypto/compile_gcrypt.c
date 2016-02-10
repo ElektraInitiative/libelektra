f9c90585f385b38675f9b216357e62a5ee624c3f
Peter Nirschl <peter.nirschl@gmail.com>
2016-02-10 21:46:56 +0100

    improve build file gen. for crypto-plugin on OS X
    fixes #487

---
+++ src/plugins/crypto/compile_gcrypt.c
@@ -0,0 +1,16 @@
+/**
+ * @file
+ *
+ * @brief tests if compilation works (include and build paths set correct, etc...)
+ *
+ * @copyright BSD License (see doc/COPYING or http://www.libelektra.org)
+ *
+ */
+ 
+#include <gcrypt.h>
+
+int main()
+{
+	gcry_cipher_hd_t elektraCryptoHandle;
+	return 0;
+}
