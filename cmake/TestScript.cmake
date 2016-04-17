ba80da57a470f11b495ccb0a53dacd282d72a4ce
Markus Raab <elektra@markus-raab.org>
2016-04-17 17:02:45 +0200

    remove TestScript
    see #595

--- cmake/TestScript.cmake
+++
@@ -1,6 +0,0 @@
-set (CTEST_SOURCE_DIRECTORY /home/markus/Projekte/Elektra/current)
-set (CTEST_BINARY_DIRECTORY /home/markus/Projekte/Elektra/current/build)
-set (CTEST_COMMAND ctest)
-set ($ENV{srcdir} /home/markus/Projekte/Elektra/current/build/tests)
-
-ctest_memcheck ()
