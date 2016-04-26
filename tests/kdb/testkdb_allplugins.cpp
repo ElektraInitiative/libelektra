25c4c0ab46b46c8968c69f22101edb74b089c5de
Markus Raab <elektra@markus-raab.org>
2016-04-26 13:09:28 +0200

    exclude semlock from shared memtests

--- tests/kdb/testkdb_allplugins.cpp
+++ tests/kdb/testkdb_allplugins.cpp
@@ -30,6 +30,7 @@ std::vector<std::string> getAllPlugins ()
 
 	// remove known problems
 	plugins.erase (std::remove (plugins.begin (), plugins.end (), "jni"), plugins.end ());
+	plugins.erase (std::remove (plugins.begin (), plugins.end (), "semlock"), plugins.end ());
 	plugins.erase (std::remove (plugins.begin (), plugins.end (), "crypto_gcrypt"), plugins.end ());
 	return plugins;
 }
