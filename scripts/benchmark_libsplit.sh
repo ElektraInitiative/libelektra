1ac429997ac0a0a8738c9db64ccd6f1d617775f7
Mihael Pranjic <mpranj@limun.org>
2016-05-05 19:33:06 +0200

    dont use seq

--- scripts/benchmark_libsplit.sh
+++ scripts/benchmark_libsplit.sh
@@ -11,8 +11,10 @@ RUNS=21
 
 echo "Runtime as IEEE Std 1003.2-1992 (``POSIX.2'')"
 echo "Doing $RUNS runs:"
-for run in $(seq 1 $RUNS); do
+i=0
+while [ $i -lt $RUNS ] ; do
 	WALLTIME=$( (time -p $BENCHMARK) 2>&1 )
 	#echo $WALLTIME | awk -F"real" '{print $1;}' # print programs stdout
 	echo $WALLTIME | awk -F"real" '{print $2;}' | awk '{print $1;}'
+	i=$((i+1))
 done
