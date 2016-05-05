aec74841b905d5f843aa3636a24f212e67818f69
Mihael Pranjic <mpranj@limun.org>
2016-05-05 18:12:55 +0200

    fix libsplit benchmark: POSIX compatibility

--- scripts/benchmark_libsplit.sh
+++ scripts/benchmark_libsplit.sh
@@ -6,13 +6,13 @@ fi
 
 BENCHMARK=./build/bin/large
 CURRTIME=`date +"%H-%M-%S_%m-%d-%Y"`
-LOGFILE="./doc/benchmark/logs/$(echo $KDB)_log_$CURRTIME.log"
+#LOGFILE="./doc/benchmark/logs/$(echo $KDB)_log_$CURRTIME.log"
+RUNS=21
 
-
-echo "Runtime as IEEE Std 1003.2-1992 (``POSIX.2'')" >> $LOGFILE
-for run in {0..20}; do
-	echo "RUN: #$run"
-	WALLTIME=$((time -p $BENCHMARK) 2>&1)
-	echo $WALLTIME | awk -F"real" '{print $1;}' # print programs stdout
-	echo $WALLTIME | awk -F"real" '{print $2;}' | awk '{print $1;}' >> $LOGFILE
+echo "Runtime as IEEE Std 1003.2-1992 (``POSIX.2'')"
+echo "Doing $RUNS runs:"
+for run in $(seq 1 $RUNS); do
+	WALLTIME=$( (time -p $BENCHMARK) 2>&1 )
+	#echo $WALLTIME | awk -F"real" '{print $1;}' # print programs stdout
+	echo $WALLTIME | awk -F"real" '{print $2;}' | awk '{print $1;}'
 done
