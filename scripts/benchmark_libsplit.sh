de709d362dd0215f4d3efe50817a0c68c2e55edd
Mihael Pranjic <mpranj@limun.org>
2016-05-05 20:43:46 +0200

    forgot double qotes

--- scripts/benchmark_libsplit.sh
+++ scripts/benchmark_libsplit.sh
@@ -12,7 +12,7 @@ RUNS=21
 echo "Runtime as IEEE Std 1003.2-1992 (``POSIX.2'')"
 echo "Doing $RUNS runs:"
 i=0
-while [ $i -lt $RUNS ] ; do
+while [ "$i" -lt "$RUNS" ] ; do
 	WALLTIME=$( (time -p $BENCHMARK) 2>&1 )
 	#echo $WALLTIME | awk -F"real" '{print $1;}' # print programs stdout
 	echo $WALLTIME | awk -F"real" '{print $2;}' | awk '{print $1;}'
