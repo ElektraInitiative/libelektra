5858ed65df506459f53793ac80b594950678452a
Peter Nirschl <peter.nirschl@gmail.com>
2016-05-17 14:04:44 +0200

    add checkconf example to template plugin

--- src/plugins/template/template.h
+++ src/plugins/template/template.h
@@ -18,6 +18,7 @@ int elektraTemplateClose (Plugin * handle, Key * errorKey);
 int elektraTemplateGet (Plugin * handle, KeySet * ks, Key * parentKey);
 int elektraTemplateSet (Plugin * handle, KeySet * ks, Key * parentKey);
 int elektraTemplateError (Plugin * handle, KeySet * ks, Key * parentKey);
+int elektraTemplateCheckConfig (Key * errorKey, KeySet * conf);
 
 Plugin * ELEKTRA_PLUGIN_EXPORT (template);
 
