1f3dd095d6367bb19580049273a96b45061d07ff
Peter Nirschl <peter.nirschl@gmail.com>
2016-07-08 20:51:04 +0200

    crypto-plugin: fix header description in botan_operations.cpp

--- src/plugins/crypto/botan_operations.cpp
+++ src/plugins/crypto/botan_operations.cpp
@@ -1,7 +1,7 @@
 /**
  * @file
  *
- * @brief cryptographic interface using the gcrypt library
+ * @brief cryptographic interface using the Botan library
  *
  * @copyright BSD License (see doc/COPYING or http://www.libelektra.org)
  *
