9addcb59654fc5acd7bdd06a67b0c9c39bb6501f
Thomas Waser <thomas.waser@libelektra.org>
2016-07-10 19:53:39 +0200

    intercept open initial

---
+++ src/libs/intercept/intercept.c
@@ -0,0 +1,250 @@
+#define _GNU_SOURCE
+#include <dlfcn.h>
+#include <fcntl.h>
+#include <kdb.h>
+#include <libgen.h>
+#include <limits.h>
+#include <linux/limits.h>
+#include <pwd.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#define PRELOAD_PATH "/preload/open"
+
+struct _Node
+{
+	char * key;
+	char * value;
+	struct _Node * next;
+};
+typedef struct _Node Node;
+static Node * head = NULL;
+
+static void canonicalizePath (char * buffer, size_t bufSize, char * toAppend)
+{
+	char * destPtr = buffer + strlen (buffer);
+	for (int i = 0; i < strlen (toAppend); ++i)
+	{
+		if (!strncmp ((toAppend + i), "../", 3))
+		{
+			i += 2;
+			const char * dir = dirname (buffer);
+			size_t dirLen = strlen (dir);
+			destPtr = buffer + dirLen;
+			if (strcmp (dir, "/")) *destPtr++ = '/';
+			*destPtr = '\0';
+			continue;
+		}
+		else if (!strncmp ((toAppend + i), "./", 2))
+		{
+			++i;
+			continue;
+		}
+		else if (!strncmp ((toAppend + i), "//", 2))
+		{
+			continue;
+		}
+		else
+		{
+			*destPtr++ = toAppend[i];
+		}
+	}
+}
+
+static char * createAbsolutePath (const char * path, const char * cwd)
+{
+	if (path[0] == '/')
+		return strdup (path);
+	else
+	{
+		char * absPath = NULL;
+		size_t pathlen;
+		char * pathPtr = NULL;
+		if (path[0] == '~')
+		{
+			struct passwd * pwd = getpwuid (getuid ());
+			pathlen = strlen (path) + strlen (pwd->pw_dir) + 2;
+			absPath = calloc (pathlen, sizeof (char));
+			snprintf (absPath, pathlen, "%s/", pwd->pw_dir);
+			pathPtr = (char *)(path + 2);
+		}
+		else
+		{
+			pathlen = strlen (path) + strlen (cwd) + 2;
+			absPath = calloc (pathlen, sizeof (char));
+			snprintf (absPath, pathlen, "%s/", cwd);
+			pathPtr = (char *)path;
+		}
+		canonicalizePath (absPath, pathlen, pathPtr);
+		return absPath;
+	}
+}
+
+static void init (void) __attribute__ ((constructor));
+static void cleanup (void) __attribute__ ((destructor));
+void init ()
+{
+	char cwd[PATH_MAX];
+	getcwd (cwd, PATH_MAX);
+	KeySet * tmpKS = ksNew (0, KS_END);
+	Key * parentKey = keyNew (PRELOAD_PATH, KEY_CASCADING_NAME, KEY_END);
+	Key * key;
+	KDB * handle = kdbOpen (parentKey);
+	kdbGet (handle, tmpKS, parentKey);
+	KeySet * ks = ksCut (tmpKS, parentKey);
+	ksRewind (ks);
+	ssize_t size = ksGetSize (ks);
+	if (size <= 1) goto CleanUp;
+	Node * current = head;
+	ksNext (ks); // skip head
+	while ((key = ksNext (ks)) != NULL)
+	{
+		Node * tmp = calloc (1, sizeof (Node));
+		tmp->key = createAbsolutePath (keyBaseName (key), cwd);
+		tmp->value = createAbsolutePath (keyString (key), cwd);
+		;
+		tmp->next = NULL;
+		if (current == NULL)
+		{
+			head = tmp;
+			current = head;
+		}
+		else
+		{
+			current->next = tmp;
+			current = current->next;
+		}
+	}
+CleanUp:
+	ksAppend (tmpKS, ks);
+	ksDel (tmpKS);
+	ksDel (ks);
+	kdbClose (handle, parentKey);
+	keyDel (parentKey);
+}
+
+
+void cleanup ()
+{
+	Node * current = head;
+	while (current)
+	{
+		Node * tmp = current;
+		free (current->key);
+		free (current->value);
+		current = current->next;
+		free (tmp);
+	}
+}
+
+typedef int (*orig_open_f_type) (const char * pathname, int flags, ...);
+
+int open (const char * pathname, int flags, ...)
+{
+	char * newPath = NULL;
+	if (pathname)
+	{
+		//        fprintf(stderr, "Path: %s\n", pathname);
+		char cwd[PATH_MAX];
+		getcwd (cwd, PATH_MAX);
+		char * resolvedPath = NULL;
+		size_t pathlen = 0;
+		if (pathname[0] != '/')
+		{
+			resolvedPath = createAbsolutePath (pathname, cwd);
+		}
+		else
+		{
+			resolvedPath = calloc (strlen (pathname), sizeof (char));
+			memset (resolvedPath, 0, sizeof (resolvedPath));
+			canonicalizePath (resolvedPath, sizeof (resolvedPath), (char *)pathname);
+		}
+		//        fprintf(stderr, "resolved: %s\n", resolvedPath);
+		Node * current = head;
+		while (current)
+		{
+			if (!strcmp (current->key, resolvedPath))
+			{
+				newPath = current->value;
+				break;
+			}
+			current = current->next;
+		}
+		if (!newPath) newPath = (char *)pathname;
+		free (resolvedPath);
+	}
+	orig_open_f_type orig_open;
+	orig_open = (orig_open_f_type)dlsym (RTLD_NEXT, "open");
+	int fd;
+	if (flags & O_CREAT)
+	{
+		int mode;
+		va_list argptr;
+		va_start (argptr, flags);
+		mode = va_arg (argptr, int);
+		va_end (argptr);
+		fd = orig_open (newPath, flags, mode);
+	}
+	else
+	{
+		fd = orig_open (newPath, flags);
+	}
+	return fd;
+}
+
+
+int open64 (const char * pathname, int flags, ...)
+{
+	char * newPath = NULL;
+	if (pathname)
+	{
+		char cwd[PATH_MAX];
+		getcwd (cwd, PATH_MAX);
+		char * resolvedPath = NULL;
+		size_t pathlen = 0;
+		if (pathname[0] != '/')
+		{
+			resolvedPath = createAbsolutePath (pathname, cwd);
+		}
+		else
+		{
+			resolvedPath = calloc (strlen (pathname), sizeof (char));
+			memset (resolvedPath, 0, sizeof (resolvedPath));
+			canonicalizePath (resolvedPath, sizeof (resolvedPath), (char *)pathname);
+		}
+		Node * current = head;
+		while (current)
+		{
+			if (!strcmp (current->key, resolvedPath))
+			{
+				newPath = current->value;
+				break;
+			}
+			current = current->next;
+		}
+		if (!newPath) newPath = (char *)pathname;
+		free (resolvedPath);
+	}
+	orig_open_f_type orig_open;
+	orig_open = (orig_open_f_type)dlsym (RTLD_NEXT, "open64");
+	int fd;
+	if (flags & O_CREAT)
+	{
+		int mode;
+		va_list argptr;
+		va_start (argptr, flags);
+		mode = va_arg (argptr, int);
+		va_end (argptr);
+		fd = orig_open (newPath, flags, mode);
+	}
+	else
+	{
+		fd = orig_open (newPath, flags);
+	}
+	return fd;
+}
