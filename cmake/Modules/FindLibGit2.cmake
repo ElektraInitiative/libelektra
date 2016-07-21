5d980858ff95d1b9cd978cc1d797001f9fcf50d0
Markus Raab <elektra@markus-raab.org>
2016-07-21 10:52:58 +0200

    cmake: libgit
    use new syntax of find_package_handle_standard_args
    set FOUND_VAR correctly

--- cmake/Modules/FindLibGit2.cmake
+++ cmake/Modules/FindLibGit2.cmake
@@ -8,6 +8,9 @@ find_path (LibGit2_INCLUDE_DIR NAMES git2.h)
 find_library (LibGit2_LIBRARIES NAMES git2)
 
 include (FindPackageHandleStandardArgs)
-find_package_handle_standard_args (LibGit2 DEFAULT_MSG LibGit2_LIBRARIES LibGit2_INCLUDE_DIR)
+find_package_handle_standard_args (LibGit2
+		FOUND_VAR LibGit2_FOUND
+		REQUIRED_VARS LibGit2_LIBRARIES LibGit2_INCLUDE_DIR
+		)
 
 mark_as_advanced (LibGit2_INCLUDE_DIR LibGit2_LIBRARIES)
