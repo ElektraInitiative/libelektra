235a45f66796ba5e6427b8f67e0a5b53951cff20
Daniel Bugl <me@omnidan.net>
2016-10-08 19:52:34 +0200

    refactor database into multiple files

---
+++ src/tools/web/clusterd/src/db/index.js
@@ -0,0 +1,8 @@
+export {
+  getInstances, createInstance, getInstance, updateInstance, deleteInstance
+} from './instances'
+
+export {
+  getClusters, createCluster, getCluster, updateCluster, deleteCluster,
+  getClusterInstances, addInstanceToCluster, removeInstanceFromCluster
+} from './clusters'
---
+++ src/tools/web/clusterd/src/db/utils.js
@@ -0,0 +1,28 @@
+export { v4 as generateId } from 'node-uuid'
+
+const ROOT_PATH = 'user/sw/elektra/web/#0/current'
+
+export const path = (path) =>
+  ROOT_PATH + '/' + path
+
+export const findById = (id) =>
+  elements => elements.find(
+    element => element.id === id
+  )
+
+export const updateById = (id, update) =>
+  elements => elements.reduce(
+    (acc, element) => {
+      if (element.id === id) {
+        acc.push(update(element))
+      } else {
+        acc.push(element)
+      }
+      return acc
+    }, []
+  )
+
+export const deleteById = (id) =>
+  elements => elements.filter(
+    element => element.id !== id
+  )
