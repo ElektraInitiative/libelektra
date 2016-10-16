7936bec225b0ffb9e1431f69917b18e6da225350
Markus Raab <elektra@markus-raab.org>
2016-10-16 11:01:30 +0200

    simplespeclang: also set require

--- src/plugins/simplespeclang/simplespeclang.cpp
+++ src/plugins/simplespeclang/simplespeclang.cpp
@@ -131,6 +131,7 @@ int unserialise (std::istream & is, ckdb::Key * errorKey, ckdb::KeySet * ks, Plu
 		}
 
 		keySetMeta (cur, "required", "yes"); // TODO bug: required key removed by spec?
+		keySetMeta (cur, "require", "yes");
 		keySetMeta (cur, "mandatory", "yes");
 
 		ckdb::ksAppendKey (ks, cur);
