c45bacb155434ee39d719bd68539e8e74ae618c8
Daniel Bugl <me@omnidan.net>
2016-10-19 18:18:01 +0200

    fix instances api

--- src/tools/web/clusterd/src/routes/instances.js
+++ src/tools/web/clusterd/src/routes/instances.js
@@ -38,41 +38,36 @@ export default function initInstanceRoutes (app) {
     )
 
   app.get('/instances/:id/version', (req, res) =>
-    getInstance(req.params.id, (err, instance) =>
-      remoteKdb.version(instance.host)
-        .then(output => successResponse(res, output))
-        .catch(err => errorResponse(res, err))
-    )
+    getInstance(req.params.id)
+      .then(instance => remoteKdb.version(instance.host))
+      .then(output => successResponse(res, output))
+      .catch(err => errorResponse(res, err))
   )
 
   app.get('/instances/:id/kdb', (req, res) =>
-    getInstance(req.params.id, (err, instance) =>
-      remoteKdb.get(instance.host)
-        .then(output => successResponse(res, output))
-        .catch(err => errorResponse(res, err))
-    )
+    getInstance(req.params.id)
+      .then(instance => remoteKdb.get(instance.host))
+      .then(output => successResponse(res, output))
+      .catch(err => errorResponse(res, err))
   )
 
   app.route('/instances/:id/kdb/*')
     .get((req, res) =>
-      getInstance(req.params.id, (err, instance) =>
-        remoteKdb.get(instance.host, req.params[0])
-          .then(output => successResponse(res, output))
-          .catch(err => errorResponse(res, err))
-      )
+      getInstance(req.params.id)
+        .then(instance => remoteKdb.get(instance.host, req.params[0]))
+        .then(output => successResponse(res, output))
+        .catch(err => errorResponse(res, err))
     )
     .put((req, res) =>
-      getInstance(req.params.id, (err, instance) =>
-        remoteKdb.set(instance.host, req.params[0], req.body)
-          .then(output => successResponse(res, output))
-          .catch(err => errorResponse(res, err))
-      )
+      getInstance(req.params.id)
+        .then(instance => remoteKdb.set(instance.host, req.params[0], req.body))
+        .then(output => successResponse(res, output))
+        .catch(err => errorResponse(res, err))
     )
     .delete((req, res) =>
-      getInstance(req.params.id, (err, instance) =>
-        remoteKdb.rm(instance.host, req.params[0])
-          .then(output => successResponse(res, output))
-          .catch(err => errorResponse(res, err))
-      )
+      getInstance(req.params.id)
+        .then(instance => remoteKdb.rm(instance.host, req.params[0]))
+        .then(output => successResponse(res, output))
+        .catch(err => errorResponse(res, err))
     )
 }
