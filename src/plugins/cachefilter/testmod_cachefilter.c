6e0334298d0e621e32acf9f1ad430fb65cb6af28
Namoshek <marvin-mall@msn.com>
2016-11-11 09:28:16 +0100

    cachefilter plugin update

--- src/plugins/cachefilter/testmod_cachefilter.c
+++ src/plugins/cachefilter/testmod_cachefilter.c
@@ -45,6 +45,22 @@ static KeySet * createTestKeysToNotCache ()
 		      keyNew ("user/tests/cachefilter/will/not/be/cached", KEY_VALUE, "not-cached", KEY_END), KS_END);
 }
 
+static KeySet * createTestKeysToNotCacheCascading ()
+{
+	return ksNew (3, 
+			keyNew ("user/tests/cachefilter/will/not/be/cached/with/directory/key1", KEY_END),
+			keyNew ("user/tests/cachefilter/will/not/be/cached/with/directory/key2", KEY_END),
+			keyNew ("user/tests/cachefilter/will/not/be/cached/with/directory/key3", KEY_END));
+}
+
+static KeySet * createTestKeysToNotCacheSiblings ()
+{
+	return ksNew (3,
+			keyNew ("user/tests/cachefilter/will/not/be/cached/for/whatever/key1", KEY_END),
+			keyNew ("user/tests/cachefilter/will/not/be/cached/for/whatever/key2", KEY_END),
+			keyNew ("user/tests/cachefilter/will/not/be/cached/for/whatever/key3", KEY_END));
+}
+
 static void test_successfulCache ()
 {
 	Key * parentKey = keyNew ("user/tests/cachefilter/will/not/be/cached", KEY_END);
@@ -233,6 +249,154 @@ static void test_successfulGetSetGetSet ()
 	PLUGIN_CLOSE ();
 }
 
+static void test_successfulGetGetGet ()
+{
+	Key * parentKey = keyNew ("user/tests/cachefilter/will/not/be/cached", KEY_END);
+	Key * parentKey2 = keyNew ("user/tests/cachefilter/will/not/be/cached/with", KEY_END);
+	Key * parentKey3 = keyNew ("user/tests/cachefilter/will/not/be/cached/with/directory", KEY_END);
+	KeySet * conf = ksNew (0, KS_END);
+	PLUGIN_OPEN ("cachefilter");
+
+	KeySet * ks;
+	KeySet * expected;
+	KeySet * testKeysCache = createTestKeysToCache ();
+	KeySet * testKeysNoCache = createTestKeysToNotCache ();
+	KeySet * testKeysNoCacheCascading = createTestKeysToNotCacheCascading ();
+	
+	// first kdbGet()
+	ks = ksNew (0, KS_END);
+	ksAppend (ks, testKeysCache);
+	ksAppend (ks, testKeysNoCache);
+	ksAppend (ks, testKeysNoCacheCascading);
+
+	succeed_if (plugin->kdbGet (plugin, ks, parentKey) >= 1, "call to kdbGet was not successful");
+	succeed_if (output_error (parentKey), "error in kdbGet");
+	succeed_if (output_warnings (parentKey), "warnings in kdbGet");
+	succeed_if (ksGetSize (ks) == 6, "wrong number of keys in result, expected 6");
+
+	expected = ksNew (0, KS_END);
+	ksAppend (expected, testKeysNoCache);
+	ksAppend (expected, testKeysNoCacheCascading);
+	compare_keyset (ks, expected);
+	ksDel (expected);
+	ksDel (ks);
+	
+	// second kdbGet(), this keys do now all come from cache
+	ks = ksNew (0, KS_END);
+
+	succeed_if (plugin->kdbGet (plugin, ks, parentKey2) >= 1, "call to kdbGet was not successful");
+	succeed_if (output_error (parentKey2), "error in kdbGet");
+	succeed_if (output_warnings (parentKey2), "warnings in kdbGet");
+	succeed_if (ksGetSize (ks) == 3, "wrong number of keys in result, expected 3");
+
+	expected = ksNew (0, KS_END);
+	ksAppend (expected, testKeysNoCacheCascading);
+	compare_keyset (ks, expected);
+	ksDel (expected);
+	ksDel (ks);
+	
+	// third kdbGet(), this keys do also all come from cache
+	ks = ksNew (0, KS_END);
+
+	succeed_if (plugin->kdbGet (plugin, ks, parentKey3) >= 1, "call to kdbGet was not successful");
+	succeed_if (output_error (parentKey3), "error in kdbGet");
+	succeed_if (output_warnings (parentKey3), "warnings in kdbGet");
+	succeed_if (ksGetSize (ks) == 3, "wrong number of keys in result, expected 3");
+
+	expected = ksNew (0, KS_END);
+	ksAppend (expected, testKeysNoCacheCascading);
+	compare_keyset (ks, expected);
+	ksDel (expected);
+	ksDel (ks);
+
+	keyDel (parentKey);
+	keyDel (parentKey2);
+	keyDel (parentKey3);
+	ksDel (testKeysCache);
+	ksDel (testKeysNoCache);
+	ksDel (testKeysNoCacheCascading);
+	
+	PLUGIN_CLOSE ();
+}
+
+static void test_successfulSiblingGets ()
+{
+	Key * parentKey = keyNew ("user/tests/cachefilter/will/not/be/cached/with", KEY_END);
+	Key * parentKey2 = keyNew ("user/tests/cachefilter/will/not/be/cached/for", KEY_END);
+	KeySet * conf = ksNew (0, KS_END);
+	PLUGIN_OPEN ("cachefilter");
+
+	KeySet * ks;
+	KeySet * expected;
+	KeySet * testKeysNoCacheCascading = createTestKeysToNotCacheCascading ();
+	KeySet * testKeysNoCacheSiblings = createTestKeysToNotCacheSiblings ();
+	
+	// first kdbGet()
+	ks = ksNew (0, KS_END);
+	ksAppend (ks, testKeysNoCacheCascading);
+
+	succeed_if (plugin->kdbGet (plugin, ks, parentKey) >= 1, "call to kdbGet was not successful");
+	succeed_if (output_error (parentKey), "error in kdbGet");
+	succeed_if (output_warnings (parentKey), "warnings in kdbGet");
+	succeed_if (ksGetSize (ks) == 3, "wrong number of keys in result, expected 3");
+
+	expected = ksNew (0, KS_END);
+	ksAppend (expected, testKeysNoCacheCascading);
+	compare_keyset (ks, expected);
+	ksDel (expected);
+	ksDel (ks);
+	
+	// second kdbGet()
+	ks = ksNew (0, KS_END);
+	ksAppend (ks, testKeysNoCacheSiblings);
+	
+	succeed_if (plugin->kdbGet (plugin, ks, parentKey2) >= 1, "call to kdbGet was not successful");
+	succeed_if (output_error (parentKey2), "error in kdbGet");
+	succeed_if (output_warnings (parentKey2), "warnings in kdbGet");
+	succeed_if (ksGetSize (ks) == 3, "wrong number of keys in result, expected 3");
+
+	expected = ksNew (0, KS_END);
+	ksAppend (expected, testKeysNoCacheSiblings);
+	compare_keyset (ks, expected);
+	ksDel (expected);
+	ksDel (ks);
+	
+	// third kdbGet()
+	ks = ksNew (0, KS_END);
+	
+	succeed_if (plugin->kdbGet (plugin, ks, parentKey) >= 1, "call to kdbGet was not successful");
+	succeed_if (output_error (parentKey), "error in kdbGet");
+	succeed_if (output_warnings (parentKey), "warnings in kdbGet");
+	succeed_if (ksGetSize (ks) == 3, "wrong number of keys in result, expected 3");
+
+	expected = ksNew (0, KS_END);
+	ksAppend (expected, testKeysNoCacheCascading);
+	compare_keyset (ks, expected);
+	ksDel (expected);
+	ksDel (ks);
+	
+	// fourth kdbGet()
+	ks = ksNew (0, KS_END);
+	
+	succeed_if (plugin->kdbGet (plugin, ks, parentKey2) >= 1, "call to kdbGet was not successful");
+	succeed_if (output_error (parentKey2), "error in kdbGet");
+	succeed_if (output_warnings (parentKey2), "warnings in kdbGet");
+	succeed_if (ksGetSize (ks) == 3, "wrong number of keys in result, expected 3");
+
+	expected = ksNew (0, KS_END);
+	ksAppend (expected, testKeysNoCacheSiblings);
+	compare_keyset (ks, expected);
+	ksDel (expected);
+	ksDel (ks);
+
+	keyDel (parentKey);
+	keyDel (parentKey2);
+	ksDel (testKeysNoCacheCascading);
+	ksDel (testKeysNoCacheSiblings);
+	
+	PLUGIN_CLOSE ();
+}
+
 int main (int argc, char ** argv)
 {
 	printf ("CACHEFILTER     TESTS\n");
@@ -243,6 +407,8 @@ int main (int argc, char ** argv)
 	test_successfulCache ();
 	test_successfulCacheLong ();
 	test_successfulGetSetGetSet ();
+	test_successfulGetGetGet ();
+	test_successfulSiblingGets ();
 
 	printf ("\ntestmod_cachefilter RESULTS: %d test(s) done. %d error(s).\n", nbTest, nbError);
 
