231ade50853922190d4c06d194874adf91a57607
Namoshek <marvin-mall@msn.com>
2016-12-01 20:48:11 +0100

    rest-backend: remove KEY_CASCADING_NAME from test keys

--- src/tools/rest-backend/tests/test_service_configengine.cpp
+++ src/tools/rest-backend/tests/test_service_configengine.cpp
@@ -46,21 +46,21 @@ TEST (kdbrestServiceConfigengineTest, TransformKeysetToJsonValue)
 
 	std::string conf_root = "/sw/elektra/restbackend/#0/current";
 
-	kdb::Key k1 (conf_root + "/nested/object/var", KEY_CASCADING_NAME, KEY_VALUE, "simple string", KEY_END);
-	kdb::Key k2 (conf_root + "/nested/object/var2", KEY_CASCADING_NAME, KEY_VALUE, "12345", KEY_END);
-	kdb::Key k3 (conf_root + "/nested/object/var3", KEY_CASCADING_NAME, KEY_VALUE, "1", KEY_END);
-	kdb::Key k4 (conf_root + "/nested/object/var4", KEY_CASCADING_NAME, KEY_VALUE, "0", KEY_END);
-	kdb::Key k5 (conf_root + "/object/array/#0", KEY_CASCADING_NAME, KEY_VALUE, "first element", KEY_END);
-	kdb::Key k6 (conf_root + "/object/array/#1", KEY_CASCADING_NAME, KEY_VALUE, "second element", KEY_END);
-	kdb::Key k7 (conf_root + "/object/array2/#0/var", KEY_CASCADING_NAME, KEY_VALUE, "first var", KEY_END);
-	kdb::Key k8 (conf_root + "/object/array2/#0/var2", KEY_CASCADING_NAME, KEY_VALUE, "second var", KEY_END);
-	kdb::Key k9 (conf_root + "/object/array2/#0/var3", KEY_CASCADING_NAME, KEY_VALUE, "third var", KEY_END);
-	kdb::Key k10 (conf_root + "/object/array2/#1/var", KEY_CASCADING_NAME, KEY_VALUE, "first var", KEY_END);
-	kdb::Key k11 (conf_root + "/object/array2/#2/var", KEY_CASCADING_NAME, KEY_VALUE, "first var", KEY_END);
-	kdb::Key k12 (conf_root + "/object/array2/#2/var2", KEY_CASCADING_NAME, KEY_VALUE, "54321", KEY_END);
-	kdb::Key k13 (conf_root + "/object/array3/#0/var/#0", KEY_CASCADING_NAME, KEY_VALUE, "arr0-in-arr0", KEY_END);
-	kdb::Key k14 (conf_root + "/object/array3/#0/var/#1", KEY_CASCADING_NAME, KEY_VALUE, "arr1-in-arr0", KEY_END);
-	kdb::Key k15 (conf_root + "/object/array3/#1/var/#0", KEY_CASCADING_NAME, KEY_VALUE, "arr0-in-arr1", KEY_END);
+	kdb::Key k1 (conf_root + "/nested/object/var", KEY_VALUE, "simple string", KEY_END);
+	kdb::Key k2 (conf_root + "/nested/object/var2", KEY_VALUE, "12345", KEY_END);
+	kdb::Key k3 (conf_root + "/nested/object/var3", KEY_VALUE, "1", KEY_END);
+	kdb::Key k4 (conf_root + "/nested/object/var4", KEY_VALUE, "0", KEY_END);
+	kdb::Key k5 (conf_root + "/object/array/#0", KEY_VALUE, "first element", KEY_END);
+	kdb::Key k6 (conf_root + "/object/array/#1", KEY_VALUE, "second element", KEY_END);
+	kdb::Key k7 (conf_root + "/object/array2/#0/var", KEY_VALUE, "first var", KEY_END);
+	kdb::Key k8 (conf_root + "/object/array2/#0/var2", KEY_VALUE, "second var", KEY_END);
+	kdb::Key k9 (conf_root + "/object/array2/#0/var3", KEY_VALUE, "third var", KEY_END);
+	kdb::Key k10 (conf_root + "/object/array2/#1/var", KEY_VALUE, "first var", KEY_END);
+	kdb::Key k11 (conf_root + "/object/array2/#2/var", KEY_VALUE, "first var", KEY_END);
+	kdb::Key k12 (conf_root + "/object/array2/#2/var2", KEY_VALUE, "54321", KEY_END);
+	kdb::Key k13 (conf_root + "/object/array3/#0/var/#0", KEY_VALUE, "arr0-in-arr0", KEY_END);
+	kdb::Key k14 (conf_root + "/object/array3/#0/var/#1", KEY_VALUE, "arr1-in-arr0", KEY_END);
+	kdb::Key k15 (conf_root + "/object/array3/#1/var/#0", KEY_VALUE, "arr0-in-arr1", KEY_END);
 
 	kdb::KeySet testKeySet (15, *k1, *k2, *k3, *k4, *k5, *k6, *k7, *k8, *k9, *k10, *k11, *k12, *k13, *k14, *k15, KS_END);
 
