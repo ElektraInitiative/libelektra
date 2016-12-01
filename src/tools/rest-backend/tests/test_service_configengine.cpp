fa02bb85f28d1bd44b6d62e2cc672a21e95a8d25
Namoshek <marvin-mall@msn.com>
2016-12-01 21:37:52 +0100

    rest-backend: add more tests for keyset transformation

--- src/tools/rest-backend/tests/test_service_configengine.cpp
+++ src/tools/rest-backend/tests/test_service_configengine.cpp
@@ -46,23 +46,21 @@ TEST (kdbrestServiceConfigengineTest, TransformKeysetToJsonValue)
 
 	std::string conf_root = "/sw/elektra/restbackend/#0/current";
 
-	kdb::Key k1 (conf_root + "/nested/object/var", KEY_VALUE, "simple string", KEY_END);
-	kdb::Key k2 (conf_root + "/nested/object/var2", KEY_VALUE, "12345", KEY_END);
-	kdb::Key k3 (conf_root + "/nested/object/var3", KEY_VALUE, "1", KEY_END);
-	kdb::Key k4 (conf_root + "/nested/object/var4", KEY_VALUE, "0", KEY_END);
-	kdb::Key k5 (conf_root + "/object/array/#0", KEY_VALUE, "first element", KEY_END);
-	kdb::Key k6 (conf_root + "/object/array/#1", KEY_VALUE, "second element", KEY_END);
-	kdb::Key k7 (conf_root + "/object/array2/#0/var", KEY_VALUE, "first var", KEY_END);
-	kdb::Key k8 (conf_root + "/object/array2/#0/var2", KEY_VALUE, "second var", KEY_END);
-	kdb::Key k9 (conf_root + "/object/array2/#0/var3", KEY_VALUE, "third var", KEY_END);
-	kdb::Key k10 (conf_root + "/object/array2/#1/var", KEY_VALUE, "first var", KEY_END);
-	kdb::Key k11 (conf_root + "/object/array2/#2/var", KEY_VALUE, "first var", KEY_END);
-	kdb::Key k12 (conf_root + "/object/array2/#2/var2", KEY_VALUE, "54321", KEY_END);
-	kdb::Key k13 (conf_root + "/object/array3/#0/var/#0", KEY_VALUE, "arr0-in-arr0", KEY_END);
-	kdb::Key k14 (conf_root + "/object/array3/#0/var/#1", KEY_VALUE, "arr1-in-arr0", KEY_END);
-	kdb::Key k15 (conf_root + "/object/array3/#1/var/#0", KEY_VALUE, "arr0-in-arr1", KEY_END);
-
-	kdb::KeySet testKeySet (15, *k1, *k2, *k3, *k4, *k5, *k6, *k7, *k8, *k9, *k10, *k11, *k12, *k13, *k14, *k15, KS_END);
+	kdb::KeySet testKeySet (15, *kdb::Key (conf_root + "/nested/object/var", KEY_VALUE, "simple string", KEY_END),
+				*kdb::Key (conf_root + "/nested/object/var2", KEY_VALUE, "12345", KEY_END),
+				*kdb::Key (conf_root + "/nested/object/var3", KEY_VALUE, "1", KEY_END),
+				*kdb::Key (conf_root + "/nested/object/var4", KEY_VALUE, "0", KEY_END),
+				*kdb::Key (conf_root + "/object/array/#0", KEY_VALUE, "first element", KEY_END),
+				*kdb::Key (conf_root + "/object/array/#1", KEY_VALUE, "second element", KEY_END),
+				*kdb::Key (conf_root + "/object/array2/#0/var", KEY_VALUE, "first var", KEY_END),
+				*kdb::Key (conf_root + "/object/array2/#0/var2", KEY_VALUE, "second var", KEY_END),
+				*kdb::Key (conf_root + "/object/array2/#0/var3", KEY_VALUE, "third var", KEY_END),
+				*kdb::Key (conf_root + "/object/array2/#1/var", KEY_VALUE, "first var", KEY_END),
+				*kdb::Key (conf_root + "/object/array2/#2/var", KEY_VALUE, "first var", KEY_END),
+				*kdb::Key (conf_root + "/object/array2/#2/var2", KEY_VALUE, "54321", KEY_END),
+				*kdb::Key (conf_root + "/object/array3/#0/var/#0", KEY_VALUE, "arr0-in-arr0", KEY_END),
+				*kdb::Key (conf_root + "/object/array3/#0/var/#1", KEY_VALUE, "arr1-in-arr0", KEY_END),
+				*kdb::Key (conf_root + "/object/array3/#1/var/#0", KEY_VALUE, "arr0-in-arr1", KEY_END), KS_END);
 
 	cppcms::json::value expected;
 	expected["nested"]["object"]["var"] = "simple string";
@@ -82,6 +80,119 @@ TEST (kdbrestServiceConfigengineTest, TransformKeysetToJsonValue)
 	expected["object"]["array3"][1]["var"][0] = "arr0-in-arr1";
 
 	cppcms::json::value result = kdbrest::service::ConfigEngine::instance ().transformKeysetToJsonValue (testKeySet, conf_root);
+
+	ASSERT_TRUE (result == expected);
+}
+
+TEST (kdbrestServiceConfigengineTest, TransformKeysetToJsonValue2)
+{
+
+	std::string conf_root = "system/sw/elektra/restbackend/#0/current";
+	std::string conf_root_cascading = "/sw/elektra/restbackend/#0/current";
+
+	kdb::KeySet testKeySet (
+		23, *kdb::Key (conf_root + "/backend/api/description/html", KEY_VALUE, "https://libelektra.org", KEY_END),
+		*kdb::Key (conf_root + "/backend/api/description/raw", KEY_VALUE,
+			   "https://raw.githubusercontent.com/ElektraInitiative/libelektra/master/doc/api_blueprints/snippet-sharing.apib",
+			   KEY_END),
+		*kdb::Key (conf_root + "/backend/jwt/encryption/secret", KEY_VALUE, "some_random_string", KEY_END),
+		*kdb::Key (conf_root + "/backend/jwt/validity", KEY_VALUE, "7200", KEY_END),
+		*kdb::Key (conf_root + "/backend/kdb/path/configs", KEY_VALUE, "dir/configs", KEY_END),
+		*kdb::Key (conf_root + "/backend/kdb/path/users", KEY_VALUE, "dir/users", KEY_END),
+		*kdb::Key (conf_root + "/backend/output/default/entry/filterby", KEY_VALUE, "all", KEY_END),
+		*kdb::Key (conf_root + "/backend/output/default/entry/sort", KEY_VALUE, "asc", KEY_END),
+		*kdb::Key (conf_root + "/backend/output/default/entry/sortby", KEY_VALUE, "key", KEY_END),
+		*kdb::Key (conf_root + "/backend/output/default/user/filterby", KEY_VALUE, "all", KEY_END),
+		*kdb::Key (conf_root + "/backend/output/default/user/sort", KEY_VALUE, "asc", KEY_END),
+		*kdb::Key (conf_root + "/backend/output/default/user/sortby", KEY_VALUE, "username", KEY_END),
+		*kdb::Key (conf_root + "/backend/permissions/entry/create", KEY_VALUE, "10", KEY_END),
+		*kdb::Key (conf_root + "/backend/permissions/entry/delete", KEY_VALUE, "50", KEY_END),
+		*kdb::Key (conf_root + "/backend/permissions/entry/edit", KEY_VALUE, "50", KEY_END),
+		*kdb::Key (conf_root + "/backend/permissions/rank/default", KEY_VALUE, "10", KEY_END),
+		*kdb::Key (conf_root + "/backend/permissions/user/delete", KEY_VALUE, "100", KEY_END),
+		*kdb::Key (conf_root + "/backend/permissions/user/edit", KEY_VALUE, "100", KEY_END),
+		*kdb::Key (conf_root + "/backend/permissions/user/view", KEY_VALUE, "100", KEY_END),
+		*kdb::Key (conf_root + "/cppcms/http/script_names/#0", KEY_VALUE, "/", KEY_END),
+		*kdb::Key (conf_root + "/cppcms/service/api", KEY_VALUE, "http", KEY_END),
+		*kdb::Key (conf_root + "/cppcms/service/ip", KEY_VALUE, "0.0.0.0", KEY_END),
+		*kdb::Key (conf_root + "/cppcms/service/port", KEY_VALUE, "8080", KEY_END), KS_END);
+
+	cppcms::json::value expected;
+	expected["backend"]["api"]["description"]["html"] = "https://libelektra.org";
+	expected["backend"]["api"]["description"]["raw"] =
+		"https://raw.githubusercontent.com/ElektraInitiative/libelektra/master/doc/api_blueprints/snippet-sharing.apib";
+	expected["backend"]["jwt"]["encryption"]["secret"] = "some_random_string";
+	expected["backend"]["jwt"]["validity"] = 7200;
+	expected["backend"]["kdb"]["path"]["configs"] = "dir/configs";
+	expected["backend"]["kdb"]["path"]["users"] = "dir/users";
+	expected["backend"]["output"]["default"]["entry"]["filterby"] = "all";
+	expected["backend"]["output"]["default"]["entry"]["sort"] = "asc";
+	expected["backend"]["output"]["default"]["entry"]["sortby"] = "key";
+	expected["backend"]["output"]["default"]["user"]["filterby"] = "all";
+	expected["backend"]["output"]["default"]["user"]["sort"] = "asc";
+	expected["backend"]["output"]["default"]["user"]["sortby"] = "username";
+	expected["backend"]["permissions"]["entry"]["create"] = 10;
+	expected["backend"]["permissions"]["entry"]["edit"] = 50;
+	expected["backend"]["permissions"]["entry"]["delete"] = 50;
+	expected["backend"]["permissions"]["user"]["view"] = 100;
+	expected["backend"]["permissions"]["user"]["edit"] = 100;
+	expected["backend"]["permissions"]["user"]["delete"] = 100;
+	expected["backend"]["permissions"]["rank"]["default"] = 10;
+	expected["cppcms"]["http"]["script_names"][0] = "/";
+	expected["cppcms"]["service"]["api"] = "http";
+	expected["cppcms"]["service"]["ip"] = "0.0.0.0";
+	expected["cppcms"]["service"]["port"] = 8080;
+
+	cppcms::json::value result =
+		kdbrest::service::ConfigEngine::instance ().transformKeysetToJsonValue (testKeySet, conf_root_cascading);
+
+	ASSERT_TRUE (result == expected);
+}
+
+TEST (kdbrestServiceConfigengineTest, TransformKeysetToJsonValue3)
+{
+
+	std::string conf_root_dir = "dir/sw/elektra/restbackend/#0/current";
+	std::string conf_root_proc = "proc/sw/elektra/restbackend/#0/current";
+	std::string conf_root_spec = "spec/sw/elektra/restbackend/#0/current";
+	std::string conf_root_system = "system/sw/elektra/restbackend/#0/current";
+	std::string conf_root_user = "user/sw/elektra/restbackend/#0/current";
+	std::string conf_root_cascading = "/sw/elektra/restbackend/#0/current";
+
+	kdb::KeySet testKeySet (
+		18, *kdb::Key (conf_root_spec + "/some/test/var", KEY_META, "default", "spec-default-val", KEY_END),
+		*kdb::Key (conf_root_system + "/some/test/var", KEY_VALUE, "test-var-system", KEY_END),
+		*kdb::Key (conf_root_user + "/some/test/var", KEY_VALUE, "test-var-user", KEY_END),
+		*kdb::Key (conf_root_dir + "/some/test/var", KEY_VALUE, "test-var-dir", KEY_END),
+		*kdb::Key (conf_root_proc + "/some/test/var", KEY_VALUE, "test-var-proc", KEY_END),
+
+		*kdb::Key (conf_root_spec + "/some/other/var", KEY_META, "default", "spec-default-val", KEY_END),
+		*kdb::Key (conf_root_system + "/some/other/var", KEY_END), *kdb::Key (conf_root_user + "/some/other/var", KEY_END),
+		*kdb::Key (conf_root_dir + "/some/other/var", KEY_END), *kdb::Key (conf_root_proc + "/some/other/var", KEY_END),
+
+		*kdb::Key (conf_root_spec + "/some/special/var", KEY_META, "default", "spec-default-val", KEY_END),
+		*kdb::Key (conf_root_dir + "/some/special/var", KEY_VALUE, "test-var-dir", KEY_END),
+
+		*kdb::Key (conf_root_spec + "/some/default/var", KEY_META, "default", "spec-default-val", KEY_END),
+
+		*kdb::Key (conf_root_spec + "/some/array/var/#0", KEY_META, "default", "spec-default-val", KEY_END),
+		*kdb::Key (conf_root_user + "/some/array/var/#0", KEY_VALUE, "test-var-user", KEY_END),
+		*kdb::Key (conf_root_spec + "/some/array/var/#1", KEY_META, "default", "spec-default-val", KEY_END),
+
+		*kdb::Key (conf_root_system + "/no/spec/var/", KEY_VALUE, "test-var-system", KEY_END),
+		*kdb::Key (conf_root_user + "/no/spec/var/", KEY_VALUE, "test-var-user", KEY_END), KS_END);
+
+	cppcms::json::value expected;
+	expected["some"]["test"]["var"] = "test-var-proc";
+	expected["some"]["other"]["var"] = "";
+	expected["some"]["special"]["var"] = "test-var-dir";
+	expected["some"]["default"]["var"] = "spec-default-val";
+	expected["some"]["array"]["var"][0] = "test-var-user";
+	expected["some"]["array"]["var"][1] = "spec-default-val";
+	expected["no"]["spec"]["var"] = "test-var-user";
+
+	cppcms::json::value result =
+		kdbrest::service::ConfigEngine::instance ().transformKeysetToJsonValue (testKeySet, conf_root_cascading);
 
 	ASSERT_TRUE (result == expected);
 }
