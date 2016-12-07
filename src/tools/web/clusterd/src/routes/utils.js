ddcca507a262c474059aeb81add963ccfc017129
Daniel Bugl <me@omnidan.net>
2016-12-07 15:10:37 +0100

    set application/json response header

--- src/tools/web/clusterd/src/routes/utils.js
+++ src/tools/web/clusterd/src/routes/utils.js
@@ -2,7 +2,10 @@ export const prettyprint = (obj) =>
   JSON.stringify(obj, null, 2)
 
 export const successResponse = (res, output) =>
-  output ? res.send(prettyprint(output)) : res.status(404).send()
+  output
+    ? res.type('application/json').send(prettyprint(output))
+    : res.status(404).send() // no output -> 404
 
 export const errorResponse = (res, err) =>
-  res.status(400).send(prettyprint({ status: 400, message: err.message }))
+  res.status(400).type('application/json')
+     .send(prettyprint({ message: err.message }))
