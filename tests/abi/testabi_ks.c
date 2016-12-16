570ccf6d089c9d08089fac0aafd5682aac3d4a02
Namoshek <marvin-mall@msn.com>
2016-12-16 23:59:47 +0100

    testabi_ks: another attempt to fix ASAN error

--- tests/abi/testabi_ks.c
+++ tests/abi/testabi_ks.c
@@ -1148,8 +1148,20 @@ static void test_ksLookupName ()
 	ksAppendKey (ks, keyNew ("system/named/syskey", KEY_VALUE, "syskey", KEY_END));
 	ksAppendKey (ks, keyNew ("system/sysonly/key", KEY_VALUE, "sysonlykey", KEY_END));
 	ksAppendKey (ks, keyNew ("user/named/bin", KEY_BINARY, KEY_SIZE, 10, KEY_VALUE, "binary\1\2data", KEY_END));
-	ksAppendKey (ks, keyNew ("system/named/bin", KEY_BINARY, KEY_SIZE, 8, KEY_VALUE, "sys\1bin\2", KEY_END));
-	ksAppendKey (ks, keyNew ("system/named/key", KEY_BINARY, KEY_SIZE, 6, KEY_VALUE, "syskey", KEY_END));
+	ksAppendKey (ks, keyNew ("system/named/bin", KEY_BINARY, KEY_SIZE,
+#ifndef __SANITIZE_ADDRESS__
+				 10,
+#else
+				 8,
+#endif
+				 KEY_VALUE, "sys\1bin\2", KEY_END));
+	ksAppendKey (ks, keyNew ("system/named/key", KEY_BINARY, KEY_SIZE,
+#ifndef __SANITIZE_ADDRESS__
+				 10,
+#else
+				 6,
+#endif
+				 KEY_VALUE, "syskey", KEY_END));
 	succeed_if (ksGetSize (ks) == 8, "could not append all keys");
 
 	// a positive testcase
@@ -2049,14 +2061,21 @@ static void test_ksLookupPop ()
 	ksAppendKey (ks, keyNew ("system/named/skey", KEY_VALUE, "syskey", KEY_END));
 	ksAppendKey (ks, keyNew ("system/sysonly/key", KEY_VALUE, "sysonlykey", KEY_END));
 	ksAppendKey (ks, keyNew ("user/named/bin", KEY_BINARY, KEY_SIZE, 10, KEY_VALUE, "binary\1\2data", KEY_END));
-	ksAppendKey (ks, keyNew ("system/named/bin", KEY_BINARY, KEY_SIZE, 8, KEY_VALUE, "sys\1bin\2", KEY_END));
-	ksAppendKey (ks, keyNew ("system/named/key", KEY_BINARY, KEY_SIZE, 6, KEY_VALUE, "syskey", KEY_END));
+	ksAppendKey (ks, keyNew ("system/named/bin", KEY_BINARY, KEY_SIZE,
 #ifndef __SANITIZE_ADDRESS__
-	ksAppendKey (ks, keyNew ("system/too/large/size", KEY_BINARY, KEY_SIZE, 10, KEY_VALUE, "tooshort", KEY_END));
-	succeed_if (ksGetSize (ks) == 9, "could not append all keys");
+				 10,
 #else
-	succeed_if (ksGetSize (ks) == 8, "could not append all keys");
+				 8,
 #endif
+				 KEY_VALUE, "sys\1bin\2", KEY_END));
+	ksAppendKey (ks, keyNew ("system/named/key", KEY_BINARY, KEY_SIZE,
+#ifndef __SANITIZE_ADDRESS__
+				 10,
+#else
+				 6,
+#endif
+				 KEY_VALUE, "syskey", KEY_END));
+	succeed_if (ksGetSize (ks) == 8, "could not append all keys");
 
 	// a positive testcase
 	found = ksLookupByName (ks, "user/named/key", KDB_O_POP);
