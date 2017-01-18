bd08f12e7f0a7f0b352df8f3e1bce8f210066f1c
René Schwaiger <sanssecours@f-m.fm>
2017-01-18 20:14:35 +0100

    Completions: Use comments to structure file

--- scripts/kdb.fish
+++ scripts/kdb.fish
@@ -1,3 +1,5 @@
+# -- Functions -----------------------------------------------------------------------------------------------------------------------------
+
 function __fish_kdb_no_subcommand -d 'Check if the current commandline buffer does not contain a subcommand'
     set subcommands (__fish_kdb_print_subcommands)
     for input in (commandline -opc)
@@ -11,5 +13,7 @@ end
 function __fish_kdb_print_subcommands -d 'Print a list of kdb subcommands'
     kdb list-commands $argv | awk '{if(NR>1)print}'
 end
+
+# -- Completions ---------------------------------------------------------------------------------------------------------------------------
 
 complete -c kdb -n '__fish_kdb_no_subcommand' -x -a '(__fish_kdb_print_subcommands -v)'
