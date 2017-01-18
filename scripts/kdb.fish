42116f483b3256f2bd549cf61ffaed235666065b
René Schwaiger <sanssecours@f-m.fm>
2017-01-18 15:08:52 +0100

    Completions: Use short options in Fish completions

--- scripts/kdb.fish
+++ scripts/kdb.fish
@@ -1,5 +1,5 @@
-function __fish_kdb_complete --description 'Print a list of kdb subcommands'
+function __fish_kdb_complete -d 'Print a list of kdb subcommands'
     kdb list-commands -v | awk '{if(NR>1)print}'
 end
 
-complete --command kdb --exclusive --arguments '(__fish_kdb_complete)'
+complete -c kdb -x -a '(__fish_kdb_complete)'
