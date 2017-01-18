2e6c79ac17b2ec667db78f5e28e15ef8e49589ee
René Schwaiger <sanssecours@f-m.fm>
2017-01-18 20:10:46 +0100

    Completions: Use more descriptive function name

--- scripts/kdb.fish
+++ scripts/kdb.fish
@@ -1,6 +1,6 @@
 function __fish_kdb_no_subcommand \
 -d 'Check if the current commandline buffer does not contain a subcommand'
-    set subcommands (__fish_kdb_complete)
+    set subcommands (__fish_kdb_print_subcommands)
     for input in (commandline -opc)
         if contains -- $input $subcommands
             return 1
@@ -9,8 +9,9 @@ function __fish_kdb_no_subcommand \
     return 0
 end
 
-function __fish_kdb_complete -d 'Print a list of kdb subcommands'
+function __fish_kdb_print_subcommands -d 'Print a list of kdb subcommands'
     kdb list-commands $argv | awk '{if(NR>1)print}'
 end
 
-complete -c kdb -n '__fish_kdb_no_subcommand' -x -a '(__fish_kdb_complete -v)'
+complete -c kdb -n '__fish_kdb_no_subcommand' -x \
+    -a  '(__fish_kdb_print_subcommands -v)'
