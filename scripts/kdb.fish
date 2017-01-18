816c9dae1087909fe240f2ec3fade8b205ba5ef3
René Schwaiger <sanssecours@f-m.fm>
2017-01-18 20:11:30 +0100

    Completions: Format file with `fish_indent`

--- scripts/kdb.fish
+++ scripts/kdb.fish
@@ -1,5 +1,4 @@
-function __fish_kdb_no_subcommand \
--d 'Check if the current commandline buffer does not contain a subcommand'
+function __fish_kdb_no_subcommand -d 'Check if the current commandline buffer does not contain a subcommand'
     set subcommands (__fish_kdb_print_subcommands)
     for input in (commandline -opc)
         if contains -- $input $subcommands
@@ -13,5 +12,4 @@ function __fish_kdb_print_subcommands -d 'Print a list of kdb subcommands'
     kdb list-commands $argv | awk '{if(NR>1)print}'
 end
 
-complete -c kdb -n '__fish_kdb_no_subcommand' -x \
-    -a  '(__fish_kdb_print_subcommands -v)'
+complete -c kdb -n '__fish_kdb_no_subcommand' -x -a '(__fish_kdb_print_subcommands -v)'
