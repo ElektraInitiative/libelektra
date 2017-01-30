c14a75e39e798fadf4ada956a8018f62b2ff039e
Daniel Bugl <me@omnidan.net>
2017-01-30 22:47:16 +0100

    log errors in dev mode

--- src/tools/web/clusterd/src/routes/utils.js
+++ src/tools/web/clusterd/src/routes/utils.js
@@ -6,6 +6,8 @@ export const successResponse = (res, output) =>
     ? res.type('application/json').send(prettyprint(output))
     : res.status(404).send() // no output -> 404
 
-export const errorResponse = (res, err) =>
-  res.status(400).type('application/json')
-     .send(prettyprint({ message: err.message }))
+export const errorResponse = (res, err) => {
+  if (process.env.NODE_ENV !== 'production') console.error(err)
+  return res.status(400).type('application/json')
+            .send(prettyprint({ message: err.message }))
+}
