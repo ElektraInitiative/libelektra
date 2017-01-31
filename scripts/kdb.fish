8b048a5bb7a79e2a8a47f13223e46cb3b3084567
René Schwaiger <sanssecours@me.com>
2017-01-31 16:24:25 +0100

    Completions: Generalize function
    We now use the function `__input_includes` to check if the current
    commandline buffer contains at least one value of a given list.
    
    This function is useful, since there are many situations where the Fish
    completion commands need to provide different completions if the
    command line buffer contains a certain word. For example, it does not
    make sense for Fish to suggest a subcommand if the current command line
    buffer already contains another subcommand.

--- scripts/kdb.fish
+++ scripts/kdb.fish
@@ -1,13 +1,16 @@
 # -- Functions -----------------------------------------------------------------------------------------------------------------------------
 
-function __fish_kdb_no_subcommand -d 'Check if the current commandline buffer does not contain a subcommand'
-    set subcommands (__fish_kdb_print_subcommands)
+function __input_includes -d 'Check if the current command buffer contains one of the given values'
     for input in (commandline -opc)
-        if contains -- $input $subcommands
-            return 1
+        if contains -- $input $argv
+            return 0
         end
     end
-    return 0
+    return 1
+end
+
+function __fish_kdb_no_subcommand -d 'Check if the current commandline buffer does not contain a subcommand'
+    not __input_includes (__fish_kdb_print_subcommands)
 end
 
 function __fish_kdb_print_subcommands -d 'Print a list of kdb subcommands'
