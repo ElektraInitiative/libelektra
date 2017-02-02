317c382eecc9fc823803eda9165ac4d57c721398
René Schwaiger <sanssecours@me.com>
2017-02-02 17:22:11 +0100

    Completions: Use more descriptive function names

--- scripts/kdb.fish
+++ scripts/kdb.fish
@@ -76,11 +76,11 @@ end
 # = Options =
 # ===========
 
-function __fish_kdb_subcommand_option_verbose -d 'Check if the current subcommand supports the option verbose'
+function __fish_kdb_subcommand_supports_option_verbose -d 'Check if the current subcommand supports the option verbose'
     not __fish_kdb_subcommand_includes export file getmeta global-mount gmount info mount qt-gui remount rm sget shell test vset help qt-gui
 end
 
-function __fish_kdb_subcommand_option_version -d 'Check if the current subcommand supports the option version'
+function __fish_kdb_subcommand_supports_option_version -d 'Check if the current subcommand supports the option version'
     not __fish_kdb_subcommand_includes help qt-gui
 end
 
@@ -97,5 +97,5 @@ complete -c kdb -n '__fish_kdb_needs_namespace' -x -a '(__fish_kdb_print_namespa
 # = Options =
 # ===========
 
-__fish_kdb_add_option '__fish_kdb_subcommand_option_verbose' 'verbose' 'v' 'Explain what is happening'
-__fish_kdb_add_option '__fish_kdb_subcommand_option_version' 'version' 'V' 'Print version info'
+__fish_kdb_add_option '__fish_kdb_subcommand_supports_option_verbose' 'verbose' 'v' 'Explain what is happening'
+__fish_kdb_add_option '__fish_kdb_subcommand_supports_option_version' 'version' 'V' 'Print version info'
