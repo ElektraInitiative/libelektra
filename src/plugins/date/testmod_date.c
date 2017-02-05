b955cfb262b6101bd046a951075e3c9c36e7f578
Thomas Waser <thomas.waser@libelektra.org>
2017-02-05 10:30:16 +0100

    Date: unittest variants

--- src/plugins/date/testmod_date.c
+++ src/plugins/date/testmod_date.c
@@ -7,6 +7,10 @@
  *
  */
 
+#ifdef __GNU_LIBRARY__
+#include <features.h>
+#endif
+
 #include <locale.h>
 #include <stdlib.h>
 #include <string.h>
@@ -32,7 +36,6 @@ static void testFmt (const char * date, const char * fmt, const short res)
 
 static void testIso (const char * date, const char * isoString, const short res)
 {
-	// fprintf (stderr, "validating %s with %s. expected result: %d\n", date, isoString, res);
 	Key * parentKey = keyNew ("user/tests/date", KEY_VALUE, "", KEY_END);
 	KeySet * ks = ksNew (5, keyNew ("user/tests/date/test", KEY_VALUE, date, KEY_META, "check/date", "ISO8601", KEY_META,
 					"check/date/format", isoString, KEY_END),
@@ -63,31 +66,44 @@ int main (int argc, char ** argv)
 {
 	printf ("DATE     TESTS\n");
 	printf ("==================\n\n");
-	const char *old_locale = setlocale(LC_ALL, NULL);
-	setlocale(LC_ALL, "C");
+	const char * old_locale = setlocale (LC_ALL, NULL);
+
 	init (argc, argv);
 
 	testFmt ("20:15:00", "%H:%M:%S", 1);
 	testFmt ("20:15:00", "%I:%M:%S", -1);
+#ifdef __GNU_LIBRARY__
+	setlocale (LC_ALL, "C");
 	testFmt ("Sat 17 Dec 2016 08:07:43 PM CET", "%a %d %b %Y %r %Z", 1);
+	setlocale (LC_ALL, old_locale);
+#else
+	testFmt ("Sat 17 Dec 2016 08:07:43 PM", "%a %d %b %Y %r", 1);
+#endif
 
+#ifdef __GNU_LIBRARY__
 	testIso ("2016-12-12T23:59:01Z", "datetime complete", 1);
 	testIso ("2016-12-12 23:59:01Z", "datetime complete noT", 1);
 	testIso ("2016-12-12T23:59:01Z", "datetime truncated", -1);
 	testIso ("-12-12T23:59:01Z", "datetime truncated", 1);
-	testIso ("2016-W23", "weekdate", 1);
 	testIso ("22:30+04", "utc extended", 1);
 	testIso ("22:30-04", "utc extended", 1);
+	testIso ("2016-W23", "weekdate", 1);
+#else
+	testIso ("2016-12-12T23:59:01", "datetime complete", 1);
+	testIso ("2016-12-12T23:59:01", "datetime truncated", -1);
+	testIso ("-12-12T23:59:01", "datetime truncated", 1);
+#endif
 	testIso ("2230", "timeofday extended", -1);
 	testIso ("2230", "timeofday basic", 1);
 
+	setlocale (LC_ALL, "C");
 	testRfc2822 ("Sat, 01 Mar 2016 23:59:01 +0400", 1);
 	testRfc2822 ("01 Mar 2016 23:59:01 -0400", 1);
 	testRfc2822 ("Sat, Mar 01 2016 23:59:01 +0400", -1);
 	testRfc2822 ("01 Mar 2016 23:59 +0400", 1);
 	testRfc2822 ("01 Mar 2016 01:00:59", -1);
-	
-	setlocale(LC_ALL, old_locale);
+	setlocale (LC_ALL, old_locale);
+
 	printf ("\ntestmod_date RESULTS: %d test(s) done. %d error(s).\n", nbTest, nbError);
 
 	return nbError;
