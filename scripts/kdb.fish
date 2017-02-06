29169b91e419666916f4b5640895a0ea6b65e04c
René Schwaiger <sanssecours@me.com>
2017-02-06 13:48:22 +0100

    Completions: Add namespace suggestions for `sget`

--- scripts/kdb.fish
+++ scripts/kdb.fish
@@ -368,7 +368,7 @@ end
 
 complete -c kdb -n 'not __fish_kdb_subcommand' -x -a '(__fish_kdb_print_subcommands -v)'
 
-set -l completion_function '__fish_kdb_needs_namespace complete editor export file fstab get getmeta import ls lsmeta rm set setmeta 1'
+set -l completion_function '__fish_kdb_needs_namespace complete editor export file fstab get getmeta import ls lsmeta rm set setmeta sget 1'
 complete -c kdb -n "$completion_function" -x -a '(__fish_kdb_print_namespaces)'
 complete -c kdb -n '__fish_kdb_needs_namespace cp mv 2' -x -a '(__fish_kdb_print_namespaces)'
 complete -c kdb -n '__fish_kdb_needs_namespace merge 4' -x -a '(__fish_kdb_print_namespaces)'
