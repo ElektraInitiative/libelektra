f3de2efeb02b138ac0c10ec73b4d35451f5f2827
René Schwaiger <sanssecours@me.com>
2017-02-06 14:34:15 +0100

    Completions: Add namespace suggestions for `vset`

--- scripts/kdb.fish
+++ scripts/kdb.fish
@@ -373,7 +373,7 @@ end
 
 complete -c kdb -n 'not __fish_kdb_subcommand' -x -a '(__fish_kdb_print_subcommands -v)'
 
-set -l arguments complete editor export file fstab get getmeta import ls lsmeta rm set setmeta sget smount spec-mount test umount 1
+set -l arguments complete editor export file fstab get getmeta import ls lsmeta rm set setmeta sget smount spec-mount test umount vset 1
 set -l completion_function "__fish_kdb_needs_namespace $arguments"
 complete -c kdb -n "$completion_function" -x -a '(__fish_kdb_print_namespaces)'
 complete -c kdb -n '__fish_kdb_needs_namespace cp mv 2' -x -a '(__fish_kdb_print_namespaces)'
