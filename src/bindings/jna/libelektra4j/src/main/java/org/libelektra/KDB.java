8669551cfd2ff48e76bc67086c7020eb741d59f6
e1528532 <e1528532@student.tuwien.ac.at>
2017-02-23 10:33:36 +0100

    jna-maven-fix-tests: add method for getting errors #1360

--- src/bindings/jna/libelektra4j/src/main/java/org/libelektra/KDB.java
+++ src/bindings/jna/libelektra4j/src/main/java/org/libelektra/KDB.java
@@ -17,7 +17,7 @@ public class KDB implements AutoCloseable {
 		final transient Key errorKey;
 
 		public KDBException(final Key k) {
-			super(new Throwable(String.format("failure in I/O to KDB: %s", KeyUtils.getWarnings(k))));
+			super(new Throwable(String.format("failure in I/O to KDB: %s%n%s", KeyUtils.getErrors(k), KeyUtils.getWarnings(k))));
 			errorKey = k;
 		}
 
