f6d17ee56cdd57ff61dbc48abd5dae3f768ac2a9
e1528532 <e1528532@student.tuwien.ac.at>
2017-02-23 10:33:36 +0100

    jna-maven-fix-tests: add method for getting errors #1360

--- src/bindings/jna/libelektra4j/src/main/java/org/libelektra/KeyUtils.java
+++ src/bindings/jna/libelektra4j/src/main/java/org/libelektra/KeyUtils.java
@@ -8,39 +8,63 @@ public final class KeyUtils {
 		// utility class
 	}
 
-	public static String getWarnings(final Key errorKey) {
-		final StringBuilder warningsBuilder = new StringBuilder();
+	public static String getErrors(final Key key) {
+		final StringBuilder sb = new StringBuilder();
 
 		try {
-			final Key warningsKey = errorKey.getMeta("warnings");
-			if (warningsKey.isNull()) {
+			if (key.getMeta("error").isNull()) {
 				// no errors were issued
-				return warningsBuilder.toString();
+				return sb.toString();
 			}
+
+			sb.append(String.format("Sorry, the error #%s occurred!%n", key.getMeta("error/number").getString()));
+			sb.append(String.format("Description: %s%n", key.getMeta("error/description").getString()));
+			sb.append(String.format("Ingroup: %s%n", key.getMeta("error/ingroup").getString()));
+			sb.append(String.format("Module: %s%n", key.getMeta("error/module").getString()));
+			sb.append(String.format("At: %s:%s%n", key.getMeta("error/file").getString(), key.getMeta("error/line").getString()));
+			sb.append(String.format("Reason: %s%n", key.getMeta("error/reason").getString()));
+			sb.append(String.format("Mountpoint: %s%n", key.getMeta("error/mountpoint").getString()));
+			sb.append(String.format("Configfile: %s%n", key.getMeta("error/configfile").getString()));
+		} catch (final KeyTypeConversion ex) {
+			sb.append(String.format("Error metadata is not set correctly by a plugin: %s%n", ex.getMessage()));
+		}
+
+		return sb.toString();
+	}
+
+	public static String getWarnings(final Key key) {
+		final StringBuilder sb = new StringBuilder();
+
+		try {
+			final Key warningsKey = key.getMeta("warnings");
+			if (warningsKey.isNull()) {
+				// no warnings were issued
+				return sb.toString();
+			}
+
 			final int nr = warningsKey.getInteger();
-			if (nr == 1) {
-				warningsBuilder.append("1 Warning was issued:\n");
+			if (nr == 0) {
+				sb.append("1 Warning was issued:\n");
 			} else {
-				warningsBuilder.append(nr + 1);
-				warningsBuilder.append(" Warnings were issued:\n");
+				sb.append(nr + 1).append(" Warnings were issued:\n");
 			}
 
 			for (int i = 0; i <= nr; i++) {
 				final String warningKeyName = String.format("warnings/#%02d", i);
-				warningsBuilder.append(String.format(" Warning number: %s%n", errorKey.getMeta(warningKeyName + "/number").getString()));
-				warningsBuilder.append(String.format("\tDescription: %s%n", errorKey.getMeta(warningKeyName + "/description").getString()));
-				warningsBuilder.append(String.format("\tIngroup: %s%n", errorKey.getMeta(warningKeyName + "/ingroup").getString()));
-				warningsBuilder.append(String.format("\tModule: %s%n", errorKey.getMeta(warningKeyName + "/module").getString()));
-				warningsBuilder.append(String.format("\tAt: %s:%s%n", errorKey.getMeta(warningKeyName + "/file").getString(),
-						errorKey.getMeta(warningKeyName + "/line").getString()));
-				warningsBuilder.append(String.format("\tReason: %s%n", errorKey.getMeta(warningKeyName + "/reason").getString()));
-				warningsBuilder.append(String.format("\tMountpoint: %s%n", errorKey.getMeta(warningKeyName + "/mountpoint").getString()));
-				warningsBuilder.append(String.format("\tConfigfile: %s%n", errorKey.getMeta(warningKeyName + "/configfile").getString()));
+				sb.append(String.format(" Warning number: %s%n", key.getMeta(warningKeyName + "/number").getString()));
+				sb.append(String.format("\tDescription: %s%n", key.getMeta(warningKeyName + "/description").getString()));
+				sb.append(String.format("\tIngroup: %s%n", key.getMeta(warningKeyName + "/ingroup").getString()));
+				sb.append(String.format("\tModule: %s%n", key.getMeta(warningKeyName + "/module").getString()));
+				sb.append(String.format("\tAt: %s:%s%n", key.getMeta(warningKeyName + "/file").getString(),
+						key.getMeta(warningKeyName + "/line").getString()));
+				sb.append(String.format("\tReason: %s%n", key.getMeta(warningKeyName + "/reason").getString()));
+				sb.append(String.format("\tMountpoint: %s%n", key.getMeta(warningKeyName + "/mountpoint").getString()));
+				sb.append(String.format("\tConfigfile: %s%n", key.getMeta(warningKeyName + "/configfile").getString()));
 			}
 		} catch (final KeyTypeConversion ex) {
-			warningsBuilder.append("Warnings metadata not set correctly by a plugin: " + ex.getMessage());
+			sb.append(String.format("Warnings metadata not set correctly by a plugin: %s%n", ex.getMessage()));
 		}
-		return warningsBuilder.toString();
+		return sb.toString();
 	}
 
 }
