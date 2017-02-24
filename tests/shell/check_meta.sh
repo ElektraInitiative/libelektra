4a5cf5525dfbd8d94f89e7c8c4ee6e794ac01bef
Markus Raab <elektra@markus-raab.org>
2017-02-24 23:38:37 +0100

    tests: tabify

--- tests/shell/check_meta.sh
+++ tests/shell/check_meta.sh
@@ -10,32 +10,33 @@ META_FILE="@CMAKE_SOURCE_DIR@/doc/METADATA.ini"
 for PLUGIN in `ls "$PLUGINS_DIR"`;
 do
 
-    if [ ! -d "${PLUGINS_DIR}/${PLUGIN}" ];
-    then
+	if [ ! -d "${PLUGINS_DIR}/${PLUGIN}" ];
+	then
 	continue
-    fi
-    
-    README="${PLUGINS_DIR}/${PLUGIN}/README.md"
-    PLUGIN_META=$(grep -Eo "infos/metadata(.*)" "$README" |cut -d '=' -f2)
-    for META in $PLUGIN_META;
-    do
-	grep -Eq "^\\[${META}\\]$" "$META_FILE"
-        succeed_if "Metadata $META of plugin $PLUGIN not present in METADATA.ini"
-    done
-    USED_BY=$(awk "/usedby\\/plugin= ([^\\n]*)${PLUGIN}( |\\n)/" RS= "$META_FILE")
-    USED_BY_META=$(echo "$USED_BY" | grep -Eo "^\\[.*\\]$")
+	fi
+	
+	README="${PLUGINS_DIR}/${PLUGIN}/README.md"
+	PLUGIN_META=$(grep -Eo "infos/metadata(.*)" "$README" |cut -d '=' -f2)
+	for META in $PLUGIN_META;
+	do
+		grep -Eq "^\\[${META}\\]$" "$META_FILE"
+		succeed_if "Metadata $META of plugin $PLUGIN not present in METADATA.ini"
+	done
 
-    OLD_IFS="$IFS"
-    IFS="$(printf '\n+')"
+	USED_BY=$(awk "/usedby\\/plugin= ([^\\n]*)${PLUGIN}( |\\n)/" RS= "$META_FILE")
+	USED_BY_META=$(echo "$USED_BY" | grep -Eo "^\\[.*\\]$")
 
-    for META in $USED_BY_META;
-    do
-	STRIPPED_META=$(echo "$META" | sed 's/\[//g' | sed 's/\]//g')
-	grep -Eq "infos/metadata(.*)${STRIPPED_META}" "$README"
-	succeed_if "$STRIPPED_META should be used by $PLUGIN, but not present in ${PLUGIN}/README.md infos/metadata"
-    done
+	OLD_IFS="$IFS"
+	IFS="$(printf '\n+')"
 
-    IFS="$OLD_IFS"
+	for META in $USED_BY_META;
+	do
+		STRIPPED_META=$(echo "$META" | sed 's/\[//g' | sed 's/\]//g')
+		grep -Eq "infos/metadata(.*)${STRIPPED_META}" "$README"
+		succeed_if "$STRIPPED_META should be used by $PLUGIN, but not present in ${PLUGIN}/README.md infos/metadata"
+	done
+
+	IFS="$OLD_IFS"
 done
 
 end_script
