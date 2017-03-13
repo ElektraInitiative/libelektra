6d9734be39c20452c1d6f321cdaf32703d14f984
Dominik Hofer <me@dominikhofer.com>
2017-03-13 02:11:28 +0100

    Elektra API interface, elektraGetString and elektraGetLongLong implementation

---
+++ src/include/elektra.h
@@ -0,0 +1,56 @@
+/**
+ * @file
+ *
+ * @brief Elektra High Level API.
+ *
+ * @copyright BSD License (see doc/LICENSE.md or http://www.libelektra.org)
+ */
+
+#ifndef ELEKTRA_H
+#define ELEKTRA_H
+
+#include "kdb.h"
+#include "kdbhelper.h"
+#include "kdbtypes.h"
+
+struct _ElektraError
+{
+    int errorNr;
+    const char * msg;
+};
+
+typedef struct _ElektraError ElektraError;
+
+struct _Elektra
+{
+    KDB * kdb;
+    KeySet * config;
+    Key * parentKey;
+
+    ElektraError * error;
+};
+
+typedef struct _Elektra Elektra;
+
+Elektra * elektraOpen (const char * application);
+void elektraClose (Elektra * elektra);
+
+kdb_boolean_t elektraHasError (const Elektra * elektra);
+const char * elektraErrorMessage (const Elektra * elektra);
+void elektraClearError (Elektra * elektra);
+
+const char * elektraGetString (Elektra * elektra, const char * name);
+kdb_boolean_t elektraGetBoolean (Elektra * elektra, const char * name);
+kdb_char_t elektraGetChar (Elektra * elektra, const char * name);
+kdb_octet_t elektraGetOctet (Elektra * elektra, const char * name);
+kdb_short_t elektraGetShort (Elektra * elektra, const char * name);
+kdb_unsigned_short_t elektraGetUnsignedShort (Elektra * elektra, const char * name);
+kdb_long_t elektraGetLong (Elektra * elektra, const char * name);
+kdb_unsigned_long_t elektraGetUnsignedLong (Elektra * elektra, const char * name);
+kdb_long_long_t elektraGetLongLong (Elektra * elektra, const char * name);
+kdb_unsigned_long_long_t elektraGetUnsignedLongLong (Elektra * elektra, const char * name);
+kdb_float_t elektraGetFloat (Elektra * elektra, const char * name);
+kdb_double_t elektraGetDouble (Elektra * elektra, const char * name);
+kdb_long_double_t elektraGetLongDouble (Elektra * elektra, const char * name);
+
+#endif //ELEKTRA_H
