360ff5d33df0690c52ac7a12cc7b8e068463ac01
Markus Raab <elektra@markus-raab.org>
2017-04-04 14:28:40 +0200

    resolver: reformat assert and also output handle

--- src/plugins/resolver/resolver.c
+++ src/plugins/resolver/resolver.c
@@ -75,7 +75,7 @@ static void resolverInit (resolverHandle * p, const char * path)
 static resolverHandle * elektraGetResolverHandle (Plugin * handle, Key * parentKey)
 {
 	resolverHandles * pks = elektraPluginGetData (handle);
-	ELEKTRA_ASSERT (pks != NULL, "Unable to retrieve plugin data for %s", keyName(parentKey));
+	ELEKTRA_ASSERT (pks != NULL, "Unable to retrieve plugin data for handle %p with parentKey %s", handle, keyName (parentKey));
 
 	switch (keyGetNamespace (parentKey))
 	{
