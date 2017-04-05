20ac24946dc0d1503135a56c295066ed1272cd2f
Thomas Waser <thomas.waser@libelektra.org>
2017-04-05 16:09:46 +0200

    revert kdbutiliy.h

--- src/include/kdbutility.h
+++ src/include/kdbutility.h
@@ -1,8 +1,6 @@
 #ifndef KDBUTILITY_H
 #define KDBUTILITY_H
 
-#include <kdb.h>
-
 #ifdef __cplusplus
 namespace ckdb
 {
@@ -14,11 +12,6 @@ char * elektraLskip (char const * const keyname);
 char * elektraRstrip (char * const start, char ** end);
 char * elektraStrip (char * text);
 char * elektraReplace (char const * const text, char const * const pattern, char const * replacement);
-
-
-/* Resolver helper */
-
-Key * elektraResolveFilename(const char *, const char *, Key *, elektraNamespace);
 
 #ifdef __cplusplus
 }
