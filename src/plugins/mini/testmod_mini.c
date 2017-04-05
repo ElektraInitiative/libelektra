f1673db300b4dde99b3df2d04f31e05d676a7051
René Schwaiger <sanssecours@me.com>
2017-04-05 15:02:06 +0200

    mINI: Extend test for configuration write back

--- src/plugins/mini/testmod_mini.c
+++ src/plugins/mini/testmod_mini.c
@@ -86,17 +86,27 @@ static void test_set_simple ()
 	printf ("• Write configuration data\n");
 
 	char const * const fileName = "Examples/write.ini";
+	char const * const prefix = "user/mini/tests/write";
 
-	Key * parentKey = keyNew ("user/mini/tests/write", KEY_VALUE, elektraFilename (), KEY_END);
+	Key * parentKey = keyNew (prefix, KEY_VALUE, elektraFilename (), KEY_END);
 	KeySet * conf = ksNew (0, KS_END);
 	PLUGIN_OPEN ("mini");
 
-	KeySet * keySet = ksNew (10, keyNew ("user/mini/tests/write/key", KEY_VALUE, "value", KEY_END), KS_END);
+	char keyValues[][2][50] = { { "key", "value" }, { "space", "wide open	 spaces" } };
+	char text[MAX_LENGTH_TEXT];
+	KeySet * keySet = ksNew (0, KS_END);
+	for (size_t pair = 0; pair < sizeof (keyValues) / sizeof (keyValues[0]); pair++)
+	{
+		char * name = keyValues[pair][0];
+		char * value = keyValues[pair][1];
+		snprintf (text, MAX_LENGTH_TEXT, "%s/%s", prefix, name);
+		ksAppendKey (keySet, keyNew (text, KEY_VALUE, value, KEY_END));
+	}
+
 	succeed_if (plugin->kdbSet (plugin, keySet, parentKey) == KEYSET_UNCHANGED, "Unable to write to file");
 	succeed_if (output_error (parentKey), "Received unexpected error while writing the configuration");
 	succeed_if (output_warnings (parentKey), "Received unexpected warning while writing the configuration");
 
-	char text[MAX_LENGTH_TEXT];
 	snprintf (text, MAX_LENGTH_TEXT, "Output of plugin stored in “%s” does not match the expected output stored in “%s”",
 		  keyString (parentKey), srcdir_file (fileName));
 	succeed_if (compare_line_files (srcdir_file (fileName), keyString (parentKey)), text);
