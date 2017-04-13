d44694bc69a896a05e3601a94bb6443f1589eeaf
René Schwaiger <sanssecours@me.com>
2017-04-13 17:48:44 +0200

    mINI: Fix detection of invalid key value pairs

--- src/plugins/mini/mini.c
+++ src/plugins/mini/mini.c
@@ -78,7 +78,7 @@ static inline void parseLine (char * line, size_t lineNumber, KeySet * keySet, K
 	}
 
 	char * equals = findUnescapedEquals (pair);
-	if (!equals)
+	if (*equals == '\0' || equals == pair)
 	{
 		fprintf (stderr, "Ignored line %lu since “%s” does not contain a valid key value pair\n", lineNumber, pair);
 		return;
