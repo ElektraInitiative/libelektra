23b39dfc56ab672ce21209fc44e3ece50bea18c6
René Schwaiger <sanssecours@me.com>
2017-04-14 08:32:52 +0200

    MSR: Support double quotes in standard output

--- tests/shell/shell_recorder/common.sh
+++ tests/shell/shell_recorder/common.sh
@@ -4,7 +4,7 @@ replace_newline_return () {
 
 regex_escape () {
 	sed 's/\\/\\\\/g' | sed 's/\[/\\\[/g' | sed 's/\]/\\\]/g' | sed 's/\./\\\./g' | sed 's/\*/\\\*/g' | sed 's/\?/\\\?/g' \
-		| sed 's/(/\\\(/g' | sed 's/)/\\\)/g'
+		| sed 's/(/\\\(/g' | sed 's/)/\\\)/g' |  sed 's/"/\\"/g'
 }
 
 if [ -z "@USE_CMAKE_KDB_COMMAND@" ]; then
