7240a86b3bb07e16be45bc2c5ed61c307978938f
Peter Nirschl <peter.nirschl@gmail.com>
2017-05-21 18:20:08 +0200

    fcrypt: add sign/verify feature
    See #1009 for full discussion.

--- src/plugins/fcrypt/fcrypt.c
+++ src/plugins/fcrypt/fcrypt.c
@@ -44,6 +44,7 @@ struct _fcryptState
 typedef struct _fcryptState fcryptState;
 
 #define ELEKTRA_FCRYPT_TMP_FILE_SUFFIX "XXXXXX"
+#define ELEKTRA_FCRYPT_SIGN_KEY "/fcrypt/sign"
 
 /**
  * @brief Allocates a new string holding the name of the temporary file.
@@ -112,13 +113,14 @@ static int inTestMode (KeySet * conf)
 /**
  * @brief Read number of total GPG recipient keys from the plugin configuration.
  * @param config holds the plugin configuration
+ * @param keyName holds the name of the root key to look up
  * @returns the number of GPG recipient keys.
  */
-static size_t getRecipientCount (KeySet * config)
+static size_t getRecipientCount (KeySet * config, const char * keyName)
 {
 	Key * k;
 	size_t recipientCount = 0;
-	Key * root = ksLookupByName (config, ELEKTRA_CRYPTO_PARAM_GPG_KEY, 0);
+	Key * root = ksLookupByName (config, keyName, 0);
 
 	if (!root) return 0;
 
@@ -245,25 +247,27 @@ static int fcryptGpgCallAndCleanup (Key * parentKey, KeySet * pluginConfig, char
 }
 
 /**
- * @brief encrypt the file specified at parentKey
+ * @brief encrypt or sign the file specified at parentKey
  * @param pluginConfig holds the plugin configuration
- * @pararm parentKey holds the path to the file to be encrypted. Will hold an error description in case of failure.
+ * @param parentKey holds the path to the file to be encrypted. Will hold an error description in case of failure.
  * @retval 1 on success
  * @retval -1 on error, errorKey holds an error description
  */
 static int fcryptEncrypt (KeySet * pluginConfig, Key * parentKey)
 {
-	const size_t recipientCount = getRecipientCount (pluginConfig);
-	if (recipientCount == 0)
+	const size_t recipientCount = getRecipientCount (pluginConfig, ELEKTRA_CRYPTO_PARAM_GPG_KEY);
+	const size_t signatureCount = getRecipientCount (pluginConfig, ELEKTRA_FCRYPT_SIGN_KEY);
+
+	if (recipientCount == 0 && signatureCount == 0)
 	{
-		ELEKTRA_SET_ERRORF (ELEKTRA_ERROR_CRYPTO_CONFIG_FAULT, parentKey,
-				    "Missing GPG key (specified as %s) in plugin configuration.", ELEKTRA_CRYPTO_PARAM_GPG_KEY);
+		ELEKTRA_SET_ERRORF (
+			ELEKTRA_ERROR_FCRYPT_OPERATION_MODE, parentKey,
+			"Missing GPG recipient key (specified as %s) or GPG signature key (specified as %s) in plugin configuration.",
+			ELEKTRA_CRYPTO_PARAM_GPG_KEY, ELEKTRA_FCRYPT_SIGN_KEY);
 		return -1;
 	}
 
-	const size_t testMode = inTestMode (pluginConfig);
 	int tmpFileFd = -1;
-
 	char * tmpFile = getTemporaryFileName (keyString (parentKey), &tmpFileFd);
 	if (!tmpFile)
 	{
@@ -271,8 +275,19 @@ static int fcryptEncrypt (KeySet * pluginConfig, Key * parentKey)
 		return -1;
 	}
 
+	const size_t testMode = inTestMode (pluginConfig);
+
 	// prepare argument vector for gpg call
-	int argc = 8 + (2 * recipientCount) + (2 * testMode);
+	// 7 static arguments (magic number below) are:
+	//   1. path to the binary
+	//   2. --batch
+	//   3. -o
+	//   4. path to tmp file
+	//   5. yes
+	//   6. file to be encrypted
+	//   7. NULL terminator
+	int argc = 7 + (2 * recipientCount) + (2 * signatureCount) + (2 * testMode) + (recipientCount > 0 ? 1 : 0) +
+		   (signatureCount > 0 ? 1 : 0);
 	kdb_unsigned_short_t i = 0;
 	char * argv[argc];
 	argv[i++] = NULL;
@@ -305,6 +320,29 @@ static int fcryptEncrypt (KeySet * pluginConfig, Key * parentKey)
 		}
 	}
 
+	// add signature keys
+	root = ksLookupByName (pluginConfig, ELEKTRA_FCRYPT_SIGN_KEY, 0);
+
+	// append root signature key
+	if (root && strlen (keyString (root)) > 0)
+	{
+		argv[i++] = "-u";
+		// NOTE argv[] values will not be modified, so const can be discarded safely
+		argv[i++] = (char *)keyString (root);
+	}
+
+	// append keys beneath root (fcrypt/sign/#_) as gpg signature keys
+	ksRewind (pluginConfig);
+	while ((k = ksNext (pluginConfig)) != 0)
+	{
+		if (keyIsBelow (k, root))
+		{
+			argv[i++] = "-u";
+			// NOTE argv[] values will not be modified, so const can be discarded safely
+			argv[i++] = (char *)keyString (k);
+		}
+	}
+
 	// if we are in test mode we add the trust model
 	if (testMode > 0)
 	{
@@ -313,7 +351,16 @@ static int fcryptEncrypt (KeySet * pluginConfig, Key * parentKey)
 	}
 
 	// prepare rest of the argument vector
-	argv[i++] = "-e";
+	if (recipientCount > 0)
+	{
+		// encrypt the file
+		argv[i++] = "-e";
+	}
+	if (signatureCount > 0)
+	{
+		// sign the file
+		argv[i++] = "-s";
+	}
 	argv[i++] = (char *)keyString (parentKey);
 	argv[i++] = NULL;
 
@@ -327,7 +374,7 @@ static int fcryptEncrypt (KeySet * pluginConfig, Key * parentKey)
 /**
  * @brief decrypt the file specified at parentKey
  * @param pluginConfig holds the plugin configuration
- * @pararm parentKey holds the path to the file to be encrypted. Will hold an error description in case of failure.
+ * @param parentKey holds the path to the file to be encrypted. Will hold an error description in case of failure.
  * @retval 1 on success
  * @retval -1 on error, errorKey holds an error description
  */
@@ -342,6 +389,17 @@ static int fcryptDecrypt (KeySet * pluginConfig, Key * parentKey)
 	}
 
 	const size_t testMode = inTestMode (pluginConfig);
+
+	// prepare argument vector for gpg call
+	// 8 static arguments (magic number below) are:
+	//   1. path to the binary
+	//   2. --batch
+	//   3. -o
+	//   4. path to tmp file
+	//   5. yes
+	//   6. -d
+	//   7. file to be encrypted
+	//   8. NULL terminator
 	int argc = 8 + (2 * testMode);
 	char * argv[argc];
 	int i = 0;
@@ -351,7 +409,7 @@ static int fcryptDecrypt (KeySet * pluginConfig, Key * parentKey)
 	argv[i++] = "--yes";
 
 	// if we are in test mode we add the trust model
-	if (inTestMode (pluginConfig))
+	if (testMode)
 	{
 		argv[i++] = "--trust-model";
 		argv[i++] = "always";
@@ -363,7 +421,6 @@ static int fcryptDecrypt (KeySet * pluginConfig, Key * parentKey)
 	// safely discarding const from keyString() return value
 	argv[i++] = (char *)keyString (parentKey);
 	argv[i++] = NULL;
-
 
 	// NOTE the decryption process works like this:
 	// gpg2 --batch --yes -o tmpfile -d configFile
@@ -494,16 +551,7 @@ int ELEKTRA_PLUGIN_FUNCTION (ELEKTRA_PLUGIN_NAME, set) (Plugin * handle, KeySet 
 }
 
 /**
- * @brief Checks for the existence of the master password, that is used for encryption and decryption.
- *
- * If the master password can not be found it will be generated randomly.
- * Then it will be encrypted and stored in conf.
- *
- * If the master password can be found, it will be decrypted temporarily in order to verify its correctness.
- * conf will not be modified in this case.
- *
- * An error might occur during the password generation, encryption and decryption.
- * The error will be appended to errorKey.
+ * @brief Checks if at least one GPG recipient or at least one GPG signature key hast been provided within the plugin configuration.
  *
  * @retval 0 no changes were made to the configuration
  * @retval 1 the master password has been appended to the configuration
@@ -511,10 +559,13 @@ int ELEKTRA_PLUGIN_FUNCTION (ELEKTRA_PLUGIN_NAME, set) (Plugin * handle, KeySet 
  */
 int ELEKTRA_PLUGIN_FUNCTION (ELEKTRA_PLUGIN_NAME, checkconf) (Key * errorKey, KeySet * conf)
 {
-	if (getRecipientCount (conf) == 0)
+	const size_t recipientCount = getRecipientCount (conf, ELEKTRA_CRYPTO_PARAM_GPG_KEY);
+	const size_t signatureCount = getRecipientCount (conf, ELEKTRA_FCRYPT_SIGN_KEY);
+
+	if (recipientCount == 0 && signatureCount == 0)
 	{
 		char * errorDescription = CRYPTO_PLUGIN_FUNCTION (getMissingGpgKeyErrorText) (conf);
-		ELEKTRA_SET_ERROR (ELEKTRA_ERROR_CRYPTO_CONFIG_FAULT, errorKey, errorDescription);
+		ELEKTRA_SET_ERROR (ELEKTRA_ERROR_FCRYPT_OPERATION_MODE, errorKey, errorDescription);
 		elektraFree (errorDescription);
 		return -1;
 	}
