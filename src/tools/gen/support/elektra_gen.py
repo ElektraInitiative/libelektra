e4f7b4bd7b98c329a1d232669e9a8773d6535e29
Dominik Hofer <me@dominikhofer.com>
2017-06-01 09:43:24 +0200

    codegen: Favor gen/type over type

--- src/tools/gen/support/elektra_gen.py
+++ src/tools/gen/support/elektra_gen.py
@@ -5,21 +5,34 @@ from support.util import *
 class ElektraGenSupport(Support):
 	def tag_name(self, key):
 		if key.startswith('/'):
-			return self.tag_pretty(key[1:])
+			return self.tag_name_pretty(key[1:])
 		elif key.startswith('user/'):
-			return self.tag_pretty(key[5:])
+			return self.tag_name_pretty(key[5:])
 		elif key.startswith('system/'):
-			return self.tag_pretty(key[7:])
+			return self.tag_name_pretty(key[7:])
 		else:
 			raise Exception("invalid keyname '" + key + "'")
 
-	def tag_pretty(self, key):
+	def tag_name_pretty(self, key):
 		return "ELEKTRA_TAG_" + key.upper().replace('/','_').replace('#','')
 
 	def upcase_first_letter(self, s):
 		return s[:1].upper() + s[1:]
 
-	def tag_type(self, type):
+	def type_of(self, info):
+		if "gen/type" in info:
+			type = info["gen/type"]
+		elif "type" in info:
+			type = info["type"]
+		else:
+			raise Exception("Missing type.")
+
+		if type in ["string", "boolean", "char", "octet", "short", "unsigned_short", "long", "unsigned_long", "long_long", "unsigned_long_long", "float", "double", "long_double"]:
+			return type
+		else:
+			raise Exception("Invalid type '" + type + "'")
+
+	def generate_tag_type(self, type):
 		result = ""
 		for part in type.split('_'):
 			result += self.upcase_first_letter(part)
@@ -28,11 +41,9 @@ class ElektraGenSupport(Support):
 
 	def tag_type_of(self, info):
 		"""Return the type for given parameter"""
-		type = info["type"]
-		if type in ["string", "boolean", "char", "octet", "short", "unsigned_short", "long", "unsigned_long", "long_long", "unsigned_long_long", "float", "double", "long_double"]:
-			return self.tag_type(type)
-		else:
-			raise Exception("invalid type '" + type + "'")
+		type = self.type_of(info)
+		return self.generate_tag_type(type)
+
 
 if __name__ == "__main__":
 	import doctest
