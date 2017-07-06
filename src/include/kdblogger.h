8496df6480cf21a79b5c29ce9f741b5d87cb2d22
René Schwaiger <sanssecours@me.com>
2017-07-06 10:34:07 +0200

    Documentation: Add very basic logging tutorial

--- src/include/kdblogger.h
+++ src/include/kdblogger.h
@@ -53,11 +53,6 @@ enum ElektraLogLevel
 	ELEKTRA_LOG_LEVEL_INFO = 2,
 
 	/**
-	 * @brief Alias for the standard log level.
-	 */
-	ELEKTRA_LOG_LEVEL = ELEKTRA_LOG_LEVEL_INFO,
-
-	/**
 	 * @brief The debug log level.
 	 *
 	 * Can be used to log anything which was useful to debug
@@ -67,6 +62,11 @@ enum ElektraLogLevel
 	 * suppressions in log.c).
 	 */
 	ELEKTRA_LOG_LEVEL_DEBUG = 1,
+
+	/**
+	 * @brief Alias for the standard log level.
+	 */
+	ELEKTRA_LOG_LEVEL = ELEKTRA_LOG_LEVEL_INFO,
 };
 
 #ifdef __cplusplus
