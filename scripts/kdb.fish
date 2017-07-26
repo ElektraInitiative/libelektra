070df53b762262bac35c81020223639579a24b0d
René Schwaiger <sanssecours@me.com>
2017-07-26 20:43:42 +0200

    Completions: Add depth options for command `ls`

--- scripts/kdb.fish
+++ scripts/kdb.fish
@@ -420,12 +420,12 @@ __fish_kdb_add_option '__fish_kdb_subcommand_includes info' 'load' 'l' 'Load plu
 # --max-depth -M
 set -l description 'Specify the maximum depth of completion suggestions (unlimited by default, 1 to show only the next level), inclusive'
 set -l argument_function '__fish_kdb_print_option_depth_arguments most 1'
-__fish_kdb_add_option '__fish_kdb_subcommand_includes complete' 'max-depth' 'M' "$description" "($argument_function)"
+__fish_kdb_add_option '__fish_kdb_subcommand_includes complete ls' 'max-depth' 'M' "$description" "($argument_function)"
 
 # --min-depth -m
 set -l description 'Specify the minimum depth of completion suggestions (0 by default), exclusive'
 set -l argument_function '__fish_kdb_print_option_depth_arguments least 0'
-__fish_kdb_add_option '__fish_kdb_subcommand_includes complete' 'min-depth' 'm' "$description" "($argument_function)"
+__fish_kdb_add_option '__fish_kdb_subcommand_includes complete ls' 'min-depth' 'm' "$description" "($argument_function)"
 
 # --namespace -N
 set -l description 'Specify the namespace to use for cascading keys'
