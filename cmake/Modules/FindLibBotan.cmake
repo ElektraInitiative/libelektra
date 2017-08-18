1e78cdf42dd2b7af3154b32d43e94f253c857020
René Schwaiger <sanssecours@me.com>
2017-08-18 09:58:24 +0200

    Botan: Remove useless whitespace from CMake code

--- cmake/Modules/FindLibBotan.cmake
+++ cmake/Modules/FindLibBotan.cmake
@@ -7,7 +7,7 @@
 #  BOTAN_INCLUDE_DIRS - the path to the include files of the Botan library
 #
 # Copyright (c) 2017 Peter Nirschl <peter.nirschl@gmail.com>
-#                                                                                                                    
+#
 # Redistribution and use is allowed according to the terms of the BSD license.
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 #
@@ -18,7 +18,7 @@ if (NOT BOTAN_FOUND)
 endif ()
 
 if (BOTAN_FOUND)
-  # try to compile and link a minimal sample program against libbotan  
+  # try to compile and link a minimal sample program against libbotan
   try_compile (HAS_BOTAN_4SURE
     "${CMAKE_BINARY_DIR}"
     "${PROJECT_SOURCE_DIR}/src/plugins/crypto/compile_botan.cpp"
