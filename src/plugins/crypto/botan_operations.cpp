ElektraInitiative/libelektra

commit 5dd90d37162d912fbbbe4e546446e2677dfbd33f

crypto: add magic number and version in order to identify the encrypted payload
This measure acts as a guard for further releases, should we ever modify the format of the encrypted payload.

See #1575 for details.

src/plugins/crypto/botan_operations.cpp
@@ -214,10 +214,16 @@ int elektraCryptoBotanEncrypt (KeySet * pluginConfig, Key * k, Key * errorKey, K
 		const size_t msgLength = encryptor.remaining ();
 		if (msgLength > 0)
 		{
-			auto buffer = unique_ptr<byte[]>{ new byte[msgLength + sizeof (kdb_unsigned_long_t) + saltLen] };
+			auto buffer = unique_ptr<byte[]>{
+				new byte[msgLength + ELEKTRA_CRYPTO_MAGIC_NUMBER_LEN + sizeof (kdb_unsigned_long_t) + saltLen]
+			};
+			size_t bufferIndex = 0;
 
-			memcpy (&buffer[0], &saltLen, sizeof (kdb_unsigned_long_t));
-			memcpy (&buffer[sizeof (kdb_unsigned_long_t)], salt, saltLen);
+			memcpy (&buffer[bufferIndex], ELEKTRA_CRYPTO_MAGIC_NUMBER, ELEKTRA_CRYPTO_MAGIC_NUMBER_LEN);
+			bufferIndex += ELEKTRA_CRYPTO_MAGIC_NUMBER_LEN;
+			memcpy (&buffer[bufferIndex], &saltLen, sizeof (kdb_unsigned_long_t));
+			bufferIndex += sizeof (kdb_unsigned_long_t);
+			memcpy (&buffer[bufferIndex], salt, saltLen);
 
 			const size_t buffered = encryptor.read (&buffer[sizeof (kdb_unsigned_long_t) + saltLen], msgLength);
 			keySetBinary (k, &buffer[0], buffered + sizeof (kdb_unsigned_long_t) + saltLen);
@@ -253,8 +259,8 @@ int elektraCryptoBotanDecrypt (KeySet * pluginConfig, Key * k, Key * errorKey, K
 	saltLen += sizeof (kdb_unsigned_long_t);
 
 	// set payload pointer
-	const byte * payload = reinterpret_cast<const byte *> (keyValue (k)) + saltLen;
-	const size_t payloadLen = keyGetValueSize (k) - saltLen;
+	const byte * payload = reinterpret_cast<const byte *> (keyValue (k)) + saltLen + ELEKTRA_CRYPTO_MAGIC_NUMBER_LEN;
+	const size_t payloadLen = keyGetValueSize (k) - saltLen - ELEKTRA_CRYPTO_MAGIC_NUMBER_LEN;
 
 	try
 	{

src/error/specification
@@ -1118,3 +1118,17 @@ severity:warning
 ingroup:plugin
 macro:FCRYPT_UNLINK
 module:fcrypt
+
+number:181
+description:The provided data could not be recognized as valid cryptographic payload. The data is possibly corrupted.
+severity:error
+ingroup:plugin
+macro:CRYPTO_PAYLOAD
+module:crypto
+
+number:182
+description:The version of the cryptographic payload is not compatible with the version of the plugin.
+severity:error
+ingroup:plugin
+macro:CRYPTO_VERSION
+module:crypto

src/plugins/crypto/crypto.c
@@ -68,6 +68,40 @@ static inline int isSpecNamespace (const Key * k)
 #endif
 
 /**
+ * @brief verify the version of the cryptographic payload of the given key.
+ * @param k holds the encrypted payload.
+ * @param errorKey holds an error description if the version does not match or the format is wrong at all.
+ * @return 1 if the payload version could be verified.
+ * @return 0 otherwise.
+ */
+static int checkPayloadVersion (Key * k, Key * errorKey)
+{
+	if (keyGetValueSize (k) < ((ssize_t)ELEKTRA_CRYPTO_MAGIC_NUMBER_LEN))
+	{
+		ELEKTRA_SET_ERRORF (ELEKTRA_ERROR_CRYPTO_PAYLOAD, errorKey, "%s", keyName (k));
+		return 0; // failure
+	}
+
+	// check the magic number without the version
+	const kdb_octet_t * value = (kdb_octet_t *)keyValue (k);
+	if (memcmp (value, ELEKTRA_CRYPTO_MAGIC_NUMBER, ELEKTRA_CRYPTO_MAGIC_NUMBER_LEN - 2))
+	{
+		ELEKTRA_SET_ERRORF (ELEKTRA_ERROR_CRYPTO_PAYLOAD, errorKey, "%s", keyName (k));
+		return 0; // failure
+	}
+
+	// check the version
+	const size_t versionOffset = ELEKTRA_CRYPTO_MAGIC_NUMBER_LEN - 2;
+	if (memcmp (&value[versionOffset], ELEKTRA_CRYPTO_PAYLOAD_VERSION, 2))
+	{
+		ELEKTRA_SET_ERRORF (ELEKTRA_ERROR_CRYPTO_VERSION, errorKey, "%s", keyName (k));
+		return 0; // failure
+	}
+
+	return 1; // success
+}
+
+/**
  * @brief initialize the crypto backend
  * @retval 1 on success
  * @retval -1 on failure
@@ -257,7 +291,7 @@ error:
  * @retval 1 on success
  * @retval -1 on failure. errorKey holds an error description.
  */
-static int elektraCryptoDecrypt (Plugin * handle ELEKTRA_UNUSED, KeySet * data ELEKTRA_UNUSED, Key * errorKey ELEKTRA_UNUSED)
+static int elektraCryptoDecrypt (Plugin * handle ELEKTRA_UNUSED, KeySet * data, Key * errorKey)
 {
 	Key * k;
 	Key * masterKey = NULL;
@@ -281,6 +315,12 @@ static int elektraCryptoDecrypt (Plugin * handle ELEKTRA_UNUSED, KeySet * data E
 		if (!isMarkedForEncryption (k) || isSpecNamespace (k))
 		{
 			continue;
+		}
+
+		if (!checkPayloadVersion (k, errorKey))
+		{
+			// error has been set by checkPayloadVersion()
+			goto error;
 		}
 
 #if defined(ELEKTRA_CRYPTO_API_GCRYPT)

src/plugins/crypto/crypto.h
@@ -26,6 +26,29 @@ enum ElektraCryptoOperation
 	ELEKTRA_CRYPTO_DECRYPT = 1
 };
 
+/*
+ * A NOTE ABOUT THE CRYPTOGRAPHIC PAYLOAD
+ *
+ * Every compilation variant shall keep to the following format when writing encrypted payload to Keys.
+ *
+ * The following header is stored before the encrypted data.
+ *
+ * +----+-------------------------------------------+---------+--------+-----------------------+-----------+
+ * |    | Element                                   |  Offset | Length | Data Type             | Encrypted |
+ * +----+-------------------------------------------+---------+--------+-----------------------+-----------+
+ * |    | magic number #!crypto                     |       0 |    8 B | char                  | NO        |
+ * |    | crypto payload version                    |       8 |    2 B | char                  | NO        |
+ * | Ls | length of the salt                        |      10 |    4 B | unsigned long integer | NO        |
+ * | S  | salt                                      |      14 |   Ls B | byte                  | NO        |
+ * | T  | original data type (string, binary, null) | 14 + Ls |    1 B | byte                  | YES       |
+ * | Lc | original content length                   | 15 + Ls |    4 B | unsigned long integer | YES       |
+ * +----+-------------------------------------------+---------+--------+-----------------------+-----------+
+ *
+ */
+#define ELEKTRA_CRYPTO_PAYLOAD_VERSION "00"
+#define ELEKTRA_CRYPTO_MAGIC_NUMBER "#!crypto" ELEKTRA_CRYPTO_PAYLOAD_VERSION
+#define ELEKTRA_CRYPTO_MAGIC_NUMBER_LEN (sizeof (ELEKTRA_CRYPTO_MAGIC_NUMBER) - 1)
+
 // plugin defaults
 #define ELEKTRA_CRYPTO_DEFAULT_MASTER_PWD_LENGTH (30)
 #define ELEKTRA_CRYPTO_DEFAULT_ITERATION_COUNT (15000)

src/plugins/crypto/gcrypt_operations.c
@@ -285,6 +285,7 @@ int elektraCryptoGcryEncrypt (elektraCryptoHandle * handle, Key * k, Key * error
 		outputLen = (contentLen / ELEKTRA_CRYPTO_GCRY_BLOCKSIZE) + 2;
 	}
 	outputLen *= ELEKTRA_CRYPTO_GCRY_BLOCKSIZE;
+	outputLen += ELEKTRA_CRYPTO_MAGIC_NUMBER_LEN;
 	outputLen += sizeof (kdb_unsigned_long_t) + saltLen;
 	kdb_octet_t * output = elektraMalloc (outputLen);
 	if (!output)
@@ -294,9 +295,13 @@ int elektraCryptoGcryEncrypt (elektraCryptoHandle * handle, Key * k, Key * error
 		return -1;
 	}
 
-	// encode the salt into the crypto payload
 	kdb_octet_t * current = output;
 
+	// output of the magic number
+	memcpy (current, ELEKTRA_CRYPTO_MAGIC_NUMBER, ELEKTRA_CRYPTO_MAGIC_NUMBER_LEN);
+	current += ELEKTRA_CRYPTO_MAGIC_NUMBER_LEN;
+
+	// encode the salt into the crypto payload
 	memcpy (current, &saltLen, sizeof (kdb_unsigned_long_t));
 	current += sizeof (kdb_unsigned_long_t);
 	memcpy (current, salt, saltLen);
@@ -350,8 +355,8 @@ int elektraCryptoGcryDecrypt (elektraCryptoHandle * handle, Key * k, Key * error
 	saltLen += sizeof (kdb_unsigned_long_t);
 
 	// set payload pointer
-	const kdb_octet_t * payload = ((kdb_octet_t *)keyValue (k)) + saltLen;
-	const size_t payloadLen = keyGetValueSize (k) - saltLen;
+	const kdb_octet_t * payload = ((kdb_octet_t *)keyValue (k)) + saltLen + ELEKTRA_CRYPTO_MAGIC_NUMBER_LEN;
+	const size_t payloadLen = keyGetValueSize (k) - saltLen - ELEKTRA_CRYPTO_MAGIC_NUMBER_LEN;
 
 	// plausibility check
 	if (payloadLen % ELEKTRA_CRYPTO_GCRY_BLOCKSIZE != 0)

src/plugins/crypto/helper.c
@@ -61,9 +61,7 @@ int CRYPTO_PLUGIN_FUNCTION (getSaltFromMetakey) (Key * errorKey, Key * k, kdb_oc
 int CRYPTO_PLUGIN_FUNCTION (getSaltFromPayload) (Key * errorKey, Key * k, kdb_octet_t ** salt, kdb_unsigned_long_t * saltLen)
 {
 	static const size_t headerLen = sizeof (kdb_unsigned_long_t);
-	const ssize_t payloadLen = keyGetValueSize (k);
-	const kdb_octet_t * payload = (kdb_octet_t *)keyValue (k);
-	kdb_unsigned_long_t restoredSaltLen = 0;
+	const ssize_t payloadLen = keyGetValueSize (k) - ELEKTRA_CRYPTO_MAGIC_NUMBER_LEN;
 
 	// validate payload length
 	if ((size_t)payloadLen < sizeof (size_t) || payloadLen < 0)
@@ -73,6 +71,10 @@ int CRYPTO_PLUGIN_FUNCTION (getSaltFromPayload) (Key * errorKey, Key * k, kdb_oc
 		if (salt) *salt = NULL;
 		return -1;
 	}
+
+	const kdb_octet_t * value = (kdb_octet_t *)keyValue (k);
+	const kdb_octet_t * payload = &value[ELEKTRA_CRYPTO_MAGIC_NUMBER_LEN];
+	kdb_unsigned_long_t restoredSaltLen = 0;
 
 	// restore salt length
 	memcpy (&restoredSaltLen, payload, headerLen);

src/plugins/crypto/openssl_operations.c
@@ -304,6 +304,9 @@ int elektraCryptoOpenSSLEncrypt (elektraCryptoHandle * handle, Key * k, Key * er
 		return -1;
 	}
 
+	// write out the magic number
+	BIO_write (encrypted, ELEKTRA_CRYPTO_MAGIC_NUMBER, ELEKTRA_CRYPTO_MAGIC_NUMBER_LEN);
+
 	// encode the salt into the payload
 	BIO_write (encrypted, &saltLen, sizeof (saltLen));
 	BIO_write (encrypted, salt, saltLen);
@@ -401,8 +404,8 @@ int elektraCryptoOpenSSLDecrypt (elektraCryptoHandle * handle, Key * k, Key * er
 	saltLen += sizeof (kdb_unsigned_long_t);
 
 	// set payload pointer
-	const kdb_octet_t * payload = ((kdb_octet_t *)keyValue (k)) + saltLen;
-	const size_t payloadLen = keyGetValueSize (k) - saltLen;
+	const kdb_octet_t * payload = ((kdb_octet_t *)keyValue (k)) + saltLen + ELEKTRA_CRYPTO_MAGIC_NUMBER_LEN;
+	const size_t payloadLen = keyGetValueSize (k) - saltLen - ELEKTRA_CRYPTO_MAGIC_NUMBER_LEN;
 
 	// initialize crypto header data
 	kdb_unsigned_long_t contentLen = 0;

commit 94be29e89c23aeaa4c314703207106dcefd64dfe

crypto_botan: fix encryption
The last commit 5dd90d371 broke the encryption in crypto_botan.
The issues is related to #1575 .

src/plugins/crypto/botan_operations.cpp
@@ -224,9 +224,10 @@ int elektraCryptoBotanEncrypt (KeySet * pluginConfig, Key * k, Key * errorKey, K
 			memcpy (&buffer[bufferIndex], &saltLen, sizeof (kdb_unsigned_long_t));
 			bufferIndex += sizeof (kdb_unsigned_long_t);
 			memcpy (&buffer[bufferIndex], salt, saltLen);
+			bufferIndex += saltLen;
 
-			const size_t buffered = encryptor.read (&buffer[sizeof (kdb_unsigned_long_t) + saltLen], msgLength);
-			keySetBinary (k, &buffer[0], buffered + sizeof (kdb_unsigned_long_t) + saltLen);
+			const size_t buffered = encryptor.read (&buffer[bufferIndex], msgLength);
+			keySetBinary (k, &buffer[0], buffered + bufferIndex);
 		}
 	}
 	catch (std::exception & e)