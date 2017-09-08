16fdb639e4ddef2edf3485c1d16091eb58cd9702
Kurt Micheli <e1026558@student.tuwien.ac.at>
2017-09-08 13:19:34 +0200

    Elektra: Core: introduce ELEKTRA_BIG_ENDIAN

--- src/libs/elektra/opmphm.c
+++ src/libs/elektra/opmphm.c
@@ -464,6 +464,8 @@ uint32_t opmphmHashfunction (const void * key, size_t length, uint32_t initval)
 {
 	uint32_t a, b, c;
 	a = b = c = 0xdeadbeef + ((uint32_t)length) + initval;
+#ifndef ELEKTRA_BIG_ENDIAN
+	// little endian
 	const uint32_t * k = (const uint32_t *)key;
 	while (length > 12)
 	{
@@ -527,6 +529,58 @@ uint32_t opmphmHashfunction (const void * key, size_t length, uint32_t initval)
 	case 0:
 		return c;
 	}
+#else
+	// big endian
+	const uint8_t * k = (const uint8_t *)key;
+	while (length > 12)
+	{
+		a += k[0];
+		a += ((uint32_t)k[1]) << 8;
+		a += ((uint32_t)k[2]) << 16;
+		a += ((uint32_t)k[3]) << 24;
+		b += k[4];
+		b += ((uint32_t)k[5]) << 8;
+		b += ((uint32_t)k[6]) << 16;
+		b += ((uint32_t)k[7]) << 24;
+		c += k[8];
+		c += ((uint32_t)k[9]) << 8;
+		c += ((uint32_t)k[10]) << 16;
+		c += ((uint32_t)k[11]) << 24;
+		mix (a, b, c);
+		length -= 12;
+		k += 12;
+	}
+	switch (length)
+	{
+	case 12:
+		c += ((uint32_t)k[11]) << 24;
+	case 11:
+		c += ((uint32_t)k[10]) << 16;
+	case 10:
+		c += ((uint32_t)k[9]) << 8;
+	case 9:
+		c += k[8];
+	case 8:
+		b += ((uint32_t)k[7]) << 24;
+	case 7:
+		b += ((uint32_t)k[6]) << 16;
+	case 6:
+		b += ((uint32_t)k[5]) << 8;
+	case 5:
+		b += k[4];
+	case 4:
+		a += ((uint32_t)k[3]) << 24;
+	case 3:
+		a += ((uint32_t)k[2]) << 16;
+	case 2:
+		a += ((uint32_t)k[1]) << 8;
+	case 1:
+		a += k[0];
+		break;
+	case 0:
+		return c;
+	}
+#endif
 	OPMPHM_HASHFUNCTION_FINAL (a, b, c);
 	return c;
 }
