4699517f228daee31dbe00ebd3d0781fca2b0d7c
René Schwaiger <sanssecours@me.com>
2017-10-14 13:10:51 +0200

    Network: Merge plugin tests

--- src/plugins/ipaddr/test_ipaddr.h
+++ src/plugins/ipaddr/test_ipaddr.h
@@ -44,23 +44,29 @@ static inline void testIPAny (char const * const ip, int ret)
 
 static void testIPAll (void)
 {
+	testIPv4 ("192.168.0.1", 1);
 	testIPv4 ("192.168.1.1", 1);
+	testIPv4 ("0.0.0.0", 1);
+
+	testIPv4 ("192.168.0.x", -1);
 	testIPv4 ("300.168.1.1", -1);
 	testIPv4 ("192.168.a.1", -1);
+	testIPv4 ("localhost", -1);
 
 	testIPv6 ("2001:0db8:85a3:0000:0000:8a2e:0370:7334", 1);
 	testIPv6 ("2001:0db8:85a3:0:0:8a2e:0370:7334", 1);
 	testIPv6 ("2001:0db8:85a3::8a2e:0370:7334", 1);
-
 	testIPv6 ("::1", 1);
 	testIPv6 ("2001::7334", 1);
 	testIPv6 ("::ffff:192.0.2.128", 1);
 
+	testIPv6 (":::", -1);
 	testIPv6 ("2001:0db8:85a3:1234:0000:0000:8a2e:0370:7334", -1);
 	testIPv6 ("2001:0db8:85a3:0:0:z:0370:7334", -1);
 	testIPv6 ("0db8:85a3:0370:7334", -1);
 	testIPv6 (":0db8:85a3:0000:0000:1234:8a2e:0370:7334", -1);
 	testIPv6 ("::ffff:192.0.128", -1);
+	testIPv6 ("localhost", -1);
 
 	if (strncmp (PLUGIN_NAME, "network", sizeof ("ipaddr") - 1) == 0)
 	{
@@ -78,8 +84,11 @@ static void testIPAll (void)
 		testIPv4 ("192.168.1", -1); // Invalid
 	}
 
+	testIPAny ("::1", 1);
+	testIPAny ("192.168.0.1", 1);
+	testIPAny ("42.42.42.42", 1);
+
 	testIPAny ("::ffff:192.0.128", -1);
 	testIPAny ("1.2.3.", -1);
-	testIPAny ("::1", 1);
-	testIPAny ("42.42.42.42", 1);
+	testIPAny ("x", -1);
 }
