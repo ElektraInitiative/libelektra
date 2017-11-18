7ff67ce44bf8aa71ff9c423c53f422409e541e48
René Schwaiger <sanssecours@me.com>
2017-11-18 13:03:36 +0100

    Directory Value: Split test code

--- src/plugins/directoryvalue/testmod_directoryvalue.c
+++ src/plugins/directoryvalue/testmod_directoryvalue.c
@@ -14,27 +14,28 @@
 
 #include <tests_plugin.h>
 
-static void test_basics (void)
+#define INIT_PLUGIN(parent)                                                                                                                \
+	Key * parentKey = keyNew (parent, KEY_END);                                                                                        \
+	KeySet * conf = ksNew (0, KS_END);                                                                                                 \
+	PLUGIN_OPEN ("directoryvalue");                                                                                                    \
+	KeySet * keySet = ksNew (0, KS_END)
+
+#define CLOSE_PLUGIN()                                                                                                                     \
+	keyDel (parentKey);                                                                                                                \
+	ksDel (keySet);                                                                                                                    \
+	PLUGIN_CLOSE ()
+
+static void test_contract (void)
 #ifdef __llvm__
 	__attribute__ ((annotate ("oclint:suppress[high ncss method]")))
 #endif
 {
-	printf ("test basics\n");
+	printf ("• Retrieve plugin contract\n");
 
-	Key * parentKey = keyNew ("user/tests/directoryvalue", KEY_END);
-	KeySet * conf = ksNew (0, KS_END);
-	PLUGIN_OPEN ("directoryvalue");
-
-	KeySet * keySet = ksNew (0, KS_END);
-
-	succeed_if (plugin->kdbGet (plugin, keySet, parentKey) == ELEKTRA_PLUGIN_STATUS_NO_UPDATE, "call to kdbGet was not successful");
-	succeed_if (plugin->kdbSet (plugin, keySet, parentKey) == ELEKTRA_PLUGIN_STATUS_NO_UPDATE, "call to kdbSet was not successful");
-
-	keyDel (parentKey);
-	ksDel (keySet);
-	PLUGIN_CLOSE ();
+	INIT_PLUGIN ("system/elektra/modules/directoryvalue");
+	succeed_if (plugin->kdbGet (plugin, keySet, parentKey) == ELEKTRA_PLUGIN_STATUS_SUCCESS, "Could not retrieve plugin contract");
+	CLOSE_PLUGIN ();
 }
-
 
 int main (int argc, char ** argv)
 {
@@ -43,7 +44,7 @@ int main (int argc, char ** argv)
 
 	init (argc, argv);
 
-	test_basics ();
+	test_contract ();
 
 	print_result ("testmod_directoryvalue");
 
