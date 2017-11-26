7de1ea621168c76b3d51a8b405a1a4bbfff4406e
Markus Raab <elektra@markus-raab.org>
2017-11-26 13:26:16 +0100

    cmake: add proposal how to print deps
    see #1016

---
+++ cmake/PrintDependencies.cmake
@@ -0,0 +1,25 @@
+#
+# Print a list of Elektra's dependencies
+#
+# (currently only essential deps, excluding CMake which is needed
+# to run this script)
+#
+# == Usage ==
+#
+# On Debian/Ubuntu:
+#
+# apt-get install `cmake -DPLUGINS="ALL" -P cmake/PrintDependencies.cmake`
+#
+# On Apple:
+#
+# brew install `cmake -DPLUGINS="ALL" -P cmake/PrintDependencies.cmake`
+#
+
+execute_process (COMMAND lsb_release -is OUTPUT_VARIABLE LSB_DISTRIB)
+
+if ("${LSB_DISTRIB}" MATCHES "Ubuntu|Debian")
+	set (DEPS "build-essential")
+elseif (APPLE)
+endif ()
+
+message (${DEPS})
