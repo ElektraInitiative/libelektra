d28c5adf4438f966bd8f29871a9d3712034b22c3
Daniel Bugl <me@omnidan.net>
2017-12-18 19:14:12 +0100

    render key actions (add, remove)

--- src/tools/web/client/src/components/TreeItem/index.js
+++ src/tools/web/client/src/components/TreeItem/index.js
@@ -9,6 +9,9 @@
 import React from 'react'
 
 import TextField from 'material-ui/TextField'
+import IconButton from 'material-ui/IconButton'
+import ActionDelete from 'material-ui/svg-icons/action/delete'
+import ContentAdd from 'material-ui/svg-icons/content/add'
 
 // TODO: render various input fields here
 const renderValue = ({ value, meta }) => {
@@ -26,14 +29,45 @@ const renderValue = ({ value, meta }) => {
 
 const TreeItem = ({ data, item, inputs }) => {
   // console.log('item', { data, item, inputs })
-  return (data && data.value)
-    ? (
-        <a>
-            <b>{item.name + ': '}</b>
-            <span style={{ marginLeft: 6 }}>{renderValue(data)}</span>
-        </a>
-      )
-    : <a><b>{item.name}</b></a>
+
+  const titleStyle = { marginTop: -3 }
+
+  // TODO: allow editing value of nested items
+  const main =
+    (data && data.value && !(item.children && item.children.length > 0))
+      ? (
+          <span>
+              <b style={titleStyle}>{item.name + ': '}</b>
+              <span style={{ marginLeft: 6 }}>{renderValue(data)}</span>
+          </span>
+        )
+      : <b style={titleStyle}>{item.name}</b>
+
+  const deleteAction = (
+      <IconButton
+        style={{ width: 16, height: 16, paddingTop: '1px' }}
+        iconStyle={{ width: 14, height: 14 }}
+        onTouchTap={() => { /* TODO */ console.log('deleted', item)}}
+      >
+          <ActionDelete />
+      </IconButton>
+  )
+
+  const addAction = (
+      <IconButton
+        style={{ width: 16, height: 16, paddingTop: '1px' }}
+        iconStyle={{ width: 14, height: 14 }}
+        onTouchTap={() => { /* TODO */ console.log('creating', item)}}
+      >
+          <ContentAdd />
+      </IconButton>
+  )
+
+  return (
+      <a style={{ display: 'flex', alignItems: 'center' }}>
+          {main} <span className="actions">{addAction} {deleteAction}</span>
+      </a>
+  )
 }
 
 export default TreeItem
