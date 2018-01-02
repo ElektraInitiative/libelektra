103dc0a95b3ebfcc096a2e556b393823614fc4f0
Kurt Micheli <e1026558@student.tuwien.ac.at>
2018-01-02 12:39:38 +0100

    Elektra: Plugins: Semlock: Test: add /dev/shm mounted as tempfs check

--- src/plugins/semlock/testmod_semlock.c
+++ src/plugins/semlock/testmod_semlock.c
@@ -54,6 +54,13 @@ int main (int argc, char ** argv)
 	printf ("==================\n\n");
 
 	init (argc, argv);
+	
+	// check if /dev/shm is a mounted tempfs
+	if (system ("mount | grep /dev/shm | grep -q tmpfs"))
+	{
+		fprintf (stderr, "/dev/shm not mounted as tempfs, look in the README of semlock for more info!\n");
+		return 1;
+	}
 
 	test_OpenClose ();
 	test_GetSet ();
