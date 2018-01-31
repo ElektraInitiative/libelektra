45f8c830b02b722908ba5c8b2197bb3dadfe71d6
René Schwaiger <sanssecours@me.com>
2018-01-31 13:57:50 +0100

    YAML CPP: Update CMake code for yaml-cpp 0.6

--- cmake/Modules/Findyaml-cpp.cmake
+++ cmake/Modules/Findyaml-cpp.cmake
@@ -6,6 +6,7 @@
 #  YAML-CPP_FOUND		System has yaml-cpp
 #  YAML-CPP_INCLUDE_DIRS	The yaml-cpp include directories
 #  YAML-CPP_LIBRARIES		The libraries needed to use yaml-cpp
+#  YAML-CPP_VERSION		The version string of yaml-cpp
 #
 # This script is a modified version of the code available here:
 # 	https://cmake.org/Wiki/CMake:How_To_Find_Libraries#Writing_find_modules
@@ -22,11 +23,13 @@ find_library(YAML-CPP_LIBRARY
 	NAMES yaml-cpp
 	HINTS ${PC_YAML-CPP_LIBDIR} ${PC_YAML-CPP_LIBRARY_DIRS})
 
+set(YAML-CPP_VERSION ${PC_YAML-CPP_VERSION})
+
 include(FindPackageHandleStandardArgs)
 # Handle the QUIETLY and REQUIRED arguments and set YAML-CPP_FOUND to TRUE, if all listed variables are TRUE
-find_package_handle_standard_args(yaml-cpp DEFAULT_MSG YAML-CPP_LIBRARY YAML-CPP_INCLUDE_DIR)
+find_package_handle_standard_args(yaml-cpp REQUIRED_VARS YAML-CPP_LIBRARY YAML-CPP_INCLUDE_DIR VERSION_VAR YAML-CPP_VERSION)
 
-mark_as_advanced(YAML-CPP_INCLUDE_DIR YAML-CPP_LIBRARY)
+mark_as_advanced(YAML-CPP_INCLUDE_DIR YAML-CPP_LIBRARY YAML-CPP_VERSION)
 
 set(YAML-CPP_LIBRARIES ${YAML-CPP_LIBRARY})
 set(YAML-CPP_INCLUDE_DIRS ${YAML-CPP_INCLUDE_DIR})
