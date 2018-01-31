182064f983a5ac81919e2664fe5c019f5f62d37b
René Schwaiger <sanssecours@me.com>
2018-01-31 15:04:22 +0100

    Build: Reformat CMake code

--- cmake/Modules/Findyaml-cpp.cmake
+++ cmake/Modules/Findyaml-cpp.cmake
@@ -9,27 +9,27 @@
 #  YAML-CPP_VERSION		The version string of yaml-cpp
 #
 # This script is a modified version of the code available here:
-# 	https://cmake.org/Wiki/CMake:How_To_Find_Libraries#Writing_find_modules
+#	https://cmake.org/Wiki/CMake:How_To_Find_Libraries#Writing_find_modules
 
-find_package(PkgConfig)
-pkg_check_modules(PC_YAML-CPP QUIET yaml-cpp)
+find_package (PkgConfig)
+pkg_check_modules (PC_YAML-CPP QUIET yaml-cpp)
 
-find_path(YAML-CPP_INCLUDE_DIR
+find_path (YAML-CPP_INCLUDE_DIR
 	NAMES yaml-cpp/yaml.h
 	HINTS ${PC_YAML-CPP_INCLUDEDIR} ${PC_YAML-CPP_INCLUDE_DIRS}
 	PATH_SUFFIXES yaml-cpp)
 
-find_library(YAML-CPP_LIBRARY
+find_library (YAML-CPP_LIBRARY
 	NAMES yaml-cpp
 	HINTS ${PC_YAML-CPP_LIBDIR} ${PC_YAML-CPP_LIBRARY_DIRS})
 
-set(YAML-CPP_VERSION ${PC_YAML-CPP_VERSION})
+set (YAML-CPP_VERSION ${PC_YAML-CPP_VERSION})
 
-include(FindPackageHandleStandardArgs)
+include (FindPackageHandleStandardArgs)
 # Handle the QUIETLY and REQUIRED arguments and set YAML-CPP_FOUND to TRUE, if all listed variables are TRUE
-find_package_handle_standard_args(yaml-cpp REQUIRED_VARS YAML-CPP_LIBRARY YAML-CPP_INCLUDE_DIR VERSION_VAR YAML-CPP_VERSION)
+find_package_handle_standard_args (yaml-cpp REQUIRED_VARS YAML-CPP_LIBRARY YAML-CPP_INCLUDE_DIR VERSION_VAR YAML-CPP_VERSION)
 
-mark_as_advanced(YAML-CPP_INCLUDE_DIR YAML-CPP_LIBRARY YAML-CPP_VERSION)
+mark_as_advanced (YAML-CPP_INCLUDE_DIR YAML-CPP_LIBRARY YAML-CPP_VERSION)
 
-set(YAML-CPP_LIBRARIES ${YAML-CPP_LIBRARY})
-set(YAML-CPP_INCLUDE_DIRS ${YAML-CPP_INCLUDE_DIR})
+set (YAML-CPP_LIBRARIES ${YAML-CPP_LIBRARY})
+set (YAML-CPP_INCLUDE_DIRS ${YAML-CPP_INCLUDE_DIR})
