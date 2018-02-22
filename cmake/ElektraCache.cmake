91c3247edcd1dd5b48579911b323eca62bad9ad6
Markus Raab <elektra@markus-raab.org>
2018-02-22 20:48:07 +0100

    cmake: switch back to dump by default

--- cmake/ElektraCache.cmake
+++ cmake/ElektraCache.cmake
@@ -125,7 +125,7 @@ set (KDB_DB_INIT "elektra.ecf" CACHE STRING
 		"This configuration file will be used for bootstrapping."
 		)
 
-set (KDB_DEFAULT_STORAGE "dini" CACHE STRING
+set (KDB_DEFAULT_STORAGE "dump" CACHE STRING
 	"This storage plugin will be used initially (as default and for bootstrapping).")
 
 if (KDB_DEFAULT_STORAGE STREQUAL "storage")
