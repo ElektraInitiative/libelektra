4d159b18f967c068699f952dc629006d043da524
Daniel Bugl <me@omnidan.net>
2018-03-28 13:02:35 +0200

    build when starting via `npm start`, cmake: npm install before building

--- src/tools/web/build.sh
+++ src/tools/web/build.sh
@@ -2,9 +2,11 @@
 cd @CMAKE_INSTALL_PREFIX@/@install_directory@
 
 cd elektrad
+npm install
 npm run build
 cd ..
 
 cd client
+npm install
 npm run build
 cd ..
--- src/tools/web/run.sh
+++ src/tools/web/run.sh
@@ -2,4 +2,4 @@
 cd @CMAKE_INSTALL_PREFIX@/@install_directory@
 
 cd client
-npm start
+npm run start:prod
