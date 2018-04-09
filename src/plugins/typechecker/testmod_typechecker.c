2a4267f89c4794c96b291342f8ffcae3e2dc788d
e1528532 <e1528532@student.tuwien.ac.at>
2018-04-09 18:32:38 +0200

    typechecker-regex-prototype: prepare simple test case

--- src/plugins/typechecker/testmod_typechecker.c
+++ src/plugins/typechecker/testmod_typechecker.c
@@ -14,15 +14,88 @@
 
 #include <tests_plugin.h>
 
+/*
+#@META check/range = 0-5000
+key1 = 2500
+
+#@META check/range = 6000-7000
+#@META fallback/#1 = key1
+key2 = 6500
+
+#@META comment = implicitly they are .* aka anything, how to treat fallback inference?
+#@META fallback/#1 = key1
+#@META fallback/#2 = key2
+key3 = 
+
+#@META elektra/spec/type = RegexContains b a => Key a -> Key b -> Key a
+#@META elektra/spec/impl = fallback (Key Nothing) b = b \n fallback a _ = a
+elektra/spec/fallback/# = 
+
+#@META elektra/spec/type = RegexContains b a => Key b -> Key a -> Key b
+#@META elektra/spec/impl = override (Key Nothing) b = b \n override a _ = a
+elektra/spec/override/# = 
+
+#@META spec/type = RegexIntersects a b => Key a -> Range b -> Key (RegexIntersection a b)
+#@META spec/impl = checkRange a _ = a
+elektra/spec/check/range = b
+
+#@META elektra/spec/type = RegexIntersects a => Key a -> Key (RegexIntersection a ".")
+#@META elektra/spec/impl = checkLong a = a
+elektra/spec/check/long =
+*/
+
+#define LONG_REGEX "-[1-9]|-214748364[0-8]|-?[1-9][0-9]|-?[1-9][0-9][0-9]|-?[1-9][0-9][0-9][0-9]|-?[1-9][0-9][0-9][0-9][0-9]|-?[1-9][0-9][0-9][0-9][0-9][0-9]|-?[1-9][0-9][0-9][0-9][0-9][0-9][0-9]|-?[1-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]|-?[1-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]|-?1[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]|-?20[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]|-?21[0-3][0-9][0-9][0-9][0-9][0-9][0-9][0-9]|-?214[0-6][0-9][0-9][0-9][0-9][0-9][0-9]|-?2147[0-3][0-9][0-9][0-9][0-9][0-9]|-?21474[0-7][0-9][0-9][0-9][0-9]|-?214748[0-2][0-9][0-9][0-9]|-?2147483[0-5][0-9][0-9]|-?21474836[0-3][0-9]|[0-9]|214748364[0-7]"
+#define PARENT_KEY_NAME "user/tests/typechecker"
+#define KEY1_NAME PARENT_KEY_NAME "/key1"
+#define KEY2_NAME PARENT_KEY_NAME "/key2"
+#define KEY3_NAME PARENT_KEY_NAME "/key3"
+
 static void test_basics (void)
 {
 	printf ("test basics\n");
 
-	Key * parentKey = keyNew ("user/tests/typechecker", KEY_END);
+	Key * parentKey = keyNew (PARENT_KEY_NAME, KEY_END);
 	KeySet * conf = ksNew (0, KS_END);
 	PLUGIN_OPEN ("typechecker");
 
 	KeySet * ks = ksNew (0, KS_END);
+
+	ksAppendKey (ks, keyNew (KEY1_NAME,
+		KEY_VALUE, "2500",
+		KEY_META, "check/range", "0-5000",
+		KEY_END));
+
+	ksAppendKey (ks, keyNew (KEY2_NAME,
+		KEY_VALUE, "6500",
+		KEY_META, "check/range", "6000-7000",
+		KEY_META, "fallback/#1", KEY1_NAME,
+		KEY_END));
+
+	ksAppendKey (ks, keyNew (KEY3_NAME,
+		KEY_META, "fallback/#1", KEY1_NAME,
+		KEY_META, "fallback/#2", KEY2_NAME,
+		KEY_END));
+
+	ksAppendKey (ks, keyNew (PARENT_KEY_NAME "/elektra/spec/fallback/#",
+		KEY_META, "elektra/spec/type", "RegexContains b a => Key a -> Key b -> Key a",
+		KEY_META, "elektra/spec/impl", "fallback (Key Nothing) b = b \n fallback a _ = a",
+		KEY_END));
+
+	ksAppendKey (ks, keyNew (PARENT_KEY_NAME "/elektra/spec/override/#",
+		KEY_META, "elektra/spec/type", "RegexContains b a => Key b -> Key a -> Key b",
+		KEY_META, "elektra/spec/impl", "override (Key Nothing) b = b \n override a _ = a",
+		KEY_END));
+
+	ksAppendKey (ks, keyNew (PARENT_KEY_NAME "/elektra/spec/check/range",
+		KEY_VALUE, "b",
+		KEY_META, "elektra/spec/type", "RegexIntersects a b => Key a -> Proxy b -> Key (RegexIntersection a b)",
+		KEY_META, "elektra/spec/impl", "checkRange a _ = a",
+		KEY_END));
+
+	ksAppendKey (ks, keyNew (PARENT_KEY_NAME "/elektra/spec/check/long",
+		KEY_META, "elektra/spec/type", "RegexIntersects a \"" LONG_REGEX "\" => Key a -> Proxy b -> Key (RegexIntersection a " LONG_REGEX ")",
+		KEY_META, "elektra/spec/impl", "checkLong a = a",
+		KEY_END));
 
 	succeed_if (plugin->kdbOpen (plugin, parentKey) == ELEKTRA_PLUGIN_STATUS_SUCCESS, "call to kdbOpen was not successful");
 
