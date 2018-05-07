293064437819c8774b5aec99b6ef5b311d1888d1
René Schwaiger <sanssecours@me.com>
2018-05-08 01:10:08 +0200

    Find Lib OpenSSL: Reformat CMake code

--- cmake/Modules/FindLibOpenSSL.cmake
+++ cmake/Modules/FindLibOpenSSL.cmake
@@ -1,3 +1,4 @@
+# ~~~
 # Try to find the OpenSSL library.
 #
 # Defines:
@@ -10,29 +11,28 @@
 #
 # Redistribution and use is allowed according to the terms of the BSD license.
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
-#
+# ~~~
 
 if (NOT OPENSSL_FOUND)
-  include (FindPkgConfig)
-  pkg_search_module (OPENSSL QUIET openssl)
+	include (FindPkgConfig)
+	pkg_search_module (OPENSSL QUIET openssl)
 endif ()
 
 if (OPENSSL_FOUND)
-  # try to compile and link a minimal sample program against libcrypto
-  try_compile (HAS_OPENSSL_4SURE
-    "${CMAKE_BINARY_DIR}"
-    "${PROJECT_SOURCE_DIR}/src/plugins/crypto/compile_openssl.c"
-    CMAKE_FLAGS
-      -DINCLUDE_DIRECTORIES:STRING=${OPENSSL_INCLUDE_DIR}
-      -DLINK_LIBRARIES:PATH=${OPENSSL_LIBRARIES}
-  )
 
-  if (NOT HAS_OPENSSL_4SURE)
-    message (STATUS "OpenSSL compile/linker test failed")
-    set (OPENSSL_FOUND OFF)
-    set (OPENSSL_INCLUDE_DIR "")
-    set (OPENSSL_LIBRARIES "")
-  endif ()
+	# try to compile and link a minimal sample program against libcrypto
+	try_compile (HAS_OPENSSL_4SURE
+		     "${CMAKE_BINARY_DIR}"
+		     "${PROJECT_SOURCE_DIR}/src/plugins/crypto/compile_openssl.c"
+		     CMAKE_FLAGS -DINCLUDE_DIRECTORIES:STRING=${OPENSSL_INCLUDE_DIR}
+				 -DLINK_LIBRARIES:PATH=${OPENSSL_LIBRARIES})
 
-  unset (HAS_OPENSSL_4SURE)
+	if (NOT HAS_OPENSSL_4SURE)
+		message (STATUS "OpenSSL compile/linker test failed")
+		set (OPENSSL_FOUND OFF)
+		set (OPENSSL_INCLUDE_DIR "")
+		set (OPENSSL_LIBRARIES "")
+	endif ()
+
+	unset (HAS_OPENSSL_4SURE)
 endif ()
