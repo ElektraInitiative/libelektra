d7441fc85b47032efc9d9eb13a7caf084f0c7d97
René Schwaiger <sanssecours@me.com>
2018-05-08 01:17:09 +0200

    Find GTest: Reformat CMake code

--- cmake/Modules/FindGTest.cmake
+++ cmake/Modules/FindGTest.cmake
@@ -1,3 +1,4 @@
+# ~~~
 # This script reads the GTEST_ROOT variable, if available,
 # looking into it as base directory of the GoogleTest sources.
 #
@@ -6,27 +7,20 @@
 # Must be included only once
 #
 # GTEST_ROOT will only be honored in the very first invocation
+# ~~~
 
 if (NOT GOOGLETEST_ROOT)
 	if (NOT (GTEST_ROOT STREQUAL ""))
 		if (EXISTS ${GTEST_ROOT}/CMakeLists.txt)
-			set(GOOGLETEST_ROOT
-				"${GTEST_ROOT}"
-				CACHE INTERNAL
-				"Folder which gtest will use"
-				FORCE)
+			set (GOOGLETEST_ROOT "${GTEST_ROOT}" CACHE INTERNAL "Folder which gtest will use" FORCE)
 			message (STATUS "used external gtest below ${GTEST_ROOT}")
-		endif()
-	else()
-		set(GOOGLETEST_ROOT
-			${CMAKE_SOURCE_DIR}/tests/gtest-1.7.0
-			CACHE INTERNAL
-			"Folder which gtest will use"
-			FORCE)
-	endif()
-endif()
+		endif ()
+	else ()
+		set (GOOGLETEST_ROOT ${CMAKE_SOURCE_DIR}/tests/gtest-1.7.0 CACHE INTERNAL "Folder which gtest will use" FORCE)
+	endif ()
+endif ()
 
-add_subdirectory(${GOOGLETEST_ROOT} ${CMAKE_BINARY_DIR}/gtest)
+add_subdirectory (${GOOGLETEST_ROOT} ${CMAKE_BINARY_DIR}/gtest)
 
-set_property (TARGET gtest PROPERTY COMPILE_FLAGS "-w" )
-set_property (TARGET gtest_main PROPERTY COMPILE_FLAGS "-w" )
+set_property (TARGET gtest PROPERTY COMPILE_FLAGS "-w")
+set_property (TARGET gtest_main PROPERTY COMPILE_FLAGS "-w")
