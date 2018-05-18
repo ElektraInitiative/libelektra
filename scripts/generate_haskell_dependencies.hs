d9e5e8f3f7d54cd13a7bb67120b56402533e3d43
e1528532 <e1528532@student.tuwien.ac.at>
2018-05-18 11:35:34 +0200

    haskell-cmake-improvements: improve dependency fetching

--- scripts/generate_haskell_dependencies.hs
+++ scripts/generate_haskell_dependencies.hs
@@ -1,3 +1,13 @@
+--
+-- @file
+--
+-- @brief Reads the given cabal files and generates a cabal install
+-- command including each one of them. Used together with the script
+-- ./generate_haskell_dependencies which scans for all cabal files.
+--
+-- @copyright BSD License (see LICENSE.md or https://www.libelektra.org)
+-- 
+
 import Distribution.PackageDescription
 import Distribution.PackageDescription.Parse
 import Distribution.Verbosity
@@ -23,8 +33,14 @@ generateHaskellDependencies cabalFiles = do
   where
     generateDeps pd = let gpd = packageDescription pd
                           cl  = generateBuildDeps <$> condLibrary pd
+                          csl = gen condSubLibraries pd
+                          cfl = gen condForeignLibs pd
+                          ce  = gen condExecutables pd
+                          cts = gen condTestSuites pd
+                          cb  = gen condBenchmarks pd
                           sd  = generateSetupDeps <$> setupBuildInfo gpd
-                      in concat $ catMaybes [sd, cl]
+                      in concat . catMaybes $ [sd, cl, csl, cfl, ce, cts, cb, sd]
+                      where gen f = Just . concatMap (generateBuildDeps . snd) . f
     generateSetupDeps (SetupBuildInfo d _) = d
     generateBuildDeps (CondNode _ d _) = d
     wrap x = '\'':x ++ "'"
