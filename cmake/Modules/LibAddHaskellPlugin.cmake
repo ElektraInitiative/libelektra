90d3fcee316bd4e801b470ea5facfc1f689942e1
e1528532 <e1528532@student.tuwien.ac.at>
2018-05-18 13:27:37 +0200

    haskell-cmake-improvements: always add the bindings every plugin needs them

--- cmake/Modules/LibAddHaskellPlugin.cmake
+++ cmake/Modules/LibAddHaskellPlugin.cmake
@@ -336,7 +336,8 @@ macro (configure_haskell_sandbox)
 			       ${ARGN})
 
 	add_custom_command (OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/cabal.sandbox.config"
-		COMMAND ${CABAL_EXECUTABLE} sandbox init --sandbox ${ARG_SHARED_SANDBOX} -v0)
+			    COMMAND ${CABAL_EXECUTABLE} sandbox init --sandbox ${ARG_SHARED_SANDBOX} -v0
+			    COMMAND ${CABAL_EXECUTABLE} sandbox add-source "$CMAKE_SOURCE_DIR/src/bindings/haskell")
 
 	if (ARG_SANDBOX_ADD_SOURCES)
 		foreach (SANDBOX_ADD_SOURCE ${ARG_SANDBOX_ADD_SOURCES}) # to avoid generating files in the source folders, copy over the
