5b3e5c59b96ec8cd9631ca9ce0b8748d4f2705d7
e1528532 <e1528532@student.tuwien.ac.at>
2018-05-22 12:49:24 +0200

    haskell-cmake-improvements: fix issue with markdown tests and custom sandbox locations

--- cmake/Modules/LibAddHaskellPlugin.cmake
+++ cmake/Modules/LibAddHaskellPlugin.cmake
@@ -154,7 +154,7 @@ macro (add_haskell_plugin target)
 									configure_file (${CABAL_CUSTOM_SETUP_FILE}
 											"${CMAKE_CURRENT_BINARY_DIR}/Setup.hs"
 											@ONLY)
-									
+
 									configure_haskell_sandbox (SHARED_SANDBOX
 												   ${HASKELL_SHARED_SANDBOX}
 												   SANDBOX_ADD_SOURCES
@@ -368,7 +368,7 @@ macro (configure_haskell_sandbox)
 	# Therefore we use the --offline flag to make the build guaranteed independent
 	# Furthermore we implicitly add the haskell bindings as they are always required
 	add_custom_command (OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/cabal.sandbox.config"
-				COMMAND ${CABAL_EXECUTABLE} sandbox add-source ${CMAKE_BINARY_DIR}/src/bindings/haskell/ -v0
+			    COMMAND ${CABAL_EXECUTABLE} sandbox add-source ${CMAKE_BINARY_DIR}/src/bindings/haskell/ -v0
 			    COMMAND ${CABAL_EXECUTABLE} install ${CABAL_OPTS} --only-dependencies --avoid-reinstalls --offline -v0 || true
 			    APPEND)
 	add_custom_target (${HASKELL_ADD_SOURCES_TARGET} ALL DEPENDS "${CMAKE_CURRENT_BINARY_DIR}/cabal.sandbox.config")
