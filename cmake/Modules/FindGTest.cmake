386c013570fcc00aa0e102ff196cb1c97b9afa2c
derwinlu <derwinlu+git@gmail.com>
2018-05-28 18:07:44 +0200

    cmake: Default GTEST_ROOT to ENV GTEST_ROOT

--- cmake/Modules/FindGTest.cmake
+++ cmake/Modules/FindGTest.cmake
@@ -4,12 +4,6 @@
 # ~~~
 
 if (NOT GOOGLETEST_ROOT)
-	# if GTEST_ROOT is not set but ENV GTEST_ROOT exists use the ENV var
-	if (GTEST_ROOT STREQUAL "")
-		if (NOT ($ENV{GTEST_ROOT} STREQUAL ""))
-			set (GTEST_ROOT "$ENV{GTEST_ROOT}")
-		endif ()
-	endif ()
 	if (NOT (GTEST_ROOT STREQUAL ""))
 		if (EXISTS ${GTEST_ROOT}/CMakeLists.txt)
 			set (GOOGLETEST_ROOT "${GTEST_ROOT}" CACHE INTERNAL "Path to a local copy of Google Test" FORCE)
