a133fc6ff9c00e0cf0c4e49b9846d447bdd70f57
Peter Nirschl <peter.nirschl@gmail.com>
2018-06-16 20:57:13 +0200

    crypto, fcrypt: avoid code duplication between unit tests

---
+++ src/plugins/crypto/common_gpg_tests.c
@@ -0,0 +1,29 @@
+/**
+ * @file
+ *
+ * @brief common functions for gpg-related unit testing.
+ *
+ * @copyright BSD License (see LICENSE.md or https://www.libelektra.org)
+ *
+ */
+
+static int gpg_available (KeySet * conf)
+{
+	int available = 0;
+	char * gpgPath = NULL;
+	Key * parentKey = keyNew ("system", KEY_END);
+
+	int gpg_search_result = CRYPTO_PLUGIN_FUNCTION (gpgGetBinary) (&gpgPath, conf, parentKey);
+	if (gpg_search_result == 1)
+	{
+		available = 1;
+	}
+
+	if (gpgPath)
+	{
+		elektraFree (gpgPath);
+	}
+	keyDel (parentKey);
+	ksDel (conf);
+	return available;
+}
