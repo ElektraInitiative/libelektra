ab6f672b6c53ec93a1b45a1672f504154686b8dd
derwinlu <derwinlu+git@gmail.com>
2018-06-17 21:05:06 +0200

    libelektra: check for strcasecmp avail

--- src/libs/elektra/internal.c
+++ src/libs/elektra/internal.c
@@ -38,6 +38,26 @@
 #include <ctype.h>
 #endif
 
+#ifdef USE_STRINGS_H
+#include <strings.h>
+#else
+// MUSL libc implementations
+int strcasecmp(const char *_l, const char *_r)
+{
+	const unsigned char *l=(void *)_l, *r=(void *)_r;
+	for (; *l && *r && (*l == *r || tolower(*l) == tolower(*r)); l++, r++);
+	return tolower(*l) - tolower(*r);
+}
+
+int strncasecmp(const char *_l, const char *_r, size_t n)
+{
+	const unsigned char *l=(void *)_l, *r=(void *)_r;
+	if (!n--) return 0;
+	for (; *l && *r && n && (*l == *r || tolower(*l) == tolower(*r)); l++, r++, n--);
+	return tolower(*l) - tolower(*r);
+}
+#endif
+
 #include "kdbinternal.h"
 #include <kdbassert.h>
 
