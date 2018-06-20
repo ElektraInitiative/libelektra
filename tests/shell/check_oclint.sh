6ce898dd7709c19aaee245d2f13404b2d7b026cd
René Schwaiger <sanssecours@me.com>
2018-06-20 10:27:13 +0200

    OCLint: Do not check non-existing files

--- tests/shell/check_oclint.sh
+++ tests/shell/check_oclint.sh
@@ -14,7 +14,7 @@ oclint -p "@PROJECT_BINARY_DIR@" -enable-global-analysis -enable-clang-static-an
 	"@CMAKE_SOURCE_DIR@/src/libs/ease/keyname.c" \
 	"@CMAKE_SOURCE_DIR@/src/libs/utility/text.c" \
 	"@CMAKE_SOURCE_DIR@/src/plugins/base64/"*.c \
-	"@CMAKE_SOURCE_DIR@/src/plugins/ccode/"*.{c,cpp} \
+	"@CMAKE_SOURCE_DIR@/src/plugins/ccode/"*.cpp \
 	"@CMAKE_SOURCE_DIR@/src/plugins/camel/camel.c" \
 	"@CMAKE_SOURCE_DIR@/src/plugins/directoryvalue/"*.c \
 	"@CMAKE_SOURCE_DIR@/src/plugins/mini/mini.c" \
