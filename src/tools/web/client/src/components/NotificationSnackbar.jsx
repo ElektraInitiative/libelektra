19ddf44ab79e3e793088285eadacc9148a7ba29e
Daniel Bugl <me@omnidan.net>
2018-06-25 11:28:08 +0200

    web ui: show success messages on top and errors on bottom

--- src/tools/web/client/src/components/NotificationSnackbar.jsx
+++ src/tools/web/client/src/components/NotificationSnackbar.jsx
@@ -9,9 +9,18 @@
 import React from 'react'
 import Snackbar from 'material-ui/Snackbar'
 
+const fromTop = (message) => ({
+  top: 0,
+  bottom: 'auto',
+  left: (window.innerWidth - 288) / 2,
+  transform: message ?
+      'translate3d(0, 0, 0)' :
+      `translate3d(0, -50px, 0)`
+})
+
 const NotificationSnackbar = ({ message }) => {
   return (
-      <Snackbar open={!!message} message={message} autoHideDuration={4500} />
+      <Snackbar open={!!message} message={message} autoHideDuration={4500} style={fromTop(message)} />
   )
 }
 
