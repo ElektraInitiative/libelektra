d17b221e9b180424d1e73024895d56fde294b1c5
Markus Raab <elektra@markus-raab.org>
2018-07-27 16:41:20 +0200

    python: reformat source

--- src/plugins/python/python.cpp
+++ src/plugins/python/python.cpp
@@ -159,8 +159,7 @@ static unsigned open_cnt = 0;
 
 static void Python_Shutdown (moduleData * data)
 {
-	if (!data->shutdown)
-		return;
+	if (!data->shutdown) return;
 
 	/* destroy python if plugin isn't used anymore */
 	if (Py_IsInitialized ())
