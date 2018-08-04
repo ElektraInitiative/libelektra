9896c01f92efd51953ed274373dcd5fcd9cbf31c
René Schwaiger <sanssecours@me.com>
2018-08-04 15:42:57 +0200

    Leaf: Add function for plugin contract

--- src/plugins/leaf/leaf.cpp
+++ src/plugins/leaf/leaf.cpp
@@ -14,6 +14,35 @@
 
 using elektra::LeafDelegate;
 
+using CppKey = kdb::Key;
+using CppKeySet = kdb::KeySet;
+
+namespace
+{
+
+/**
+ * @brief This function returns a key set containing the plugin contract.
+ *
+ * @return A key set specifying the capabilities of the plugin
+ */
+CppKeySet getContract ()
+{
+	return CppKeySet{ 30,
+			  keyNew ("system/elektra/modules/leaf", KEY_VALUE, "leaf plugin waits for your orders", KEY_END),
+			  keyNew ("system/elektra/modules/leaf/exports", KEY_END),
+			  keyNew ("system/elektra/modules/leaf/exports/open", KEY_FUNC, elektraLeafOpen, KEY_END),
+			  keyNew ("system/elektra/modules/leaf/exports/close", KEY_FUNC, elektraLeafClose, KEY_END),
+			  keyNew ("system/elektra/modules/leaf/exports/get", KEY_FUNC, elektraLeafGet, KEY_END),
+			  keyNew ("system/elektra/modules/leaf/exports/set", KEY_FUNC, elektraLeafSet, KEY_END),
+			  keyNew ("system/elektra/modules/leaf/exports/error", KEY_FUNC, elektraLeafError, KEY_END),
+			  keyNew ("system/elektra/modules/leaf/exports/checkconf", KEY_FUNC, elektraLeafCheckConfig, KEY_END),
+#include ELEKTRA_README (leaf)
+			  keyNew ("system/elektra/modules/leaf/infos/version", KEY_VALUE, PLUGINVERSION, KEY_END),
+			  KS_END };
+}
+
+} // end namespace
+
 extern "C" {
 
 typedef Delegator<LeafDelegate> delegator;
@@ -34,26 +63,19 @@ int elektraLeafClose (Plugin * handle, Key * key)
 /** @see elektraDocGet */
 int elektraLeafGet (Plugin * handle ELEKTRA_UNUSED, KeySet * returned, Key * parentKey)
 {
-	if (!elektraStrCmp (keyName (parentKey), "system/elektra/modules/leaf"))
-	{
-		KeySet * contract = ksNew (
-			30, keyNew ("system/elektra/modules/leaf", KEY_VALUE, "leaf plugin waits for your orders", KEY_END),
-			keyNew ("system/elektra/modules/leaf/exports", KEY_END),
-			keyNew ("system/elektra/modules/leaf/exports/open", KEY_FUNC, elektraLeafOpen, KEY_END),
-			keyNew ("system/elektra/modules/leaf/exports/close", KEY_FUNC, elektraLeafClose, KEY_END),
-			keyNew ("system/elektra/modules/leaf/exports/get", KEY_FUNC, elektraLeafGet, KEY_END),
-			keyNew ("system/elektra/modules/leaf/exports/set", KEY_FUNC, elektraLeafSet, KEY_END),
-			keyNew ("system/elektra/modules/leaf/exports/error", KEY_FUNC, elektraLeafError, KEY_END),
-			keyNew ("system/elektra/modules/leaf/exports/checkconf", KEY_FUNC, elektraLeafCheckConfig, KEY_END),
-#include ELEKTRA_README (leaf)
-			keyNew ("system/elektra/modules/leaf/infos/version", KEY_VALUE, PLUGINVERSION, KEY_END), KS_END);
-		ksAppend (returned, contract);
-		ksDel (contract);
+	CppKeySet keys{ returned };
+	CppKey parent{ parentKey };
+	bool updated = false;
 
-		return ELEKTRA_PLUGIN_STATUS_SUCCESS;
+	if (parent.getName () == "system/elektra/modules/leaf")
+	{
+		keys.append (getContract ());
+		updated = true;
 	}
 
-	return ELEKTRA_PLUGIN_STATUS_NO_UPDATE;
+	parent.release ();
+	keys.release ();
+	return updated ? ELEKTRA_PLUGIN_STATUS_SUCCESS : ELEKTRA_PLUGIN_STATUS_NO_UPDATE;
 }
 
 /** @see elektraDocSet */
