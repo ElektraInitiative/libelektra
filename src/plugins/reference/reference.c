2752a640ff960996808bd660a93bee767a7a1582
Klemens Böswirth <k.boeswirth+git@gmail.com>
2018-10-09 23:04:07 +0200

    reference: first tests and bugfixes

--- src/plugins/reference/reference.c
+++ src/plugins/reference/reference.c
@@ -14,23 +14,6 @@
 #include <kdbhelper.h>
 #include <stdbool.h>
 
-
-int elektraReferenceOpen (Plugin * handle ELEKTRA_UNUSED, Key * errorKey ELEKTRA_UNUSED)
-{
-	// plugin initialization logic
-	// this function is optional
-
-	return ELEKTRA_PLUGIN_STATUS_SUCCESS;
-}
-
-int elektraReferenceClose (Plugin * handle ELEKTRA_UNUSED, Key * errorKey ELEKTRA_UNUSED)
-{
-	// free all plugin resources and shut it down
-	// this function is optional
-
-	return ELEKTRA_PLUGIN_STATUS_SUCCESS;
-}
-
 int elektraReferenceGet (Plugin * handle ELEKTRA_UNUSED, KeySet * returned, Key * parentKey)
 {
 	if (!elektraStrCmp (keyName (parentKey), "system/elektra/modules/reference"))
@@ -38,12 +21,8 @@ int elektraReferenceGet (Plugin * handle ELEKTRA_UNUSED, KeySet * returned, Key 
 		KeySet * contract = ksNew (
 			30, keyNew ("system/elektra/modules/reference", KEY_VALUE, "reference plugin waits for your orders", KEY_END),
 			keyNew ("system/elektra/modules/reference/exports", KEY_END),
-			keyNew ("system/elektra/modules/reference/exports/open", KEY_FUNC, elektraReferenceOpen, KEY_END),
-			keyNew ("system/elektra/modules/reference/exports/close", KEY_FUNC, elektraReferenceClose, KEY_END),
 			keyNew ("system/elektra/modules/reference/exports/get", KEY_FUNC, elektraReferenceGet, KEY_END),
 			keyNew ("system/elektra/modules/reference/exports/set", KEY_FUNC, elektraReferenceSet, KEY_END),
-			keyNew ("system/elektra/modules/reference/exports/error", KEY_FUNC, elektraReferenceError, KEY_END),
-			keyNew ("system/elektra/modules/reference/exports/checkconf", KEY_FUNC, elektraReferenceCheckConfig, KEY_END),
 #include ELEKTRA_README (reference)
 			keyNew ("system/elektra/modules/reference/infos/version", KEY_VALUE, PLUGINVERSION, KEY_END), KS_END);
 		ksAppend (returned, contract);
@@ -189,16 +168,29 @@ static int checkSingleReference (const Key * key, KeySet * allKeys, Key * parent
 		const char * elementName = keyName (arrayElement);
 
 		Key * refKey = resolveReference (allKeys, ref, elementName, parentKey);
+		bool error = false;
+		if (refKey == NULL)
+		{
+			ELEKTRA_SET_ERRORF (ELEKTRA_ERROR_REFERENCE_NOT_FOUND, parentKey,
+					    "Reference '%s', set in key '%s', does not reference an existing key.", ref, elementName);
+			error = true;
+		}
+
 		if (!checkRestriction (keyName (refKey), restriction))
 		{
-			ksDel (refArray);
-			keyDel (refKey);
 			ELEKTRA_SET_ERRORF (ELEKTRA_ERROR_REFERENCE_RESTRICTION, parentKey,
 					    "Reference '%s', set in key '%s', does not match restriction '%s'.", ref, elementName,
 					    restriction);
+			error = true;
+		}
+
+		keyDel (refKey);
+
+		if (error)
+		{
+			ksDel (refArray);
 			return ELEKTRA_PLUGIN_STATUS_ERROR;
 		}
-		keyDel (refKey);
 	}
 	ksDel (refArray);
 
@@ -385,7 +377,24 @@ static int checkRecursiveReference (const Key * rootKey, KeySet * allKeys, Key *
 				const char * elementName = keyName (arrayElement);
 
 				Key * refKey = resolveReference (allKeys, ref, elementName, parentKey);
+				bool error = false;
+				if (refKey == NULL)
+				{
+					ELEKTRA_SET_ERRORF (ELEKTRA_ERROR_REFERENCE_NOT_FOUND, parentKey,
+							    "Reference '%s', set in key '%s', does not reference an existing key.", ref,
+							    elementName);
+					error = true;
+				}
+
 				if (!checkRestriction (keyName (refKey), restriction))
+				{
+					ELEKTRA_SET_ERRORF (ELEKTRA_ERROR_REFERENCE_RESTRICTION, parentKey,
+							    "Reference '%s', set in key '%s', does not match restriction '%s'.", ref,
+							    elementName, restriction);
+					error = true;
+				}
+
+				if (error)
 				{
 					ksDel (referenceGraph);
 					ksDel (keysToCheck);
@@ -394,12 +403,8 @@ static int checkRecursiveReference (const Key * rootKey, KeySet * allKeys, Key *
 					keyDel (cur);
 					keyDel (curRoot);
 					keyDel (refKey);
-					ELEKTRA_SET_ERRORF (ELEKTRA_ERROR_REFERENCE_RESTRICTION, parentKey,
-							    "Reference '%s', set in key '%s', does not match restriction '%s'.", ref,
-							    elementName, restriction);
 					return ELEKTRA_PLUGIN_STATUS_ERROR;
 				}
-
 
 				ksAppendKey (keysToCheck, keyDup (refKey));
 				ksAppendKey (referenceGraph,
@@ -460,30 +465,11 @@ int elektraReferenceSet (Plugin * handle ELEKTRA_UNUSED, KeySet * returned, Key 
 	return status;
 }
 
-int elektraReferenceError (Plugin * handle ELEKTRA_UNUSED, KeySet * returned ELEKTRA_UNUSED, Key * parentKey ELEKTRA_UNUSED)
-{
-	// handle errors (commit failed)
-	// this function is optional
-
-	return ELEKTRA_PLUGIN_STATUS_SUCCESS;
-}
-
-int elektraReferenceCheckConfig (Key * errorKey ELEKTRA_UNUSED, KeySet * conf ELEKTRA_UNUSED)
-{
-	// validate plugin configuration
-	// this function is optional
-
-	return ELEKTRA_PLUGIN_STATUS_NO_UPDATE;
-}
-
 Plugin * ELEKTRA_PLUGIN_EXPORT (reference)
 {
 	// clang-format off
 	return elektraPluginExport ("reference",
-		ELEKTRA_PLUGIN_OPEN,	&elektraReferenceOpen,
-		ELEKTRA_PLUGIN_CLOSE,	&elektraReferenceClose,
 		ELEKTRA_PLUGIN_GET,	&elektraReferenceGet,
 		ELEKTRA_PLUGIN_SET,	&elektraReferenceSet,
-		ELEKTRA_PLUGIN_ERROR,	&elektraReferenceError,
 		ELEKTRA_PLUGIN_END);
 }
--- src/plugins/reference/testmod_reference.c
+++ src/plugins/reference/testmod_reference.c
@@ -14,25 +14,106 @@
 
 #include <tests_plugin.h>
 
+#include "reference.h"
+
+#define BASE_KEY "user/tests/reference"
+
+static Key * keyNewReference (const char * name, const char * target, const char * type, const char * restriction)
+{
+	if (restriction != NULL)
+	{
+		return keyNew (name, KEY_VALUE, target, KEY_META, CHECK_REFERENCE_KEYNAME, type, KEY_META, CHECK_REFERENCE_RESTRICT_KEYNAME,
+			       restriction, KEY_END);
+	}
+	else
+	{
+		return keyNew (name, KEY_VALUE, target, KEY_META, CHECK_REFERENCE_KEYNAME, type, KEY_END);
+	}
+}
+
+static void test_single_reference (void)
+{
+	printf ("test single reference\n");
+
+	Key * parentKey = keyNew (BASE_KEY, KEY_END);
+	KeySet * conf = ksNew (0, KS_END);
+	PLUGIN_OPEN ("reference");
+
+	// positive tests
+	KeySet * ks = ksNew (2, keyNewReference (BASE_KEY "/ref/full", BASE_KEY "/target", CHECK_REFERNCE_VALUE_SINGLE, NULL),
+			     keyNew (BASE_KEY "/target", KEY_END), KS_END);
+	succeed_if (plugin->kdbSet (plugin, ks, parentKey) == ELEKTRA_PLUGIN_STATUS_NO_UPDATE, "full reference failed");
+	ksDel (ks);
+
+	ks = ksNew (2, keyNewReference (BASE_KEY "/ref/relative1", "../../target", CHECK_REFERNCE_VALUE_SINGLE, NULL),
+		    keyNew (BASE_KEY "/target", KEY_END), KS_END);
+	succeed_if (plugin->kdbSet (plugin, ks, parentKey) == ELEKTRA_PLUGIN_STATUS_NO_UPDATE, "relative1 reference failed");
+	ksDel (ks);
+
+	ks = ksNew (2, keyNewReference (BASE_KEY "/ref/relative2", "./target", CHECK_REFERNCE_VALUE_SINGLE, NULL),
+		    keyNew (BASE_KEY "/ref/relative2/target", KEY_END), KS_END);
+	succeed_if (plugin->kdbSet (plugin, ks, parentKey) == ELEKTRA_PLUGIN_STATUS_NO_UPDATE, "relative2 reference failed");
+	ksDel (ks);
+
+	ks = ksNew (2, keyNewReference (BASE_KEY "/ref/relative3", "@/ref/target", CHECK_REFERNCE_VALUE_SINGLE, NULL),
+		    keyNew (BASE_KEY "/ref/target", KEY_END), KS_END);
+	succeed_if (plugin->kdbSet (plugin, ks, parentKey) == ELEKTRA_PLUGIN_STATUS_NO_UPDATE, "relative2 reference failed");
+	ksDel (ks);
+
+	ks = ksNew (
+		5, keyNew (BASE_KEY "/ref/array", KEY_VALUE, "#1", KEY_META, CHECK_REFERENCE_KEYNAME, CHECK_REFERNCE_VALUE_SINGLE, KEY_END),
+		keyNew (BASE_KEY "/ref/array/#0", KEY_VALUE, BASE_KEY "/target0", KEY_END),
+		keyNew (BASE_KEY "/ref/array/#1", KEY_VALUE, BASE_KEY "/target1", KEY_END), keyNew (BASE_KEY "/target0", KEY_END),
+		keyNew (BASE_KEY "/target1", KEY_END), KS_END);
+	succeed_if (plugin->kdbSet (plugin, ks, parentKey) == ELEKTRA_PLUGIN_STATUS_NO_UPDATE, "array reference failed");
+	ksDel (ks);
+
+	// negative tests
+	ks = ksNew (2, keyNewReference (BASE_KEY "/ref/neg/full", BASE_KEY "/target", CHECK_REFERNCE_VALUE_SINGLE, NULL),
+		    keyNew (BASE_KEY "/hidden/target", KEY_END), KS_END);
+	succeed_if (plugin->kdbSet (plugin, ks, parentKey) == ELEKTRA_PLUGIN_STATUS_ERROR, "full-negative reference failed");
+	ksDel (ks);
+
+	ks = ksNew (2, keyNewReference (BASE_KEY "/ref/neg/relative1", "../../target", CHECK_REFERNCE_VALUE_SINGLE, NULL),
+		    keyNew (BASE_KEY "/hidden/target", KEY_END), KS_END);
+	succeed_if (plugin->kdbSet (plugin, ks, parentKey) == ELEKTRA_PLUGIN_STATUS_ERROR, "relative1-negative reference failed");
+	ksDel (ks);
+
+	ks = ksNew (2, keyNewReference (BASE_KEY "/ref/neg/relative2", "./target", CHECK_REFERNCE_VALUE_SINGLE, NULL),
+		    keyNew (BASE_KEY "/hidden/ref/relative2/target", KEY_END), KS_END);
+	succeed_if (plugin->kdbSet (plugin, ks, parentKey) == ELEKTRA_PLUGIN_STATUS_ERROR, "relative2-negative reference failed");
+	ksDel (ks);
+
+	ks = ksNew (2, keyNewReference (BASE_KEY "/ref/neg/relative3", "@/ref/target", CHECK_REFERNCE_VALUE_SINGLE, NULL),
+		    keyNew (BASE_KEY "/hidden/ref/target", KEY_END), KS_END);
+	succeed_if (plugin->kdbSet (plugin, ks, parentKey) == ELEKTRA_PLUGIN_STATUS_ERROR, "relative2-negative reference failed");
+	ksDel (ks);
+
+	ks = ksNew (
+		5, keyNew (BASE_KEY "/ref/array", KEY_VALUE, "#1", KEY_META, CHECK_REFERENCE_KEYNAME, CHECK_REFERNCE_VALUE_SINGLE, KEY_END),
+		keyNew (BASE_KEY "/ref/array/#0", KEY_VALUE, BASE_KEY "/target0", KEY_END),
+		keyNew (BASE_KEY "/ref/array/#1", KEY_VALUE, BASE_KEY "/target1", KEY_END), keyNew (BASE_KEY "/hidden/target0", KEY_END),
+		keyNew (BASE_KEY "/hidden/target1", KEY_END), KS_END);
+	succeed_if (plugin->kdbSet (plugin, ks, parentKey) == ELEKTRA_PLUGIN_STATUS_ERROR, "array-negative reference failed");
+	ksDel (ks);
+
+	keyDel (parentKey);
+	PLUGIN_CLOSE ();
+}
+
+
 static void test_basics (void)
 {
 	printf ("test basics\n");
 
-	Key * parentKey = keyNew ("user/tests/reference", KEY_END);
+	Key * parentKey = keyNew (BASE_KEY, KEY_END);
 	KeySet * conf = ksNew (0, KS_END);
 	PLUGIN_OPEN ("reference");
 
 	KeySet * ks = ksNew (0, KS_END);
 
-	succeed_if (plugin->kdbOpen (plugin, parentKey) == ELEKTRA_PLUGIN_STATUS_SUCCESS, "call to kdbOpen was not successful");
-
 	succeed_if (plugin->kdbGet (plugin, ks, parentKey) == ELEKTRA_PLUGIN_STATUS_NO_UPDATE, "call to kdbGet was not successful");
-
 	succeed_if (plugin->kdbSet (plugin, ks, parentKey) == ELEKTRA_PLUGIN_STATUS_NO_UPDATE, "call to kdbSet was not successful");
-
-	succeed_if (plugin->kdbError (plugin, ks, parentKey) == ELEKTRA_PLUGIN_STATUS_SUCCESS, "call to kdbError was not successful");
-
-	succeed_if (plugin->kdbClose (plugin, parentKey) == ELEKTRA_PLUGIN_STATUS_SUCCESS, "call to kdbClose was not successful");
 
 	keyDel (parentKey);
 	ksDel (ks);
@@ -48,6 +129,7 @@ int main (int argc, char ** argv)
 	init (argc, argv);
 
 	test_basics ();
+	test_single_reference ();
 
 	print_result ("testmod_reference");
 
