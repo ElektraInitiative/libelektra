20a7acf46bd160d9bf20bef1e5974b3a7c6d73e0
René Schwaiger <sanssecours@me.com>
2018-10-10 15:07:23 +0200

    fcrypt: Handle return value of `system` call

--- src/plugins/fcrypt/testmod_fcrypt.c
+++ src/plugins/fcrypt/testmod_fcrypt.c
@@ -117,7 +117,11 @@ static int isTestFileCorrect (const char * file)
 
 static void test_teardown (void)
 {
-	system ("gpg-connect-agent --quiet KILLAGENT /bye");
+	int status = system ("gpg-connect-agent --quiet KILLAGENT /bye");
+	if (status != 0)
+	{
+		fprintf (stderr, "Terminating gpg-agent returned with status “%d”", status);
+	}
 }
 
 static void test_init (void)
