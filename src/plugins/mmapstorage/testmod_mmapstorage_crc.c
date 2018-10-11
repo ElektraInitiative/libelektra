7ad627e0c9f98d467d42ad6106a65ff3b178f705
René Schwaiger <sanssecours@me.com>
2018-10-11 18:53:14 +0200

    mmapstorage: Fix additional format specifiers

--- src/plugins/mmapstorage/testmod_mmapstorage_crc.c
+++ src/plugins/mmapstorage/testmod_mmapstorage_crc.c
@@ -87,7 +87,7 @@ static void test_mmap_crc_wrong_checksum (const char * tmpFile)
 	char * mappedRegion = mmap (0, sbuf.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
 	if (mappedRegion == MAP_FAILED)
 	{
-		ELEKTRA_LOG_WARNING ("error mapping file %s\nmmapSize: %lld", tmpFile, sbuf.st_size);
+		ELEKTRA_LOG_WARNING ("error mapping file %s\nmmapSize: " ELEKTRA_STAT_ST_SIZE_F, tmpFile, sbuf.st_size);
 		yield_error ("mmap() error");
 		return;
 	}
