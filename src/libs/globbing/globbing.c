770fb570028b3d6942c7985d268f5de88b129106
Klemens Böswirth <k.boeswirth+git@gmail.com>
2018-10-19 22:13:45 +0200

    libglobbing: add tests and fix bugs

--- src/libs/globbing/globbing.c
+++ src/libs/globbing/globbing.c
@@ -15,12 +15,23 @@
 #include <stdlib.h>
 #include <string.h>
 
+static size_t strcnt (const char * s, char c)
+{
+	size_t count = 0;
+	while ((s = strchr (s + 1, c)) != NULL)
+	{
+		count++;
+	}
+	return count;
+}
+
 static char * elektraToFnmatchGlob (char * pattern)
 {
 	char * ptr = pattern;
 	while ((ptr = strchr (ptr, '/')) != NULL)
 	{
-		if (*(ptr + 2) == '/' && (*(ptr + 1) == '#' || *(ptr + 1) == '_'))
+		++ptr;
+		if ((*(ptr + 1) == '/' || *(ptr + 1) == '\0') && (*ptr == '#' || *ptr == '_'))
 		{
 			*ptr = '*'; // replace /#/ and /_/ with /*/
 		}
@@ -39,12 +50,14 @@ static char * elektraToFnmatchGlob (char * pattern)
  * @retval true if @p name is a valid array item
  * @retval false otherwise
  */
-bool isArrayName (const char * name) // TODO: move? definitely useful elsewhere, e.g. reference plugin
+bool isArrayName (const char * name) // TODO: move? definitely useful elsewhere, e.g. reference plugin, libease, etc.
 {
 	if (name == NULL || *name != '#')
 	{
 		return false;
 	}
+
+	++name;
 
 	int underscores = 0;
 	while (*name == '_')
@@ -71,48 +84,88 @@ bool isArrayName (const char * name) // TODO: move? definitely useful elsewhere,
  * <ul>
  * 	<li> '*' matches any series of characters other than '/'</li>
  * 	<li> '?' matches any single character except '/' </li>
- * 	<li> '#' (when used as "/#/") matches a valid array item </li>
- * 	<li> '_' (when used as "/_/") matches a key part that is <b>not</b> a valid array item </li>
+ * 	<li> '#', when used as "/#/" (or "/#" at the end of @p pattern), matches a valid array item </li>
+ * 	<li> '_', when used as "/_/"(or "/_" at the end of @p pattern), matches a key part that is <b>not</b> a valid array item </li>
+ * 	<li> if the pattern ends with "&#47;**", matching key names may contain arbitrary suffixes </li>
  * </ul>
+ *
+ * @note '*' cannot match an empty key name part. This also means patterns like "something&#47;*" will
+ * not match the key "something". This is because each slash ('/') in the pattern has to correspond to
+ * a slash in the canonical key name, which neither end in a slash nor contain multiple slashes in sequence.
  *
  * @param key the Key to match against the globbing pattern
  * @param pattern the globbing pattern used
- * @retval 0 if the Key matches the given pattern
+ * @retval 0 if @p key is not NULL, @p pattern is not NULL and @p pattern matches @p key
  * @retval ELEKTRA_GLOB_NOMATCH otherwise
  *
  * @see isArrayName(), for info on valid array items
  */
 int keyGlob (const Key * key, const char * pattern)
 {
-	const char * noNamespaceName = strchr (keyName (key), '/');
-
-	if (noNamespaceName == NULL)
+	if (key == NULL || pattern == NULL)
 	{
+		return ELEKTRA_GLOB_NOMATCH;
+	}
+
+	size_t nameSize = (size_t) keyGetNameSize (key);
+	char * name = elektraMalloc (nameSize);
+	keyGetName (key, name, nameSize);
+
+	const size_t patternSlashes = strcnt (pattern, '/');
+
+	size_t len = strlen (pattern);
+	bool prefixMode = len >= 2 && elektraStrCmp (pattern + len - 3, "/**") == 0;
+
+	char * patternEnd = name;
+	for (size_t i = 0; i < patternSlashes; ++i)
+	{
+		patternEnd = strchr (patternEnd + 1, '/');
+
+		if (patternEnd == NULL)
+		{
+			// more slashes in pattern, cannot match
+			free (name);
+			return ELEKTRA_GLOB_NOMATCH;
+		}
+	}
+
+	if (prefixMode)
+	{
+		// mark end of relevant part
+		*(patternEnd + 1) = '\0';
+	}
+	else if (strchr (patternEnd + 1, '/') != NULL)
+	{
+		// more slashes in name, cannot match
+		free (name);
 		return ELEKTRA_GLOB_NOMATCH;
 	}
 
 	char * fnmPattern = elektraToFnmatchGlob (elektraStrDup (pattern));
-	int rc = fnmatch (fnmPattern, noNamespaceName, FNM_PATHNAME);
+	int rc = fnmatch (fnmPattern, name, FNM_PATHNAME);
 	elektraFree (fnmPattern);
 
 	if (rc == FNM_NOMATCH)
 	{
+		free (name);
 		return ELEKTRA_GLOB_NOMATCH;
 	}
 
 	const char * ptr = pattern;
-	const char * keyPtr = noNamespaceName;
-	while ((ptr = strchr (ptr, '/')) != NULL && (keyPtr = strchr (keyPtr, '/')) != NULL)
+	const char * keyPtr = name;
+	while ((ptr = strchr (ptr + 1, '/')) != NULL && (keyPtr = strchr (keyPtr + 1, '/')) != NULL)
 	{
-		if (*(ptr + 2) == '/')
+		if (*(ptr + 2) == '/' || *(ptr + 2) == '\0')
 		{
 			if (*(ptr + 1) == '#' && !isArrayName (keyPtr + 1))
 			{
+				free (name);
 				return ELEKTRA_GLOB_NOMATCH;
 			}
 
 			if (*(ptr + 1) == '_' && isArrayName (keyPtr + 1))
 			{
+				free (name);
 				return ELEKTRA_GLOB_NOMATCH;
 			}
 		}
