aac8680f286f78fb62af749510a417630d999794
René Schwaiger <sanssecours@me.com>
2019-01-16 17:16:58 +0100

    YAy PEG: Remove example code

--- src/plugins/yaypeg/yaypeg.cpp
+++ src/plugins/yaypeg/yaypeg.cpp
@@ -8,14 +8,14 @@
  */
 
 #include "yaypeg.hpp"
-#include "yaypeg_delegate.hpp"
 
+#include <stdexcept>
+
+#include <kdb.hpp>
 #include <kdberrors.h>
 #include <kdbhelper.h>
 
 using std::exception;
-
-using elektra::YaypegDelegate;
 
 using CppKey = kdb::Key;
 using CppKeySet = kdb::KeySet;
@@ -33,12 +33,8 @@ CppKeySet getContract ()
 	return CppKeySet{ 30,
 			  keyNew ("system/elektra/modules/yaypeg", KEY_VALUE, "yaypeg plugin waits for your orders", KEY_END),
 			  keyNew ("system/elektra/modules/yaypeg/exports", KEY_END),
-			  keyNew ("system/elektra/modules/yaypeg/exports/open", KEY_FUNC, elektraYaypegOpen, KEY_END),
-			  keyNew ("system/elektra/modules/yaypeg/exports/close", KEY_FUNC, elektraYaypegClose, KEY_END),
 			  keyNew ("system/elektra/modules/yaypeg/exports/get", KEY_FUNC, elektraYaypegGet, KEY_END),
 			  keyNew ("system/elektra/modules/yaypeg/exports/set", KEY_FUNC, elektraYaypegSet, KEY_END),
-			  keyNew ("system/elektra/modules/yaypeg/exports/error", KEY_FUNC, elektraYaypegError, KEY_END),
-			  keyNew ("system/elektra/modules/yaypeg/exports/checkconf", KEY_FUNC, elektraYaypegCheckConfig, KEY_END),
 #include ELEKTRA_README (yaypeg)
 			  keyNew ("system/elektra/modules/yaypeg/infos/version", KEY_VALUE, PLUGINVERSION, KEY_END),
 			  KS_END };
@@ -48,36 +44,8 @@ CppKeySet getContract ()
 
 extern "C" {
 
-typedef Delegator<YaypegDelegate> delegator;
-
-/** @see elektraDocOpen */
-int elektraYaypegOpen (Plugin * handle, Key * key)
-{
-	int status = ELEKTRA_PLUGIN_STATUS_ERROR;
-
-	try
-	{
-		// - The function below calls the constructor `YaypegDelegate(config)`.
-		// - After the call to `delegator::open` you can retrieve a pointer to the delegate via `delegator::get (handle)`.
-		status = delegator::open (handle, key);
-	}
-	catch (exception const & error)
-	{
-		ELEKTRA_SET_ERROR (ELEKTRA_ERROR_UNCAUGHT_EXCEPTION, key, error.what ());
-	}
-
-	return status;
-}
-
-/** @see elektraDocClose */
-int elektraYaypegClose (Plugin * handle, Key * key)
-{
-	// The function `delegator::close` calls the destructor of `YaypegDelegate`.
-	return delegator::close (handle, key);
-}
-
 /** @see elektraDocGet */
-int elektraYaypegGet (Plugin * handle, KeySet * returned, Key * parentKey)
+int elektraYaypegGet (Plugin * handle ELEKTRA_UNUSED, KeySet * returned, Key * parentKey)
 {
 	CppKeySet keys{ returned };
 	CppKey parent{ parentKey };
@@ -85,11 +53,6 @@ int elektraYaypegGet (Plugin * handle, KeySet * returned, Key * parentKey)
 	if (parent.getName () == "system/elektra/modules/yaypeg")
 	{
 		keys.append (getContract ());
-	}
-	else
-	{
-		// This is only an example, to show you how to call a method of the delegate
-		keys.append (delegator::get (handle)->getConfig (parent));
 	}
 
 	parent.release ();
@@ -103,28 +66,10 @@ int elektraYaypegSet (Plugin * handle ELEKTRA_UNUSED, KeySet * returned ELEKTRA_
 	return ELEKTRA_PLUGIN_STATUS_NO_UPDATE;
 }
 
-/** @see elektraDocError */
-int elektraYaypegError (Plugin * handle ELEKTRA_UNUSED, KeySet * returned ELEKTRA_UNUSED, Key * parentKey ELEKTRA_UNUSED)
-{
-	return ELEKTRA_PLUGIN_STATUS_SUCCESS;
-}
-
-/** @see elektraDocCheckConf */
-int elektraYaypegCheckConfig (Key * errorKey ELEKTRA_UNUSED, KeySet * conf ELEKTRA_UNUSED)
-{
-	return ELEKTRA_PLUGIN_STATUS_NO_UPDATE;
-}
-
 Plugin * ELEKTRA_PLUGIN_EXPORT (yaypeg)
 {
-	// clang-format off
-	return elektraPluginExport ("yaypeg",
-		ELEKTRA_PLUGIN_OPEN,	&elektraYaypegOpen,
-		ELEKTRA_PLUGIN_CLOSE,	&elektraYaypegClose,
-		ELEKTRA_PLUGIN_GET,	&elektraYaypegGet,
-		ELEKTRA_PLUGIN_SET,	&elektraYaypegSet,
-		ELEKTRA_PLUGIN_ERROR,	&elektraYaypegError,
-		ELEKTRA_PLUGIN_END);
+	return elektraPluginExport ("yaypeg", ELEKTRA_PLUGIN_GET, &elektraYaypegGet, ELEKTRA_PLUGIN_SET, &elektraYaypegSet,
+				    ELEKTRA_PLUGIN_END);
 }
 
 } // end extern "C"
--- src/plugins/yaypeg/yaypeg.hpp
+++ src/plugins/yaypeg/yaypeg.hpp
@@ -17,12 +17,8 @@ using ckdb::KeySet;
 using ckdb::Plugin;
 
 extern "C" {
-int elektraYaypegOpen (Plugin * handle, Key * errorKey);
-int elektraYaypegClose (Plugin * handle, Key * errorKey);
 int elektraYaypegGet (Plugin * handle, KeySet * returned, Key * parentKey);
 int elektraYaypegSet (Plugin * handle, KeySet * returned, Key * parentKey);
-int elektraYaypegError (Plugin * handle, KeySet * conf, Key * parentKey);
-int elektraYaypegCheckConfig (Key * errorKey, KeySet * conf);
 
 Plugin * ELEKTRA_PLUGIN_EXPORT (yaypeg);
 } // end extern "C"
