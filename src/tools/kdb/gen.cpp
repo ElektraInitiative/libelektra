0c47220cde3434cbf987196dae6fc28591f7ca1a
Klemens Böswirth <k.boeswirth+git@gmail.com>
2019-02-01 18:32:24 +0100

    codgen: more work on highlevel template

--- src/tools/kdb/gen.cpp
+++ src/tools/kdb/gen.cpp
@@ -12,26 +12,36 @@
 #include <cmdline.hpp>
 #include <kdb.hpp>
 
-#include <unistd.h>
+#include <fstream>
 
 using namespace kdb;
 using namespace std;
 
 int GenCommand::execute (Cmdline const & cl)
 {
-	GenTemplateList templates;
+	auto & templates = GenTemplateList::getInstance ();
+
+	if (cl.arguments.size () < 2)
+	{
+		throw invalid_argument ("need at least 2 arguments");
+	}
+
+	auto templateName = cl.arguments[0];
+	auto outputName = cl.arguments[1];
+
+	std::unordered_map<std::string, std::string> parameters = {};
+
+	const auto tmpl = templates.getTemplate (templateName, outputName, parameters);
 
 	KeySet ks;
 	ks.append (Key ("/test", KEY_META, "type", "long", KEY_END));
 	ks.append (Key ("/test", KEY_META, "type", "enum", KEY_META, "check/enum", "#3", KEY_META, "check/enum/#2", "two", KEY_END));
 
-	auto tmpl = templates.getTemplate ("elektra");
-	tmpl->setParameter ("outputName", "test_example");
-	std::cout << "=== Header File ===" << std::endl;
-	tmpl->render (std::cout, ".h", ks);
-	std::cout << std::endl << std::endl;
-	std::cout << "=== Code File ===" << std::endl;
-	tmpl->render (std::cout, ".c", ks);
+	for (const auto & part : tmpl->getParts ())
+	{
+		auto output = std::ofstream (outputName + part);
+		tmpl->render (output, part, ks);
+	}
 
 	return 0;
 }
--- src/tools/kdb/gen/template.cpp
+++ src/tools/kdb/gen/template.cpp
@@ -15,7 +15,22 @@
 
 #include "elektragen.hpp"
 
-void GenTemplate::render (std::ostream & output, const std::string & part, const kdb::KeySet & ks)
+GenTemplate::GenTemplate (std::string templateBaseName, std::vector<std::string> parts,
+			  const std::unordered_map<std::string, bool> & parameters)
+: _templateBaseName (std::move (templateBaseName)), _parts (std::move (parts)), _parameters (), _requiredParameters ()
+{
+	_parameters["outputName"] = "";
+	_requiredParameters.insert ("outputName");
+	std::for_each (parameters.begin (), parameters.end (), [this](const std::pair<std::string, bool> & p) {
+		_parameters[p.first] = "";
+		if (p.second)
+		{
+			_requiredParameters.insert (p.first);
+		}
+	});
+}
+
+void GenTemplate::render (std::ostream & output, const std::string & part, const kdb::KeySet & ks) const
 {
 	using namespace kainjow::mustache;
 
@@ -31,6 +46,12 @@ void GenTemplate::render (std::ostream & output, const std::string & part, const
 	tmpl.render (getTemplateData (ks), [&](const std::string & str) { output << str; });
 }
 
+std::string GenTemplate::getParameter (const std::string & name) const
+{
+	auto param = _parameters.find (name);
+	return param != _parameters.end () ? param->second : "";
+}
+
 void GenTemplate::setParameter (const std::string & name, const std::string & value)
 {
 	auto param = _parameters.find (name);
@@ -40,30 +61,42 @@ void GenTemplate::setParameter (const std::string & name, const std::string & va
 	}
 }
 
-std::string GenTemplate::getName ()
+void GenTemplate::clearParameters ()
 {
-	return _name;
+	std::for_each (_parameters.begin (), _parameters.end (),
+		       [this](const std::pair<std::string, std::string> & param) { _parameters[param.first] = ""; });
 }
 
-std::string GenTemplate::getParameter (const std::string & name)
+std::vector<std::string> GenTemplate::getParts () const
 {
-	auto param = _parameters.find (name);
-	return param != _parameters.end () ? param->second : "";
+	return _parts;
 }
 
 template <class genClass>
-void GenTemplateList::addTemplate ()
+void GenTemplateList::addTemplate (const std::string & name)
 {
 	std::unique_ptr<GenTemplate> tmpl (new genClass ());
-	_templates[tmpl->getName ()] = std::move (tmpl);
+	_templates[name] = std::move (tmpl);
 }
 
-GenTemplate * GenTemplateList::getTemplate (const std::string & name)
+const GenTemplate * GenTemplateList::getTemplate (const std::string & name, const std::string & outputName,
+						  const std::unordered_map<std::string, std::string> & parameters) const
 {
-	return _templates[name].get ();
+	auto search = _templates.find (name);
+	if (search == _templates.end ())
+	{
+		return &EmptyGenTemplate::getInstance ();
+	}
+
+	auto tmpl = search->second.get ();
+	tmpl->clearParameters ();
+	tmpl->setParameter ("outputName", outputName);
+	std::for_each (parameters.begin (), parameters.end (),
+		       [tmpl](const std::pair<std::string, std::string> & param) { tmpl->setParameter (param.first, param.second); });
+	return tmpl;
 }
 
 GenTemplateList::GenTemplateList () : _templates ()
 {
-	addTemplate<ElektraGenTemplate> ();
+	addTemplate<ElektraGenTemplate> ("elektra");
 }
