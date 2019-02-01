5367ff9c6c10fbdc4303ea9a926fb3b78f308608
Peter Nirschl <peter.nirschl@gmail.com>
2019-02-01 16:22:34 +0100

    CMake: provide plugin name as compiler definition
    Provides ELEKTRA_PLUGIN_NAME and ELEKTRA_PLUGIN_NAME_C as compiler definitions via CMake.
    See #1042 for details.

--- cmake/Modules/LibAddPlugin.cmake
+++ cmake/Modules/LibAddPlugin.cmake
@@ -2,6 +2,41 @@ include (LibAddMacros)
 include (LibAddTest)
 
 # ~~~
+# Provides additional compiler definitions:
+#
+# - ELEKTRA_PLUGIN_NAME containing the short plugin name as string
+# - ELEKTRA_PLUGIN_NAME_C contai the short plugin name which can be used in function names
+#
+# shortname:
+#   pass the PLUGIN_SHORT_NAME as this argument
+#
+# ADDITIONAL_COMPILE_DEFINITIONS:
+#   will contain the additional compiler definitions mentioned above.
+#   will be set to the parent scope, i.e. the caller of the function
+#
+# ~~~
+function (set_additional_compile_definitions shortname)
+    # provide the plugin name as string to the compiler/preprocessor
+
+    if (NOT "${ARG_COMPILE_DEFINITIONS}" MATCHES "ELEKTRA_PLUGIN_NAME")
+        set(ADDITIONAL_COMPILE_DEFINITIONS_PART1 "ELEKTRA_PLUGIN_NAME=\"${shortname}\"")
+    endif ()
+
+    # provide the plugin name as macro that can be used for building function names, etc.
+    if (NOT "${ARG_COMPILE_DEFINITIONS}" MATCHES "ELEKTRA_PLUGIN_NAME_C")
+        if (ADDITIONAL_COMPILE_DEFINITIONS_PART1)
+            set(ADDITIONAL_COMPILE_DEFINITIONS_PART2 ";ELEKTRA_PLUGIN_NAME_C=${shortname}")
+        else ()
+            set(ADDITIONAL_COMPILE_DEFINITIONS_PART2 "ELEKTRA_PLUGIN_NAME_C=${shortname}")
+        endif ()
+    endif ()
+
+    set(ADDITIONAL_COMPILE_DEFINITIONS "${ADDITIONAL_COMPILE_DEFINITIONS_PART1}${ADDITIONAL_COMPILE_DEFINITIONS_PART2}" PARENT_SCOPE)
+    unset (ADDITIONAL_COMPILE_DEFINITIONS_PART1)
+    unset (ADDITIONAL_COMPILE_DEFINITIONS_PART2)
+endfunction (set_additional_compile_definitions)
+
+# ~~~
 # Add a test for a plugin
 #
 # Will include the common tests.h file + its source file
@@ -149,14 +184,19 @@ function (add_plugintest testname)
 			target_link_libraries (${testexename} gtest_main)
 		endif (ARG_CPP)
 
+		set_additional_compile_definitions (${testname})
+		message (STATUS "DEBUG ${PLUGIN_NAME} (${PLUGIN_SHORT_NAME}) [${testname}] = ${ADDITIONAL_COMPILE_DEFINITIONS}")
+
 		target_link_libraries (${testexename} ${ARG_LINK_LIBRARIES} ${ARG_TEST_LINK_LIBRARIES})
 		set_target_properties (${testexename}
 				       PROPERTIES COMPILE_DEFINITIONS
-						  "HAVE_KDBCONFIG_H;ELEKTRA_PLUGIN_TEST;${ARG_COMPILE_DEFINITIONS}")
+						  "HAVE_KDBCONFIG_H;ELEKTRA_PLUGIN_TEST;${ARG_COMPILE_DEFINITIONS};${ADDITIONAL_COMPILE_DEFINITIONS}")
 		set_property (TARGET ${testexename}
 			      APPEND
 			      PROPERTY INCLUDE_DIRECTORIES
 				       ${ARG_INCLUDE_DIRECTORIES})
+
+        unset (ADDITIONAL_COMPILE_DEFINITIONS)
 
 		foreach (DIR ${ARG_INCLUDE_SYSTEM_DIRECTORIES})
 
@@ -561,11 +601,13 @@ function (add_plugin PLUGIN_SHORT_NAME)
 	endif ()
 
 	generate_readme (${PLUGIN_SHORT_NAME})
+	set_additional_compile_definitions (${PLUGIN_SHORT_NAME})
 
 	set_property (TARGET ${PLUGIN_OBJS}
 		      APPEND
 		      PROPERTY COMPILE_DEFINITIONS
 			       ${ARG_COMPILE_DEFINITIONS}
+			       ${ADDITIONAL_COMPILE_DEFINITIONS}
 			       "ELEKTRA_STATIC"
 			       "HAVE_KDBCONFIG_H"
 			       "PLUGIN_SHORT_NAME=${PLUGIN_SHORT_NAME}"
@@ -618,6 +660,7 @@ function (add_plugin PLUGIN_SHORT_NAME)
 			      APPEND
 			      PROPERTY COMPILE_DEFINITIONS
 				       ${ARG_COMPILE_DEFINITIONS}
+				       ${ADDITIONAL_COMPILE_DEFINITIONS}
 				       "HAVE_KDBCONFIG_H"
 				       "PLUGIN_SHORT_NAME=${PLUGIN_SHORT_NAME}"
 				       "README=readme_${PLUGIN_SHORT_NAME}.c")
@@ -661,4 +704,7 @@ function (add_plugin PLUGIN_SHORT_NAME)
 			      PROPERTY "elektra-full_LIBRARIES"
 				       "${ARG_LINK_LIBRARIES}")
 	endif (NOT ARG_ONLY_SHARED)
+
+    # cleanup
+	unset (ADDITIONAL_COMPILE_DEFINITIONS)
 endfunction (add_plugin)
