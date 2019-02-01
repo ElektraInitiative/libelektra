df2695db736c9733fea001d9c29f3a594d856b21
Klemens Böswirth <k.boeswirth+git@gmail.com>
2019-02-01 15:32:43 +0100

    codgen: add highlevel template

--- src/tools/kdb/gen.cpp
+++ src/tools/kdb/gen.cpp
@@ -19,10 +19,19 @@ using namespace std;
 
 int GenCommand::execute (Cmdline const & cl)
 {
-	GenTemplateList templates (std::cout);
+	GenTemplateList templates;
 
-	auto tmpl = templates.getTemplate ("html");
-	tmpl->render ();
+	KeySet ks;
+	ks.append (Key ("/test", KEY_META, "type", "long", KEY_END));
+	ks.append (Key ("/test", KEY_META, "type", "enum", KEY_META, "check/enum", "#3", KEY_META, "check/enum/#2", "two", KEY_END));
+
+	auto tmpl = templates.getTemplate ("elektra");
+	tmpl->setParameter ("outputName", "test_example");
+	std::cout << "=== Header File ===" << std::endl;
+	tmpl->render (std::cout, ".h", ks);
+	std::cout << std::endl << std::endl;
+	std::cout << "=== Code File ===" << std::endl;
+	tmpl->render (std::cout, ".c", ks);
 
 	return 0;
 }
