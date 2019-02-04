1cc36e906ce841d6cf5bba841680702be187a923
Mihael Pranjic <mpranj@limun.org>
2019-02-04 16:35:43 +0100

    plugins: add pregetcache and postgetcache placements to contract

--- src/libs/tools/src/plugin.cpp
+++ src/libs/tools/src/plugin.cpp
@@ -207,10 +207,12 @@ void Plugin::check (vector<string> & warnings)
 		pp.push_back ("rollback");
 		pp.push_back ("postrollback");
 		pp.push_back ("getresolver");
+		pp.push_back ("pregetcache");
 		pp.push_back ("pregetstorage");
 		pp.push_back ("getstorage");
 		pp.push_back ("procgetstorage");
 		pp.push_back ("postgetstorage");
+		pp.push_back ("postgetcache");
 		pp.push_back ("setresolver");
 		pp.push_back ("postgetcleanup");
 		pp.push_back ("presetstorage");
