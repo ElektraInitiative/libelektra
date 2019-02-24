8e811b5374fb916b8a0405abeaa815f26fb1e7a6
Klemens Böswirth <k.boeswirth+git@gmail.com>
2019-02-24 23:07:07 +0100

    codegen: enum conversion

--- src/tools/kdb/gen/elektragen.cpp
+++ src/tools/kdb/gen/elektragen.cpp
@@ -9,11 +9,36 @@
  */
 
 #include "elektragen.hpp"
+
 #include <command.hpp>
 #include <kdbtypes.h>
 
+#include <memory>
+#include <set>
+
 const char * ElektraGenTemplate::Params::InitFunctionName = "initFn";
 const char * ElektraGenTemplate::Params::TagPrefix = "tagPrefix";
+const char * ElektraGenTemplate::Params::OptimizeFromString = "optimizeFromString";
+const char * ElektraGenTemplate::Params::AdditionalHeaders = "headers";
+
+struct EnumTrie
+{
+	explicit EnumTrie (const std::set<std::pair<std::string, std::string>> & values);
+
+	EnumTrie () : children (), stringValue (), name ()
+	{
+	}
+
+	std::string createSwitch ();
+
+private:
+	std::unordered_map<char, std::unique_ptr<EnumTrie>> children;
+	std::string stringValue;
+	std::string name;
+
+	void insert (const std::string & prefix, const std::set<std::pair<std::string, std::string>> & values);
+	bool createSwitch (std::stringstream & ss, size_t index);
+};
 
 static inline const std::unordered_set<std::string> getAllowedTypes ()
 {
@@ -108,7 +133,7 @@ static std::string camelCaseToMacroCase (const std::string & s)
 	return ss.str ();
 }
 
-static kainjow::mustache::list getEnumValues (const std::string & prefix, const kdb::Key & key)
+static kainjow::mustache::list getEnumValues (const std::string & prefix, const kdb::Key & key, std::string & fromStringSwitch)
 {
 	using namespace kainjow::mustache;
 
@@ -119,6 +144,8 @@ static kainjow::mustache::list getEnumValues (const std::string & prefix, const 
 
 	list values;
 
+	std::set<std::pair<std::string, std::string>> stringValues;
+
 	const auto end = key.getMeta<std::string> ("check/enum");
 	kdb::long_long_t i = 0;
 	auto cur = "#" + std::to_string (i);
@@ -127,14 +154,19 @@ static kainjow::mustache::list getEnumValues (const std::string & prefix, const 
 		if (key.hasMeta ("check/enum/" + cur))
 		{
 			auto name = prefix + "_";
-			name += camelCaseToMacroCase (key.getMeta<std::string> ("check/enum/" + cur));
+			const std::string & stringValue = key.getMeta<std::string> ("check/enum/" + cur);
+			name += camelCaseToMacroCase (stringValue);
 			const auto value = std::to_string (i);
 			// TODO: custom values
-			values.emplace_back (object{ { "name", name }, { "value", value } });
+			values.emplace_back (object{ { "name", name }, { "value", value }, { "string_value", stringValue } });
+			stringValues.insert ({ stringValue, name });
 		}
 		++i;
 		cur = "#" + std::to_string (i);
 	}
+
+	EnumTrie trie (stringValues);
+	fromStringSwitch = trie.createSwitch ();
 
 	return values;
 }
@@ -153,6 +185,7 @@ static inline bool shouldGenerateTypeDef (const kdb::Key & key)
 kainjow::mustache::data ElektraGenTemplate::getTemplateData (const std::string & outputName, const kdb::KeySet & ks,
 							     const std::string & parentKey) const
 {
+	// TODO: check illegal names
 	if (parentKey[0] != '/')
 	{
 		throw CommandAbortException ("parentKey has to be cascading");
@@ -163,16 +196,20 @@ kainjow::mustache::data ElektraGenTemplate::getTemplateData (const std::string &
 	auto headerFile = outputName + ".h";
 	auto includeGuard = createIncludeGuard (headerFile);
 	auto initFunctionName = getParameter (Params::InitFunctionName, "loadConfiguration");
+	auto additionalHeaders = split (getParameter (Params::AdditionalHeaders), ',');
+	auto optimizeFromString = getParameter (Params::OptimizeFromString, "off") != "off";
 
 	auto data = object{ { "header_file", headerFile },
 			    { "include_guard", includeGuard },
 			    { "parent_key", parentKey },
-			    { "init_function_name", initFunctionName } };
+			    { "init_function_name", initFunctionName },
+			    { "more_headers", list (additionalHeaders.begin (), additionalHeaders.end ()) } };
 
 	auto enums = list{};
 	auto keys = list{};
 
 	auto specParent = kdb::Key ("spec" + parentKey, KEY_END);
+
 
 	std::unordered_map<std::string, std::pair<std::string, std::string>> enumTypes;
 
@@ -213,7 +250,9 @@ kainjow::mustache::data ElektraGenTemplate::getTemplateData (const std::string &
 			auto typeName = "Enum" + enumType;
 
 			auto nativeType = genType ? enumType : "Elektra" + typeName;
-			auto values = getEnumValues (camelCaseToMacroCase (nativeType), key);
+			std::string fromStringSwitch;
+
+			auto values = getEnumValues (camelCaseToMacroCase (nativeType), key, fromStringSwitch);
 
 			auto generateTypeDef = shouldGenerateTypeDef (key);
 			if (genType && generateTypeDef)
@@ -246,13 +285,16 @@ kainjow::mustache::data ElektraGenTemplate::getTemplateData (const std::string &
 
 			keyObject["type_name"] = typeName;
 
-
 			enums.emplace_back (object{ { "name", name },
 						    { "tag_name", snakeCaseToMacroCase (tagName) },
 						    { "type_name", typeName },
 						    { "native_type", nativeType },
 						    { "generate_typedef?", generateTypeDef },
-						    { "values", values } });
+						    { "values", values },
+						    { "switch_from_string?", optimizeFromString },
+						    { "from_key", "elektraKeyTo" + nativeType },
+						    { "to_string", "elektra" + nativeType + "ToString" },
+						    { "from_string_code", fromStringSwitch } });
 		}
 
 		keys.emplace_back (keyObject);
@@ -263,4 +305,102 @@ kainjow::mustache::data ElektraGenTemplate::getTemplateData (const std::string &
 	data["enums"] = enums;
 
 	return data;
+}
+
+EnumTrie::EnumTrie (const std::set<std::pair<std::string, std::string>> & values)
+{
+	for (auto v = values.begin (); v != values.end ();)
+	{
+		char c = v->first.at (0);
+		std::string p;
+		if (c != '\0')
+		{
+			p += c;
+		}
+		std::set<std::pair<std::string, std::string>> vals;
+
+		while (v != values.end () && v->first.at (0) == c)
+		{
+			vals.insert (*v);
+			++v;
+		}
+		insert (p, vals);
+	}
+}
+
+void EnumTrie::insert (const std::string & prefix, const std::set<std::pair<std::string, std::string>> & values)
+{
+	std::unique_ptr<EnumTrie> child (new EnumTrie ());
+	if (values.size () == 1)
+	{
+		child->stringValue = values.begin ()->first;
+		child->name = values.begin ()->second;
+	}
+	else
+	{
+		for (auto v = values.begin (); v != values.end ();)
+		{
+			if (v->first.size () == prefix.size ())
+			{
+				child->stringValue = values.begin ()->first;
+				child->name = values.begin ()->second;
+				++v;
+				continue;
+			}
+
+			char c = v->first.at (prefix.size ());
+			std::string p = prefix;
+			p += c;
+			std::set<std::pair<std::string, std::string>> vals;
+
+			while (v != values.end () && (v->first.size () > prefix.size () ? v->first.at (prefix.size ()) : '\0') == c)
+			{
+				vals.insert (*v);
+				++v;
+			}
+			child->insert (p, vals);
+		}
+	}
+
+	children[prefix.back ()] = std::move (child);
+}
+
+std::string EnumTrie::createSwitch ()
+{
+	std::stringstream ss;
+	createSwitch (ss, 0);
+	return ss.str ();
+}
+
+bool EnumTrie::createSwitch (std::stringstream & ss, size_t index)
+{
+	if (children.empty ())
+	{
+		if (stringValue.empty ())
+		{
+			return false;
+		}
+
+		ss << "return " << name << ";" << std::endl;
+		return false;
+	}
+
+	ss << "switch (string[" << index << "])" << std::endl;
+	ss << "{" << std::endl;
+	for (auto & child : children)
+	{
+		ss << "case '" << child.first << "':" << std::endl;
+		if (child.second->createSwitch (ss, index + 1))
+		{
+			ss << "break;" << std::endl;
+		}
+	}
+	ss << "}" << std::endl;
+
+	if (!stringValue.empty ())
+	{
+		ss << "return " << name << ";" << std::endl;
+		return false;
+	}
+	return true;
 }
