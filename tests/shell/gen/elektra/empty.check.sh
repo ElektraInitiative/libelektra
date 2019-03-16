ed9ef85414c700f8301592154daa84e2367d1e8b
Klemens Böswirth <k.boeswirth+git@gmail.com>
2019-03-16 18:17:43 +0100

    codegen: use cmake for testing generated code

--- tests/shell/gen/elektra/empty.check.sh
+++ tests/shell/gen/elektra/empty.check.sh
@@ -1,1 +1,17 @@
-"@CMAKE_C_COMPILER@" -c empty.actual.c -I "@CMAKE_BINARY_DIR@/src/include" -I "@CMAKE_SOURCE_DIR@/src/include" -o empty.actual.o && rm empty.actual.o
+cat << EOF > CMakeLists.txt
+cmake_minimum_required(VERSION 3.0)
+
+add_library (dummy OBJECT empty.actual.c)
+target_include_directories(dummy PRIVATE "@CMAKE_BINARY_DIR@/src/include" "@CMAKE_SOURCE_DIR@/src/include")
+EOF
+
+mkdir build && cd build
+
+cmake .. -DCMAKE_C_COMPILER="@CMAKE_C_COMPILER@" && cmake --build .
+res=$?
+
+cd ..
+rm -r build
+rm CMakeLists.txt
+
+exit $res
