465bcb43cf39c59d7ecffb07519f63b5ea8ff242
Klemens Böswirth <k.boeswirth+git@gmail.com>
2019-03-16 02:15:05 +0100

    codegen: space handling

--- tests/shell/gen/elektra/struct.check.sh
+++ tests/shell/gen/elektra/struct.check.sh
@@ -1,1 +1,2 @@
-@CMAKE_C_COMPILER@ -c struct.actual.c -I @CMAKE_BINARY_DIR@/src/include -I @CMAKE_SOURCE_DIR@/src/include -o struct.actual.o && rm struct.actual.o
+"@CMAKE_C_COMPILER@" -c struct.actual.c -I "@CMAKE_BINARY_DIR@/src/include" -I "@CMAKE_SOURCE_DIR@/src/include" -o struct.actual.o && rm struct.actual.o
+
