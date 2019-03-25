3a7c6d3e9c93ae973b131e40720936a284c6d32f
René Schwaiger <sanssecours@me.com>
2019-03-25 14:45:47 +0100

    YAML CPP: Fix update of array metadata

--- src/plugins/yamlcpp/read.cpp
+++ src/plugins/yamlcpp/read.cpp
@@ -150,12 +150,8 @@ void convertNodeToKeySet (YAML::Node const & node, KeySet & mappings, Key & pare
 		for (auto element : node)
 		{
 			Key key = node.IsMap () ? newKey (element.first.as<string> (), parent) : newArrayKey (mappings, parent);
-			// Add intermediate key for array parent
-			if ((node.IsMap () ? element.second : element).IsSequence ())
-			{
-				key.setMeta ("array", "");
-				mappings.append (key);
-			}
+			if (!node.IsMap ()) mappings.append (parent); // Update array metadata
+
 			convertNodeToKeySet (node.IsMap () ? element.second : element, mappings, key);
 		}
 	}
