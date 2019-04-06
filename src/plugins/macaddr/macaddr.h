d28cc22c8cf052452d59ff3e0c6cedbe6bbeea21
T B <e01306821@student.tuwien.ac.at>
2019-04-06 15:20:01 +0200

    Created template for plugin

---
+++ src/plugins/macaddr/macaddr.h
@@ -0,0 +1,25 @@
+/**
+ * @file
+ *
+ * @brief Header for macaddr plugin
+ *
+ * @copyright BSD License (see LICENSE.md or https://www.libelektra.org)
+ *
+ */
+
+#ifndef ELEKTRA_PLUGIN_MACADDR_H
+#define ELEKTRA_PLUGIN_MACADDR_H
+
+#include <kdbplugin.h>
+
+
+int elektraMacaddrOpen (Plugin * handle, Key * errorKey);
+int elektraMacaddrClose (Plugin * handle, Key * errorKey);
+int elektraMacaddrGet (Plugin * handle, KeySet * ks, Key * parentKey);
+int elektraMacaddrSet (Plugin * handle, KeySet * ks, Key * parentKey);
+int elektraMacaddrError (Plugin * handle, KeySet * ks, Key * parentKey);
+int elektraMacaddrCheckConfig (Key * errorKey, KeySet * conf);
+
+Plugin * ELEKTRA_PLUGIN_EXPORT;
+
+#endif
