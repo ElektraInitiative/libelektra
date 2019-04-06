4f810be57789039133825a2279a3d7af5ed6979d
T B <e01306821@student.tuwien.ac.at>
2019-04-06 15:20:01 +0200

    Created template for plugin

---
+++ src/plugins/macaddr/README.md
@@ -0,0 +1,56 @@
+- infos = Information about the macaddr plugin is in keys below
+- infos/author = Author Name <elektra@libelektra.org>
+- infos/licence = BSD
+- infos/needs =
+- infos/provides =
+- infos/recommends =
+- infos/placements = prerollback rollback postrollback getresolver pregetstorage getstorage procgetstorage postgetstorage setresolver presetstorage setstorage precommit commit postcommit
+- infos/status = recommended productive maintained reviewed conformant compatible coverage specific unittest shelltest tested nodep libc configurable final preview memleak experimental difficult unfinished old nodoc concept orphan obsolete discouraged -1000000
+- infos/metadata =
+- infos/description = one-line description of macaddr
+
+## Introduction
+
+Copy this macaddr if you want to start a new
+plugin written in C.
+
+## Usage
+
+You can use `scripts/copy-macaddr`
+to automatically rename everything to your
+plugin name:
+
+```bash
+cd src/plugins
+../../scripts/copy-macaddr yourplugin
+```
+
+Then update the README.md of your newly created plugin:
+
+- enter your full name+email in `infos/author`
+- make sure `status`, `placements`, and other clauses conform to
+  descriptions in `doc/CONTRACT.ini`
+- update the one-line description above
+- add your plugin in `src/plugins/README.md`
+- and rewrite the rest of this `README.md` to give a great
+  explanation of what your plugin does
+
+## Dependencies
+
+None.
+
+## Examples
+
+```sh
+# Backup-and-Restore: user/tests/macaddr
+
+kdb set user/tests/macaddr/key value
+#> Create a new key user/tests/macaddr/key with string "value"
+
+kdb get /tests/macaddr/key
+#> value
+```
+
+## Limitations
+
+None.
---
+++ src/plugins/macaddr/macaddr.c
@@ -0,0 +1,90 @@
+/**
+ * @file
+ *
+ * @brief Source for macaddr plugin
+ *
+ * @copyright BSD License (see LICENSE.md or https://www.libelektra.org)
+ *
+ */
+
+#include "macaddr.h"
+
+#include <kdbhelper.h>
+
+
+int elektraMacaddrOpen (Plugin * handle ELEKTRA_UNUSED, Key * errorKey ELEKTRA_UNUSED)
+{
+	// plugin initialization logic
+	// this function is optional
+
+	return ELEKTRA_PLUGIN_STATUS_SUCCESS;
+}
+
+int elektraMacaddrClose (Plugin * handle ELEKTRA_UNUSED, Key * errorKey ELEKTRA_UNUSED)
+{
+	// free all plugin resources and shut it down
+	// this function is optional
+
+	return ELEKTRA_PLUGIN_STATUS_SUCCESS;
+}
+
+int elektraMacaddrGet (Plugin * handle ELEKTRA_UNUSED, KeySet * returned, Key * parentKey)
+{
+	if (!elektraStrCmp (keyName (parentKey), "system/elektra/modules/macaddr"))
+	{
+		KeySet * contract =
+			ksNew (30, keyNew ("system/elektra/modules/macaddr", KEY_VALUE, "macaddr plugin waits for your orders", KEY_END),
+			       keyNew ("system/elektra/modules/macaddr/exports", KEY_END),
+			       keyNew ("system/elektra/modules/macaddr/exports/open", KEY_FUNC, elektraMacaddrOpen, KEY_END),
+			       keyNew ("system/elektra/modules/macaddr/exports/close", KEY_FUNC, elektraMacaddrClose, KEY_END),
+			       keyNew ("system/elektra/modules/macaddr/exports/get", KEY_FUNC, elektraMacaddrGet, KEY_END),
+			       keyNew ("system/elektra/modules/macaddr/exports/set", KEY_FUNC, elektraMacaddrSet, KEY_END),
+			       keyNew ("system/elektra/modules/macaddr/exports/error", KEY_FUNC, elektraMacaddrError, KEY_END),
+			       keyNew ("system/elektra/modules/macaddr/exports/checkconf", KEY_FUNC, elektraMacaddrCheckConfig, KEY_END),
+#include ELEKTRA_README
+			       keyNew ("system/elektra/modules/macaddr/infos/version", KEY_VALUE, PLUGINVERSION, KEY_END), KS_END);
+		ksAppend (returned, contract);
+		ksDel (contract);
+
+		return ELEKTRA_PLUGIN_STATUS_SUCCESS;
+	}
+	// get all keys
+
+	return ELEKTRA_PLUGIN_STATUS_NO_UPDATE;
+}
+
+int elektraMacaddrSet (Plugin * handle ELEKTRA_UNUSED, KeySet * returned ELEKTRA_UNUSED, Key * parentKey ELEKTRA_UNUSED)
+{
+	// set all keys
+	// this function is optional
+
+	return ELEKTRA_PLUGIN_STATUS_NO_UPDATE;
+}
+
+int elektraMacaddrError (Plugin * handle ELEKTRA_UNUSED, KeySet * returned ELEKTRA_UNUSED, Key * parentKey ELEKTRA_UNUSED)
+{
+	// handle errors (commit failed)
+	// this function is optional
+
+	return ELEKTRA_PLUGIN_STATUS_SUCCESS;
+}
+
+int elektraMacaddrCheckConfig (Key * errorKey ELEKTRA_UNUSED, KeySet * conf ELEKTRA_UNUSED)
+{
+	// validate plugin configuration
+	// this function is optional
+
+	return ELEKTRA_PLUGIN_STATUS_NO_UPDATE;
+}
+
+Plugin * ELEKTRA_PLUGIN_EXPORT
+{
+	// clang-format off
+	return elektraPluginExport ("macaddr",
+		ELEKTRA_PLUGIN_OPEN,	&elektraMacaddrOpen,
+		ELEKTRA_PLUGIN_CLOSE,	&elektraMacaddrClose,
+		ELEKTRA_PLUGIN_GET,	&elektraMacaddrGet,
+		ELEKTRA_PLUGIN_SET,	&elektraMacaddrSet,
+		ELEKTRA_PLUGIN_ERROR,	&elektraMacaddrError,
+		ELEKTRA_PLUGIN_END);
+}
