66c4795ef32648ec6691138bb66facee1959470a
Dmytro Moiseiuk <dmoisej@gmail.com>
2019-04-10 10:04:11 +0200

    Add template for the new 'Unique Values' plugin in Java

---
+++ src/bindings/jna/libelektra4j/src/main/java/org/libelektra/plugin/UniqueValues.java
@@ -0,0 +1,37 @@
+package org.libelektra.plugin;
+
+import org.libelektra.Key;
+import org.libelektra.KeySet;
+import org.libelektra.Plugin;
+
+public class UniqueValues implements Plugin {
+
+    public UniqueValues() {
+        System.out.println("construct plugin");
+    }
+
+    @Override
+    public int open(final KeySet conf, final Key errorKey) {
+        return 0;
+    }
+
+    @Override
+    public int get(final KeySet ks, final Key parentKey) {
+        return 0;
+    }
+
+    @Override
+    public int set(final KeySet ks, final Key parentKey) {
+        return 0;
+    }
+
+    @Override
+    public int error(final KeySet ks, final Key parentKey) {
+        return 0;
+    }
+
+    @Override
+    public int close(final Key parentKey) {
+        return 0;
+    }
+}
