3b7d967cfc87c7d45c01e51d8c9d03c8a28fc8af
Peter Nirschl <peter.nirschl@gmail.com>
2019-04-15 19:37:05 +0200

    reformat source

--- src/tools/elektra-gpg-testkey/elektra-gpg-testkey.c
+++ src/tools/elektra-gpg-testkey/elektra-gpg-testkey.c
@@ -10,18 +10,18 @@
 #include <gpgme.h>
 #include <stdio.h>
 
-int main()
+int main ()
 {
-    gpgme_error_t err;
-    gpgme_ctx_t ctx;
-    gpgme_key_t key;
-    gpgme_genkey_result_t res;
+	gpgme_error_t err;
+	gpgme_ctx_t ctx;
+	gpgme_key_t key;
+	gpgme_genkey_result_t res;
 
 	gpgme_check_version (NULL);
 	err = gpgme_engine_check_version (GPGME_PROTOCOL_OpenPGP);
 	if (err)
 	{
-        fprintf (stderr, "gpgme version check failed: %s\n",gpgme_strerror (err));
+		fprintf (stderr, "gpgme version check failed: %s\n", gpgme_strerror (err));
 		return -1;
 	}
 
@@ -32,41 +32,42 @@ int main()
 		fprintf (stderr, "gpgme error: %s\n", gpgme_strerror (err));
 		return -1;
 	}
-	
-    // look for the elektra key
-    err = gpgme_op_keylist_start(ctx,"elektra",1 /* secret keys only! */);
-    if(err)
-    {
-        fprintf (stderr, "gpgme error: %s\n", gpgme_strerror (err));
-        goto cleanup;
-    }
-    
-    err = gpgme_op_keylist_next(ctx,&key);
-    if(err && gpg_err_code(err) != GPG_ERR_EOF)
-    {
-        fprintf (stderr, "gpgme error: %s\n", gpgme_strerror (err));
-        goto cleanup;
-    }
-    
-    if(err && gpg_err_code(err) == GPG_ERR_EOF)
-    {
-        // create the elektra demo key
-        err = gpgme_op_createkey(ctx, "elektra testkey", "default", 0, 0, NULL, GPGME_CREATE_SIGN | GPGME_CREATE_ENCR | GPGME_CREATE_NOEXPIRE | GPGME_CREATE_NOPASSWD );
-        res = gpgme_op_genkey_result(ctx);
-        fprintf(stdout, "%s", res->fpr);
-    }
-    else
-    {
-        // display the key ID
-        fprintf(stdout, "%s", key->subkeys->fpr);
-        gpgme_key_release(key);
-        gpgme_op_keylist_end(ctx);
-    }
-	
-	gpgme_release(ctx);
+
+	// look for the elektra key
+	err = gpgme_op_keylist_start (ctx, "elektra", 1 /* secret keys only! */);
+	if (err)
+	{
+		fprintf (stderr, "gpgme error: %s\n", gpgme_strerror (err));
+		goto cleanup;
+	}
+
+	err = gpgme_op_keylist_next (ctx, &key);
+	if (err && gpg_err_code (err) != GPG_ERR_EOF)
+	{
+		fprintf (stderr, "gpgme error: %s\n", gpgme_strerror (err));
+		goto cleanup;
+	}
+
+	if (err && gpg_err_code (err) == GPG_ERR_EOF)
+	{
+		// create the elektra demo key
+		err = gpgme_op_createkey (ctx, "elektra testkey", "default", 0, 0, NULL,
+					  GPGME_CREATE_SIGN | GPGME_CREATE_ENCR | GPGME_CREATE_NOEXPIRE | GPGME_CREATE_NOPASSWD);
+		res = gpgme_op_genkey_result (ctx);
+		fprintf (stdout, "%s", res->fpr);
+	}
+	else
+	{
+		// display the key ID
+		fprintf (stdout, "%s", key->subkeys->fpr);
+		gpgme_key_release (key);
+		gpgme_op_keylist_end (ctx);
+	}
+
+	gpgme_release (ctx);
 	return 0;
-    
+
 cleanup:
-    gpgme_release(ctx);
-    return -1;
+	gpgme_release (ctx);
+	return -1;
 }
