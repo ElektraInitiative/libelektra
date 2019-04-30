b0891282bf55d4a20c999517a7663a8457b01b76
René Schwaiger <sanssecours@me.com>
2019-04-30 16:02:04 +0200

    YAy PEG: Support Elektra’s boolean data type

--- src/plugins/yaypeg/listener.cpp
+++ src/plugins/yaypeg/listener.cpp
@@ -89,7 +89,14 @@ Listener::Listener (Key const & parent)
 void Listener::exitValue (string const & text)
 {
 	Key key = parents.top ();
-	key.setString (scalarToText (text));
+	if (text == "true" || text == "false")
+	{
+		key.set<bool> (text == "true");
+	}
+	else
+	{
+		key.set<string> (scalarToText (text));
+	}
 	keys.append (key);
 }
 
