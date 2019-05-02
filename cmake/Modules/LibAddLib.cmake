a031519e2411ffb4f674edc0c18af915641098f4
Michael Zronek <michael.zronek@gmail.com>
2019-05-02 21:04:25 +0200

    error: cmake reformat

--- cmake/Modules/LibAddLib.cmake
+++ cmake/Modules/LibAddLib.cmake
@@ -1,7 +1,7 @@
 function (set_additional_compile_definitions shortname)
 	if (NOT "${ARG_COMPILE_DEFINITIONS}" MATCHES "ELEKTRA_MODULE_NAME")
 		list (APPEND ADDITIONAL_COMPILE_DEFINITIONS_PARTS
-			"ELEKTRA_MODULE_NAME=${shortname}")
+			     "ELEKTRA_MODULE_NAME=${shortname}")
 	endif ()
 
 	set (ADDITIONAL_COMPILE_DEFINITIONS "${ADDITIONAL_COMPILE_DEFINITIONS_PARTS}" PARENT_SCOPE)
@@ -10,10 +10,10 @@ endfunction (set_additional_compile_definitions)
 
 function (add_lib name)
 	cmake_parse_arguments (ARG
-		"CPP" # optional keywords
-		"" # one value keywords
-		"SOURCES;LINK_LIBRARIES;LINK_ELEKTRA" # multi value keywords
-		${ARGN})
+			       "CPP" # optional keywords
+			       "" # one value keywords
+			       "SOURCES;LINK_LIBRARIES;LINK_ELEKTRA" # multi value keywords
+			       ${ARGN})
 	add_headers (ARG_SOURCES)
 	if (ARG_CPP)
 		add_cppheaders (ARG_SOURCES)
@@ -23,15 +23,15 @@ function (add_lib name)
 	add_dependencies (elektra-${name}-objects kdberrors_generated elektra_error_codes_generated)
 
 	set_property (TARGET elektra-${name}-objects
-		PROPERTY POSITION_INDEPENDENT_CODE
-		ON)
+		      PROPERTY POSITION_INDEPENDENT_CODE
+			       ON)
 
 	set_additional_compile_definitions (${name})
 
 	set_property (TARGET elektra-${name}-objects
-		APPEND
-		PROPERTY COMPILE_DEFINITIONS
-		${ADDITIONAL_COMPILE_DEFINITIONS})
+		      APPEND
+		      PROPERTY COMPILE_DEFINITIONS
+			       ${ADDITIONAL_COMPILE_DEFINITIONS})
 
 	if (BUILD_SHARED)
 		add_library (elektra-${name} SHARED $<TARGET_OBJECTS:elektra-${name}-objects>)
@@ -40,14 +40,14 @@ function (add_lib name)
 	endif (BUILD_SHARED)
 
 	set_property (GLOBAL
-		APPEND
-		PROPERTY "elektra-full_SRCS"
-		${ARG_SOURCES})
+		      APPEND
+		      PROPERTY "elektra-full_SRCS"
+			       ${ARG_SOURCES})
 
 	set_property (GLOBAL
-		APPEND
-		PROPERTY "elektra-extension_LIBRARIES"
-		elektra-${name})
+		      APPEND
+		      PROPERTY "elektra-extension_LIBRARIES"
+			       elektra-${name})
 
 	if (BUILD_SHARED)
 		target_link_libraries (elektra-${name} ${ARG_LINK_LIBRARIES})
