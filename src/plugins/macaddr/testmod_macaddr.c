65c59c0910ddd6875fdb377d5b4c22f79f8c38f8
T B <e01306821@student.tuwien.ac.at>
2019-05-25 21:31:31 +0200

    Added testmod-tests

--- src/plugins/macaddr/testmod_macaddr.c
+++ src/plugins/macaddr/testmod_macaddr.c
@@ -14,23 +14,129 @@
 
 #include <tests_plugin.h>
 
-static void test_basics (void)
+#define PLUGIN_NAME "macaddr"
+#define META "check/macaddr"
+#define MAXMACINT 281474976710655
+
+static void convertLong (char * returned, long long i)
 {
-	/*printf ("test basics\n");
+	sprintf (returned, "%lld", i);
+}
 
-	Key * parentKey = keyNew ("user/tests/macaddr", KEY_END);
+static int setKey (KeySet * testKs)
+{
+	Key * parent = keyNew ("user/tests/mac", KEY_VALUE, "", KEY_END);
+
 	KeySet * conf = ksNew (0, KS_END);
-	PLUGIN_OPEN ("macaddr");
+	PLUGIN_OPEN (PLUGIN_NAME);
+	ksRewind (testKs);
+	int ret = plugin->kdbSet (plugin, testKs, parent);
+	keyDel (parent);
+	PLUGIN_CLOSE ();
+	return ret;
+}
 
-	KeySet * ks = ksNew (0, KS_END);
+static const char * getKeyString (KeySet * ks, char * keyName)
+{
+	Key * parent = keyNew ("user/tests/mac", KEY_VALUE, "", KEY_END);
+	KeySet * conf = ksNew (0, KS_END);
+	PLUGIN_OPEN (PLUGIN_NAME);
+	ksRewind (ks);
+	plugin -> kdbGet(plugin, ks, parent);
+	keyDel (parent);
+	PLUGIN_CLOSE ();
+	return keyString (ksLookupByName (ks, keyName, 0));
+}
 
-	succeed_if (plugin->kdbGet (plugin, ks, parentKey) == ELEKTRA_PLUGIN_STATUS_NO_UPDATE, "call to kdbGet was not successful");
+static void testAddressSet (const char * keyValue, int retValue)
+{
+	KeySet * testKs = ksNew (10, keyNew ("user/tests/mac/addr", KEY_VALUE, keyValue, KEY_META, META, "", KEY_END), KS_END);
+	succeed_if (setKey (testKs) == retValue, "error");
+}
 
-	succeed_if (plugin->kdbSet (plugin, ks, parentKey) == ELEKTRA_PLUGIN_STATUS_NO_UPDATE, "call to kdbSet was not successful");
+static void testAddressesSetGet (const char * keyValue, long long longValue)
+{
+	char intChar[15];
+	KeySet * testKs = ksNew (10, keyNew ("user/tests/mac/addr", KEY_VALUE, keyValue, KEY_META, META, "", KEY_END), KS_END);
+	setKey (testKs);
+	convertLong (intChar, longValue);
+	succeed_if (!strcmp (getKeyString (testKs, "user/tests/mac/addr"), intChar), "error");
+}
 
-	keyDel (parentKey);
-	ksDel (ks);
-	PLUGIN_CLOSE ();*/
+static void testAddressesReturn ()
+{
+	testAddressesSetGet ("00:00:00:00:00:00", 0);
+	testAddressesSetGet ("FF:FF:FF:FF:FF:FF", 281474976710655);
+	testAddressesSetGet ("0d:b6:8c:44:cc:f9", 15077688528121);
+	testAddressesSetGet ("aB-Cd-8f-f3-e5-d7", 188899371771351);
+	testAddressesSetGet ("A1B2C3-4D5E6F", 177789152878191);
+
+}
+
+static void testAddressesStandardColons ()
+{
+	testAddressSet ("00:00:00:00:00:00", 1);
+	testAddressSet ("FF:FF:FF:FF:FF:FF", 1);
+	testAddressSet ("AA:BB:CC:DD:EE:FF", 1);
+	testAddressSet ("99:99:99:99:99:99", 1);
+	testAddressSet ("A1:B2:C3:4D:5E:6F", 1);
+	testAddressSet ("aB:Cd:8f:f3:e5:d7", 1);
+	testAddressSet ("aB:Cd:8f:f3:e5:g7", -1);
+	testAddressSet ("aB:Cd:8f:f3:e5e:d7", -1);
+	testAddressSet ("aB:Cd:8f:f3:e5", -1);
+	testAddressSet ("aB:Cd:8f:f3:e5", -1);
+}
+
+static void testAddressesStandardHyphens ()
+{
+
+	testAddressSet ("00-00-00-00-00-00", 1);
+	testAddressSet ("FF-FF-FF-FF-FF-FF", 1);
+	testAddressSet ("AA-BB-CC-DD-EE-FF", 1);
+	testAddressSet ("99-99-99-99-99-99", 1);
+	testAddressSet ("A1-B2-C3-4D-5E-6F", 1);
+	testAddressSet ("aB-Cd-8f-f3-e5-d7", 1);
+	testAddressSet ("aB-Cd-8f-f3-e5-g7", -1);
+	testAddressSet ("aB-Cd-8f-f3-e5e-d7", -1);
+	testAddressSet ("aB-Cd-8f-f3-e5", -1);
+	testAddressSet ("aB-Cd-8f-f3-e5", -1);
+}
+static void testAddressesSingleHyphen ()
+{
+	testAddressSet ("000000-000000", 1);
+	testAddressSet ("FFFFFF-FFFFFF", 1);
+	testAddressSet ("AABBCC-DDEEFF", 1);
+	testAddressSet ("999999-999999", 1);
+	testAddressSet ("A1B2C3-4D5E6F", 1);
+	testAddressSet ("aBCd8f-f3e5d7", 1);
+	testAddressSet ("aBCd8f-f3e5g7", -1);
+	testAddressSet ("aBCd8f-f3e5ed7", -1);
+	testAddressSet ("aBCd8f-f3e5", -1);
+	testAddressSet ("aBCd8f-f3e5", -1);
+}
+
+static void testAddressesNumber ()
+{
+	char intChar[15];
+	convertLong (intChar, 0);
+	testAddressSet (intChar, 1);
+
+	convertLong (intChar, MAXMACINT);
+	testAddressSet (intChar, 1);
+
+	convertLong (intChar, -1);
+	testAddressSet (intChar, -1);
+
+	convertLong (intChar, MAXMACINT + 1);
+	testAddressSet (intChar, -1);
+}
+static void testAll ()
+{
+	testAddressesStandardColons ();
+	testAddressesStandardHyphens ();
+	testAddressesSingleHyphen ();
+	testAddressesNumber ();
+	testAddressesReturn ();
 }
 
 
@@ -41,7 +147,7 @@ int main (int argc, char ** argv)
 
 	init (argc, argv);
 
-	test_basics ();
+	testAll ();
 
 	print_result ("testmod_macaddr");
 
