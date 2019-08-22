3607da41bfca1344be2d063446d3ca3168e006cc
Raphael <raphi@localhost.localdomain>
2019-08-22 11:54:23 +0200

    tool: elektradv2 add kdbFind handler, move handlers into seperate files

---
+++ src/tools/web/elektrad-v2/move_handler.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"net/http"
+)
+
+func PostMoveHandler(w http.ResponseWriter, r *http.Request) {
+	// kdb := kdbHandle()
+
+	// ks, err := elektra.CreateKeySet()
+
+	// if err != nil {
+	// 	writeError(w, err)
+	// 	return
+	// }
+
+	// keyName := parseKeyNameFromURL(r)
+
+	// key, _ := elektra.CreateKey(keyName)
+	// _, err = kdb.Get(ks, key)
+
+	// writeResponse(w, response)
+}
