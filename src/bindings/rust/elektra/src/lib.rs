28d28947fc561b2026ff720095d8c4343015fefe
PhilippGackstatter <philipp.gackstatter@student.tuwien.ac.at>
2019-08-23 09:37:38 +0200

    Rust: Leave only key-related bindings

--- src/bindings/rust/elektra/src/lib.rs
+++ src/bindings/rust/elektra/src/lib.rs
@@ -1,24 +1,13 @@
 extern crate bitflags;
 extern crate elektra_sys;
 
-pub mod iterator;
-pub mod kdb;
 pub mod key;
 pub mod keybuilder;
-pub mod keyset;
 pub mod readable;
 pub mod readonly;
 pub mod writable;
 
-pub use self::kdb::KDB;
-pub use self::key::{BinaryKey,KeyError, StringKey};
-pub use self::keyset::{Cursor, KeySet,KeySetError};
-
-pub use self::kdb::{
-    KDBError, KDBErrorWrapper,LogicalError, PermanentError, ResourceError,
-    ValidationError,
-};
-pub use self::iterator::{ReadOnlyStringKeyIter, StringKeyIter};
+pub use self::key::{BinaryKey, KeyError, StringKey};
 pub use self::keybuilder::KeyBuilder;
 pub use self::readable::ReadableKey;
 pub use self::readonly::ReadOnly;
