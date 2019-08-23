652f32ecfbcd832722a6ba464c1f5e47c42a88c7
Klemens Böswirth <k.boeswirth+git@gmail.com>
2019-08-23 20:29:34 +0200

    codegen: remove static help key

--- tests/shell/gen/elektra/struct.expected.h
+++ tests/shell/gen/elektra/struct.expected.h
@@ -833,7 +833,7 @@ static inline void ELEKTRA_SET (PersonName) (Elektra * elektra,
 
 
 int loadConfiguration (Elektra ** elektra, ElektraError ** error);
-void printHelpMessage (const char * usage, const char * prefix);
+void printHelpMessage (Elektra * elektra, const char * usage, const char * prefix);
 void specloadCheck (int argc, const char ** argv);
 
 
