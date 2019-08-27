f97dd89dc2f036d99ce9fa6fa33ea36f47be1b9b
PhilippGackstatter <philipp.gackstatter@student.tuwien.ac.at>
2019-08-27 10:19:21 +0200

    Rust: Add KeySet

--- src/bindings/rust/elektra/src/lib.rs
+++ src/bindings/rust/elektra/src/lib.rs
@@ -6,9 +6,11 @@ pub mod keybuilder;
 pub mod readable;
 pub mod readonly;
 pub mod writable;
+pub mod keyset;
 
 pub use self::key::{BinaryKey, StringKey, MetaIter, NameIter, KeyNameInvalidError, KeyNameReadOnlyError, KeyNotFoundError};
 pub use self::keybuilder::KeyBuilder;
 pub use self::readable::ReadableKey;
 pub use self::readonly::ReadOnly;
 pub use self::writable::WriteableKey;
+pub use self::keyset::{KeySet, KeySetError, ReadOnlyStringKeyIter, StringKeyIter, Cursor, LookupOption};
