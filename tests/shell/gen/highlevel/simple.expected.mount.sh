bda1292a7d8c95cfa07d361b2806d998592738c3
Klemens Böswirth <k.boeswirth+git@gmail.com>
2019-08-30 14:58:25 +0200

    codegen: check specload config in mountscript

--- tests/shell/gen/highlevel/simple.expected.mount.sh
+++ tests/shell/gen/highlevel/simple.expected.mount.sh
@@ -10,10 +10,25 @@ if ! [ -f "$APP_PATH" ]; then
 	exit 1
 fi
 
+error_other_mp() {
+	echo "ERROR: another mountpoint already exists on spec/tests/script/gen/highlevel/simple. Please umount first." 1>&2
+	exit 1
+}
+
 if kdb mount -13 | grep -Fxq 'spec/tests/script/gen/highlevel/simple'; then
 	if ! kdb mount | grep -Fxq 'tests_script_gen_highlevel_simple.overlay.spec.eqd on spec/tests/script/gen/highlevel/simple with name spec/tests/script/gen/highlevel/simple'; then
-		echo "ERROR: another mountpoint already exists on spec/tests/script/gen/highlevel/simple. Please umount first." 1>&2
-		exit 1
+		error_other_mp
+	fi
+
+	MP=$(echo "spec/tests/script/gen/highlevel/simple" | sed 's:\\:\\\\:g' | sed 's:/:\\/:g')
+	if [ -n "$(kdb get "system/elektra/mountpoints/$MP/getplugins/#5#specload#specload#/config/file")" ]; then
+		error_other_mp
+	fi
+	if [ "$(kdb get "system/elektra/mountpoints/$MP/getplugins/#5#specload#specload#/config/app")" != "$APP_PATH" ]; then
+		error_other_mp
+	fi
+	if [ -n "$(kdb ls "system/elektra/mountpoints/$MP/getplugins/#5#specload#specload#/config/app/args")" ]; then
+		error_other_mp
 	fi
 else
 	sudo kdb mount -R noresolver "tests_script_gen_highlevel_simple.overlay.spec.eqd" "spec/tests/script/gen/highlevel/simple" specload "app=$APP_PATH"
