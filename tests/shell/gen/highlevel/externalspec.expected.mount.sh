a02e3451d698037047c1b7f780e46dbd7b5593b4
Klemens Böswirth <k.boeswirth+git@gmail.com>
2019-08-30 14:58:25 +0200

    codegen: check specload config in mountscript

--- tests/shell/gen/highlevel/externalspec.expected.mount.sh
+++ tests/shell/gen/highlevel/externalspec.expected.mount.sh
@@ -10,10 +10,19 @@ if ! [ -f "$SPEC_FILE" ]; then
 	exit 1
 fi
 
+error_other_mp() {
+	echo "ERROR: another mountpoint already exists on spec/tests/script/gen/highlevel/externalspec. Please umount first." 1>&2
+	exit 1
+}
+
 if kdb mount -13 | grep -Fxq 'spec/tests/script/gen/highlevel/externalspec'; then
 	if ! kdb mount | grep -Fxq 'tests_script_gen_highlevel_externalspec.overlay.spec.eqd on spec/tests/script/gen/highlevel/externalspec with name spec/tests/script/gen/highlevel/externalspec'; then
-		echo "ERROR: another mountpoint already exists on spec/tests/script/gen/highlevel/externalspec. Please umount first." 1>&2
-		exit 1
+		error_other_mp
+	fi
+
+	MP=$(echo "spec/tests/script/gen/highlevel/externalspec" | sed 's:\\:\\\\:g' | sed 's:/:\\/:g')
+	if [ "$(kdb get "system/elektra/mountpoints/$MP/getplugins/#5#specload#specload#/config/file")" != "$SPEC_FILE" ]; then
+		error_other_mp
 	fi
 else
 	sudo kdb mount -R noresolver "tests_script_gen_highlevel_externalspec.overlay.spec.eqd" "spec/tests/script/gen/highlevel/externalspec" specload "file=$SPEC_FILE"
--- tests/shell/gen/highlevel/nosetter.expected.mount.sh
+++ tests/shell/gen/highlevel/nosetter.expected.mount.sh
@@ -10,10 +10,25 @@ if ! [ -f "$APP_PATH" ]; then
 	exit 1
 fi
 
+error_other_mp() {
+	echo "ERROR: another mountpoint already exists on spec/tests/script/gen/highlevel/nosetter. Please umount first." 1>&2
+	exit 1
+}
+
 if kdb mount -13 | grep -Fxq 'spec/tests/script/gen/highlevel/nosetter'; then
 	if ! kdb mount | grep -Fxq 'tests_script_gen_highlevel_nosetter.overlay.spec.eqd on spec/tests/script/gen/highlevel/nosetter with name spec/tests/script/gen/highlevel/nosetter'; then
-		echo "ERROR: another mountpoint already exists on spec/tests/script/gen/highlevel/nosetter. Please umount first." 1>&2
-		exit 1
+		error_other_mp
+	fi
+
+	MP=$(echo "spec/tests/script/gen/highlevel/nosetter" | sed 's:\\:\\\\:g' | sed 's:/:\\/:g')
+	if [ -n "$(kdb get "system/elektra/mountpoints/$MP/getplugins/#5#specload#specload#/config/file")" ]; then
+		error_other_mp
+	fi
+	if [ "$(kdb get "system/elektra/mountpoints/$MP/getplugins/#5#specload#specload#/config/app")" != "$APP_PATH" ]; then
+		error_other_mp
+	fi
+	if [ -n "$(kdb ls "system/elektra/mountpoints/$MP/getplugins/#5#specload#specload#/config/app/args")" ]; then
+		error_other_mp
 	fi
 else
 	sudo kdb mount -R noresolver "tests_script_gen_highlevel_nosetter.overlay.spec.eqd" "spec/tests/script/gen/highlevel/nosetter" specload "app=$APP_PATH"
