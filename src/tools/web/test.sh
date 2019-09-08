e2af8875d1e3ae1a32e2326f609a325d5dcb9332
Raphael Gruber <raphi011@gmail.com>
2019-09-08 10:52:45 +0200

    tool: elektrad adapt cmake config

--- src/tools/web/test.sh
+++ src/tools/web/test.sh
@@ -3,8 +3,7 @@ cd @CMAKE_CURRENT_SOURCE_DIR@
 
 echo " -~- Testing API elektra-web > elektrad -~- "
 cd elektrad
-npm install
-npm test
+go test ./...
 cd ..
 
 echo " -~- Testing API elektra-web > webd -~- "
