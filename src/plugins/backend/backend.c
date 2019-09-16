adc1700765d03089c49b091ae4a21ca1322d3a6d
Vid Leskovar <vid.leskovar5@gmail.com>
2019-09-16 21:16:12 +0200

    backend: added basic files for backend plugin
    Discussed in #2963

--- src/plugins/backend/backend.c
+++ src/plugins/backend/backend.c
@@ -1,4 +1,41 @@
-//
-// Created by toasterpro on 16.9.2019.
-//
 
+
+#include "backend.h"
+
+#include <kdbprivate.h>
+
+int elektraBackendOpen (Plugin * handle, Key * errorKey)
+{
+	return ELEKTRA_PLUGIN_STATUS_SUCCESS;
+}
+
+int elektraBackendClose (Plugin * handle, Key * errorKey)
+{
+	return ELEKTRA_PLUGIN_STATUS_SUCCESS;
+}
+
+int elektraBackendGet (Plugin * handle, KeySet * ks, Key * parentKey)
+{
+	return ELEKTRA_PLUGIN_STATUS_SUCCESS;
+}
+
+int elektraBackendSet (Plugin * handle, KeySet * ks, Key * parentKey)
+{
+	return ELEKTRA_PLUGIN_STATUS_SUCCESS;
+}
+
+int elektraBackendCommit (Plugin * handle, KeySet * ks, Key * parentKey)
+{
+	return ELEKTRA_PLUGIN_STATUS_SUCCESS;
+}
+
+Plugin * ELEKTRA_PLUGIN_EXPORT
+{
+	return elektraPluginExport ("backend",
+		ELEKTRA_PLUGIN_OPEN,	&elektraBackendOpen,
+		ELEKTRA_PLUGIN_CLOSE,	&elektraBackendClose,
+		ELEKTRA_PLUGIN_GET,	&elektraBackendGet,
+		ELEKTRA_PLUGIN_SET,	&elektraBackendSet,
+		ELEKTRA_PLUGIN_COMMIT,	&elektraBackendCommit,
+		ELEKTRA_PLUGIN_END);
+}
--- src/plugins/backend/backend.h
+++ src/plugins/backend/backend.h
@@ -1,8 +1,22 @@
-//
-// Created by toasterpro on 16.9.2019.
-//
+/**
+ * @file
+ *
+ * @brief Header file for the backend plugin
+ *
+ * @copyright BSD License (see LICENSE.md or https://www.libelektra.org)
+ */
 
 #ifndef ELEKTRA_BACKEND_H
 #define ELEKTRA_BACKEND_H
+
+#include <kdbplugin.h>
+
+int elektraBackendOpen (Plugin * handle, Key * errorKey);
+int elektraBackendClose (Plugin * handle, Key * errorKey);
+int elektraBackendGet (Plugin * handle, KeySet * ks, Key * parentKey);
+int elektraBackendSet (Plugin * handle, KeySet * ks, Key * parentKey);
+int elektraBackendCommit (Plugin * handle, KeySet * ks, Key * parentKey);
+
+Plugin * ELEKTRA_PLUGIN_EXPORT;
 
 #endif //ELEKTRA_BACKEND_H
