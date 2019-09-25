16cab112f391b81884e787cb8361e10505ff4c3b
Dominic Jäger <dominic.jaeger@gmail.com>
2019-09-25 15:17:38 +0200

    Merge: Initial array handling

--- src/tools/kdb/cmerge.cpp
+++ src/tools/kdb/cmerge.cpp
@@ -110,6 +110,7 @@ int CMergeCommand::execute (Cmdline const & cl ELEKTRA_UNUSED)
 	Key * informationKey = keyNew (0, KEY_END);
 	ckdb::KeySet * c_merge_result = elektraMerge (c_ours, oursRoot.getKey (), c_theirs, theirsRoot.getKey (), c_base,
 						      baseRoot.getKey (), resultRoot.getKey (), strategy, informationKey);
+	keyDel (informationKey);
 	if (c_merge_result != NULL)
 	{
 		kdb::KeySet merge_result = c_merge_result;
