bac331426f16446730a8b2bd165e498de35de3e2
Restyled.io <commits@restyled.io>
2019-10-13 20:11:22 +0000

    Restyled by prettier

--- src/tools/web/webd/src/index.js
+++ src/tools/web/webd/src/index.js
@@ -10,45 +10,50 @@
  * @copyright BSD License (see LICENSE.md or https://www.libelektra.org)
  */
 
-import makeLog from './log'
-const { info, error } = makeLog()
+import makeLog from "./log";
+const { info, error } = makeLog();
 
-import { name as packageName, version as packageVersion } from '../package.json'
-import getVersions from './versions'
-import initApp from './app'
-import kdb from '../../kdb'
+import {
+  name as packageName,
+  version as packageVersion
+} from "../package.json";
+import getVersions from "./versions";
+import initApp from "./app";
+import kdb from "../../kdb";
 
-import { getInstances } from './db'
+import { getInstances } from "./db";
 
-info(`%s v%s starting`, packageName, packageVersion)
+info(`%s v%s starting`, packageName, packageVersion);
 getVersions()
   .then(versions => {
     if (!versions.elektra) {
-      error(`couldn't detect elektra version`)
-      error(`are you sure you have libelektra and kdb installed?`)
-      process.exit(1)
+      error(`couldn't detect elektra version`);
+      error(`are you sure you have libelektra and kdb installed?`);
+      process.exit(1);
     } else {
-      const { major, minor, micro } = versions.elektra
-      const versionSupported = major >= 0 && minor >= 9 && micro >= 0
+      const { major, minor, micro } = versions.elektra;
+      const versionSupported = major >= 0 && minor >= 9 && micro >= 0;
       if (!versionSupported) {
-        error(`you are running an old libelektra version, which is not supported`)
-        error(`please upgrade to libelektra 0.9.0 or higher`)
-        process.exit(1)
+        error(
+          `you are running an old libelektra version, which is not supported`
+        );
+        error(`please upgrade to libelektra 0.9.0 or higher`);
+        process.exit(1);
       }
       return getInstances() // make sure yajl is installed
         .then(() => {
-          if (kdb.KDB_COMMAND === 'kdb') info(`|- using default kdb command`)
-          else info(`|- using kdb from: ${kdb.KDB_COMMAND}`)
-          info(`|- versions: %o`, versions)
-          initApp(port => info(`\`-> running on http://localhost:${port}`))
+          if (kdb.KDB_COMMAND === "kdb") info(`|- using default kdb command`);
+          else info(`|- using kdb from: ${kdb.KDB_COMMAND}`);
+          info(`|- versions: %o`, versions);
+          initApp(port => info(`\`-> running on http://localhost:${port}`));
         })
-        .catch((err) => {
-          if (err.message.includes('Was not able to load such a plugin!')) {
-            error(`missing dependencies`)
-            error(`the yajl plugin is not installed for libelektra`)
-            process.exit(1)
-          } else throw err // re-throw error
-        })
+        .catch(err => {
+          if (err.message.includes("Was not able to load such a plugin!")) {
+            error(`missing dependencies`);
+            error(`the yajl plugin is not installed for libelektra`);
+            process.exit(1);
+          } else throw err; // re-throw error
+        });
     }
   })
-  .catch(err => error(`error while starting %s: %o`, packageName, err))
+  .catch(err => error(`error while starting %s: %o`, packageName, err));
