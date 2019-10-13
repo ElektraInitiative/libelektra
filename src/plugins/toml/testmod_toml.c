5c3e8bf38511c01c4f0d816f22ee0f8906cb794e
Jakob Fischer <jakobfischer93@gmail.com>
2019-10-13 19:09:30 +0200

    toml: fixed table array meta key, improved tests

--- src/plugins/toml/testmod_toml.c
+++ src/plugins/toml/testmod_toml.c
@@ -13,28 +13,50 @@
 
 #include "toml.h"
 
+#define PREFIX "user/tests/toml-read"
 
 void testRead (const char * filename, KeySet * expected)
 {
-	Key * parentKey = keyNew ("user/tests/toml-read", KEY_VALUE, srcdir_file (filename), KEY_END);
+	Key * parentKey = keyNew (PREFIX, KEY_VALUE, srcdir_file (filename), KEY_END);
 	KeySet * conf = ksNew (0, KS_END);
 	PLUGIN_OPEN ("toml");
+	Key * root = ksLookupByName (expected, PREFIX, KDB_O_POP);
+	if (root != NULL)
+	{
+		if (strcmp (keyString (root), "@CONFIG_FILEPATH@") == 0)
+		{
+			keySetString (root, srcdir_file (filename));
+			ksAppendKey (expected, root);
+		}
+	}
 	KeySet * ks = ksNew (0, KS_END);
-	succeed_if (plugin->kdbGet (plugin, ks, parentKey) == 1, "kdbGet failed");
+	succeed_if (plugin->kdbGet (plugin, ks, parentKey) == ELEKTRA_PLUGIN_STATUS_SUCCESS, "kdbGet failed");
+	compare_keyset (expected, ks);
 
 	PLUGIN_CLOSE ();
 }
+
 
 int main (int argc, char ** argv)
 {
 	init (argc, argv);
 
-    // TODO: proper testing with expectations
-	// testRead ("toml/basic.toml", NULL);
-    // testRead ("toml/table.toml", NULL);
-    // testRead ("toml/array.toml", NULL);
-    testRead ("toml/table_array_basic.toml", NULL);
-    testRead ("toml/table_array_sub.toml", NULL);
+	{
+#include "toml/basic.h"
+		testRead ("toml/basic.toml", expected);
+		ksDel (expected);
+	}
+
+	{
+#include "toml/array.h"
+		testRead ("toml/array.toml", expected);
+		ksDel (expected);
+	}
+	{
+#include "toml/table_array_basic.h"
+		testRead ("toml/table_array_basic.toml", expected);
+		ksDel (expected);
+    }
 
 	print_result ("testmod_toml");
 	return nbError;
---
+++ src/plugins/toml/toml/basic.h
@@ -0,0 +1,7 @@
+KeySet * expected = ksNew (4,
+    keyNew (PREFIX, KEY_VALUE, "@CONFIG_FILEPATH@", KEY_END),
+    keyNew (PREFIX "/a", KEY_VALUE, "3", KEY_END),
+    keyNew (PREFIX "/a/b/c/d", KEY_VALUE, "5", KEY_END),
+    keyNew (PREFIX "/x", KEY_VALUE, "hello", KEY_END),
+    KS_END
+);
