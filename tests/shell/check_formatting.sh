684b196a5b6140b8ea7bca32ed122d4ccb2ac73c
René Schwaiger <sanssecours@me.com>
2019-10-13 13:06:56 +0200

    Check Formatting: Also check JavaScript code

--- tests/shell/check_formatting.sh
+++ tests/shell/check_formatting.sh
@@ -28,6 +28,7 @@ reformat() {
 
 reformat reformat-source &
 reformat reformat-cmake &
+reformat reformat-javascript &
 reformat reformat-markdown &
 reformat reformat-shfmt &
 wait
@@ -39,13 +40,14 @@ If you see this message on one of the build servers, you can either install one 
 
 - [`clang-format`](https://clang.llvm.org/docs/ClangFormat.html) to format C and C++ source code,
 - [`cmake_format`](https://github.com/cheshirekow/cmake_format) to format CMake code,
-- [`prettier`](https://prettier.io) to format Markdown code, and
+- [`prettier`](https://prettier.io) to format JavaScript & Markdown code, and
 - [`shfmt`](https://github.com/mvdan/sh) to format Shell code
 
 . Afterwards you can use the following scripts to fix the formatting problems
 
 - `reformat-source` to format C/C++ source files,
 - `reformat-cmake` to format CMake files,
+- `reformat-javascript` to format JavaScript files,
 - `reformat-markdown` to format Markdown files, and
 - `reformat-shfmt` to format files that contain shell code
 
