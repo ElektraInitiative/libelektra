8dd6a04db931e0293bf3b3c05813c93eaa0e93f9
Jakob Fischer <jakobfischer93@gmail.com>
2019-10-21 15:40:52 +0200

    toml: Added tests for utf8

--- src/plugins/toml/testmod_toml.c
+++ src/plugins/toml/testmod_toml.c
@@ -16,30 +16,97 @@
 #define PREFIX "user/tests/toml-read"
 
 
-static void printKs (KeySet * ks, const char * name) {
-    printf("######KEYSET: %s\n", name);
-    ksRewind (ks);
-    Key * key = ksNext(ks);
-    while (key != NULL) {
-        printf("Key: '%s'\t->\t'%s'", keyName(key), keyString(key));
+static void printKs (KeySet * ks, const char * name)
+{
+	printf ("######KEYSET: %s\n", name);
+	ksRewind (ks);
+	Key * key = ksNext (ks);
+	while (key != NULL)
+	{
+		printf ("Key: '%s'\t->\t'%s'", keyName (key), keyString (key));
 
-        keyRewindMeta (key);
-        const Key * meta = keyNextMeta (key);
-        while (meta != NULL) {
-            printf("\n\tMeta: '%s'\t->\t'%s'", keyName(meta), keyString(meta));
-            meta = keyNextMeta (key);
-        }
-        key = ksNext(ks);
-        printf("\n");
-    }
+		keyRewindMeta (key);
+		const Key * meta = keyNextMeta (key);
+		while (meta != NULL)
+		{
+			printf ("\n\tMeta: '%s'\t->\t'%s'", keyName (meta), keyString (meta));
+			meta = keyNextMeta (key);
+		}
+		key = ksNext (ks);
+		printf ("\n");
+	}
 }
 
-void testRead (const char * filename, KeySet * expected)
+static void showDiff (KeySet * expected, KeySet * is, const char * name, bool stopOnFirstDiff)
+{
+	printf ("###### Diffs in '%s'\n", name);
+	ksRewind (expected);
+	ksRewind (is);
+	Key * kExp = ksNext (expected);
+	Key * kIs = ksNext (is);
+	while (kExp != NULL && kIs != NULL)
+	{
+		if (keyCmp (kExp, kIs) != 0)
+		{
+			printf ("KeyName diff:\n\texpected\t= '%s'\n\tfound\t\t= '%s'\n", keyName (kExp), keyName (kIs));
+			if (stopOnFirstDiff)
+			{
+				return;
+			}
+		}
+		keyRewindMeta (kExp);
+		keyRewindMeta (kIs);
+		const Key * metaExp = keyNextMeta (kExp);
+		const Key * metaIs = keyNextMeta (kIs);
+		while (metaExp != NULL && metaIs != NULL)
+		{
+			if (keyCmp (metaExp, metaIs) != 0 || strcmp (keyString (metaExp), keyString (metaIs)) != 0)
+			{
+				printf ("MetaKey diff:\n\texpected\t= '%s'\n\tcontent:\t'%s' = '%s'\n\tfound\t\t= '%s'\n\tcontent:\t'%s' = '%s'\n", keyName (kExp),
+					keyName (metaExp), keyString (metaExp), keyName (kIs), keyName (metaIs), keyString (metaIs));
+				if (stopOnFirstDiff)
+				{
+					return;
+				}
+			}
+			metaExp = keyNextMeta (kExp);
+			metaIs = keyNextMeta (kIs);
+		}
+		if (metaExp != NULL || metaIs != NULL)
+		{
+			printf ("Mismatching metakeys count, there are %s metakeys generated than expected\n",
+				metaIs == NULL ? "less" : "too much");
+			printf ("Affected keys:\n\texpected\t= '%s'\n\tfound\t\t= '%s'\n", keyName (kExp), keyName (kIs));
+			Key * overhead = metaExp != NULL ? kExp : kIs;
+			const Key * meta = metaExp != NULL ? metaExp : metaIs;
+			do
+			{
+				printf ("%s Metakeys: '%s': '%s'\n", metaIs == NULL ? "Missing" : "Overhead", keyName (meta),
+					keyValue (meta));
+				meta = keyNextMeta (overhead);
+			} while (meta != NULL);
+			if (stopOnFirstDiff)
+			{
+				return;
+			}
+		}
+		kExp = ksNext (expected);
+		kIs = ksNext (is);
+	}
+	if (kExp != NULL || kIs != NULL)
+	{
+		printf ("Mismatching keyset size, there are %s keys generated than expected\n", kIs == NULL ? "less" : "too much");
+		return;
+	}
+	printf ("No diffs found!\n");
+}
+
+static void testRead (const char * filename, KeySet * expected)
 {
 	// printf ("################################################\n");
 	// printf ("############ testRead (%s)\n", filename);
 	// printf ("################################################\n");
-    printf("Reading '%s'\n", filename);
+	printf ("Reading '%s'\n", filename);
 	Key * parentKey = keyNew (PREFIX, KEY_VALUE, srcdir_file (filename), KEY_END);
 	KeySet * conf = ksNew (0, KS_END);
 	PLUGIN_OPEN ("toml");
@@ -56,8 +123,9 @@ void testRead (const char * filename, KeySet * expected)
 	succeed_if (plugin->kdbGet (plugin, ks, parentKey) == ELEKTRA_PLUGIN_STATUS_SUCCESS, "kdbGet failed");
 	compare_keyset (expected, ks);
 
-    // printKs (expected, "expected");
-    // printKs (ks, "parsed");
+	// printKs (expected, "expected");
+	// printKs (ks, "parsed");
+	showDiff (expected, ks, filename, true);
 
 	PLUGIN_CLOSE ();
 	ksDel (expected);
@@ -71,6 +139,9 @@ int main (int argc, char ** argv)
 	testRead ("toml/basic.toml",
 #include "toml/basic.h"
 	);
+	testRead ("toml/utf8.toml",
+#include "toml/utf8.h"
+	);
 	testRead ("toml/date.toml",
 #include "toml/date.h"
 	);
@@ -78,8 +149,8 @@ int main (int argc, char ** argv)
 	testRead ("toml/array.toml",
 #include "toml/array.h"
 	);
-	
-    testRead ("toml/simple_table.toml",
+
+	testRead ("toml/simple_table.toml",
 #include "toml/simple_table.h"
 	);
 
@@ -91,25 +162,25 @@ int main (int argc, char ** argv)
 #include "toml/table_array_nested.h"
 	);
 
-    testRead ("toml/table_array_table_mixed.toml",
+	testRead ("toml/table_array_table_mixed.toml",
 #include "toml/table_array_table_mixed.h"
-    );
+	);
 
 	testRead ("toml/inline_table.toml",
 #include "toml/inline_table.h"
 	);
 
-    testRead ("toml/inline_table_empty.toml",
+	testRead ("toml/inline_table_empty.toml",
 #include "toml/inline_table_empty.h"
-    );
+	);
 
-    testRead ("toml/inline_table_multiline_values.toml",
+	testRead ("toml/inline_table_multiline_values.toml",
 #include "toml/inline_table_multiline_values.h"
-    );
-    
-    testRead ("toml/comment.toml",
+	);
+
+	testRead ("toml/comment.toml",
 #include "toml/comment.h"
-    );
+	);
 
 	print_result ("testmod_toml");
 	return nbError;
