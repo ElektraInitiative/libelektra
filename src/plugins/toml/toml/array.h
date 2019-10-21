1f8316a6fe38a9fc85c1759b74c535ed438569e0
Jakob Fischer <jakobfischer93@gmail.com>
2019-10-21 20:02:57 +0200

    toml: Working on array parsing
    Trailing commas seem to work when ] is on same line, but not otherwise. Must look further into it.

--- src/plugins/toml/toml/array.h
+++ src/plugins/toml/toml/array.h
@@ -8,6 +8,14 @@ ksNew (16,
     keyNew (PREFIX "/array_1/#2", KEY_VALUE, "3", KEY_END),
     keyNew (PREFIX "/array_1/#3", KEY_VALUE, "4", KEY_END),
     keyNew (PREFIX "/array_1/#4", KEY_VALUE, "5", KEY_END),
+    
+    keyNew (PREFIX "/array_trailing_comma_1", KEY_META, "array", "#1", KEY_END),
+    keyNew (PREFIX "/array_trailing_comma_1/#0", KEY_VALUE, "1", KEY_END),
+    keyNew (PREFIX "/array_trailing_comma_1/#1", KEY_VALUE, "2", KEY_END),
+
+    keyNew (PREFIX "/array_trailing_comma_2", KEY_META, "array", "#1", KEY_END),
+    keyNew (PREFIX "/array_trailing_comma_2/#0", KEY_VALUE, "1", KEY_END),
+    keyNew (PREFIX "/array_trailing_comma_2/#1", KEY_VALUE, "2", KEY_END),
 
     keyNew (PREFIX "/array_empty", KEY_META, "array", "", KEY_END),
 
