06a763632e08abead5c911e780ccff75b243a009
Jakob Fischer <jakobfischer93@gmail.com>
2019-10-24 19:02:08 +0200

    toml: Fixing tests with order metakey

--- src/plugins/toml/toml/date.h
+++ src/plugins/toml/toml/date.h
@@ -3,15 +3,16 @@ ksNew (16,
     keyNew (PREFIX, KEY_VALUE, "@CONFIG_FILEPATH@", KEY_END),
 
     keyNew (PREFIX "/local_date_1", KEY_META, "order", "0", KEY_VALUE, "0000-01-01", KEY_END),
-    keyNew (PREFIX "/local_date_2", KEY_VALUE,KEY_META, "order", "1", "9999-12-31", KEY_END),
-    keyNew (PREFIX "/local_time_1", KEY_VALUE, "00:00:00", KEY_END),
-    keyNew (PREFIX "/local_time_2", KEY_VALUE, "23:59:60", KEY_END),
-    keyNew (PREFIX "/local_time_3", KEY_VALUE, "23:59:59.99999", KEY_END),
-    keyNew (PREFIX "/local_datetime_1", KEY_VALUE, "2000-12-31T10:00:00Z", KEY_END),
-    keyNew (PREFIX "/local_datetime_2", KEY_VALUE, "1985-04-12T23:20:50.52Z", KEY_END),
-    keyNew (PREFIX "/local_datetime_3", KEY_VALUE, "1990-12-31T23:59:60Z", KEY_END),
-    keyNew (PREFIX "/offset_datetime_1", KEY_VALUE, "1600-02-29T23:59:59+23:59", KEY_END),
-    keyNew (PREFIX "/offset_datetime_2", KEY_VALUE, "1937-01-01T12:00:27.87+00:20", KEY_END),
+    keyNew (PREFIX "/local_date_2", KEY_META, "order", "1", KEY_VALUE,"9999-12-31", KEY_END),
+    keyNew (PREFIX "/local_time_1", KEY_META, "order", "2", KEY_VALUE, "00:00:00", KEY_END),
+    keyNew (PREFIX "/local_time_2", KEY_META, "order", "3", KEY_VALUE, "23:59:60", KEY_END),
+    keyNew (PREFIX "/local_time_3", KEY_META, "order", "4", KEY_VALUE, "23:59:59.99999", KEY_END),
+    keyNew (PREFIX "/local_datetime_1", KEY_META, "order", "5", KEY_VALUE, "2000-12-31T10:00:00Z", KEY_END),
+    keyNew (PREFIX "/local_datetime_2", KEY_META, "order", "6", KEY_VALUE, "1985-04-12T23:20:50.52Z", KEY_END),
+    keyNew (PREFIX "/local_datetime_3", KEY_META, "order", "7", KEY_VALUE, "1990-12-31T23:59:60Z", KEY_END),
+    keyNew (PREFIX "/offset_datetime_1", KEY_META, "order", "8", KEY_VALUE, "1600-02-29T23:59:59+23:59", KEY_END),
+    keyNew (PREFIX "/offset_datetime_2", KEY_META, "order", "9", KEY_VALUE, "1937-01-01T12:00:27.87+00:20", KEY_END),
+
 
     KS_END
 )
--- src/plugins/toml/toml/table_array_table_mixed.h
+++ src/plugins/toml/toml/table_array_table_mixed.h
@@ -1,13 +1,38 @@
-ksNew (16, keyNew (PREFIX, KEY_VALUE, "@CONFIG_FILEPATH@", KEY_END),
+// clang-format off
+ksNew (16,
 
-       keyNew (PREFIX "/table_array", KEY_META, "array", "#1", KEY_META, "type", "tablearray", KEY_END),
+	keyNew (PREFIX, KEY_VALUE, "@CONFIG_FILEPATH@", KEY_END),
 
-       keyNew (PREFIX "/table_array/#0/a", KEY_VALUE, "0", KEY_END),
-       keyNew (PREFIX "/table_array/#0/table", KEY_META, "type", "simpletable", KEY_END),
-       keyNew (PREFIX "/table_array/#0/table/b", KEY_VALUE, "1", KEY_END),
+	keyNew (PREFIX "/table_array",
+		KEY_META, "array", "#1",
+		KEY_META, "type", "tablearray",
+		KEY_META, "order", "0",
+	KEY_END),
 
-       keyNew (PREFIX "/table_array/#1/a", KEY_VALUE, "10", KEY_END),
-       keyNew (PREFIX "/table_array/#1/table", KEY_META, "type", "simpletable", KEY_END),
-       keyNew (PREFIX "/table_array/#1/table/b", KEY_VALUE, "20", KEY_END),
+	keyNew (PREFIX "/table_array/#0/a",
+		KEY_VALUE, "0",
+		KEY_META, "order", "1",
+	KEY_END),
+	keyNew (PREFIX "/table_array/#0/table",
+		KEY_META, "type", "simpletable",
+		KEY_META, "order", "2",
+	KEY_END),
+	keyNew (PREFIX "/table_array/#0/table/b",
+		KEY_VALUE, "1",
+		KEY_META, "order", "3",
+	KEY_END),
 
-       KS_END)
+	keyNew (PREFIX "/table_array/#1/a",
+		KEY_VALUE, "10",
+		KEY_META, "order", "4",
+	KEY_END),
+	keyNew (PREFIX "/table_array/#1/table",
+		KEY_META, "type", "simpletable",
+		KEY_META, "order", "5",
+	KEY_END),
+	keyNew (PREFIX "/table_array/#1/table/b",
+		KEY_VALUE, "20",
+		KEY_META, "order", "6",
+	KEY_END),
+
+KS_END)
