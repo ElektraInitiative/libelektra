18c9b05cbcf5e75210a8a4438027083aa1e21398
Jakob Fischer <jakobfischer93@gmail.com>
2019-10-25 21:38:39 +0200

    toml: Changes in error handling.
    Not sure why cant find ELEKTRA_SET_ERROR?

--- src/plugins/toml/error.c
+++ src/plugins/toml/error.c
@@ -1,11 +1,12 @@
 #include "error.h"
 
 #include "driver.h"
+#include "error_code.h"
 
 extern int yylineno;
 
 int yyerror (Driver * driver, const char * msg)
 {
-	driverError (driver, yylineno, msg);
+	driverError (driver, ERROR_SYNTACTIC, yylineno, msg);
 	return 0;
 }
