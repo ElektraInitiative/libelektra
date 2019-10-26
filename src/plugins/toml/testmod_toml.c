8a016cf4883bf47288bed5e7721f6ff73a18c3b0
Jakob Fischer <jakobfischer93@gmail.com>
2019-10-26 13:51:30 +0200

    toml: Added infrastructure for testing errors

--- src/plugins/toml/testmod_toml.c
+++ src/plugins/toml/testmod_toml.c
@@ -7,6 +7,7 @@
  *
  */
 
+#include <assert.h>
 #include <kdbprivate.h>
 #include <tests.h>
 #include <tests_plugin.h>
@@ -15,6 +16,84 @@
 
 #define PREFIX "user/tests/toml-read"
 
+static void testReadCompare (const char * filename, KeySet * expected);
+static void testReadCompareError (const char * filename, KeySet * expected);
+static void testCompareMetakey (Key * expected, Key * found, const char * metaKeyName);
+static void testCompareErrors (Key * expected, Key * found);
+static void printKs (KeySet * ks, const char * name);
+static void showDiff (KeySet * expected, KeySet * is, const char * name, bool stopOnFirstDiff);
+
+
+static void testsPositiveCompareKeySets (void)
+{
+	testReadCompare ("toml/basic.toml",
+#include "toml/basic.h"
+	);
+	testReadCompare ("toml/utf8.toml",
+#include "toml/utf8.h"
+	);
+
+	/*    testReadCompare ("toml/multiline_strings.toml",
+	#include "toml/multiline_strings.h"
+	    );*/
+
+	testReadCompare ("toml/date.toml",
+#include "toml/date.h"
+	);
+
+	testReadCompare ("toml/array.toml",
+#include "toml/array.h"
+	);
+
+	testReadCompare ("toml/simple_table.toml",
+#include "toml/simple_table.h"
+	);
+
+	testReadCompare ("toml/table_array.toml",
+#include "toml/table_array.h"
+	);
+
+	testReadCompare ("toml/table_array_nested.toml",
+#include "toml/table_array_nested.h"
+	);
+
+	testReadCompare ("toml/table_array_table_mixed.toml",
+#include "toml/table_array_table_mixed.h"
+	);
+
+	testReadCompare ("toml/inline_table.toml",
+#include "toml/inline_table.h"
+	);
+
+	testReadCompare ("toml/inline_table_empty.toml",
+#include "toml/inline_table_empty.h"
+	);
+
+	testReadCompare ("toml/inline_table_multiline_values.toml",
+#include "toml/inline_table_multiline_values.h"
+	);
+
+	testReadCompare ("toml/comment.toml",
+#include "toml/comment.h"
+	);
+}
+
+static void testNegativeCompareErrors(void) {
+	testReadCompareError ("toml/duplicate_key.toml",
+#include "toml/duplicate_key.h"
+	);
+}
+
+int main (int argc, char ** argv)
+{
+	init (argc, argv);
+
+	testsPositiveCompareKeySets ();
+	testNegativeCompareErrors ();
+
+	print_result ("testmod_toml");
+	return nbError;
+}
 
 static void printKs (KeySet * ks, const char * name)
 {
@@ -35,6 +114,100 @@ static void printKs (KeySet * ks, const char * name)
 		key = ksNext (ks);
 		printf ("\n");
 	}
+}
+
+
+static void testReadCompare (const char * filename, KeySet * expected)
+{
+	printf ("Reading '%s'\n", filename);
+	Key * parentKey = keyNew (PREFIX, KEY_VALUE, srcdir_file (filename), KEY_END);
+	KeySet * conf = ksNew (0, KS_END);
+	PLUGIN_OPEN ("toml");
+	Key * root = ksLookupByName (expected, PREFIX, KDB_O_POP);
+	if (root != NULL)
+	{
+		if (strcmp (keyString (root), "@CONFIG_FILEPATH@") == 0)
+		{
+			keySetString (root, srcdir_file (filename));
+			ksAppendKey (expected, root);
+		}
+	}
+	KeySet * ks = ksNew (0, KS_END);
+	succeed_if (plugin->kdbGet (plugin, ks, parentKey) == ELEKTRA_PLUGIN_STATUS_SUCCESS,
+		    "Expected kdbGet to succeed, but got failure.");
+	compare_keyset (expected, ks);
+
+	showDiff (expected, ks, filename, true);
+
+	PLUGIN_CLOSE ();
+	ksDel (expected);
+}
+
+static void testReadCompareError (const char * filename, KeySet * expected)
+{
+	printf ("Reading '%s'\n", filename);
+	Key * parentKey = keyNew (PREFIX, KEY_VALUE, srcdir_file (filename), KEY_END);
+	KeySet * conf = ksNew (0, KS_END);
+	PLUGIN_OPEN ("toml");
+	Key * root = ksLookupByName (expected, PREFIX, KDB_O_POP);
+	if (root != NULL)
+	{
+		if (strcmp (keyString (root), "@CONFIG_FILEPATH@") == 0)
+		{
+			keySetString (root, srcdir_file (filename));
+			ksAppendKey (expected, root);
+		}
+	}
+	KeySet * ks = ksNew (0, KS_END);
+	succeed_if (plugin->kdbGet (plugin, ks, parentKey) == ELEKTRA_PLUGIN_STATUS_ERROR, "Expected kdbGet to fail, but got success.");
+
+	Key * foundRoot = ksLookupByName (ks, PREFIX, 0);
+	succeed_if (foundRoot != NULL, "Could not find root key");
+	testCompareErrors (root, foundRoot);
+
+	PLUGIN_CLOSE ();
+	ksDel (expected);
+}
+
+static void testCompareErrors (Key * expected, Key * found)
+{
+	assert(expected != NULL);
+	assert(found != NULL);
+	char * metaNames[] = { "error/module", "error/description", NULL };
+	for (int i = 0; metaNames[i] != NULL; i++)
+	{
+		testCompareMetakey (expected, found, metaNames[i]);
+	}
+}
+
+static void testCompareMetakey (Key * expected, Key * found, const char * metaKeyName)
+{
+	printf ("Comparing metakey '%s'\n", metaKeyName);
+	keyRewindMeta (expected);
+	keyRewindMeta (found);
+	const Key * metaExpected = keyNextMeta (expected);
+	const Key * metaFound = keyNextMeta (found);
+	while (metaExpected != NULL)
+	{
+		if (strcmp (keyName (metaExpected), metaKeyName) == 0)
+		{
+			printf ("\tExpected = %s\n", keyString (metaFound));
+			break;
+		}
+		metaExpected = keyNextMeta (expected);
+	}
+	while (metaFound != NULL)
+	{
+		if (strcmp (keyName (metaFound), metaKeyName) == 0)
+		{
+			printf ("\tFound = %s\n", keyString (metaFound));
+			break;
+		}
+		metaFound = keyNextMeta (found);
+	}
+	succeed_if (metaExpected != NULL, "Could not find metakey in expected key");
+	succeed_if (metaFound != NULL, "Could not find metakey in found key");
+	succeed_if (strcmp (keyString (metaExpected), keyString (metaFound)) == 0, "Different metakey values");
 }
 
 static void showDiff (KeySet * expected, KeySet * is, const char * name, bool stopOnFirstDiff)
@@ -123,116 +296,3 @@ static void showDiff (KeySet * expected, KeySet * is, const char * name, bool st
 	}
 }
 
-static void testReadCompare (const char * filename, KeySet * expected)
-{
-	printf ("Reading '%s'\n", filename);
-	Key * parentKey = keyNew (PREFIX, KEY_VALUE, srcdir_file (filename), KEY_END);
-	KeySet * conf = ksNew (0, KS_END);
-	PLUGIN_OPEN ("toml");
-	Key * root = ksLookupByName (expected, PREFIX, KDB_O_POP);
-	if (root != NULL)
-	{
-		if (strcmp (keyString (root), "@CONFIG_FILEPATH@") == 0)
-		{
-			keySetString (root, srcdir_file (filename));
-			ksAppendKey (expected, root);
-		}
-	}
-	KeySet * ks = ksNew (0, KS_END);
-	succeed_if (plugin->kdbGet (plugin, ks, parentKey) == ELEKTRA_PLUGIN_STATUS_SUCCESS, "kdbGet failed");
-	compare_keyset (expected, ks);
-
-	showDiff (expected, ks, filename, true);
-
-	PLUGIN_CLOSE ();
-	ksDel (expected);
-}
-
-/*static void testReadExpectFail(const char * filename) {
-	printf ("Reading '%s'\n", filename);
-	Key * parentKey = keyNew (PREFIX, KEY_VALUE, srcdir_file (filename), KEY_END);
-	KeySet * conf = ksNew (0, KS_END);
-	PLUGIN_OPEN ("toml");
-	Key * root = ksLookupByName (expected, PREFIX, KDB_O_POP);
-	if (root != NULL)
-	{
-		if (strcmp (keyString (root), "@CONFIG_FILEPATH@") == 0)
-		{
-			keySetString (root, srcdir_file (filename));
-			ksAppendKey (expected, root);
-		}
-	}
-	KeySet * ks = ksNew (0, KS_END);
-	succeed_if (plugin->kdbGet (plugin, ks, parentKey) == ELEKTRA_PLUGIN_STATUS_SUCCESS, "kdbGet failed");
-
-	showDiff (expected, ks, filename, true);
-	// printKs (expected, "expected");
-	// printKs (ks, "parsed");
-
-	PLUGIN_CLOSE ();
-	ksDel (expected);
-}*/
-
-static void testsCompare(void) {
-	testReadCompare ("toml/basic.toml",
-#include "toml/basic.h"
-	);
-	testReadCompare ("toml/utf8.toml",
-#include "toml/utf8.h"
-	);
-
-	/*    testReadCompare ("toml/multiline_strings.toml",
-	#include "toml/multiline_strings.h"
-	    );*/
-
-	testReadCompare ("toml/date.toml",
-#include "toml/date.h"
-	);
-
-	testReadCompare ("toml/array.toml",
-#include "toml/array.h"
-	);
-
-	testReadCompare ("toml/simple_table.toml",
-#include "toml/simple_table.h"
-	);
-
-	testReadCompare ("toml/table_array.toml",
-#include "toml/table_array.h"
-	);
-
-	testReadCompare ("toml/table_array_nested.toml",
-#include "toml/table_array_nested.h"
-	);
-
-	testReadCompare ("toml/table_array_table_mixed.toml",
-#include "toml/table_array_table_mixed.h"
-	);
-
-	testReadCompare ("toml/inline_table.toml",
-#include "toml/inline_table.h"
-	);
-
-	testReadCompare ("toml/inline_table_empty.toml",
-#include "toml/inline_table_empty.h"
-	);
-
-	testReadCompare ("toml/inline_table_multiline_values.toml",
-#include "toml/inline_table_multiline_values.h"
-	);
-
-	testReadCompare ("toml/comment.toml",
-#include "toml/comment.h"
-	);
-
-}
-
-int main (int argc, char ** argv)
-{
-	init (argc, argv);
-
-	testsCompare();
-
-	print_result ("testmod_toml");
-	return nbError;
-}
---
+++ src/plugins/toml/toml/duplicate_key.h
@@ -0,0 +1,9 @@
+// clang-format off
+ksNew (8,
+	keyNew (PREFIX,
+		KEY_VALUE, "@CONFIG_FILEPATH@",
+		KEY_META, "error/module", "toml",
+		KEY_META, "error/description", "Validation Semantic",
+	KEY_END),
+KS_END)
+
