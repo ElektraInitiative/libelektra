23c952238edec162aaae2383dcbfe0f84221536b
René Schwaiger <sanssecours@me.com>
2019-10-27 18:26:21 +0100

    CMake: Revert man pages with date only update

--- scripts/cmake/ElektraManPage.cmake
+++ scripts/cmake/ElektraManPage.cmake
@@ -8,3 +8,23 @@ execute_process (COMMAND ${CMAKE_COMMAND}
 			 --pipe
 			 ${MDFILE}
 		 OUTPUT_FILE ${OUTFILE})
+
+# We revert newly generated man pages, where only the date changed.
+if (GIT_COMMAND)
+	execute_process (COMMAND "${GIT_COMMAND}"
+				 diff
+				 --numstat
+				 "${OUTFILE}"
+			 OUTPUT_VARIABLE GIT_OUTPUT)
+	string (REGEX
+		REPLACE "^([0-9]+).*"
+			"\\1"
+			GIT_ADDED_LINES
+			"${GIT_OUTPUT}")
+	if (GIT_ADDED_LINES STREQUAL 1)
+		execute_process (COMMAND "${GIT_COMMAND}" diff ${OUTFILE} OUTPUT_VARIABLE GIT_DIFF)
+		if (GIT_DIFF MATCHES "\n-.TH")
+			execute_process (COMMAND "${GIT_COMMAND}" checkout ${OUTFILE})
+		endif (GIT_DIFF MATCHES "\n-.TH")
+	endif (GIT_ADDED_LINES STREQUAL 1)
+endif (GIT_COMMAND)
--- scripts/cmake/Modules/LibAddMacros.cmake
+++ scripts/cmake/Modules/LibAddMacros.cmake
@@ -555,6 +555,13 @@ function (generate_manpage NAME)
 		set (OUTFILE "${CMAKE_SOURCE_DIR}/${MAN_PAGE_LOCATION}")
 
 		if (RONN_LOC)
+			# The script `ElektraManpage.cmake` uses `git` – if the command is available – to revert date only changes in man
+			# pages. Since `git` needs exclusive access to the repository the build system can not run multiple versions of
+			# `ElektraManpage.cmake` concurrently.
+			if (GIT_COMMAND)
+				set (ADD_CUSTOM_COMMAND_EXTRA_ARG USES_TERMINAL)
+			endif (GIT_COMMAND)
+
 			add_custom_command (OUTPUT ${OUTFILE}
 					    DEPENDS ${MDFILE}
 					    COMMAND ${CMAKE_COMMAND}
@@ -568,7 +575,8 @@ function (generate_manpage NAME)
 						    -D
 						    OUTFILE=${OUTFILE}
 						    -P
-						    ${CMAKE_SOURCE_DIR}/scripts/cmake/ElektraManpage.cmake)
+						    ${CMAKE_SOURCE_DIR}/scripts/cmake/ElektraManpage.cmake
+						    ${ADD_CUSTOM_COMMAND_EXTRA_ARG})
 			add_custom_target (man-${NAME} ALL DEPENDS ${OUTFILE})
 			add_dependencies (man man-${NAME})
 		endif (RONN_LOC)
