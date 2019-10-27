43565b6bc29cf8b8e980f4a5224c10b67c2d2227
René Schwaiger <sanssecours@me.com>
2019-10-27 13:05:10 +0100

    Check Bashisms: Print checked files

--- tests/shell/check_bashisms.sh
+++ tests/shell/check_bashisms.sh
@@ -17,7 +17,7 @@ find -version > /dev/null 2>&1 > /dev/null && FIND='find scripts -regextype egre
 # - The script `reformat-c` uses `command -v`, which is optional in POSIX. However, since `which` is not part of POSIX at all
 #   `command -v` is probably the most portable solution to detect the location of a command.
 #   See also: https://stackoverflow.com/questions/592620
-scripts=$(
+set $(
 	$FIND -type f -not \( \
 		-path '*COPYING-CMAKE-SCRIPTS' -or \
 		-path '*find-tools' -or \
@@ -32,10 +32,14 @@ scripts=$(
 		-path '*zsh' -or \
 		-regex '.+(Docker|Jenkins|Vagrant)file.*' -or \
 		-regex '.+\.(cmake|fish|ini?|kdb|md|txt|rb)$' \
-		\) | xargs
+		\) | sort
 )
 exit_if_fail 'Unable to locate shell scripts via `find`'
-checkbashisms $scripts
+printf 'Checking Scripts\n'
+printf '————————————————\n\n'
+for file; do printf '%s\n' "$file"; done
+printf '\n'
+checkbashisms "$@"
 ret=$?
 # 2 means skipped file, e.g. README.md, that is fine
 # only 1, 3 and 4 are actually bad
