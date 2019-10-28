a89a648acbe61e56d10af8b97ddf1c7a2ba57c94
Raphael Gruber <raphi011@gmail.com>
2019-10-28 07:56:41 +0100

    improve version handler

--- src/tools/web/elektrad/version_handler.go
+++ src/tools/web/elektrad/version_handler.go
@@ -2,20 +2,11 @@ package main
 
 import (
 	"net/http"
-	"strconv"
-	"strings"
 )
 
 type versionResult struct {
 	API     int            `json:"api"`
 	Elektra elektraVersion `json:"elektra"`
-}
-
-type elektraVersion struct {
-	Version string `json:"version"`
-	Major   int    `json:"major"`
-	Minor   int    `json:"minor"`
-	Micro   int    `json:"micro"`
 }
 
 // getVersionHandler returns the current version of Elektra via
@@ -26,35 +17,10 @@ type elektraVersion struct {
 //
 // Example: `curl localhost:33333/version`
 func getVersionHandler(w http.ResponseWriter, r *http.Request) {
-	kdb := getHandle(r)
-
-	version, _ := kdb.Version()
-
-	major, minor, micro := parseSemVer(version)
-
 	response := versionResult{
-		API: 1,
-		Elektra: elektraVersion{
-			Version: version,
-			Major:   major,
-			Minor:   minor,
-			Micro:   micro,
-		},
+		API:     1,
+		Elektra: version,
 	}
 
 	writeResponse(w, response)
-}
-
-func parseSemVer(version string) (major, minor, micro int) {
-	parts := strings.SplitN(version, ".", 3)
-
-	if len(parts) != 3 {
-		return
-	}
-
-	major, _ = strconv.Atoi(parts[0])
-	minor, _ = strconv.Atoi(parts[1])
-	micro, _ = strconv.Atoi(parts[2])
-
-	return
 }
