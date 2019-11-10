b0628d59cdba93ce420d254d85811ed7884f472c
Dominic Jäger <dominic.jaeger@gmail.com>
2019-11-10 23:15:28 +0100

    KDB_EXEC_PATH: Fix pointer after getenv

--- src/tools/kdb/external.cpp
+++ src/tools/kdb/external.cpp
@@ -52,17 +52,25 @@ void tryExternalCommand (char ** argv)
 {
 	std::vector<std::string> pathes;
 
-	std::string execPath = getenv ("KDB_EXEC_PATH");
-	size_t pos = 0;
-	std::string token;
-	std::string delimiter = ":";
-	while ((pos = execPath.find (delimiter)) != std::string::npos)
+	char * execPathPtr = getenv ("KDB_EXEC_PATH");
+	if (execPathPtr)
 	{
-		token = execPath.substr (0, pos);
-		pathes.push_back (token);
-		execPath.erase (0, pos + delimiter.length ());
+		// The string pointed by the pointer returned by getenv shall
+		// not be modified. The constructor of std::string constructs a
+		// copy of the string.
+		std::string execPath (execPathPtr);
+		size_t pos = 0;
+		std::string token;
+		std::string delimiter = ":";
+		while ((pos = execPath.find (delimiter)) != std::string::npos)
+		{
+			token = execPath.substr (0, pos);
+			pathes.push_back (token);
+			execPath.erase (0, pos + delimiter.length ());
+		}
+		// There is one last path in execPath after the while
+		pathes.push_back (execPath);
 	}
-	pathes.push_back (execPath);
 	pathes.push_back (buildinExecPath);
 
 	for (auto & pathe : pathes)
