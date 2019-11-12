d3a09c7ce2f05409735dc2c60ca221e9072ced75
Klemens Böswirth <k.boeswirth+git@gmail.com>
2019-11-12 21:20:15 +0100

    tests: update keynames in data

--- tests/data/data_dbelow.c
+++ tests/data/data_dbelow.c
@@ -9,66 +9,66 @@
 // clang-format off
 
 ksNew( 28 ,
-	keyNew ("user/test/keyset/dir1"
+	keyNew ("user:/test/keyset/dir1"
 		, KEY_DIR
 	, KEY_END),
-	keyNew ("user/test/keyset/dir2"
+	keyNew ("user:/test/keyset/dir2"
 		, KEY_DIR
 	, KEY_END),
-	keyNew ("user/test/keyset/dir3"
+	keyNew ("user:/test/keyset/dir3"
 		, KEY_DIR
 	, KEY_END),
-	keyNew ("user/test/keyset/dir4"
+	keyNew ("user:/test/keyset/dir4"
 		, KEY_DIR
 	, KEY_END),
-	keyNew ("user/test/keyset/dir5"
+	keyNew ("user:/test/keyset/dir5"
 		, KEY_DIR
 	, KEY_END),
-	keyNew ("user/test/keyset/dir6"
+	keyNew ("user:/test/keyset/dir6"
 		, KEY_DIR
 	, KEY_END),
-	keyNew ("user/test/keyset/dir7"
+	keyNew ("user:/test/keyset/dir7"
 		, KEY_DIR
 	, KEY_END),
-	keyNew ("user/test/keyset/dir8"
+	keyNew ("user:/test/keyset/dir8"
 		, KEY_DIR
 	, KEY_END),
-	keyNew ("user/test/keyset/dir9"
+	keyNew ("user:/test/keyset/dir9"
 		, KEY_DIR
 	, KEY_END),
-	keyNew ("user/test/keyset/key1"
+	keyNew ("user:/test/keyset/key1"
 		, KEY_VALUE, "value1"
 		, KEY_COMMENT, "comment1"
 	, KEY_END),
-	keyNew ("user/test/keyset/key2"
+	keyNew ("user:/test/keyset/key2"
 		, KEY_VALUE, "value2"
 		, KEY_COMMENT, "comment2"
 	, KEY_END),
-	keyNew ("user/test/keyset/key3"
+	keyNew ("user:/test/keyset/key3"
 		, KEY_VALUE, "value3"
 		, KEY_COMMENT, "comment3"
 	, KEY_END),
-	keyNew ("user/test/keyset/key4"
+	keyNew ("user:/test/keyset/key4"
 		, KEY_VALUE, "value4"
 		, KEY_COMMENT, "comment4"
 	, KEY_END),
-	keyNew ("user/test/keyset/key5"
+	keyNew ("user:/test/keyset/key5"
 		, KEY_VALUE, "value5"
 		, KEY_COMMENT, "comment5"
 	, KEY_END),
-	keyNew ("user/test/keyset/key6"
+	keyNew ("user:/test/keyset/key6"
 		, KEY_VALUE, "value6"
 		, KEY_COMMENT, "comment6"
 	, KEY_END),
-	keyNew ("user/test/keyset/key7"
+	keyNew ("user:/test/keyset/key7"
 		, KEY_VALUE, "value7"
 		, KEY_COMMENT, "comment7"
 	, KEY_END),
-	keyNew ("user/test/keyset/key8"
+	keyNew ("user:/test/keyset/key8"
 		, KEY_VALUE, "value8"
 		, KEY_COMMENT, "comment8"
 	, KEY_END),
-	keyNew ("user/test/keyset/key9"
+	keyNew ("user:/test/keyset/key9"
 		, KEY_VALUE, "value9"
 		, KEY_COMMENT, "comment9"
 	, KEY_END),KS_END);
--- tests/data/data_one.c
+++ tests/data/data_one.c
@@ -9,432 +9,432 @@
 // clang-format off
 
 ksNew( 102 ,
-	keyNew ("user/tests/filesys/0-27042916"
+	keyNew ("user:/tests/filesys/0-27042916"
 		, KEY_VALUE, "0 216905227"
 		, KEY_COMMENT, "2551516588474823843"
 		, KEY_TYPE, 43
 	, KEY_END),
-	keyNew ("user/tests/filesys/1-2449524622"
+	keyNew ("user:/tests/filesys/1-2449524622"
 		, KEY_VALUE, "1 1679328197"
 		, KEY_COMMENT, "3246436893195629244"
 		, KEY_TYPE, 253
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-0"
+	keyNew ("user:/tests/filesys/dir-1-0"
 		, KEY_DIR
 		, KEY_TYPE, KEY_TYPE_STRING
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-0/0-294164813"
+	keyNew ("user:/tests/filesys/dir-1-0/0-294164813"
 		, KEY_VALUE, "0 216245011"
 		, KEY_COMMENT, "18454108762891828026"
 		, KEY_TYPE, 114
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-0/1-1479930365"
+	keyNew ("user:/tests/filesys/dir-1-0/1-1479930365"
 		, KEY_VALUE, "1 2732423037"
 		, KEY_COMMENT, "24597295372375238"
 		, KEY_TYPE, 135
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-0/dir-2-0"
+	keyNew ("user:/tests/filesys/dir-1-0/dir-2-0"
 		, KEY_DIR
 		, KEY_TYPE, KEY_TYPE_STRING
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-0/dir-2-0/0-215571059"
+	keyNew ("user:/tests/filesys/dir-1-0/dir-2-0/0-215571059"
 		, KEY_VALUE, "0 264857705"
 		, KEY_COMMENT, "2188631490667217086"
 		, KEY_TYPE, 144
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-0/dir-2-0/1-172696863"
+	keyNew ("user:/tests/filesys/dir-1-0/dir-2-0/1-172696863"
 		, KEY_VALUE, "1 1413010492"
 		, KEY_COMMENT, "2009260691056619998"
 		, KEY_TYPE, 217
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-0/dir-2-1"
+	keyNew ("user:/tests/filesys/dir-1-0/dir-2-1"
 		, KEY_DIR
 		, KEY_TYPE, KEY_TYPE_STRING
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-0/dir-2-1/0-2000814607"
+	keyNew ("user:/tests/filesys/dir-1-0/dir-2-1/0-2000814607"
 		, KEY_VALUE, "0 1955026514"
 		, KEY_COMMENT, "634720937883220825"
 		, KEY_TYPE, 173
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-0/dir-2-1/1-1242717281"
+	keyNew ("user:/tests/filesys/dir-1-0/dir-2-1/1-1242717281"
 		, KEY_VALUE, "1 1279621809"
 		, KEY_COMMENT, "10835219082633423494"
 		, KEY_TYPE, KEY_TYPE_BINARY
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-0/dir-2-2"
+	keyNew ("user:/tests/filesys/dir-1-0/dir-2-2"
 		, KEY_DIR
 		, KEY_TYPE, KEY_TYPE_STRING
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-0/dir-2-2/0-215623221"
+	keyNew ("user:/tests/filesys/dir-1-0/dir-2-2/0-215623221"
 		, KEY_VALUE, "0 2615831169"
 		, KEY_COMMENT, "439048222054330242"
 		, KEY_TYPE, 28
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-0/dir-2-2/1-268626445"
+	keyNew ("user:/tests/filesys/dir-1-0/dir-2-2/1-268626445"
 		, KEY_VALUE, "1 211926272"
 		, KEY_COMMENT, "2671264801133512045"
 		, KEY_TYPE, 239
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-0/dir-2-3"
+	keyNew ("user:/tests/filesys/dir-1-0/dir-2-3"
 		, KEY_DIR
 		, KEY_TYPE, KEY_TYPE_STRING
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-0/dir-2-3/0-72095994"
+	keyNew ("user:/tests/filesys/dir-1-0/dir-2-3/0-72095994"
 		, KEY_VALUE, "0 3082914659"
 		, KEY_COMMENT, "2462099381906519637"
 		, KEY_TYPE, 145
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-0/dir-2-3/1-1112526887"
+	keyNew ("user:/tests/filesys/dir-1-0/dir-2-3/1-1112526887"
 		, KEY_VALUE, "1 13790397"
 		, KEY_COMMENT, "29552250853230432415"
 		, KEY_TYPE, 26
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-0/dir-2-4"
+	keyNew ("user:/tests/filesys/dir-1-0/dir-2-4"
 		, KEY_DIR
 		, KEY_TYPE, KEY_TYPE_STRING
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-0/dir-2-4/0-43814952"
+	keyNew ("user:/tests/filesys/dir-1-0/dir-2-4/0-43814952"
 		, KEY_VALUE, "0 245627749"
 		, KEY_COMMENT, "287081235381626635"
 		, KEY_TYPE, 242
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-0/dir-2-4/1-312568090"
+	keyNew ("user:/tests/filesys/dir-1-0/dir-2-4/1-312568090"
 		, KEY_VALUE, "1 36219472"
 		, KEY_COMMENT, "2056467162314026779"
 		, KEY_TYPE, 223
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-1"
+	keyNew ("user:/tests/filesys/dir-1-1"
 		, KEY_DIR
 		, KEY_TYPE, KEY_TYPE_STRING
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-1/0-2993514317"
+	keyNew ("user:/tests/filesys/dir-1-1/0-2993514317"
 		, KEY_VALUE, "0 3007016338"
 		, KEY_COMMENT, "6513136152238131569"
 		, KEY_TYPE, 88
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-1/1-2673925626"
+	keyNew ("user:/tests/filesys/dir-1-1/1-2673925626"
 		, KEY_VALUE, "1 3085029208"
 		, KEY_COMMENT, "595310088267681325"
 		, KEY_TYPE, 161
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-1/dir-2-0"
+	keyNew ("user:/tests/filesys/dir-1-1/dir-2-0"
 		, KEY_DIR
 		, KEY_TYPE, KEY_TYPE_STRING
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-1/dir-2-0/0-164223741"
+	keyNew ("user:/tests/filesys/dir-1-1/dir-2-0/0-164223741"
 		, KEY_VALUE, "0 64307894"
 		, KEY_COMMENT, "22531187641857031662"
 		, KEY_TYPE, 247
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-1/dir-2-0/1-3182527006"
+	keyNew ("user:/tests/filesys/dir-1-1/dir-2-0/1-3182527006"
 		, KEY_VALUE, "1 152121106"
 		, KEY_COMMENT, "6050128332068515722"
 		, KEY_TYPE, 190
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-1/dir-2-1"
+	keyNew ("user:/tests/filesys/dir-1-1/dir-2-1"
 		, KEY_DIR
 		, KEY_TYPE, KEY_TYPE_STRING
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-1/dir-2-1/0-3219021027"
+	keyNew ("user:/tests/filesys/dir-1-1/dir-2-1/0-3219021027"
 		, KEY_VALUE, "0 240036888"
 		, KEY_COMMENT, "16238284472997719576"
 		, KEY_TYPE, 229
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-1/dir-2-1/1-2486914396"
+	keyNew ("user:/tests/filesys/dir-1-1/dir-2-1/1-2486914396"
 		, KEY_VALUE, "1 145207467"
 		, KEY_COMMENT, "3883281333052412037"
 		, KEY_TYPE, 159
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-1/dir-2-2"
+	keyNew ("user:/tests/filesys/dir-1-1/dir-2-2"
 		, KEY_DIR
 		, KEY_TYPE, KEY_TYPE_STRING
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-1/dir-2-2/0-46903647"
+	keyNew ("user:/tests/filesys/dir-1-1/dir-2-2/0-46903647"
 		, KEY_VALUE, "0 2268621888"
 		, KEY_COMMENT, "32753327232797919976"
 		, KEY_TYPE, 82
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-1/dir-2-2/1-905011462"
+	keyNew ("user:/tests/filesys/dir-1-1/dir-2-2/1-905011462"
 		, KEY_VALUE, "1 1273310721"
 		, KEY_COMMENT, "14800138803221317405"
 		, KEY_TYPE, 115
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-1/dir-2-3"
+	keyNew ("user:/tests/filesys/dir-1-1/dir-2-3"
 		, KEY_DIR
 		, KEY_TYPE, KEY_TYPE_STRING
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-1/dir-2-3/0-3208912452"
+	keyNew ("user:/tests/filesys/dir-1-1/dir-2-3/0-3208912452"
 		, KEY_VALUE, "0 1169512568"
 		, KEY_COMMENT, "2676924538788523596"
 		, KEY_TYPE, 197
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-1/dir-2-3/1-1091331377"
+	keyNew ("user:/tests/filesys/dir-1-1/dir-2-3/1-1091331377"
 		, KEY_VALUE, "1 199987266"
 		, KEY_COMMENT, "4522262551362712874"
 		, KEY_TYPE, 186
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-1/dir-2-4"
+	keyNew ("user:/tests/filesys/dir-1-1/dir-2-4"
 		, KEY_DIR
 		, KEY_TYPE, KEY_TYPE_STRING
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-1/dir-2-4/0-294181683"
+	keyNew ("user:/tests/filesys/dir-1-1/dir-2-4/0-294181683"
 		, KEY_VALUE, "0 3146316501"
 		, KEY_COMMENT, "2101827480127277145"
 		, KEY_TYPE, 202
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-1/dir-2-4/1-175659776"
+	keyNew ("user:/tests/filesys/dir-1-1/dir-2-4/1-175659776"
 		, KEY_VALUE, "1 2334131443"
 		, KEY_COMMENT, "198753828813576"
 		, KEY_TYPE, 179
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-2"
+	keyNew ("user:/tests/filesys/dir-1-2"
 		, KEY_DIR
 		, KEY_TYPE, KEY_TYPE_STRING
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-2/0-972431067"
+	keyNew ("user:/tests/filesys/dir-1-2/0-972431067"
 		, KEY_VALUE, "0 133203847"
 		, KEY_COMMENT, "1347011419147246413"
 		, KEY_TYPE, 100
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-2/1-75222444"
+	keyNew ("user:/tests/filesys/dir-1-2/1-75222444"
 		, KEY_VALUE, "1 1867917733"
 		, KEY_COMMENT, "31620169261048613078"
 		, KEY_TYPE, 129
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-2/dir-2-0"
+	keyNew ("user:/tests/filesys/dir-1-2/dir-2-0"
 		, KEY_DIR
 		, KEY_TYPE, KEY_TYPE_STRING
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-2/dir-2-0/0-315168369"
+	keyNew ("user:/tests/filesys/dir-1-2/dir-2-0/0-315168369"
 		, KEY_VALUE, "0 1810024091"
 		, KEY_COMMENT, "17609274262950321182"
 		, KEY_TYPE, 92
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-2/dir-2-0/1-18334792"
+	keyNew ("user:/tests/filesys/dir-1-2/dir-2-0/1-18334792"
 		, KEY_VALUE, "1 355327020"
 		, KEY_COMMENT, "26036252041174319415"
 		, KEY_TYPE, 110
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-2/dir-2-1"
+	keyNew ("user:/tests/filesys/dir-1-2/dir-2-1"
 		, KEY_DIR
 		, KEY_TYPE, KEY_TYPE_STRING
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-2/dir-2-1/0-43779371"
+	keyNew ("user:/tests/filesys/dir-1-2/dir-2-1/0-43779371"
 		, KEY_VALUE, "0 2717329532"
 		, KEY_COMMENT, "1847528778209268968"
 		, KEY_TYPE, 167
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-2/dir-2-1/1-211724603"
+	keyNew ("user:/tests/filesys/dir-1-2/dir-2-1/1-211724603"
 		, KEY_VALUE, "1 1274718100"
 		, KEY_COMMENT, "16075148982907919368"
 		, KEY_TYPE, 33
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-2/dir-2-2"
+	keyNew ("user:/tests/filesys/dir-1-2/dir-2-2"
 		, KEY_DIR
 		, KEY_TYPE, KEY_TYPE_STRING
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-2/dir-2-2/0-23186501"
+	keyNew ("user:/tests/filesys/dir-1-2/dir-2-2/0-23186501"
 		, KEY_VALUE, "0 162585077"
 		, KEY_COMMENT, "1823923665139346176"
 		, KEY_TYPE, 93
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-2/dir-2-2/1-117112769"
+	keyNew ("user:/tests/filesys/dir-1-2/dir-2-2/1-117112769"
 		, KEY_VALUE, "1 124652661"
 		, KEY_COMMENT, "15563326831112072"
 		, KEY_TYPE, 238
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-2/dir-2-3"
+	keyNew ("user:/tests/filesys/dir-1-2/dir-2-3"
 		, KEY_DIR
 		, KEY_TYPE, KEY_TYPE_STRING
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-2/dir-2-3/0-1045212001"
+	keyNew ("user:/tests/filesys/dir-1-2/dir-2-3/0-1045212001"
 		, KEY_VALUE, "0 2042429548"
 		, KEY_COMMENT, "2095516044228183782"
 		, KEY_TYPE, KEY_TYPE_BINARY
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-2/dir-2-3/1-2201810163"
+	keyNew ("user:/tests/filesys/dir-1-2/dir-2-3/1-2201810163"
 		, KEY_VALUE, "1 121814102"
 		, KEY_COMMENT, "2139722875720320817"
 		, KEY_TYPE, 35
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-2/dir-2-4"
+	keyNew ("user:/tests/filesys/dir-1-2/dir-2-4"
 		, KEY_DIR
 		, KEY_TYPE, KEY_TYPE_STRING
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-2/dir-2-4/0-1913626837"
+	keyNew ("user:/tests/filesys/dir-1-2/dir-2-4/0-1913626837"
 		, KEY_VALUE, "0 2913127539"
 		, KEY_COMMENT, "776043343146721534"
 		, KEY_TYPE, 33
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-2/dir-2-4/1-2016421284"
+	keyNew ("user:/tests/filesys/dir-1-2/dir-2-4/1-2016421284"
 		, KEY_VALUE, "1 753117627"
 		, KEY_COMMENT, "255613898214632040"
 		, KEY_TYPE, 187
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-3"
+	keyNew ("user:/tests/filesys/dir-1-3"
 		, KEY_DIR
 		, KEY_TYPE, KEY_TYPE_STRING
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-3/0-3155020840"
+	keyNew ("user:/tests/filesys/dir-1-3/0-3155020840"
 		, KEY_VALUE, "0 990825763"
 		, KEY_COMMENT, "21852226343272631185"
 		, KEY_TYPE, 176
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-3/1-874922"
+	keyNew ("user:/tests/filesys/dir-1-3/1-874922"
 		, KEY_VALUE, "1 1759916191"
 		, KEY_COMMENT, "2623735621954316926"
 		, KEY_TYPE, 200
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-3/dir-2-0"
+	keyNew ("user:/tests/filesys/dir-1-3/dir-2-0"
 		, KEY_DIR
 		, KEY_TYPE, KEY_TYPE_STRING
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-3/dir-2-0/0-630724305"
+	keyNew ("user:/tests/filesys/dir-1-3/dir-2-0/0-630724305"
 		, KEY_VALUE, "0 2995613338"
 		, KEY_COMMENT, "1964917880248812636"
 		, KEY_TYPE, 223
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-3/dir-2-0/1-169830617"
+	keyNew ("user:/tests/filesys/dir-1-3/dir-2-0/1-169830617"
 		, KEY_VALUE, "1 2619012633"
 		, KEY_COMMENT, "6277190786814312"
 		, KEY_TYPE, 157
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-3/dir-2-1"
+	keyNew ("user:/tests/filesys/dir-1-3/dir-2-1"
 		, KEY_DIR
 		, KEY_TYPE, KEY_TYPE_STRING
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-3/dir-2-1/0-1313819824"
+	keyNew ("user:/tests/filesys/dir-1-3/dir-2-1/0-1313819824"
 		, KEY_VALUE, "0 1076430387"
 		, KEY_COMMENT, "143682467514357045"
 		, KEY_TYPE, KEY_TYPE_BINARY
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-3/dir-2-1/1-1642029463"
+	keyNew ("user:/tests/filesys/dir-1-3/dir-2-1/1-1642029463"
 		, KEY_VALUE, "1 1695720003"
 		, KEY_COMMENT, "165992136954451860"
 		, KEY_TYPE, 78
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-3/dir-2-2"
+	keyNew ("user:/tests/filesys/dir-1-3/dir-2-2"
 		, KEY_DIR
 		, KEY_TYPE, KEY_TYPE_STRING
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-3/dir-2-2/0-153568349"
+	keyNew ("user:/tests/filesys/dir-1-3/dir-2-2/0-153568349"
 		, KEY_VALUE, "0 802220498"
 		, KEY_COMMENT, "2544318414240793465"
 		, KEY_TYPE, 176
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-3/dir-2-2/1-203925831"
+	keyNew ("user:/tests/filesys/dir-1-3/dir-2-2/1-203925831"
 		, KEY_VALUE, "1 1773615811"
 		, KEY_COMMENT, "326122332660329661"
 		, KEY_TYPE, 220
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-3/dir-2-3"
+	keyNew ("user:/tests/filesys/dir-1-3/dir-2-3"
 		, KEY_DIR
 		, KEY_TYPE, KEY_TYPE_STRING
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-3/dir-2-3/0-132335709"
+	keyNew ("user:/tests/filesys/dir-1-3/dir-2-3/0-132335709"
 		, KEY_VALUE, "0 753610877"
 		, KEY_COMMENT, "194216610785116060"
 		, KEY_TYPE, 205
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-3/dir-2-3/1-267732539"
+	keyNew ("user:/tests/filesys/dir-1-3/dir-2-3/1-267732539"
 		, KEY_VALUE, "1 29164318"
 		, KEY_COMMENT, "990832569759630797"
 		, KEY_TYPE, 203
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-3/dir-2-4"
+	keyNew ("user:/tests/filesys/dir-1-3/dir-2-4"
 		, KEY_DIR
 		, KEY_TYPE, KEY_TYPE_STRING
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-3/dir-2-4/0-3199312703"
+	keyNew ("user:/tests/filesys/dir-1-3/dir-2-4/0-3199312703"
 		, KEY_VALUE, "0 1001316339"
 		, KEY_COMMENT, "21741123163068723187"
 		, KEY_TYPE, 209
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-3/dir-2-4/1-1790727245"
+	keyNew ("user:/tests/filesys/dir-1-3/dir-2-4/1-1790727245"
 		, KEY_VALUE, "1 289129416"
 		, KEY_COMMENT, "53059801514130705"
 		, KEY_TYPE, 93
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-4"
+	keyNew ("user:/tests/filesys/dir-1-4"
 		, KEY_DIR
 		, KEY_TYPE, KEY_TYPE_STRING
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-4/0-387126160"
+	keyNew ("user:/tests/filesys/dir-1-4/0-387126160"
 		, KEY_VALUE, "0 204394545"
 		, KEY_COMMENT, "21188322531851320535"
 		, KEY_TYPE, 133
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-4/1-151647309"
+	keyNew ("user:/tests/filesys/dir-1-4/1-151647309"
 		, KEY_VALUE, "1 2842511711"
 		, KEY_COMMENT, "9883314882767419206"
 		, KEY_TYPE, 137
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-4/dir-2-0"
+	keyNew ("user:/tests/filesys/dir-1-4/dir-2-0"
 		, KEY_DIR
 		, KEY_TYPE, KEY_TYPE_STRING
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-4/dir-2-0/0-526230536"
+	keyNew ("user:/tests/filesys/dir-1-4/dir-2-0/0-526230536"
 		, KEY_VALUE, "0 1181112782"
 		, KEY_COMMENT, "1065225830188249547"
 		, KEY_TYPE, 53
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-4/dir-2-0/1-203866829"
+	keyNew ("user:/tests/filesys/dir-1-4/dir-2-0/1-203866829"
 		, KEY_VALUE, "1 21315999"
 		, KEY_COMMENT, "30944117441110710316"
 		, KEY_TYPE, 132
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-4/dir-2-1"
+	keyNew ("user:/tests/filesys/dir-1-4/dir-2-1"
 		, KEY_DIR
 		, KEY_TYPE, KEY_TYPE_STRING
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-4/dir-2-1/0-232282487"
+	keyNew ("user:/tests/filesys/dir-1-4/dir-2-1/0-232282487"
 		, KEY_VALUE, "0 28204903"
 		, KEY_COMMENT, "14723134441057028108"
 		, KEY_TYPE, 162
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-4/dir-2-1/1-2551523896"
+	keyNew ("user:/tests/filesys/dir-1-4/dir-2-1/1-2551523896"
 		, KEY_VALUE, "1 140713945"
 		, KEY_COMMENT, "5891315043040223589"
 		, KEY_TYPE, 80
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-4/dir-2-2"
+	keyNew ("user:/tests/filesys/dir-1-4/dir-2-2"
 		, KEY_DIR
 		, KEY_TYPE, KEY_TYPE_STRING
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-4/dir-2-2/0-2408724547"
+	keyNew ("user:/tests/filesys/dir-1-4/dir-2-2/0-2408724547"
 		, KEY_VALUE, "0 293399851"
 		, KEY_COMMENT, "11915196071822725798"
 		, KEY_TYPE, 198
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-4/dir-2-2/1-2609326364"
+	keyNew ("user:/tests/filesys/dir-1-4/dir-2-2/1-2609326364"
 		, KEY_VALUE, "1 240967228"
 		, KEY_COMMENT, "2183621105162112017"
 		, KEY_TYPE, 136
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-4/dir-2-3"
+	keyNew ("user:/tests/filesys/dir-1-4/dir-2-3"
 		, KEY_DIR
 		, KEY_TYPE, KEY_TYPE_STRING
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-4/dir-2-3/0-563328213"
+	keyNew ("user:/tests/filesys/dir-1-4/dir-2-3/0-563328213"
 		, KEY_VALUE, "0 1680912618"
 		, KEY_COMMENT, "76214949230088786"
 		, KEY_TYPE, 204
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-4/dir-2-3/1-1453831796"
+	keyNew ("user:/tests/filesys/dir-1-4/dir-2-3/1-1453831796"
 		, KEY_VALUE, "1 245126793"
 		, KEY_COMMENT, "15761313992127631931"
 		, KEY_TYPE, 64
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-4/dir-2-4"
+	keyNew ("user:/tests/filesys/dir-1-4/dir-2-4"
 		, KEY_DIR
 		, KEY_TYPE, KEY_TYPE_STRING
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-4/dir-2-4/0-257736880"
+	keyNew ("user:/tests/filesys/dir-1-4/dir-2-4/0-257736880"
 		, KEY_VALUE, "0 867028967"
 		, KEY_COMMENT, "319532359220727161"
 		, KEY_TYPE, 145
 	, KEY_END),
-	keyNew ("user/tests/filesys/dir-1-4/dir-2-4/1-2918914282"
+	keyNew ("user:/tests/filesys/dir-1-4/dir-2-4/1-2918914282"
 		, KEY_VALUE, "1 311082578"
 		, KEY_COMMENT, "1104442372487923737"
 		, KEY_TYPE, KEY_TYPE_BINARY
