a6eff053762168923d0273a0dad389235880a379
Michael Zronek <michael.zronek@willhaben.at>
2019-11-13 09:13:12 +0100

    mountbase: fix no error message when run as non-root
    References #3197

--- src/tools/kdb/mountbase.cpp
+++ src/tools/kdb/mountbase.cpp
@@ -122,21 +122,6 @@ void MountBaseCommand::askForConfirmation (Cmdline const & cl)
 	}
 }
 
-class KDBMountException : public KDBException
-{
-	std::string msg;
-
-public:
-	explicit KDBMountException (std::string const & e) : KDBException (Key ()), msg (e)
-	{
-	}
-
-	virtual const char * what () const noexcept override
-	{
-		return msg.c_str ();
-	}
-};
-
 /**
  * @brief Really write out config
  */
