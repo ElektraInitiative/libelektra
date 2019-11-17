2bb59d9cc8066d6c289edbe497fdb34c275b53a3
Ulrike Schaefer <e1327450@student.tuwien.ac.at>
2019-11-17 03:54:28 +0100

    autocompletion: fixed formatting of completion output, added previous command

--- src/tools/autocompletion/find_autocompletion_options.sh
+++ src/tools/autocompletion/find_autocompletion_options.sh
@@ -1,9 +1,16 @@
 #/usr/bin/env bash
+IFS=$'\n'
 
 _helloworld_completions() {
 	# completing for last input before a tab
-	OUTPUT="$(python3 find_autocompletion_options.py ${COMP_WORDS[-1]})"
-	COMPREPLY+=$OUTPUT
+	COMPREPLY=()
+	cur="${COMP_WORDS[COMP_CWORD]}"
+	prev="${COMP_WORDS[COMP_CWORD-1]}"
+	OUTPUT="$(python3 find_autocompletion_options.py ${cur})"
+	#echo $OUTPUT
+	COMPREPLY=($(compgen -W "${OUTPUT}"))
+	#COMPREPLY+=$OUTPUT
+
 }
 
 complete -F _helloworld_completions helloworld
