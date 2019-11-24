c2fe6dc86e4ac94cf74248a4b07cbbd2aa3cb979
Jakob Fischer <jakobfischer93@gmail.com>
2019-11-24 17:01:25 +0100

    toml: replaced usage of keyRel

--- src/plugins/toml/testmod_toml.c
+++ src/plugins/toml/testmod_toml.c
@@ -222,7 +222,6 @@ static void testReadCompare (const char * filename, KeySet * expected)
 	succeed_if (plugin->kdbGet (plugin, ks, parentKey) == ELEKTRA_PLUGIN_STATUS_SUCCESS,
 		    "Expected kdbGet to succeed, but got failure.");
 
-	dumpKS (ks);
 	compare_keyset (expected, ks);
 
 	ksDel (ks);
