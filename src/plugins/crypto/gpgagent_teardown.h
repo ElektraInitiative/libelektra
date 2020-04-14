07877966d4d1d6c647763fef30944a4592593acb
Peter Nirschl <peter.nirschl@gmail.com>
2020-04-14 21:08:12 +0200

    crypto: add shut-down logic in testmod_crypto
    Try to kill the gpg-agent if the shutdown of the agent should fail.
    This is an attempt to resolve #2341 .

--- src/plugins/crypto/gpgagent_teardown.h
+++ src/plugins/crypto/gpgagent_teardown.h
@@ -1,6 +1,3 @@
-#ifndef ELEKTRA_PLUGIN_CRYPTO_GPGAGENT_TEARDOWN_H
-#define ELEKTRA_PLUGIN_CRYPTO_GPGAGENT_TEARDOWN_H
-
 /**
  * @file
  *
@@ -9,13 +6,26 @@
  * @copyright BSD License (see LICENSE.md or https://www.libelektra.org)
  *
  */
+#ifndef ELEKTRA_PLUGIN_CRYPTO_GPGAGENT_TEARDOWN_H
+#define ELEKTRA_PLUGIN_CRYPTO_GPGAGENT_TEARDOWN_H
 
 static inline void test_teardown (void)
 {
+	// try to gracefully shut down the gpg-agent
 	int status = system ("gpg-connect-agent --quiet KILLAGENT /bye");
 	if (status != 0)
 	{
-		fprintf (stderr, "Terminating gpg-agent returned with status “%d”", status);
+		// use the hammer
+		int killStatus = system ("/bin/sh -c \"pgrep \'gpg-agent\' | xargs -d \'\\n\' \'kill\'\"");
+		if (killStatus != 0)
+		{
+			fprintf (stderr, "ERROR: Terminating gpg-agent returned with status %d.\nFailed to kill the gpg-agent (status %d).",
+				 status, killStatus);
+		}
+		else
+		{
+			fprintf (stdout, "INFO: killed gpg-agent because shutdown failed with status %d.\n", status);
+		}
 	}
 }
 
