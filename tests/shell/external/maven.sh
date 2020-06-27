9de8409e1f90f3cde04da8f138db290f8b64b5e1
Markus Raab <elektra@markus-raab.org>
2020-06-27 14:48:35 +0200

    jna: run needed mvn command first

--- tests/shell/external/maven.sh
+++ tests/shell/external/maven.sh
@@ -14,6 +14,11 @@ else
 	exit 0
 fi
 
+if test -f /usr/share/java/libelektra4j.jar; then
+	echo "libelektra4j.jar not installed, will skip"
+	exit 0
+fi
+
 check_version
 
 EXTERNAL_FOLDER="@CMAKE_SOURCE_DIR@/examples/external/java/read-keys-example"
@@ -21,6 +26,11 @@ EXTERNAL_FOLDER="@CMAKE_SOURCE_DIR@/examples/external/java/read-keys-example"
 echo "Testing build with mvn"
 
 cd "$EXTERNAL_FOLDER"
+
+mvn org.apache.maven.plugins:maven-install-plugin:install-file \
+	-Dfile=/usr/share/java/libelektra4j.jar \
+	-DpomFile=/usr/share/java/libelektra4j.pom.xml
+
 mvn clean package test
 
 rm -r target
