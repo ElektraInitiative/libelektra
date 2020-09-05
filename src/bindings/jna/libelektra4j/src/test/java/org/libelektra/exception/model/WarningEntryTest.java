1bf60ee2af90e98bf805672f8f0139505b8fd633
Klemens Böswirth <k.boeswirth+git@gmail.com>
2020-09-05 16:57:55 +0200

    java: fix after keyname overhaul

--- src/bindings/jna/libelektra4j/src/test/java/org/libelektra/exception/model/WarningEntryTest.java
+++ src/bindings/jna/libelektra4j/src/test/java/org/libelektra/exception/model/WarningEntryTest.java
@@ -19,15 +19,15 @@ public class WarningEntryTest
 		String module = "kdb";
 		String reason = "This is a testerror";
 
-		final String warningKeyName = String.format ("warnings/#00", 0);
+		final String warningKeyName = String.format ("warnings/#0", 0);
 		Key warningKey = Key.create ("user:/temporary/errorkey");
-		warningKey.setMeta ("warnings/#00/number", errorNumber);
-		warningKey.setMeta ("warnings/#00/configfile", configFile);
-		warningKey.setMeta ("warnings/#00/mountpoint", mountpoint);
-		warningKey.setMeta ("warnings/#00/file", file);
-		warningKey.setMeta ("warnings/#00/line", line);
-		warningKey.setMeta ("warnings/#00/module", module);
-		warningKey.setMeta ("warnings/#00/reason", reason);
+		warningKey.setMeta ("warnings/#0/number", errorNumber);
+		warningKey.setMeta ("warnings/#0/configfile", configFile);
+		warningKey.setMeta ("warnings/#0/mountpoint", mountpoint);
+		warningKey.setMeta ("warnings/#0/file", file);
+		warningKey.setMeta ("warnings/#0/line", line);
+		warningKey.setMeta ("warnings/#0/module", module);
+		warningKey.setMeta ("warnings/#0/reason", reason);
 
 		WarningEntry warningEntry = new WarningEntry (warningKey, 0);
 
