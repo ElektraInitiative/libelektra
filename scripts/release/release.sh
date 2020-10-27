158de342aba4b3cca6c606253e9a8d7783c29be2
Robert Sowula <robert@sowula.at>
2020-10-27 15:15:33 +0100

    release: reformat

--- scripts/release/release.sh
+++ scripts/release/release.sh
@@ -20,7 +20,7 @@ install_elektra() {
 	make install
 	export LD_LIBRARY_PATH=${WORKSPACE}/system/lib:$LD_LIBRARY_PATH
 	export PATH=${WORKSPACE}/system/bin:$PATH
-	export DBUS_SESSION_BUS_ADDRESS=`dbus-daemon --session --fork --print-address`
+	export DBUS_SESSION_BUS_ADDRESS=$(dbus-daemon --session --fork --print-address)
 	export LUA_CPATH="${WORKSPACE}/system/lib/lua/5.2/?.so;"
 
 	env
