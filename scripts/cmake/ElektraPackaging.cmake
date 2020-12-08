ac17b16ed67e9fe486f6a1483aef2b9786efd486
Robert Sowula <robert@sowula.at>
2020-12-08 02:00:58 +0100

    cpack: remove rpath

--- scripts/cmake/ElektraPackaging.cmake
+++ scripts/cmake/ElektraPackaging.cmake
@@ -40,10 +40,6 @@ set (ALL_PLUGINS ${CPACK_COMPONENTS_ALL})
 list(FILTER ALL_PLUGINS INCLUDE REGEX "^libelektra4-.*")
 string (REPLACE ";" ", " ALL_PLUGINS_STR "${ALL_PLUGINS}")
 
-set (CMAKE_BUILD_WITH_INSTALL_RPATH 1)
-
-list(APPEND CMAKE_INSTALL_RPATH "${CMAKE_CURRENT_BINARY_DIR}")
-
 set (
 	PACKAGE_DESCRIPTION
 	"Elektra provides a universal and secure framework to store configuration parameters in a global, hierarchical key database. The core is a small library implemented in C. The plugin-based framework fulfills many configuration-related tasks to avoid any unnecessary code duplication across applications while it still allows the core to stay without any external dependency.  Elektra abstracts from cross-platform-related issues with an consistent API, and allows applications to be aware of other applications' configurations, leveraging easy application integration.\n."
