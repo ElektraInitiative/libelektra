5a35769f63740225e441f88ed508f675c2d0b1b4
Klemens Böswirth <k.boeswirth+git@gmail.com>
2020-12-17 17:48:05 +0100

    cpp: fix key name iterator for root keys

--- src/bindings/cpp/include/key.hpp
+++ src/bindings/cpp/include/key.hpp
@@ -300,48 +300,38 @@ public:
 
 	const char * findNext () const
 	{
-		const char * c = current;
-		if (c >= end) return end;
-
-		if (c == begin && *c == 0)
+		if (end - begin == 3)
 		{
-			// special handling of cascading key names
-			return ++c;
+			// special handling for root key
+			return end;
 		}
 
-		do
+		const char * c = current >= end ? end - 1 : current;
+
+		while (c < end && *c != '\0')
 		{
 			++c;
-		} while (c < end && *c != 0);
-		if (c != end)
-		{
-			++c; // skip past null character
 		}
 
-		return c;
+		return c < end ? c + 1 : end;
 	}
 
 	const char * findPrevious () const
 	{
-		const char * c = current;
-		if (c <= begin) return begin;
+		if (end - begin == 3)
+		{
+			// special handling for root key
+			return begin;
+		}
 
-		--c; // go from start of string to null
+		const char * c = current >= end ? end - 1 : current - 1;
+
 		do
 		{
 			--c;
-		} while (c > begin && *c != 0);
-		if (c != begin && c + 1 != current)
-		{
-			++c; // jump back to not-null
-		}
-		else if (c == begin && *c == 0)
-		{
-			// special handling of cascading key names
-			return ++c;
-		}
+		} while (c > begin && *c != '\0');
 
-		return c;
+		return c > begin ? c + 1 : begin;
 	}
 
 	// Forward iterator requirements
--- src/bindings/cpp/tests/testcpp_iter_name.cpp
+++ src/bindings/cpp/tests/testcpp_iter_name.cpp
@@ -184,3 +184,177 @@ TEST (iterNameCascading, reverse)
 	it++;
 	EXPECT_EQ ((*it), "/key4/1") << "name wrong";
 }
+
+TEST (iterNameRoot, forward)
+{
+	Key k ("/", KEY_END);
+
+	Key::iterator it = k.begin ();
+	EXPECT_EQ ((*it), std::string{ KEY_NS_CASCADING }) << "cascading name wrong";
+	++it;
+	EXPECT_EQ (it, k.end ()) << "not at end";
+
+	--it;
+	EXPECT_EQ ((*it), std::string{ KEY_NS_CASCADING }) << "name wrong";
+	EXPECT_EQ (it, k.begin ()) << "not at begin";
+
+	k = Key ("meta:/", KEY_END);
+
+	it = k.begin ();
+	EXPECT_EQ ((*it), std::string{ KEY_NS_META }) << "cascading name wrong";
+	++it;
+	EXPECT_EQ (it, k.end ()) << "not at end";
+
+	--it;
+	EXPECT_EQ ((*it), std::string{ KEY_NS_META }) << "name wrong";
+	EXPECT_EQ (it, k.begin ()) << "not at begin";
+
+	k = Key ("spec:/", KEY_END);
+
+	it = k.begin ();
+	EXPECT_EQ ((*it), std::string{ KEY_NS_SPEC }) << "cascading name wrong";
+	++it;
+	EXPECT_EQ (it, k.end ()) << "not at end";
+
+	--it;
+	EXPECT_EQ ((*it), std::string{ KEY_NS_SPEC }) << "name wrong";
+	EXPECT_EQ (it, k.begin ()) << "not at begin";
+
+	k = Key ("proc:/", KEY_END);
+
+	it = k.begin ();
+	EXPECT_EQ ((*it), std::string{ KEY_NS_PROC }) << "cascading name wrong";
+	++it;
+	EXPECT_EQ (it, k.end ()) << "not at end";
+
+	--it;
+	EXPECT_EQ ((*it), std::string{ KEY_NS_PROC }) << "name wrong";
+	EXPECT_EQ (it, k.begin ()) << "not at begin";
+
+	k = Key ("dir:/", KEY_END);
+
+	it = k.begin ();
+	EXPECT_EQ ((*it), std::string{ KEY_NS_DIR }) << "cascading name wrong";
+	++it;
+	EXPECT_EQ (it, k.end ()) << "not at end";
+
+	--it;
+	EXPECT_EQ ((*it), std::string{ KEY_NS_DIR }) << "name wrong";
+	EXPECT_EQ (it, k.begin ()) << "not at begin";
+
+	k = Key ("user:/", KEY_END);
+
+	it = k.begin ();
+	EXPECT_EQ ((*it), std::string{ KEY_NS_USER }) << "cascading name wrong";
+	++it;
+	EXPECT_EQ (it, k.end ()) << "not at end";
+
+	--it;
+	EXPECT_EQ ((*it), std::string{ KEY_NS_USER }) << "name wrong";
+	EXPECT_EQ (it, k.begin ()) << "not at begin";
+
+	k = Key ("system:/", KEY_END);
+
+	it = k.begin ();
+	EXPECT_EQ ((*it), std::string{ KEY_NS_SYSTEM }) << "cascading name wrong";
+	++it;
+	EXPECT_EQ (it, k.end ()) << "not at end";
+
+	--it;
+	EXPECT_EQ ((*it), std::string{ KEY_NS_SYSTEM }) << "name wrong";
+	EXPECT_EQ (it, k.begin ()) << "not at begin";
+
+	k = Key ("default:/", KEY_END);
+
+	it = k.begin ();
+	EXPECT_EQ ((*it), std::string{ KEY_NS_DEFAULT }) << "cascading name wrong";
+	++it;
+	EXPECT_EQ (it, k.end ()) << "not at end";
+
+	--it;
+	EXPECT_EQ ((*it), std::string{ KEY_NS_DEFAULT }) << "name wrong";
+	EXPECT_EQ (it, k.begin ()) << "not at begin";
+}
+
+TEST (iterNameRoot, reverse)
+{
+	Key k ("/", KEY_END);
+
+	Key::reverse_iterator it = k.rend ();
+	EXPECT_EQ ((*it), "") << "name wrong";
+	--it;
+	EXPECT_EQ ((*it), std::string{ KEY_NS_CASCADING }) << "cascading name wrong";
+	EXPECT_EQ (it, k.rbegin ()) << "not at end";
+	++it;
+	EXPECT_EQ (it, k.rend ()) << "not at begin";
+
+	k = Key ("meta:/", KEY_END);
+
+	it = k.rend ();
+	EXPECT_EQ ((*it), "") << "name wrong";
+	--it;
+	EXPECT_EQ ((*it), std::string{ KEY_NS_META }) << "cascading name wrong";
+	EXPECT_EQ (it, k.rbegin ()) << "not at end";
+	++it;
+	EXPECT_EQ (it, k.rend ()) << "not at begin";
+
+	k = Key ("spec:/", KEY_END);
+
+	it = k.rend ();
+	EXPECT_EQ ((*it), "") << "name wrong";
+	--it;
+	EXPECT_EQ ((*it), std::string{ KEY_NS_SPEC }) << "cascading name wrong";
+	EXPECT_EQ (it, k.rbegin ()) << "not at end";
+	++it;
+	EXPECT_EQ (it, k.rend ()) << "not at begin";
+
+	k = Key ("proc:/", KEY_END);
+
+	it = k.rend ();
+	EXPECT_EQ ((*it), "") << "name wrong";
+	--it;
+	EXPECT_EQ ((*it), std::string{ KEY_NS_PROC }) << "cascading name wrong";
+	EXPECT_EQ (it, k.rbegin ()) << "not at end";
+	++it;
+	EXPECT_EQ (it, k.rend ()) << "not at begin";
+
+	k = Key ("dir:/", KEY_END);
+
+	it = k.rend ();
+	EXPECT_EQ ((*it), "") << "name wrong";
+	--it;
+	EXPECT_EQ ((*it), std::string{ KEY_NS_DIR }) << "cascading name wrong";
+	EXPECT_EQ (it, k.rbegin ()) << "not at end";
+	++it;
+	EXPECT_EQ (it, k.rend ()) << "not at begin";
+
+	k = Key ("user:/", KEY_END);
+
+	it = k.rend ();
+	EXPECT_EQ ((*it), "") << "name wrong";
+	--it;
+	EXPECT_EQ ((*it), std::string{ KEY_NS_USER }) << "cascading name wrong";
+	EXPECT_EQ (it, k.rbegin ()) << "not at end";
+	++it;
+	EXPECT_EQ (it, k.rend ()) << "not at begin";
+
+	k = Key ("system:/", KEY_END);
+
+	it = k.rend ();
+	EXPECT_EQ ((*it), "") << "name wrong";
+	--it;
+	EXPECT_EQ ((*it), std::string{ KEY_NS_SYSTEM }) << "cascading name wrong";
+	EXPECT_EQ (it, k.rbegin ()) << "not at end";
+	++it;
+	EXPECT_EQ (it, k.rend ()) << "not at begin";
+
+	k = Key ("default:/", KEY_END);
+
+	it = k.rend ();
+	EXPECT_EQ ((*it), "") << "name wrong";
+	--it;
+	EXPECT_EQ ((*it), std::string{ KEY_NS_DEFAULT }) << "cascading name wrong";
+	EXPECT_EQ (it, k.rbegin ()) << "not at end";
+	++it;
+	EXPECT_EQ (it, k.rend ()) << "not at begin";
+}
