912d050a9d5ce543305b58d9858daf7f4e772453
Klemens Böswirth <k.boeswirth+git@gmail.com>
2021-01-20 19:43:38 +0100

    format kdbenum.c with clang-format

--- src/libs/elektra/kdbenum.c
+++ src/libs/elektra/kdbenum.c
@@ -9,62 +9,61 @@
  * @copyright BSD License (see LICENSE.md or https://www.libelektra.org)
  */
 
-// clang-format off
 
 /**
-  * The version information in x.y.z format as string.
-  *
-  * To get the version at run-time, you can get the key
-  * system:/elektra/version/constants/KDB_VERSION
-  *
-  * @see [VERSION.md](/doc/VERSION.md).
-  * @see #KDB_VERSION_MAJOR
-  * @see #KDB_VERSION_MINOR
-  * @see #KDB_VERSION_PATCH
-  * @ingroup kdb
-  */
+ * The version information in x.y.z format as string.
+ *
+ * To get the version at run-time, you can get the key
+ * system:/elektra/version/constants/KDB_VERSION
+ *
+ * @see [VERSION.md](/doc/VERSION.md).
+ * @see #KDB_VERSION_MAJOR
+ * @see #KDB_VERSION_MINOR
+ * @see #KDB_VERSION_PATCH
+ * @ingroup kdb
+ */
 #define KDB_VERSION "x.y.z"
 
 /**
-  * The version information of the major version as number.
-  *
-  * To get the version at run-time, you can get the key
-  * system:/elektra/version/constants/KDB_VERSION_MAJOR
-  *
-  * @see [VERSION.md](/doc/VERSION.md).
-  * @see #KDB_VERSION
-  * @ingroup kdb
-  */
+ * The version information of the major version as number.
+ *
+ * To get the version at run-time, you can get the key
+ * system:/elektra/version/constants/KDB_VERSION_MAJOR
+ *
+ * @see [VERSION.md](/doc/VERSION.md).
+ * @see #KDB_VERSION
+ * @ingroup kdb
+ */
 #define KDB_VERSION_MAJOR x
 
 /**
-  * The version information of the minor version as number.
-  *
-  * To get the version at run-time, you can get the key
-  * system:/elektra/version/constants/KDB_VERSION_MINOR
-  *
-  * @see [VERSION.md](/doc/VERSION.md).
-  * @see #KDB_VERSION
-  * @ingroup kdb
-  */
+ * The version information of the minor version as number.
+ *
+ * To get the version at run-time, you can get the key
+ * system:/elektra/version/constants/KDB_VERSION_MINOR
+ *
+ * @see [VERSION.md](/doc/VERSION.md).
+ * @see #KDB_VERSION
+ * @ingroup kdb
+ */
 #define KDB_VERSION_MINOR y
 
 /**
-  * The version information of the patch version as number.
-  *
-  * To get the version at run-time, you can get the key
-  * system:/elektra/version/constants/KDB_VERSION_PATCH
-  *
-  * @see [VERSION.md](/doc/VERSION.md).
-  * @see #KDB_VERSION
-  * @ingroup kdb
-  */
+ * The version information of the patch version as number.
+ *
+ * To get the version at run-time, you can get the key
+ * system:/elektra/version/constants/KDB_VERSION_PATCH
+ *
+ * @see [VERSION.md](/doc/VERSION.md).
+ * @see #KDB_VERSION
+ * @ingroup kdb
+ */
 #define KDB_VERSION_PATCH z
 
 /** `/` is used to separate key names.
  *
  * @see @link keyname description about key names @endlink.
-  * @ingroup key
+ * @ingroup key
  * */
 #define KDB_PATH_SEPARATOR '/'
 
@@ -86,31 +85,31 @@
  */
 enum elektraKeyFlags
 {
-	KEY_NAME=1,		/*!< Flag for the key name */
-	KEY_VALUE=1<<1,		/*!< Flag for the key data */
-	KEY_FLAGS=3,		/*!< Allows to define multiple flags at once. */
-	KEY_COMMENT=1<<3,	/*!< Flag for the key comment */
-	KEY_BINARY=1<<4,	/*!< Flag if the key is binary */
-	KEY_SIZE=1<<11,		/*!< Flag for maximum size to limit value */
-	KEY_META=1<<15,		/*!< Flag for metadata */
-	KEY_NULL=1<<16,		/*!< Is *not* a flag, only as return value @deprecated do not use */
+	KEY_NAME = 1,	      /*!< Flag for the key name */
+	KEY_VALUE = 1 << 1,   /*!< Flag for the key data */
+	KEY_FLAGS = 3,	      /*!< Allows to define multiple flags at once. */
+	KEY_COMMENT = 1 << 3, /*!< Flag for the key comment */
+	KEY_BINARY = 1 << 4,  /*!< Flag if the key is binary */
+	KEY_SIZE = 1 << 11,   /*!< Flag for maximum size to limit value */
+	KEY_META = 1 << 15,   /*!< Flag for metadata */
+	KEY_NULL = 1 << 16,   /*!< Is *not* a flag, only as return value @deprecated do not use */
 	// hole for elektraLockFlags
-	KEY_END=0		/*!< Used as a parameter terminator to keyNew() */
+	KEY_END = 0 /*!< Used as a parameter terminator to keyNew() */
 };
 
 /**
  * Copy options
- * 
+ *
  * @ingroup key
  * @see keyCopy()
  */
 enum elektraCopyFlags
 {
-  	KEY_CP_NAME   = 1<<0, /*!< Flag for copying the key name */
-    KEY_CP_STRING = 1<<1, /*!< Flag for copying the key value, if it is a string */
-    KEY_CP_VALUE  = 1<<2, /*!< Flag for copying the key value */
-    KEY_CP_META   = 1<<3, /*!< Flag for copying the key metadata */
-    KEY_CP_ALL    = KEY_CP_NAME | KEY_CP_VALUE | KEY_CP_META, /*!< Shorthand for copying name, value and metadata */
+	KEY_CP_NAME = 1 << 0,				       /*!< Flag for copying the key name */
+	KEY_CP_STRING = 1 << 1,				       /*!< Flag for copying the key value, if it is a string */
+	KEY_CP_VALUE = 1 << 2,				       /*!< Flag for copying the key value */
+	KEY_CP_META = 1 << 3,				       /*!< Flag for copying the key metadata */
+	KEY_CP_ALL = KEY_CP_NAME | KEY_CP_VALUE | KEY_CP_META, /*!< Shorthand for copying name, value and metadata */
 };
 
 /**
@@ -121,9 +120,9 @@ enum elektraCopyFlags
  */
 enum elektraLockFlags
 {
-	KEY_LOCK_NAME=1<<17,	/*!< lock the name of a key */
-	KEY_LOCK_VALUE=1<<18,	/*!< lock the value of a key */
-	KEY_LOCK_META=1<<19,	/*!< lock the meta data of a key */
+	KEY_LOCK_NAME = 1 << 17,  /*!< lock the name of a key */
+	KEY_LOCK_VALUE = 1 << 18, /*!< lock the value of a key */
+	KEY_LOCK_META = 1 << 19,  /*!< lock the meta data of a key */
 };
 
 
@@ -135,15 +134,15 @@ enum elektraLockFlags
  */
 enum elektraNamespace
 {
-	KEY_NS_NONE=0,          ///< no key given as parameter to keyGetNamespace()
-	KEY_NS_CASCADING=1,     ///< cascading key, starts with /, abstract name for any of the namespaces below
-	KEY_NS_META=2,          ///< metakey, i.e. any key name not under other categories
-	KEY_NS_SPEC=3,          ///< spec contains the specification of the other namespaces
-	KEY_NS_PROC=4,          ///< proc contains process-specific configuration
-	KEY_NS_DIR=5,           ///< dir contains configuration from a specific directory
-	KEY_NS_USER=6,          ///< user key in the home directory of the current user
-	KEY_NS_SYSTEM=7,        ///< system key is shared for a computer system
-  KEY_NS_DEFAULT=8,       ///< default key used as a fallback if no other key is found
+	KEY_NS_NONE = 0,      ///< no key given as parameter to keyGetNamespace()
+	KEY_NS_CASCADING = 1, ///< cascading key, starts with /, abstract name for any of the namespaces below
+	KEY_NS_META = 2,      ///< metakey, i.e. any key name not under other categories
+	KEY_NS_SPEC = 3,      ///< spec contains the specification of the other namespaces
+	KEY_NS_PROC = 4,      ///< proc contains process-specific configuration
+	KEY_NS_DIR = 5,	      ///< dir contains configuration from a specific directory
+	KEY_NS_USER = 6,      ///< user key in the home directory of the current user
+	KEY_NS_SYSTEM = 7,    ///< system key is shared for a computer system
+	KEY_NS_DEFAULT = 8,   ///< default key used as a fallback if no other key is found
 };
 
 /**
@@ -154,9 +153,9 @@ enum elektraNamespace
  *
  * @def KS_END
  * @see ksNew() and ksVNew()
-  * @ingroup keyset
+ * @ingroup keyset
  */
-#define KS_END ((Key*)0)
+#define KS_END ((Key *) 0)
 
 /**
  * Options to change the default behavior of
@@ -170,22 +169,21 @@ enum elektraNamespace
 enum elektraLookupFlags
 {
 
-/**
- * No Option set.
- *
- * @see ksLookup()
- */
-	KDB_O_NONE=0,
-/**
- * Delete parentKey key in ksLookup().
- *
- * @see ksLookup()
- */
-	KDB_O_DEL=1,
-/** Pop Parent out of keyset key in ksLookup().
- *
- * @see ksPop().
- */
-	KDB_O_POP=1<<1
+	/**
+	 * No Option set.
+	 *
+	 * @see ksLookup()
+	 */
+	KDB_O_NONE = 0,
+	/**
+	 * Delete parentKey key in ksLookup().
+	 *
+	 * @see ksLookup()
+	 */
+	KDB_O_DEL = 1,
+	/** Pop Parent out of keyset key in ksLookup().
+	 *
+	 * @see ksPop().
+	 */
+	KDB_O_POP = 1 << 1
 };
-
