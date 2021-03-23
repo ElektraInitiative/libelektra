99439e7280adf9f6e38d3973e4255394a09282a2
Aaron Abebe <aaron.abebe@gmail.com>
2021-03-23 19:26:02 +0100

    [CM H1] added new test for jna binding plugin, added new test for KeyNameIterator.java

--- src/bindings/jna/libelektra5j/src/test/java/org/libelektra/AllTests.java
+++ src/bindings/jna/libelektra5j/src/test/java/org/libelektra/AllTests.java
@@ -4,10 +4,11 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 import org.libelektra.exception.model.WarningEntryTest;
+import org.libelektra.plugin.ReturnTest;
 
 @RunWith (Suite.class)
-@SuiteClasses ({ KeySetTest.class, KeyTest.class, KDBTest.class, ExceptionMapperTest.class, KDBExceptionTest.class, PluginLoaderIT.class,
-		 WarningEntryTest.class, ExceptionMapperIT.class, GOptsTest.class })
+@SuiteClasses ({ KeySetTest.class, KeyTest.class, KeyNameIteratorTest.class, KDBTest.class, ExceptionMapperTest.class, KDBExceptionTest.class, PluginLoaderIT.class,
+		 WarningEntryTest.class, ReturnTest.class, ExceptionMapperIT.class, GOptsTest.class })
 public class AllTests
 {
 }
