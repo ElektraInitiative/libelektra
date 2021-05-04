e9c5efcaf9d5601d89017643ebe394aeeec0e671
Andreas Kraschitzer <e1226853@student.tuwien.ac.at>
2021-05-04 15:10:45 -0700

    [copyright] remove internal link for externally used files
    closes #3822

--- src/tools/kdb/gen/templates/collect.sh
+++ src/tools/kdb/gen/templates/collect.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
 
-# Copyright BSD License (see LICENSE.md or https://www.libelektra.org)
-# BSD License (see LICENSE.md or https://www.libelektra.org)
+# @copyright BSD Zero Clause License
+#
+#     Copyright (c) Elektra Initiative (https://www.libelektra.org)
+#
+#     Permission to use, copy, modify, and/or distribute this software for any
+#     purpose with or without fee is hereby granted.
+#
+#     THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+#     REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
+#     FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+#     INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+#     LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+#     OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+#     PERFORMANCE OF THIS SOFTWARE.
 
 # shellcheck disable=SC2016
 AWK_ESCAPE_SCRIPT='
