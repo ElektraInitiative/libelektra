947a02bd2d532136b6e54d9f50b489b10379d04e
Michael Tucek <michael@tucek.eu>
2021-05-22 12:58:45 +0200

    Revert "#3825"
    This reverts commit 1960b03aa34c4c5564b4ecbff351d000dcf63e00.
    
    Revert "retrigger checks"
    
    This reverts commit 560cd3c0fd0538233524e302ff26ef6311ce6846.
    
    Revert "applied style"
    
    This reverts commit edb1a414d941c07ece18ae94b1da54a2d7b3d7c6.
    
    Revert "#3825 removed DeleteLocalRef in the hope of change for the better ;)"
    
    This reverts commit dcb4c41b53a7c364eefa8eb4954599edf5cc2144.

--- src/plugins/jni/jni.c
+++ src/plugins/jni/jni.c
@@ -153,10 +153,10 @@ static int call2Arg (Data * data, KeySet * ks, Key * errorKey, const char * meth
 	// checkException (data, method, errorKey);
 
 	// clean up local references
-	//(*data->env)->DeleteLocalRef (data->env, jkey);
-	// checkException (data, method, errorKey);
-	//(*data->env)->DeleteLocalRef (data->env, jks);
-	// checkException (data, method, errorKey);
+	(*data->env)->DeleteLocalRef (data->env, jkey);
+	checkException (data, method, errorKey);
+	(*data->env)->DeleteLocalRef (data->env, jks);
+	checkException (data, method, errorKey);
 
 	return result;
 }
@@ -338,8 +338,8 @@ int elektraJniClose (Plugin * handle, Key * errorKey)
 	int ret = call1Arg (data, errorKey, "close");
 
 	// clean up local references
-	//(*data->env)->DeleteLocalRef (data->env, data->plugin);
-	// checkException (data, "close", errorKey);
+	(*data->env)->DeleteLocalRef (data->env, data->plugin);
+	checkException (data, "close", errorKey);
 
 	(*data->jvm)->DestroyJavaVM (data->jvm);
 	elektraFree (data);
