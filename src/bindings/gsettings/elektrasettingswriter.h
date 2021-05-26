67da89ae7fc2bdd450d753912ccd194b36d92e10
Mihael Pranjic <mpranj@limun.org>
2021-05-26 11:37:42 +0200

    gsettings: add writer

---
+++ src/bindings/gsettings/elektrasettingswriter.h
@@ -0,0 +1,18 @@
+/**
+ * @file
+ *
+ * @brief Header for ElektraSettingsWriter
+ *
+ * @copyright BSD License (see LICENSE.md or https://www.libelektra.org)
+ *
+ */
+
+#ifndef ELEKTRA_SETTINGS_WRITER_H
+#define ELEKTRA_SETTINGS_WRITER_H
+
+#include <stdio.h>
+
+#include <glib.h>
+#include <gio/gio.h>
+
+#endif // ELEKTRA_SETTINGS_WRITER_H
