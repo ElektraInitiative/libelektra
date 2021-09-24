7b49799e6d6341c9f0e810ca64d63f686a78e4e5
Tobias Schubert <e1307830@student.tuwien.ac.at>
2021-09-24 18:41:34 +0200

    Execute reformat-all script

--- src/libs/highlevel/elektra.c
+++ src/libs/highlevel/elektra.c
@@ -121,13 +121,14 @@ Elektra * elektraOpen (const char * application, KeySet * defaults, KeySet * con
 	// If a warning occurs in kdbGet, it will not return -1. We need to check the parentKey for warnings.
 	if (kdbGetResult >= 0)
 	{
-		// Applications using the HL API should treat warnings as errors. Therefore, if a warning occurred, set the error param and return
-		// NULL.
+		// Applications using the HL API should treat warnings as errors. Therefore, if a warning occurred, set the error param and
+		// return NULL.
 		ElektraError * errorFromKdbGet = elektraErrorFromKey (parentKey);
-		if(errorFromKdbGet->warningCount > 0)
+		if (errorFromKdbGet->warningCount > 0)
 		{
 			// If there are warnings, pick the first warning, create a copy, set it to param "error" and return NULL.
-			// We can only report 1 error at a time. Once the user has fixed that error, they will be informed about the next one on the next execution of the application.
+			// We can only report 1 error at a time. Once the user has fixed that error, they will be informed about the next
+			// one on the next execution of the application.
 			*error = elektraErrorCreate (errorFromKdbGet->warnings[0]->code, errorFromKdbGet->description,
 						     errorFromKdbGet->module, errorFromKdbGet->file, errorFromKdbGet->line);
 
@@ -137,7 +138,7 @@ Elektra * elektraOpen (const char * application, KeySet * defaults, KeySet * con
 			keyDel (parentKey);
 			return NULL;
 		}
-		elektraErrorReset (errorFromKdbGet);
+		elektraErrorReset (&errorFromKdbGet);
 	}
 	// If kdbGet() returns -1, there was an error.
 	else if (kdbGetResult == -1)
