3ad09be0e924b43139e4d4b59907b97670768000
Markus Raab <elektra@markus-raab.org>
2021-10-01 05:41:42 +0200

    man pages: fix quoting

--- scripts/cmake/ElektraManPage.cmake
+++ scripts/cmake/ElektraManPage.cmake
@@ -12,7 +12,7 @@ if (NOT DATE)
 endif (NOT DATE)
 
 execute_process (
-	COMMAND "${CMAKE_COMMAND} -E env RUBYOPT=-Eutf-8:utf-8 LC_ALL=C.utf-8 ${RONN_COMMAND} -r --pipe ${MDFILE} --date=${DATE}"
+	COMMAND "${CMAKE_COMMAND}" -E env RUBYOPT=-Eutf-8:utf-8 LC_ALL=C.utf-8 "${RONN_COMMAND}" -r --pipe "${MDFILE}" "--date=${DATE}"
 	OUTPUT_FILE ${MANPAGE}
 	ERROR_VARIABLE ERROR_OUTPUT
 	RESULT_VARIABLE RESULT)
