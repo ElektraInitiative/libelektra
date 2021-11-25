6a8c1f6e44d9c34fa6bbef27c572bd1fc98be96a
4ydan <ghazani.aydan@gmail.com>
2021-11-25 20:28:39 +0000

    Integration: changed to tabsize 4

--- scripts/mac_setup.sh
+++ scripts/mac_setup.sh
@@ -2,8 +2,8 @@
 
 # Install brew if required
 if [[ $(command -v brew) == "" ]]; then
-  echo "Installing Hombrew"
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+	echo "Installing Hombrew"
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 fi
 
 brew update
@@ -11,38 +11,39 @@ brew config
 # Unlink parallel package, because of conflict with moreutils
 brew unlink parallel || printf >&2 'Unlinking parallel failed.`\n'
 brew install augeas \
-  antlr \
-  antlr4-cpp-runtime \
-  bison \
-  clang-format \
-  dbus \
-  discount \
-  doxygen \
-  flex \
-  glib \
-  gpgme \
-  gradle \
-  graphviz \
-  libev \
-  libgcrypt \
-  libgit2 \
-  libuv \
-  lua \
-  moreutils \
-  ninja \
-  npm \
-  openssl \
-  pkg-config \
-  qt \
-  shfmt \
-  swig \
-  tree \
-  xerces-c \
-  yajl \
-  yaml-cpp \
-  zeromq
+	antlr \
+	antlr4-cpp-runtime \
+	bison \
+	clang-format \
+	dbus \
+	discount \
+	doxygen \
+	flex \
+	glib \
+	gpgme \
+	gradle \
+	graphviz \
+	libev \
+	libgcrypt \
+	libgit2 \
+	libuv \
+	lua \
+	moreutils \
+	ninja \
+	npm \
+	openssl \
+	pkg-config \
+	qt \
+	shfmt \
+	swig \
+	tree \
+	xerces-c \
+	yajl \
+	yaml-cpp \
+	zeromq
 if [ "$CC" = 'clang' ]; then
-  brew install --cask oclint
+	brew install --cask oclint
+	brew install --cask oclint
 fi
 brew install --cask adoptopenjdk
 # Try to install `checkbashisms` (The file server that hosts the package is unfortunately quite unreliable.)
