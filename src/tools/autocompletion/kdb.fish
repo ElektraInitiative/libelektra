53f582a32d2c11696182ee5be0dbc0f278371b1e
muskater <70842226+muskater@users.noreply.github.com>
2021-12-01 19:26:48 +0100

    autocompletion: fixed formatting issue in the shell wrapper

--- src/tools/autocompletion/kdb.fish
+++ src/tools/autocompletion/kdb.fish
@@ -3,6 +3,5 @@
 # all known commands can be found here
 set -l commands '(python3 ~/workspace/src/tools/autocompletion/find_autocompletion_options.py -m system:/spec/autocomplete/kdb)'
 
-
 complete --command kdb -f
-complete  --arguments "$commands" --command kdb
+complete --arguments "$commands" --command kdb
