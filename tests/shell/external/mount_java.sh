3debbf1b91a07da028d0833560ecc887f6db463c
Klemens Böswirth <k.boeswirth+git@gmail.com>
2022-02-26 18:35:42 +0100

    process: add external test for mount-java

---
+++ tests/shell/external/mount_java.sh
@@ -0,0 +1,12 @@
+@INCLUDE_COMMON@
+
+echo
+echo ELEKTRA CHECK MOUNT JAVA
+echo
+
+"$KDB" mount-java mountjava.cfg user:/test/mountjava dump java:org.libelektra.plugin.WhitelistPlugin kdb:type
+succeed_if "mount-java not successful"
+
+"$KDB" umount user:/test/mountjava
+
+end_script mount_java
