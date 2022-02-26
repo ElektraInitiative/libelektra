be199ed0b9eb46c5e7a525500c3136294f47ae9f
Klemens Böswirth <k.boeswirth+git@gmail.com>
2022-02-26 20:21:05 +0100

    process: fix mount-java if no java found

--- tests/shell/external/mount_java.sh
+++ tests/shell/external/mount_java.sh
@@ -4,6 +4,11 @@ echo
 echo ELEKTRA CHECK MOUNT JAVA
 echo
 
+if ! command -v java; then
+	echo "No Java installation found."
+	exit 0
+fi
+
 "$KDB" mount-java mountjava.cfg user:/test/mountjava dump java:org.libelektra.plugin.WhitelistPlugin kdb:type
 succeed_if "mount-java not successful"
 
