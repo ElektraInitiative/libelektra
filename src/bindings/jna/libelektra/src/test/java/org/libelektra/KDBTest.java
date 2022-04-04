42c4a660ac7e0adc0532226cc5f7926d3b9ea375
Gratla <philippleeb@gmail.com>
2022-04-04 13:53:11 +0200

    jna: Reformatted KDBTest.java

--- src/bindings/jna/libelektra/src/test/java/org/libelektra/KDBTest.java
+++ src/bindings/jna/libelektra/src/test/java/org/libelektra/KDBTest.java
@@ -1,10 +1,10 @@
 package org.libelektra;
 
-import org.junit.Test;
-import org.libelektra.exception.KDBClosedException;
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.libelektra.exception.KDBClosedException;
 
 public class KDBTest {
 
