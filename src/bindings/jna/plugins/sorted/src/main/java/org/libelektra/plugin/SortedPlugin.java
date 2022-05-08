9028c833ddbefad9eb73c55dcea1b7e37e0bf074
Markus Bointner <bointner.markus9@gmail.com>
2022-05-08 13:41:23 +0200

    jna/plugins: added skeleton for sorted plugin

---
+++ src/bindings/jna/plugins/sorted/src/main/java/org/libelektra/plugin/SortedPlugin.java
@@ -0,0 +1,41 @@
+package org.libelektra.plugin;
+
+import org.libelektra.KDBException;
+import org.libelektra.Key;
+import org.libelektra.KeySet;
+import org.libelektra.Plugin;
+
+import javax.annotation.Nonnull;
+
+public class SortedPlugin implements Plugin {
+    @Nonnull
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public int open(KeySet config, Key errorKey) {
+        return 0;
+    }
+
+    @Override
+    public int get(KeySet keySet, Key parentKey) throws KDBException {
+        return 0;
+    }
+
+    @Override
+    public int set(KeySet keySet, Key parentKey) throws KDBException {
+        return 0;
+    }
+
+    @Override
+    public int error(KeySet keySet, Key parentKey) {
+        return 0;
+    }
+
+    @Override
+    public int close(Key parentKey) {
+        return 0;
+    }
+}
