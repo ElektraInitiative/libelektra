cc6bc9e23728784f853f3da5388a296579f2ffb2
Markus Bointner <bointner.markus9@gmail.com>
2022-05-10 21:06:08 +0200

    jna/kotlin: added project setup for kotlin binding

---
+++ src/bindings/jna/kotlin/src/test/kotlin/org/libelektra/SampleTest.kt
@@ -0,0 +1,14 @@
+package org.libelektra
+
+import org.junit.Test
+import kotlin.test.assertTrue
+
+class SampleTest {
+
+    @Test
+    fun `a simple test case`() {
+        assertTrue {
+            "a simple kotlin test".isNotEmpty()
+        }
+    }
+}
