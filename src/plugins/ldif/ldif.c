120082575a013d4367b8a4250083e8b19580d547
Burkhard Hampl <burkhard.hampl@student.tuwien.ac.at>
2022-05-11 17:53:50 +0200

    ldif: Implement key extraction

--- src/plugins/ldif/ldif.c
+++ src/plugins/ldif/ldif.c
@@ -7,11 +7,17 @@
  *
  */
 
-#include "ldif.h"
+#define _GNU_SOURCE
+#ifndef HAVE_KDBCONFIG
+#include "kdbconfig.h"
+#endif
 
+#include "ldif.h"
 #include <errno.h>
-#include <kdbhelper.h>
+
 #include <kdberrors.h>
+#include <kdbhelper.h>
+#include <kdblogger.h>
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -34,6 +40,106 @@ int elektraLdifClose (Plugin * handle ELEKTRA_UNUSED, Key * errorKey ELEKTRA_UNU
 	// this function is optional
 
 	return ELEKTRA_PLUGIN_STATUS_SUCCESS;
+}
+
+char ** parseToken (const char * in)
+{
+	char * localCopy = elektraStrDup (in);
+	if (localCopy == NULL)
+	{
+		return NULL;
+	}
+
+	int size = 1;
+	for (char * s = localCopy;; s++)
+	{
+		s = strpbrk (s, ",");
+		if (!s) break;
+		size++;
+	}
+
+	char ** res = (char **) elektraMalloc ((size + 1) * sizeof (char *));
+
+	if (res == NULL)
+	{
+		elektraFree (localCopy);
+		return NULL;
+	}
+
+	char * savePtr;
+
+	int i = 0;
+	for (char * s = strtok_r (localCopy, ",", &savePtr); s != NULL; s = strtok_r (NULL, ",", &savePtr))
+	{
+		res[i] = elektraStrDup (s);
+
+		if (res[i] == NULL)
+		{
+			for (--i; i >= 0; i--)
+			{
+				elektraFree (res[i]);
+			}
+			elektraFree (res);
+			elektraFree (localCopy);
+			return NULL;
+		}
+		i++;
+	}
+	res[i] = NULL;
+
+	elektraFree (localCopy);
+	return (res);
+}
+
+char * makeKey (char ** in, int len)
+{
+	size_t size = 0;
+	for (int i = 0; i < len && in[i] != NULL; i++)
+	{
+		char * s = in[i];
+		size += strlen (s) + 1;
+	}
+
+	if (size == 0)
+	{
+		return NULL;
+	}
+
+	/* trim extra sep len */
+	size -= 1;
+
+	char * out = elektraMalloc (size + 1);
+	if (out == NULL)
+	{
+		return NULL;
+	}
+
+	char * p = out;
+	for (int i = 0; i < len && in[i] != NULL; i++)
+	{
+		char * s = in[i];
+		if (i != 0)
+		{
+			strncpy (p, "/", 1);
+			p += 1;
+		}
+
+		strcpy (p, s);
+		p += strlen (s);
+	}
+
+	*p = '\0';
+	return out;
+}
+
+void reverse (char ** arr, int size)
+{
+	for (int i = 0; i < size / 2; i++)
+	{
+		char * temp = arr[i];
+		arr[i] = arr[size - 1 - i];
+		arr[size - 1 - i] = temp;
+	}
 }
 
 int elektraLdifGet (Plugin * handle ELEKTRA_UNUSED, KeySet * returned, Key * parentKey)
@@ -59,7 +165,8 @@ int elektraLdifGet (Plugin * handle ELEKTRA_UNUSED, KeySet * returned, Key * par
 	}
 	// get all keys
 	int errnosave = errno;
-	LDIFFP * lfp = ldif_open(keyString (parentKey), "r");
+	const char * filename = keyString (parentKey);
+	LDIFFP * lfp = ldif_open(filename, "r");
 
 	if (!lfp)
 	{
@@ -72,25 +179,102 @@ int elektraLdifGet (Plugin * handle ELEKTRA_UNUSED, KeySet * returned, Key * par
 	char *buff = NULL;
 	int buflen = 0;
 
-	// TODO replace printf with proper error handling
-	while (ldif_read_record(lfp, &lineno, &buff, &buflen)) {
-		char *line;
-		char *next = buff;
+	ssize_t ksize = 0;
 
-		while ((line = ldif_getline(&next)) != NULL) {
-			char *type = NULL;
-			char *value = NULL;
+	// TODO replace printf with ELEKTRA_LOG_DEBUG
+	while (ldif_read_record (lfp, &lineno, &buff, &buflen))
+	{
+		char * line;
+		char * next = buff;
+
+		while ((line = ldif_getline (&next)) != NULL)
+		{
+			char * type = NULL;
+			char * value = NULL;
 			ber_len_t vlen;
 
-			if (ldif_parse_line(line, &type, &value, &vlen)) {
-				printf("cannot parse line: %s\n", line);
+			if (ldif_parse_line (line, &type, &value, &vlen))
+			{
+				ELEKTRA_ADD_VALIDATION_SYNTACTIC_WARNINGF (parentKey, "cannot parse line: %s\n", line);
 				continue;
 			}
 
-			printf ("type: %s, value: %s\n", type, value);
+			if (elektraStrCmp (type, "dn") == 0)
+			{
+				// key
+				printf ("found key: %s\n", value);
 
-			free (type);
-			free (value);
+				char ** tokens = parseToken (value);
+
+				if (tokens == NULL)
+				{
+					ELEKTRA_SET_ERROR_GET (parentKey);
+
+					elektraFree (buff);
+					ldif_close (lfp);
+					return ELEKTRA_PLUGIN_STATUS_ERROR;
+				}
+
+				int size = 0;
+
+				while (tokens[size] != NULL)
+				{
+					size++;
+				}
+
+				reverse (tokens, size);
+
+				for (int i = 0; i < size; ++i)
+				{
+
+					Key * key = keyNew (keyName (parentKey), KEY_END);
+					char * domainpart = makeKey (tokens, i + 1);
+
+					printf ("saving key: %s\n", domainpart);
+
+					if (domainpart == NULL)
+					{
+						ELEKTRA_SET_ERROR_GET (parentKey);
+						elektraFree (domainpart);
+						elektraFree (tokens);
+						elektraFree (buff);
+						ldif_close (lfp);
+						return ELEKTRA_PLUGIN_STATUS_ERROR;
+					}
+
+					if (keyAddName (key, domainpart) == -1)
+					{
+						ELEKTRA_ADD_VALIDATION_SYNTACTIC_WARNINGF (
+							parentKey, "Key name '%s' is not valid, discarding key", value);
+						elektraFree (domainpart);
+						keyDel (key);
+						break;
+					}
+
+					if (ksAppendKey (returned, key) != ksize + 1)
+					{
+						// TODO re-enable duplicate check
+//						ELEKTRA_SET_VALIDATION_SYNTACTIC_ERRORF (parentKey,
+//											 "Duplicated key '%s' at position %ld in file %s",
+//											 keyName (key), ftell (lfp->fp), filename);
+//						free (buff);
+//						ldif_close (lfp);
+//						return ELEKTRA_PLUGIN_STATUS_ERROR;
+					}
+					++ksize;
+					elektraFree (domainpart);
+				}
+
+				elektraFree (tokens);
+			}
+			else
+			{
+				// value
+				printf ("found value: type: %s, value: %s\n", type, value);
+			}
+
+			elektraFree (type);
+			elektraFree (value);
 		}
 	}
 
@@ -98,7 +282,7 @@ int elektraLdifGet (Plugin * handle ELEKTRA_UNUSED, KeySet * returned, Key * par
 
 	ldif_close (lfp);
 
-	return ELEKTRA_PLUGIN_STATUS_NO_UPDATE;
+	return ELEKTRA_PLUGIN_STATUS_SUCCESS;
 }
 
 int elektraLdifSet (Plugin * handle ELEKTRA_UNUSED, KeySet * returned ELEKTRA_UNUSED, Key * parentKey ELEKTRA_UNUSED)
