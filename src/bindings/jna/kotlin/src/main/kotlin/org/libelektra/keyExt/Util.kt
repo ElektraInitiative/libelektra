bbb6ee9cd772ce2a0060a047f8194861b155c9d3
Gratla <philippleeb@gmail.com>
2022-05-14 21:35:28 +0200

    jna/kotlin: Added extension function for keyOf and custom builder for keyOf which creates a new key.

--- src/bindings/jna/kotlin/src/main/kotlin/org/libelektra/keyExt/Util.kt
+++ src/bindings/jna/kotlin/src/main/kotlin/org/libelektra/keyExt/Util.kt
@@ -1,7 +1,22 @@
 package org.libelektra.keyExt
 
 import org.libelektra.Key
+import java.util.*
 
 fun Key.isEmpty() = string.isEmpty()
 
 fun Key.isNotEmpty() = !isEmpty()
+
+fun <T> keyOf(name: String, value: T?): Key {
+    return Key.create(name, value)
+}
+
+fun <T> keyOf(name: String, value: T?, vararg metaKeys: Key): Key {
+    return Key.create(name, value).apply {
+        metaKeys.forEach {
+            setMeta(it.name, it.string)
+        }
+    }
+}
+
+fun <T> Optional<T>.orNull(): T? = orElse(null)
---
+++ src/bindings/jna/kotlin/src/test/kotlin/org/libelektra/key/UtilTest.kt
@@ -0,0 +1,29 @@
+package org.libelektra.key
+
+import org.junit.Test
+import org.libelektra.keyExt.keyOf
+import org.libelektra.keyExt.orNull
+import kotlin.test.assertEquals
+
+class UtilTest {
+
+    @Test
+    fun `keyOf with name and value, returns correct key`() {
+        val key = keyOf("/test", "1234")
+
+        assertEquals(key.name, "/test")
+        assertEquals(key.string, "1234")
+    }
+
+    @Test
+    fun `keyOf with name and value and meta keys, returns correct key including meta keys`() {
+        val key = keyOf(
+            "/test", "1234",
+            keyOf("/meta1", "value1"),
+            keyOf("/meta2", "value2")
+        )
+
+        assertEquals(key.getMeta("/meta1").orNull()!!.string, "value1")
+        assertEquals(key.getMeta("/meta2").orNull()!!.string, "value2")
+    }
+}
