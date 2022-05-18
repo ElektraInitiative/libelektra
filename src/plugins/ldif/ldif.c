66d98bfb11eff9ccbfe9242498efa51a487223a8
Richard Stöckl <richard.stoeckl@aon.at>
2022-05-18 20:09:07 +0200

    ldif: write remaining attributes to ldif file

--- src/plugins/ldif/ldif.c
+++ src/plugins/ldif/ldif.c
@@ -183,7 +183,7 @@ int elektraLdifGet (Plugin * handle ELEKTRA_UNUSED, KeySet * returned, Key * par
 
 			if (elektraStrCmp (type, "dn") == 0)
 			{
-				ELEKTRA_LOG_DEBUG("found key: %s\n", value);
+				ELEKTRA_LOG_DEBUG ("found key: %s\n", value);
 
 				char ** tokens = parseToken (value);
 
@@ -218,8 +218,8 @@ int elektraLdifGet (Plugin * handle ELEKTRA_UNUSED, KeySet * returned, Key * par
 					if (domainpart == NULL)
 					{
 						ELEKTRA_SET_VALIDATION_SYNTACTIC_ERRORF (
-							parentKey, "Failed to extract key from file %s at position %ld with key %s", filename,
-							ftell (lfp->fp), value);
+							parentKey, "Failed to extract key from file %s at position %ld with key %s",
+							filename, ftell (lfp->fp), value);
 						elektraFree (domainpart);
 						elektraFree (tokens);
 						elektraFree (buff);
@@ -290,7 +290,7 @@ int elektraLdifSet (Plugin * handle ELEKTRA_UNUSED, KeySet * returned, Key * par
 	// this function is optional
 	int errnosave = errno;
 	const char * filename = keyString (parentKey);
-	LDIFFP * lfp = ldif_open(filename, "w");
+	LDIFFP * lfp = ldif_open (filename, "w");
 
 	ELEKTRA_LOG ("Write to '%s'", keyString (parentKey));
 
@@ -303,25 +303,79 @@ int elektraLdifSet (Plugin * handle ELEKTRA_UNUSED, KeySet * returned, Key * par
 
 	Key * cur;
 	ksRewind (returned);
+	char * last_dn = NULL;
 	while ((cur = ksNext (returned)) != 0)
 	{
-		if (elektraStrCmp (keyBaseName (cur), "dn" ) == 0) {
-			const char * dn = keyString (cur);
-			ELEKTRA_LOG_DEBUG ("processing name: '%s', curr: '%s':'%s'\n", elektraKeyGetRelativeName (cur, parentKey), dn, keyBaseName (cur));
-			char *data = ldif_put_wrap (LDIF_PUT_VALUE, "dn", dn, strlen (dn), LDIF_LINE_WIDTH );
-
-			if (fputs (data, lfp->fp) == EOF || fputs ("\n", lfp->fp) == EOF)
+		const char * type = keyBaseName (cur);
+		if (index (type, '=') == NULL)
+		{
+			ELEKTRA_LOG_DEBUG ("%s is a valid ldif type, proceeding writing\n", type);
+			if (elektraStrCmp (type, "dn") != 0)
 			{
-				ELEKTRA_SET_VALIDATION_SYNTACTIC_ERRORF (parentKey, "Could not write to position %ld in file %s", ftell (lfp->fp),
-									 filename);
+				ELEKTRA_LOG_DEBUG ("%s is not the dn, looking up the corresponding dn first\n", type);
+				const char * full_key_name = elektraStrDup (keyName (cur));
+				size_t full_key_length = elektraStrLen (full_key_name);
+				size_t dn_length = elektraStrLen ("dn");
+				size_t base_name_length = elektraStrLen (type);
+				char * key_dn = elektraMalloc ((full_key_length + dn_length + 1) * sizeof (char));
+				strncpy (key_dn, full_key_name, full_key_length + 1);
+				strncpy (&key_dn[full_key_length - base_name_length], "dn\0", dn_length + 1);
+
+				if (last_dn == NULL || elektraStrCmp (last_dn, key_dn) != 0)
+				{
+					ELEKTRA_LOG_DEBUG ("first occurrence with dn %s, writing it\n", key_dn);
+					Key * dn_key = ksLookupByName (returned, key_dn, 0);
+					if (last_dn != NULL)
+					{
+						elektraFree (last_dn);
+						if (fputs ("\n", lfp->fp) == EOF)
+						{
+							ELEKTRA_SET_VALIDATION_SYNTACTIC_ERRORF (
+								parentKey, "Could not write to position %ld in file %s", ftell (lfp->fp),
+								filename);
+							ldif_close (lfp);
+							return ELEKTRA_PLUGIN_STATUS_ERROR;
+						}
+					}
+					last_dn = key_dn;
+					const char * dn = keyString (dn_key);
+					char * data = ldif_put_wrap (LDIF_PUT_VALUE, "dn", dn, strlen (dn), LDIF_LINE_WIDTH);
+					if (fputs (data, lfp->fp) == EOF)
+					{
+						ELEKTRA_SET_VALIDATION_SYNTACTIC_ERRORF (
+							parentKey, "Could not write to position %ld in file %s", ftell (lfp->fp), filename);
+						ber_memfree (data);
+						ldif_close (lfp);
+						return ELEKTRA_PLUGIN_STATUS_ERROR;
+					}
+					ber_memfree (data);
+				}
+				else
+				{
+					elektraFree (key_dn);
+				}
+			}
+
+			const char * dn = keyString (cur);
+			ELEKTRA_LOG_DEBUG ("processing name: '%s', curr: '%s':'%s'\n", elektraKeyGetRelativeName (cur, parentKey), dn,
+					   keyBaseName (cur));
+			char * data = ldif_put_wrap (LDIF_PUT_VALUE, type, dn, strlen (dn), LDIF_LINE_WIDTH);
+
+			if (fputs (data, lfp->fp) == EOF)
+			{
+				ELEKTRA_SET_VALIDATION_SYNTACTIC_ERRORF (parentKey, "Could not write to position %ld in file %s",
+									 ftell (lfp->fp), filename);
 				ber_memfree (data);
 				ldif_close (lfp);
 				return ELEKTRA_PLUGIN_STATUS_ERROR;
 			}
 
-			ber_memfree(data);
-		} else {
-			ELEKTRA_LOG_DEBUG ("not processing name: '%s', curr: '%s':'%s'\n", elektraKeyGetRelativeName (cur, parentKey), keyString (cur), keyBaseName (cur));
+			ber_memfree (data);
+		}
+		else
+		{
+			ELEKTRA_LOG_DEBUG ("not processing name: '%s', curr: '%s':'%s'\n", elektraKeyGetRelativeName (cur, parentKey),
+					   keyString (cur), keyBaseName (cur));
 		}
 	}
 
