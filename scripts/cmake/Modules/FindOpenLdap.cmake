3141b68c8f02408fc9248242832f7ad0f64ff490
Burkhard Hampl <burkhard.hampl@student.tuwien.ac.at>
2022-06-01 10:22:58 +0200

    ldif: Try to fix macOS build again

--- scripts/cmake/Modules/FindOpenLdap.cmake
+++ scripts/cmake/Modules/FindOpenLdap.cmake
@@ -1,18 +1,25 @@
 # - Find OpenLDAP C Libraries
 #
-# OPENLDAP_FOUND - True if found. OPENLDAP_INCLUDE_DIR - Path to the openldap include directory OPENLDAP_LIBRARIES - Paths to the ldap and
+# OPENLDAP_ROOT_DIR - path to the OpenLDAP installation prefix
+# OPENLDAP_FOUND - True if found. OPENLDAP_INCLUDE_DIRS - Path to the openldap include directory OPENLDAP_LIBRARIES - Paths to the ldap and
 # lber libraries
 
 # Source: https://fossies.org/linux/ceph/cmake/modules/FindOpenLdap.cmake
+# Source: https://github.com/ClickHouse/ClickHouse/blob/17a245aa4de9f0028966e5c61578a3aae0716a33/cmake/Modules/FindOpenLDAP.cmake
 
-find_path (OPENLDAP_INCLUDE_DIR ldap.h PATHS /usr/include /opt/local/include /usr/local/include)
-
-find_library (LDAP_LIBRARY ldap)
-find_library (LBER_LIBRARY lber)
+if(OPENLDAP_ROOT_DIR)
+	find_path (OPENLDAP_INCLUDE_DIRS NAMES "ldap.h" "lber.h" PATHS "${OPENLDAP_ROOT_DIR}" PATH_SUFFIXES "include" NO_DEFAULT_PATH)
+	find_library (OPENLDAP_LDAP_LIBRARY NAMES "ldap" PATHS "${OPENLDAP_ROOT_DIR}" PATH_SUFFIXES "lib" NO_DEFAULT_PATH)
+	find_library (OPENLDAP_LBER_LIBRARY NAMES "lber" PATHS "${OPENLDAP_ROOT_DIR}" PATH_SUFFIXES "lib" NO_DEFAULT_PATH)
+else()
+	find_path (OPENLDAP_INCLUDE_DIRS NAMES ldap.h lber.h PATHS /usr/include /opt/local/include /usr/local/include)
+	find_library (OPENLDAP_LDAP_LIBRARY NAMES "ldap")
+	find_library (OPENLDAP_LBER_LIBRARY NAMES "lber")
+endif()
 
 include (FindPackageHandleStandardArgs)
-find_package_handle_standard_args (OpenLdap DEFAULT_MSG OPENLDAP_INCLUDE_DIR LDAP_LIBRARY LBER_LIBRARY)
+find_package_handle_standard_args (OpenLdap DEFAULT_MSG OPENLDAP_INCLUDE_DIRS OPENLDAP_LDAP_LIBRARY OPENLDAP_LBER_LIBRARY)
 
-set (OPENLDAP_LIBRARIES ${LDAP_LIBRARY} ${LBER_LIBRARY})
+set (OPENLDAP_LIBRARIES ${OPENLDAP_LDAP_LIBRARY} ${OPENLDAP_LBER_LIBRARY})
 
-mark_as_advanced (OPENLDAP_INCLUDE_DIR LDAP_LIBRARY LBER_LIBRARY)
+mark_as_advanced (OPENLDAP_INCLUDE_DIRS OPENLDAP_LDAP_LIBRARY OPENLDAP_LBER_LIBRARY)
