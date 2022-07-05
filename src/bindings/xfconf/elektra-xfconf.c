e3403624d6d956ceb0459f07374643e6ac8ba104
Richard Stöckl <richard.stoeckl@aon.at>
2022-07-05 18:03:44 +0200

    xfconf-binding: Use gelektra for kdb initialization

--- src/bindings/xfconf/elektra-xfconf.c
+++ src/bindings/xfconf/elektra-xfconf.c
@@ -1,13 +1,22 @@
 #include "elektra-xfconf.h"
 #include "elektra-xfconf-util.h"
 
-#include <glib.h>
+#include <gelektra-key.h>
 
 GList * channel_list = NULL;
+GElektraKdb * gElektraKdb = NULL;
 
 gboolean xfconf_init (GError ** error)
 {
-	unimplemented ();
+	GElektraKeySet * empty_set = gelektra_keyset_new (0);
+	GElektraKey * elektra_error = gelektra_key_new ("/elektra_error", KEY_END);
+	gElektraKdb = gelektra_kdb_open (empty_set, elektra_error);
+	if (gElektraKdb == NULL)
+	{
+		*error = g_error_new (0, 1, "unable to open kdb: %s", gelektra_key_string (elektra_error));
+		return FALSE;
+	}
+	return TRUE;
 }
 void xfconf_shutdown (void)
 {
--- src/bindings/xfconf/elektra-xfconf.h
+++ src/bindings/xfconf/elektra-xfconf.h
@@ -2,7 +2,9 @@
 #define ELEKTRA_XFCONF_H
 
 #include <xfconf/xfconf.h>
+#include <gelektra-kdb.h>
 
 extern GList * channel_list;
+extern GElektraKdb * gElektraKdb;
 
 #endif
