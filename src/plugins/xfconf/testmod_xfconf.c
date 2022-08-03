2ba4bb3c715da52c7b5612bb025cc7d488eeda58
Richard Stöckl <richard.stoeckl@aon.at>
2022-08-03 22:42:58 +0200

    xfconf-plugin: Remove unimplemented functions from test

--- src/plugins/xfconf/testmod_xfconf.c
+++ src/plugins/xfconf/testmod_xfconf.c
@@ -30,10 +30,6 @@ static void test_basics (void)
 
 	succeed_if (plugin->kdbSet (plugin, ks, parentKey) == ELEKTRA_PLUGIN_STATUS_NO_UPDATE, "call to kdbSet was not successful");
 
-	succeed_if (plugin->kdbCommit (plugin, ks, parentKey) == ELEKTRA_PLUGIN_STATUS_SUCCESS, "call to kdbCommit was not successful");
-
-	succeed_if (plugin->kdbError (plugin, ks, parentKey) == ELEKTRA_PLUGIN_STATUS_SUCCESS, "call to kdbError was not successful");
-
 	succeed_if (plugin->kdbClose (plugin, parentKey) == ELEKTRA_PLUGIN_STATUS_SUCCESS, "call to kdbClose was not successful");
 
 	keyDel (parentKey);
