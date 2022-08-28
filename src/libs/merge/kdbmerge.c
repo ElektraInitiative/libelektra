f80f8e1fa63bd86ba5c9f26bcb4048fded2d89c1
Maximilian Irlinger <max@maxirlinger.at>
2022-08-28 16:24:42 +0200

    Merge: better symbol versioning

--- src/libs/merge/kdbmerge.c
+++ src/libs/merge/kdbmerge.c
@@ -181,6 +181,14 @@ static int getTotalNonOverlaps (Key * informationKey)
 	       getNonOverlapOnlyBaseConflicts (informationKey);
 }
 
+
+int ELEKTRA_SYMVER (getConflicts, v1) (Key * informationKey)
+{
+	return elektraMergeGetConflicts (informationKey);
+}
+
+ELEKTRA_SYMVER_DECLARE ("libelektra_0.8", getConflicts, v1)
+
 /**
  * This function returns the number of conflicts that is store in the key
  *
