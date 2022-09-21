b1e21ee6c7c778d8296a27cd2c163f6495f82fb5
Maximilian Irlinger <max@maxirlinger.at>
2022-09-21 17:27:06 +0200

    hooks: add tests for most important functions

--- tests/ctest/test_hooks.c
+++ tests/ctest/test_hooks.c
@@ -63,6 +63,110 @@ static void test_getPluginConfigFromContract_withoutConfigInContract (void)
 	ksDel (result);
 }
 
+static void test_loadPlugin(void)
+{
+	printf ("Executing %s\n", __func__);
+
+	// Arrange
+	KeySet * global = ksNew (0, KS_END);
+	KeySet * modules = ksNew (0, KS_END);
+	KeySet * contract = ksNew (30, keyNew ("system:/elektra/contract/mountglobal/gopts", KEY_END),
+				   keyNew ("system:/elektra/contract/mountglobal/gopts/offset", KEY_VALUE, "1", KEY_END), KS_END);
+
+	Key * errorKey = keyNew ("system:/hello", KEY_END);
+
+	// Act
+	Plugin * plugin = loadPlugin ("gopts", global, modules, contract, errorKey);
+
+	// Assert
+	succeed_if (plugin != NULL, "must be able to load plugin");
+	succeed_if (plugin->modules == modules, "must set modules");
+	succeed_if (plugin->global == global, "must set global");
+	succeed_if (ksGetSize (plugin->config) == 2, "config size must be 2");
+
+	ksDel (global);
+	ksDel (modules);
+	ksDel (contract);
+	elektraPluginClose (plugin, errorKey);
+	keyDel (errorKey);
+}
+
+static void test_loadPlugin_inexistent(void)
+{
+	printf ("Executing %s\n", __func__);
+
+	// Arrange
+	KeySet * global = ksNew (0, KS_END);
+	KeySet * modules = ksNew (0, KS_END);
+	KeySet * contract = ksNew (0, KS_END);
+
+	Key * errorKey = keyNew ("system:/hello", KEY_END);
+
+	// Act
+	Plugin * plugin = loadPlugin ("thisPluginSurelyDoesNotExist123", global, modules, contract, errorKey);
+
+	// Assert
+	succeed_if (plugin == NULL, "must not load plugin");
+
+	ksDel (global);
+	ksDel (modules);
+	ksDel (contract);
+	keyDel (errorKey);
+}
+
+static void test_isGoptsEnabledByContract(bool shouldBeEnabled)
+{
+	printf ("Executing %s with shouldBeEnabled=%d\n", __func__, shouldBeEnabled);
+
+	// Arrange
+	KeySet * contract = ksNew (1, KS_END);
+	if(shouldBeEnabled)
+	{
+		ksAppendKey (contract, keyNew ("system:/elektra/contract/mountglobal/gopts", KEY_END));
+	}
+
+	// Act
+	bool result = isGoptsEnabledByContract (contract);
+
+	// Assert
+	succeed_if_fmt (result == shouldBeEnabled, "result is %d but should be %d", result, shouldBeEnabled);
+
+	ksDel (contract);
+}
+
+static void test_initHooks_shouldInitAllHooksWithoutFailure(void)
+{
+	printf ("Executing %s\n", __func__);
+
+	// Arrange
+	KDB * kdb = elektraCalloc (sizeof (struct _KDB));
+	KeySet * config = ksNew (0, KS_END);
+	KeySet * modules = ksNew (0, KS_END);
+	KeySet * contract = ksNew (1, keyNew ("system:/elektra/contract/mountglobal/gopts", KEY_END), KS_END);
+
+	kdb->global = ksNew (0, KS_END);
+	kdb->modules = modules;
+
+	Key * errorKey = keyNew ("system:/elektra", KS_END);
+
+	// Act
+	int result = initHooks (kdb, config, modules, contract, errorKey);
+
+	// Assert
+	KeySet * meta = keyMeta (errorKey);
+
+	succeed_if (result == 0, "result should be 0");
+	succeed_if (ksGetSize (meta) == 0, "error key should not have meta data");
+	succeed_if (kdb->hooks.gopts.plugin != NULL, "gopts plugin should be loaded");
+	succeed_if (kdb->hooks.gopts.kdbHookGoptsGet != NULL, "gopts.kdbHookGoptsGet should be found");
+
+	ksDel (config);
+	ksDel (modules);
+	ksDel (contract);
+
+	elektraFree (kdb);
+}
+
 int main(int argc, char ** argv)
 {
 	printf ("HOOKS       TESTS\n");
@@ -71,6 +175,11 @@ int main(int argc, char ** argv)
 	init (argc, argv);
 	test_getPluginConfigFromContract_withConfigInContract ();
 	test_getPluginConfigFromContract_withoutConfigInContract();
+	test_loadPlugin();
+	test_loadPlugin_inexistent();
+	test_isGoptsEnabledByContract(true);
+	test_isGoptsEnabledByContract(false);
+	test_initHooks_shouldInitAllHooksWithoutFailure();
 
 	printf ("\ntest_hooks RESULTS: %d test(s) done. %d error(s).\n", nbTest, nbError);
 
