eccc458faa11279fca8e27d48a8ed037a1b7768c
0x6178656c <115062452+0x6178656c@users.noreply.github.com>
2022-10-20 19:38:04 +0200

    Change bash shebang

--- src/tools/fuse/docker/run_new_container.sh
+++ src/tools/fuse/docker/run_new_container.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 ./remove_container.sh
 ./build_container.sh
 docker run --privileged -it elektra-deb:1.0 /bin/bash -c "nohup ~/elektra_fuse/docker/create_keys_and_mount.sh && bash"
