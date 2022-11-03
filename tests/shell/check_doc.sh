10caf1fd4d34cf3a0296ba1e6200f81c6838216f
Klemens Böswirth <k.boeswirth+git@gmail.com>
2022-11-03 14:57:34 +0100

    tests: remove link check test for decisions

--- tests/shell/check_doc.sh
+++ tests/shell/check_doc.sh
@@ -11,9 +11,4 @@ for DEV_DOCU in $(ls "$DOC_DIR/dev" | grep -v README.md); do
 	succeed_if "Failed to find link to ${DEV_DOCU} in the file $DOC_DIR/dev/README.md"
 done
 
-for DECISION_DOCU in $(ls "$DOC_DIR/decisions" | grep -v README.md); do
-	grep -Eq "(${DECISION_DOCU})" "$DOC_DIR/decisions/README.md"
-	succeed_if "Failed to find link to ${DECISION_DOCU} in the file $DOC_DIR/decisions/README.md"
-done
-
 end_script
