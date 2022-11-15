83c35e996fca737bd9e144847ae505e4789ea505
0x6178656c <115062452+0x6178656c@users.noreply.github.com>
2022-11-15 13:10:51 +0100

    Add stubs for Key and KeySet

---
+++ src/bindings/erl_nif/elixir/lib/elektra/key_set.ex
@@ -0,0 +1,9 @@
+defmodule Elektra.KeySet do
+  use GenServer
+
+  @opaque t() :: pid
+
+  @type kdb() :: Elektra.Kdb.t()
+  @type key() :: Elektra.Key.t()
+  @type key_set() :: Elektra.KeySet.t()
+end
