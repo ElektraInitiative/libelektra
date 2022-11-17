c197cda618960006762573ba2073f94b03969a51
Richard Stöckl <richard.stoeckl@aon.at>
2022-11-17 18:15:42 +0100

    xfonf-plugin: Add gobject to FindXfconf

--- scripts/cmake/Modules/FindXfconf.cmake
+++ scripts/cmake/Modules/FindXfconf.cmake
@@ -10,42 +10,35 @@ if (XFCONF_INCLUDE_DIR) # Already in cache, be silent.
 	set (XFCONF_FIND_QUIETLY TRUE)
 endif (XFCONF_INCLUDE_DIR)
 
-# xfconf also requires glib-2.0
-find_path (
-	GLIB_INCLUDE_DIR
-	NAMES glib.h
-	PATHS ${Glib_PKGCONF_INCLUDE_DIRS}
-	PATH_SUFFIXES glib-2.0)
-
-find_library (
-	GLIB_LIBRARY
-	NAMES glib-2.0
-	PATHS ${Glib_PKGCONF_LIBRARY_DIRS})
-
-find_path (
-	GLIBCONFIG_INCLUDE_DIR
-	NAMES glibconfig.h
-	PATHS ${Glib_PKGCONF_INCLUDE_DIRS} /usr
-	PATH_SUFFIXES lib/glib-2.0/include)
+find_package (PkgConfig REQUIRED)
+pkg_search_module (GLIB REQUIRED glib-2.0)
+pkg_search_module (GOBJECT REQUIRED gobject-2.0)
 
 find_path (
 	XFCONF_INCLUDE_DIR
 	NAMES xfconf
 	PATHS /usr/include/xfce4/xfconf-0 /usr/local/include/xfce4/xfconf-0)
+
 find_library (
 	XFCONF_LIBRARY
 	NAMES xfconf-0
 	PATHS /usr/lib /usr/lib64 /usr/local/lib)
 
+find_library (
+	GLIB_LIBRARY
+	NAMES glib-2.0
+	PATHS "${GLIB_LIBDIR}")
+
+find_library (
+	GOBJECT_LIBRARY
+	NAMES gobject-2.0
+	PATHS "${GOBJECT_LIBDIR}")
+
+file (GLOB XFCONF_INCLUDE_DIRS "${XFCONF_INCLUDE_DIR}" "${GLIB_INCLUDEDIR}/glib-2.0" "${GLIB_LIBDIR}/glib-2.0/include")
+file (GLOB XFCONF_LIBRARIES "${XFCONF_LIBRARY}" "${GLIB_LIBRARY}" "${GOBJECT_LIBRARY}")
+
 # Handle the QUIETLY and REQUIRED arguments and set XFCONF_FOUND to TRUE if all listed variables are TRUE.
 include (FindPackageHandleStandardArgs)
-find_package_handle_standard_args (
-	Xfconf
-	DEFAULT_MSG
-	GLIBCONFIG_INCLUDE_DIR
-	GLIB_LIBRARY
-	GLIB_INCLUDE_DIR
-	XFCONF_LIBRARY
-	XFCONF_INCLUDE_DIR)
+find_package_handle_standard_args (Xfconf DEFAULT_MSG GLIB_LIBDIR GLIB_INCLUDEDIR XFCONF_LIBRARY XFCONF_INCLUDE_DIR)
 
-mark_as_advanced (GLIBCONFIG_INCLUDE_DIR GLIB_LIBRARY GLIB_INCLUDE_DIR XFCONF_LIBRARY XFCONF_INCLUDE_DIR)
+mark_as_advanced (GLIB_LIBDIR GLIB_INCLUDEDIR XFCONF_LIBRARY XFCONF_INCLUDE_DIR)
