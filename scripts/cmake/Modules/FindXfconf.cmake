7e92e986f9159af31f7a79114c2765825037f569
Richard Stöckl <richard.stoeckl@aon.at>
2022-11-17 10:56:02 +0100

    xfconf-plugin: Add glib2 libraries in cmake file

--- scripts/cmake/Modules/FindXfconf.cmake
+++ scripts/cmake/Modules/FindXfconf.cmake
@@ -10,10 +10,28 @@ if (XFCONF_INCLUDE_DIR) # Already in cache, be silent.
 	set (XFCONF_FIND_QUIETLY TRUE)
 endif (XFCONF_INCLUDE_DIR)
 
+# xfconf also requires glib-2.0
+find_path (
+	GLIB_INCLUDE_DIR
+	NAMES glib.h
+	PATHS ${Glib_PKGCONF_INCLUDE_DIRS}
+	PATH_SUFFIXES glib-2.0)
+
+find_library (
+	GLIB_LIBRARY
+	NAMES glib-2.0
+	PATHS ${Glib_PKGCONF_LIBRARY_DIRS})
+
+find_path (
+	GLIBCONFIG_INCLUDE_DIR
+	NAMES glibconfig.h
+	PATHS ${Glib_PKGCONF_INCLUDE_DIRS} /usr
+	PATH_SUFFIXES lib/glib-2.0/include)
+
 find_path (
 	XFCONF_INCLUDE_DIR
-	PATHS /usr/include /usr/local/include
-	PATH_SUFFIXES xfce4/xfconf-0)
+	NAMES xfconf
+	PATHS /usr/include/xfce4/xfconf-0 /usr/local/include/xfce4/xfconf-0)
 find_library (
 	XFCONF_LIBRARY
 	NAMES xfconf-0
@@ -21,6 +39,13 @@ find_library (
 
 # Handle the QUIETLY and REQUIRED arguments and set XFCONF_FOUND to TRUE if all listed variables are TRUE.
 include (FindPackageHandleStandardArgs)
-find_package_handle_standard_args (Xfconf DEFAULT_MSG XFCONF_LIBRARY XFCONF_INCLUDE_DIR)
+find_package_handle_standard_args (
+	Xfconf
+	DEFAULT_MSG
+	GLIBCONFIG_INCLUDE_DIR
+	GLIB_LIBRARY
+	GLIB_INCLUDE_DIR
+	XFCONF_LIBRARY
+	XFCONF_INCLUDE_DIR)
 
-mark_as_advanced (XFCONF_LIBRARY XFCONF_INCLUDE_DIR)
+mark_as_advanced (GLIBCONFIG_INCLUDE_DIR GLIB_LIBRARY GLIB_INCLUDE_DIR XFCONF_LIBRARY XFCONF_INCLUDE_DIR)
