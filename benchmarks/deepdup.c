fe70d9117dee55b0996fedcf070a98eb40a47291
Maximilian Irlinger <max@maxirlinger.at>
2022-12-01 15:57:15 +0100

    add benchmarks for deepduplication of a keyset, pure kdb get and kdb get and modify

---
+++ benchmarks/deepdup.c
@@ -0,0 +1,34 @@
+/**
+* @file
+*
+* @brief
+*
+* @copyright BSD License (see LICENSE.md or https://www.libelektra.org)
+*/
+
+#include <benchmarks.h>
+
+Key * key;
+
+int main (void)
+{
+	timeInit ();
+	benchmarkCreate ();
+	timePrint ("Created empty keyset");
+
+	benchmarkFillup ();
+	timePrint ("New large keyset");
+
+	KeySet * ksTemp = ksNew (0, KS_END);
+
+	for (elektraCursor i = 0; i < ksGetSize (large); i++)
+	{
+		ksAppendKey (ksTemp, keyDup (ksAtCursor (large, i), KEY_CP_ALL));
+	}
+
+	timePrint ("Deep duplication");
+
+	ksDel (large);
+	ksDel (ksTemp);
+	keyDel (key);
+}
---
+++ benchmarks/kdbget.c
@@ -0,0 +1,45 @@
+/**
+ * @file
+ *
+ * @brief Benchmark for KDB
+ *
+ * @copyright BSD License (see LICENSE.md or https://www.libelektra.org)
+ */
+
+#include <stdio.h>
+
+#include <benchmarks.h>
+#include <kdb.h>
+
+#define CSV_STR_FMT "%s;%s;%d\n"
+
+
+static void benchmarkDel (void)
+{
+	ksDel (large);
+}
+
+int main (void)
+{
+	Key * parentKey = keyNew ("user:/benchmark", KEY_END);
+	KDB * handle = kdbOpen (NULL, parentKey);
+	fprintf (stdout, CSV_STR_FMT, "core", "kdbOpen", timeGetDiffMicroseconds ());
+
+	KeySet * returned = ksNew (0, KS_END);
+	timeInit ();
+	kdbGet (handle, returned, parentKey);
+	fprintf (stdout, CSV_STR_FMT, "core", "kdbGet", timeGetDiffMicroseconds ());
+
+	if (ksGetSize (returned) == 0)
+	{
+		fprintf (stderr, "error: no keys returned. make sure you actually have something in %s!", keyName (parentKey));
+		goto error;
+	}
+
+error:
+	kdbClose (handle, parentKey);
+	ksDel (returned);
+	keyDel (parentKey);
+
+	benchmarkDel ();
+}
