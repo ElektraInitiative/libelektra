a118143a999f20a7ae5a8ffea5c0449364cd4c37
0x6178656c <115062452+0x6178656c@users.noreply.github.com>
2022-12-08 13:52:41 +0100

    tests: Add version check to check_shellcheck

--- tests/shell/check_shellcheck.sh
+++ tests/shell/check_shellcheck.sh
@@ -11,6 +11,20 @@ command -v shellcheck > /dev/null 2>&1 || {
 	exit 0
 }
 
+shellcheck_version=$(shellcheck --version | sed -n 's/version: //p')
+shellcheck_required_version="0.7.1"
+
+shellcheck_min_version=$(
+	printf '%s\n%s\n' "$shellcheck_version" "$shellcheck_required_version" |
+		sort -V |
+		head -1
+)
+
+if [ ! "$shellcheck_min_version" = "$shellcheck_required_version" ]; then
+	echo "shellcheck version ${shellcheck_required_version} or later required, aborting"
+	exit 0
+fi
+
 cd "@CMAKE_SOURCE_DIR@" || exit
 
 # shellcheck disable=SC2046
