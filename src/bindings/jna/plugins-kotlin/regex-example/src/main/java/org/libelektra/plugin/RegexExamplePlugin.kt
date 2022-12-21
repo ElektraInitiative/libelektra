1e280c55e77a9fc342bb0a66607cea7cd3b8e18d
Richard Stöckl <richard.stoeckl@aon.at>
2022-12-21 19:43:02 +0100

    kotlin-bindings: Add skeleton for example plugin

---
+++ src/bindings/jna/plugins-kotlin/regex-example/src/main/java/org/libelektra/plugin/RegexExamplePlugin.kt
@@ -0,0 +1,32 @@
+package org.libelektra.plugin
+
+import org.libelektra.Key
+import org.libelektra.KeySet
+import org.libelektra.Plugin
+
+class RegexExamplePlugin : Plugin {
+    override fun getName(): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun open(config: KeySet, errorKey: Key): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun get(keySet: KeySet, parentKey: Key): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun set(keySet: KeySet, parentKey: Key): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun error(keySet: KeySet, parentKey: Key): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun close(parentKey: Key): Int {
+        TODO("Not yet implemented")
+    }
+
+}
