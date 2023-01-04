cdb70d6f1aa3439bbe3ce351e4fb55980492ca34
0x6178656c <115062452+0x6178656c@users.noreply.github.com>
2023-01-04 15:40:53 +0100

    Lower required Elixir version

--- src/bindings/erl_nif/elixir/mix.exs
+++ src/bindings/erl_nif/elixir/mix.exs
@@ -5,7 +5,7 @@ defmodule Elektra.MixProject do
     [
       app: :elektra,
       version: "0.1.0",
-      elixir: "~> 1.13",
+      elixir: ">= 1.10.0",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
