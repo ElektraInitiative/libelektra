f4aec482eb958ca318f0796b62a56500d0b6d9bc
Klemens Böswirth <k.boeswirth+git@gmail.com>
2023-02-08 16:30:44 +0100

    scripts: fix formatting

--- scripts/dev/ci_run_tests.sh
+++ scripts/dev/ci_run_tests.sh
@@ -8,26 +8,26 @@
 #   ENABLE_ASAN       - set to 'ON', if built with ASAN (disables script tests)
 
 if [ "$ENABLE_ASAN" = 'ON' ]; then
-        EXCLUDED_TESTS="testscr_check"
+	EXCLUDED_TESTS="testscr_check"
 elif [ "$BUILD_FULL" = 'ON' ]; then
-        EXCLUDED_TESTS="testscr_check_external"
+	EXCLUDED_TESTS="testscr_check_external"
 else
-        EXCLUDED_TESTS="testscr_check_external"
+	EXCLUDED_TESTS="testscr_check_external"
 fi
 
 if [ -n "$AUTO_RERUN_TESTS" ]; then
-        EXCLUDED_TESTS="$AUTO_RERUN_TESTS|$EXCLUDED_TESTS"
+	EXCLUDED_TESTS="$AUTO_RERUN_TESTS|$EXCLUDED_TESTS"
 fi
 
 # use subshell for temporary directory switch
 (
-        set -e
-        cd "$BUILD_DIR"
-        ctest -j --force-new-ctest-process --output-on-failure -E "$EXCLUDED_TESTS"
-        ctest -j --force-new-ctest-process --output-on-failure --repeat "until-pass:5" -R "$AUTO_RERUN_TESTS"
-        set +e
+	set -e
+	cd "$BUILD_DIR"
+	ctest -j --force-new-ctest-process --output-on-failure -E "$EXCLUDED_TESTS"
+	ctest -j --force-new-ctest-process --output-on-failure --repeat "until-pass:5" -R "$AUTO_RERUN_TESTS"
+	set +e
 )
 
 if [ -n "$KDB" ]; then
-        "$KDB" run_all
+	"$KDB" run_all
 fi
