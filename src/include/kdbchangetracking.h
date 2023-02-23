b2f76598700ab2ac6f4dccb7fd461d5a6f02d57a
Maximilian Irlinger <max@maxirlinger.at>
2023-02-23 19:25:33 +0100

    kdb: put elektraDiff* functions in their own files

--- src/include/kdbchangetracking.h
+++ src/include/kdbchangetracking.h
@@ -4,6 +4,7 @@
 #include <kdb.h>
 #include <kdbtypes.h>
 #include <kdbplugin.h>
+#include <kdbdiff.h>
 
 #ifdef __cplusplus
 namespace ckdb
@@ -12,32 +13,12 @@ extern "C" {
 #endif
 
 typedef struct _ChangeTrackingContext ChangeTrackingContext;
-typedef struct _ElektraDiff ElektraDiff;
 
 const ChangeTrackingContext * elektraChangeTrackingGetContextFromKdb (KDB * kdb);
 const ChangeTrackingContext * elektraChangeTrackingGetContextFromPlugin (Plugin * plugin);
 
 ElektraDiff * elektraChangeTrackingCalculateDiff (KeySet * newKeys, const ChangeTrackingContext * context, Key * parentKey);
 
-ElektraDiff * elektraDiffCalculate (KeySet * newKeys, KeySet * oldKeys, Key * parentKey);
-
-void elektraDiffDel (ElektraDiff * ksd);
-ElektraDiff * elektraDiffNew (KeySet * addedKeys, KeySet * removedKeys, KeySet * modifiedKey, Key * parentKey);
-uint16_t elektraDiffIncRef (ElektraDiff * ksd);
-uint16_t elektraDiffDecRef (ElektraDiff * ksd);
-
-const Key * elektraDiffGetParentKey (const ElektraDiff * ksd);
-
-KeySet * elektraDiffGetAddedKeys (const ElektraDiff * ksd);
-KeySet * elektraDiffGetRemovedKeys (const ElektraDiff * ksd);
-KeySet * elektraDiffGetModifiedKeys (const ElektraDiff * ksd); // Returns old keys (pre-modification)
-
-bool elektraDiffKeyValueChanged (const ElektraDiff * ksd, Key * key);
-bool elektraDiffKeyMetaChanged (const ElektraDiff * ksd, Key * key);
-
-KeySet * elektraDiffGetAddedMetaKeys (const ElektraDiff * ksd, Key * key);
-KeySet * elektraDiffGetRemovedMetaKeys (const ElektraDiff * ksd, Key * key);
-KeySet * elektraDiffGetModifiedMetaKeys (const ElektraDiff * ksd, Key * key); // Returns old meta keys (pre-modification)
 
 // For Testing
 ChangeTrackingContext * elektraChangeTrackingCreateContextForTesting (KeySet * oldKeys);
---
+++ src/include/kdbdiff.h
@@ -0,0 +1,41 @@
+#ifndef ELEKTRA_KDBDIFF_H
+#define ELEKTRA_KDBDIFF_H
+
+#include <kdb.h>
+#include <kdbtypes.h>
+
+#ifdef __cplusplus
+namespace ckdb
+{
+extern "C" {
+#endif
+
+typedef struct _ElektraDiff ElektraDiff;
+
+ElektraDiff * elektraDiffCalculate (KeySet * newKeys, KeySet * oldKeys, Key * parentKey);
+ElektraDiff * elektraDiffNew (KeySet * addedKeys, KeySet * removedKeys, KeySet * modifiedKey, Key * parentKey);
+
+void elektraDiffDel (ElektraDiff * ksd);
+
+uint16_t elektraDiffIncRef (ElektraDiff * ksd);
+uint16_t elektraDiffDecRef (ElektraDiff * ksd);
+
+const Key * elektraDiffGetParentKey (const ElektraDiff * ksd);
+
+KeySet * elektraDiffGetAddedKeys (const ElektraDiff * ksd);
+KeySet * elektraDiffGetRemovedKeys (const ElektraDiff * ksd);
+KeySet * elektraDiffGetModifiedKeys (const ElektraDiff * ksd); // Returns old keys (pre-modification)
+
+bool elektraDiffKeyValueChanged (const ElektraDiff * ksd, Key * key);
+bool elektraDiffKeyMetaChanged (const ElektraDiff * ksd, Key * key);
+
+KeySet * elektraDiffGetAddedMetaKeys (const ElektraDiff * ksd, Key * key);
+KeySet * elektraDiffGetRemovedMetaKeys (const ElektraDiff * ksd, Key * key);
+KeySet * elektraDiffGetModifiedMetaKeys (const ElektraDiff * ksd, Key * key); // Returns old meta keys (pre-modification)
+
+#ifdef __cplusplus
+}
+}
+#endif
+
+#endif
