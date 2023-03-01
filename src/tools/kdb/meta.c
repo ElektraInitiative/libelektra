81ea1f96b23d65db0a2b6f49a0fd5feca735e87d
Hannes Laimer <hannes.laimer@tuwien.ac.at>
2023-03-01 07:35:35 +0100

    kdb-cli: add meta subcommand

---
+++ src/tools/kdb/meta.c
@@ -0,0 +1,47 @@
+/**
+ * @file
+ *
+ * @brief Implementation of kdb meta command
+ *
+ * @copyright BSD License (see LICENSE.md or https://www.libelektra.org)
+ */
+
+#include <meta.h>
+
+#include <command.h>
+#include <kdb.h>
+#include <kdberrors.h>
+
+#define COMMAND_NAME "meta"
+
+#define GET_OPTION_KEY(options, name) GET_OPT_KEY (options, COMMAND_BASE_KEY (COMMAND_NAME) "/" name)
+#define GET_OPTION(options, name) GET_OPT (options, COMMAND_BASE_KEY (COMMAND_NAME) "/" name)
+
+command metaSubcommands[] = {};
+
+void addMetaSpec (KeySet * spec)
+{
+	ksAppendKey (spec, keyNew (COMMAND_SPEC_KEY (COMMAND_NAME), KEY_META, "description", "Manage meta keys.", KEY_META, "command",
+				   COMMAND_NAME, KEY_END));
+
+	for (unsigned long i = 0; i < sizeof (metaSubcommands) / sizeof (metaSubcommands[0]); ++i)
+	{
+		metaSubcommands[i].addSpec (spec);
+	}
+}
+
+int execMeta (KeySet * options, Key * errorKey)
+{
+	const char * subcommand = keyString (ksLookupByName (options, CLI_BASE_KEY "/" COMMAND_NAME, 0));
+
+	for (unsigned long i = 0; i < sizeof (metaSubcommands) / sizeof (metaSubcommands[0]); ++i)
+	{
+		if (elektraStrCmp (subcommand, metaSubcommands[i].name) == 0)
+		{
+			return metaSubcommands[i].exec (options, errorKey);
+		}
+	}
+	// this cannot happen, since not valid sub-commands are already detected when parsing
+	return 1;
+
+}
