173bd5cec3e48332700c63f8270977b1e2b87d62
Hannes Laimer <hannes.laimer@tuwien.ac.at>
2023-03-01 08:24:15 +0100

    kdb-cli: rewrite meta-show

--- src/tools/kdb/factory.hpp
+++ src/tools/kdb/factory.hpp
@@ -48,7 +48,6 @@
 #include <rm.hpp>
 #include <sget.hpp>
 #include <shell.hpp>
-#include <showmeta.hpp>
 #include <specmount.hpp>
 #include <test.hpp>
 #include <umount.hpp>
@@ -89,7 +88,6 @@ public:
 		m_factory.insert (std::make_pair ("remount", std::make_shared<Cnstancer<RemountCommand>> ()));
 		m_factory.insert (std::make_pair ("shell", std::make_shared<Cnstancer<ShellCommand>> ()));
 		m_factory.insert (std::make_pair ("find", std::make_shared<Cnstancer<FindCommand>> ()));
-		m_factory.insert (std::make_pair ("meta-show", std::make_shared<Cnstancer<ShowMetaCommand>> ()));
 		m_factory.insert (std::make_pair ("meta-rm", std::make_shared<Cnstancer<MetaRemoveCommand>> ()));
 		m_factory.insert (std::make_pair ("plugin-info", std::make_shared<Cnstancer<PluginInfoCommand>> ()));
 		m_factory.insert (std::make_pair ("test", std::make_shared<Cnstancer<TestCommand>> ()));
