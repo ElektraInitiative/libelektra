5a8ec5052b419676b55f8193a7c55c89f06c7787
Maximilian Irlinger <max@maxirlinger.at>
2023-04-12 15:13:27 +0200

    remove weak linking from record

--- src/include/kdbrecord.h
+++ src/include/kdbrecord.h
@@ -23,9 +23,9 @@ bool elektraRecordDisableRecording (KDB * handle, Key * errorKey);
 bool elektraRecordClearSession (KDB * handle, Key * errorKey);
 bool elektraRecordExportSession (KDB * handle, Plugin * plugin, Key * parentKey, Key * errorKey);
 
-bool elektraRecordRecord (KDB * handle, KDB * sessionStorageHandle, KeySet * newKeys, Key * parentKey, Key * errorKey) ELEKTRA_WEAK;
+bool elektraRecordRecord (KDB * handle, KDB * sessionStorageHandle, KeySet * newKeys, Key * parentKey, Key * errorKey);
 bool elektraRecordUndo (KDB * handle, KDB * sessionStorageHandle, Key * parentKey, Key * errorKey);
-bool elektraRecordIsActive (KDB * handle) ELEKTRA_WEAK;
+bool elektraRecordIsActive (KDB * handle);
 
 #ifdef __cplusplus
 }
