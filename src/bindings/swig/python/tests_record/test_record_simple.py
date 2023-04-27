33c85102a5c6b7fcb75a322ecea107d0cd018012
Maximilian Irlinger <max@maxirlinger.at>
2023-04-27 14:05:23 +0200

    bindings/python: add record bindings

---
+++ src/bindings/swig/python/tests_record/test_record_simple.py
@@ -0,0 +1,11 @@
+import kdb, kdb.record, unittest
+
+class RecordSimple(unittest.TestCase):
+	def setUp(self):
+		pass
+
+	def test_load(self):
+		pass
+
+if __name__ == '__main__':
+	unittest.main()
