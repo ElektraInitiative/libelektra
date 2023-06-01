e58d3a389e510e6b9e7b6caf381d4f65c4e50efd
Klemens Böswirth <k.boeswirth+git@gmail.com>
2023-06-01 12:42:55 +0200

    general: move highlevel stuff from kdbprivate.h to private header

--- src/include/internal/kdbprivate.h
+++ src/include/internal/kdbprivate.h
@@ -671,62 +671,6 @@ size_t elektraPluginGetFunction (Plugin * plugin, const char * name);
 #ifdef __cplusplus
 }
 }
-
-#define KDB ckdb::KDB
-#define Key ckdb::Key
-#define KeySet ckdb::KeySet
-extern "C" {
-#endif
-
-#pragma region highlevel
-
-struct _Elektra
-{
-	KDB * kdb;
-	Key * parentKey;
-	KeySet * config;
-	KeySet * defaults;
-	Key * lookupKey;
-	ElektraErrorHandler fatalErrorHandler;
-	char * resolvedReference;
-	size_t parentKeyLength;
-};
-
-struct _ElektraError
-{
-	char * code;
-	char * codeFromKey;
-	char * description;
-	char * module;
-	char * file;
-	kdb_long_t line;
-	kdb_long_t warningCount;
-	kdb_long_t warningAlloc;
-	struct _ElektraError ** warnings;
-	Key * errorKey;
-};
-
-/* high-level API */
-void elektraSaveKey (Elektra * elektra, Key * key, ElektraError ** error);
-void elektraSetLookupKey (Elektra * elektra, const char * name);
-void elektraSetArrayLookupKey (Elektra * elektra, const char * name, kdb_long_long_t index);
-
-ElektraError * elektraErrorCreate (const char * code, const char * description, const char * module, const char * file, kdb_long_t line);
-void elektraErrorAddWarning (ElektraError * error, ElektraError * warning);
-ElektraError * elektraErrorFromKey (Key * key);
-
-ElektraError * elektraErrorKeyNotFound (const char * keyname);
-ElektraError * elektraErrorWrongType (const char * keyname, KDBType expectedType, KDBType actualType);
-ElektraError * elektraErrorNullError (const char * function);
-ElektraError * elektraErrorEnsureFailed (const char * reason);
-
-#pragma endregion
-
-#ifdef __cplusplus
-}
-#undef Key
-#undef KeySet
-#undef KDB
 #endif
 
 #endif /* KDBPRIVATE_H */
--- src/libs/highlevel/elektra_array_value.c
+++ src/libs/highlevel/elektra_array_value.c
@@ -6,13 +6,16 @@
  * @copyright BSD License (see doc/LICENSE.md or http://www.libelektra.org)
  */
 
+#include "./elektra.h"
+
 #include <elektra/ease/array.h>
 #include <elektra/ease/reference.h>
 #include <elektra/highlevel.h>
 #include <elektra/type/conversion.h>
-#include <internal/kdbprivate.h>
+
 #include <internal/utility/alloc.h>
 #include <internal/utility/array.h>
+
 #include <stdlib.h>
 #include <string.h>
 
